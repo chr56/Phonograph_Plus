97f6edb153d237693ab9db2acc4a83ce96d4f223
chr56 <_>
2021-09-02 12:52:57 +0800

    improve AddToPlaylistDialog

--- app/src/main/java/com/kabouzeid/gramophone/dialogs/AddToPlaylistDialog.kt
+++ app/src/main/java/com/kabouzeid/gramophone/dialogs/AddToPlaylistDialog.kt
@@ -4,46 +4,37 @@ import android.app.Dialog
 import android.os.Bundle
 import androidx.fragment.app.DialogFragment
 import com.afollestad.materialdialogs.MaterialDialog
-import com.afollestad.materialdialogs.list.listItems
+import com.afollestad.materialdialogs.list.listItemsSingleChoice
 import com.kabouzeid.gramophone.R
 import com.kabouzeid.gramophone.loader.PlaylistLoader
 import com.kabouzeid.gramophone.model.Song
 import com.kabouzeid.gramophone.util.PlaylistsUtil
-// import java.util.*
+/*TODO review*/
 
 /**
  * @author Karim Abou Zeid (kabouzeid), Aidan Follestad (afollestad)
  */
 class AddToPlaylistDialog : DialogFragment() {
     override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
+        val songs: List<Song> = requireArguments().getParcelableArrayList("songs")!!
         val playlists = PlaylistLoader.getAllPlaylists(requireActivity())
-        val playlistNames = arrayOfNulls<CharSequence>(playlists.size + 1)
-        playlistNames[0] = requireActivity().resources.getString(R.string.action_new_playlist)
-        for (i in 1 until playlistNames.size) {
-            playlistNames[i] = playlists[i - 1].name
+        val playlistNames: List<CharSequence> = List<CharSequence>(playlists.size + 1) {
+            if (it == 0) { requireActivity().resources.getString(R.string.action_new_playlist) } else playlists[it - 1].name
         }
         return MaterialDialog(requireActivity())
             .title(R.string.add_playlist_title)
-            .listItems(items = playlistNames as List<CharSequence>/*TODO*/) { materialDialog: MaterialDialog, i: Int, _: CharSequence ->
-                val songs: List<Song> = requireArguments().getParcelableArrayList("songs")
-                    ?: return@listItems
-                if (i == 0) {
+            .listItemsSingleChoice(items = playlistNames) { materialDialog: MaterialDialog, index: Int, _: CharSequence ->
+                if (index == 0) {
                     materialDialog.dismiss()
                     CreatePlaylistDialog.create(songs).show(requireActivity().supportFragmentManager, "ADD_TO_PLAYLIST")
                 } else {
                     materialDialog.dismiss()
-                    PlaylistsUtil.addToPlaylist(requireActivity(), songs, playlists[i - 1].id, true)
+                    PlaylistsUtil.addToPlaylist(requireActivity(), songs, playlists[index - 1].id, true)
                 }
             }
     }
 
     companion object {
-        fun create(song: Song): AddToPlaylistDialog {
-            val list: MutableList<Song> = ArrayList()
-            list.add(song)
-            return create(list)
-        }
-
         @JvmStatic
         fun create(songs: List<Song>?): AddToPlaylistDialog {
             val dialog = AddToPlaylistDialog()
