ff8e4845378d06a37cf5dc37a4fe23725ba597a7
chr56 <_>
2021-09-02 13:49:29 +0800

    improve DeletePlaylistDialog

--- app/src/main/java/com/kabouzeid/gramophone/dialogs/DeletePlaylistDialog.kt
+++ app/src/main/java/com/kabouzeid/gramophone/dialogs/DeletePlaylistDialog.kt
@@ -8,39 +8,29 @@ import com.afollestad.materialdialogs.MaterialDialog
 import com.kabouzeid.gramophone.R
 import com.kabouzeid.gramophone.model.Playlist
 import com.kabouzeid.gramophone.util.PlaylistsUtil
-// import java.util.*
 
+// Todo Completed
 /**
  * @author Karim Abou Zeid (kabouzeid)
  */
 class DeletePlaylistDialog : DialogFragment() {
     override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
         val playlists: List<Playlist> = requireArguments().getParcelableArrayList("playlists")!!
-        val title: Int
-        val content: CharSequence
-        if (playlists.size > 1) {
-            title = R.string.delete_playlists_title
-            content = Html.fromHtml(getString(R.string.delete_x_playlists, playlists.size))
-        } else {
-            title = R.string.delete_playlist_title
-            content = Html.fromHtml(getString(R.string.delete_playlist_x, playlists[0].name))
-        }
+        val title: Int = if (playlists.size > 1) {R.string.delete_playlists_title}
+                            else {R.string.delete_playlist_title}
+        val content: CharSequence = if (playlists.size > 1) {Html.fromHtml(getString(R.string.delete_x_playlists, playlists.size))}
+                            else {Html.fromHtml(getString(R.string.delete_playlist_x, playlists[0].name))}
+
         return MaterialDialog(requireActivity())
             .title(title)
             .message(text = content)
             .positiveButton(R.string.delete_action) {
-                if (requireActivity() != null) { PlaylistsUtil.deletePlaylists(requireActivity(), playlists) }
+                PlaylistsUtil.deletePlaylists(requireActivity(), playlists)
             }
-            .negativeButton(android.R.string.cancel)
+            .negativeButton(android.R.string.cancel){dismiss()}
     }
 
     companion object {
-        fun create(playlist: Playlist): DeletePlaylistDialog {
-            val list: MutableList<Playlist> = ArrayList()
-            list.add(playlist)
-            return create(list)
-        }
-
         @JvmStatic
         fun create(playlists: List<Playlist>?): DeletePlaylistDialog {
             val dialog = DeletePlaylistDialog()
