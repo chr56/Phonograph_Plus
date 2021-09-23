d7a3414123234a0bff3cc3b771dd971988ad18b1
chr56 <chr0056@gmail.com>
2021-09-23 12:00:06 +0800

    add new song sort order in LibraryFragment
    DATE_ADDED

--- app/src/main/java/com/kabouzeid/gramophone/ui/fragments/mainactivity/library/LibraryFragment.java
+++ app/src/main/java/com/kabouzeid/gramophone/ui/fragments/mainactivity/library/LibraryFragment.java
@@ -383,6 +383,8 @@ public class LibraryFragment extends AbsMainActivityFragment implements CabHolde
                     .setChecked(currentSortOrder.equals(SortOrder.SongSortOrder.SONG_ALBUM));
             sortOrderMenu.add(0, R.id.action_song_sort_order_year, 4, R.string.sort_order_year)
                     .setChecked(currentSortOrder.equals(SortOrder.SongSortOrder.SONG_YEAR));
+            sortOrderMenu.add(0, R.id.action_song_sort_order_date_added, 5, R.string.sort_order_date_added)
+                    .setChecked(currentSortOrder.equals(SortOrder.SongSortOrder.SONG_DATE));
         }
 
         sortOrderMenu.setGroupCheckable(0, true, true);
@@ -430,6 +432,9 @@ public class LibraryFragment extends AbsMainActivityFragment implements CabHolde
                     break;
                 case R.id.action_song_sort_order_year:
                     sortOrder = SortOrder.SongSortOrder.SONG_YEAR;
+                    break;
+                case R.id.action_song_sort_order_date_added:
+                    sortOrder = SortOrder.SongSortOrder.SONG_DATE;
                     break;
             }
         }
