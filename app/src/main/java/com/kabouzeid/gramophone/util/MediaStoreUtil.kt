42dc270a7c81c584a568642b9cf5d6412d227c3c
chr56 <chr0056@gmail.com>
2021-10-19 11:59:30 +0800

    add scanFiles() in MediaStoreUtil

--- app/src/main/java/com/kabouzeid/gramophone/util/MediaStoreUtil.kt
+++ app/src/main/java/com/kabouzeid/gramophone/util/MediaStoreUtil.kt
@@ -8,6 +8,8 @@ import android.Manifest
 import android.content.Context
 import android.content.Intent
 import android.content.pm.PackageManager
+import android.media.MediaScannerConnection
+import android.net.Uri
 import android.os.Build
 import android.os.Handler
 import android.provider.MediaStore
@@ -18,6 +20,11 @@ import com.afollestad.materialdialogs.MaterialDialog
 import com.kabouzeid.gramophone.R
 import com.kabouzeid.gramophone.model.Song
 import java.util.Locale
+import kotlin.collections.ArrayList
+import kotlin.collections.List
+import kotlin.collections.MutableList
+import kotlin.collections.indices
+import kotlin.collections.isNotEmpty
 
 object MediaStoreUtil {
     private const val TAG: String = "MediaStoreUtil"
@@ -72,11 +79,25 @@ object MediaStoreUtil {
                 .show()
         }
 
-
         Toast.makeText(
             context,
             String.format(Locale.getDefault(), context.getString(R.string.deleted_x_songs), result),
             Toast.LENGTH_SHORT
         ).show()
+    }
+
+    fun scanFiles(context: Context, paths: Array<String>, mimeTypes: Array<String>) {
+        var failed: Int = 0
+        var scanned: Int = 0
+        MediaScannerConnection.scanFile(context, paths, mimeTypes) { _: String?, uri: Uri? ->
+            if (uri == null) {
+                failed++
+            } else {
+                scanned++
+            }
+            val text = "${context.resources.getString(R.string.scanned_files, scanned, scanned + failed)} ${
+            if (failed > 0) String.format(context.resources.getString(R.string.could_not_scan_files, failed)) else ""}"
+            Toast.makeText(context, text, Toast.LENGTH_SHORT).show()
+        }
     }
 }
