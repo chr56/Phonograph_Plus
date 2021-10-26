809a559d687645c4a644c5101d681644df395d29
chr56 <chr0056@gmail.com>
2021-10-26 18:35:22 +0800

    cleanup FileSaver

--- app/src/main/java/player/phonograph/helper/FileSaver.kt
+++ app/src/main/java/player/phonograph/helper/FileSaver.kt
@@ -15,11 +15,10 @@ import player.phonograph.util.MediaStoreUtil
 
 object FileSaver {
     fun saveFile(activity: Activity, uri: Uri, data: String): Short {
-        val stream = activity.contentResolver.openOutputStream(uri) ?: return -1
-
+        val stream = activity.contentResolver.openOutputStream(uri) ?: return ERROR
         stream.write(data.encodeToByteArray())
-        return 0
-
+        stream.close()
+        return SUCCESS
     }
 
     /**
@@ -27,44 +26,17 @@ object FileSaver {
      * @param uri target file
      */
     fun savePlaylist(activity: Activity, uri: Uri, playlistId: Long): Short {
-
-        val stream = activity.contentResolver.openOutputStream(uri) ?: return -1
-
         val playlist: Playlist = MediaStoreUtil.getPlaylist(activity as Context, playlistId)
-        if (playlist.id == -1L) return -1
-
-        val songs: List<Song> =
-            if (playlist is AbsCustomPlaylist) {
-                playlist.getSongs(activity)
-            } else {
-                PlaylistSongLoader.getPlaylistSongList(activity, playlist.id)
-            }
-
-        if (songs.isEmpty()) return -1
-
-//        val bw = BufferedWriter
-        val b = StringBuffer()
-        b.append(FILE_HEADER)
-        for (song in songs) {
-            b.appendLine()
-            b.append(ENTRY_HEADER + song.duration + "," + song.artistName + " - " + song.title)
-            b.appendLine()
-            b.append(song.data)
-        }
-
-        stream.write(b.toString().encodeToByteArray())
-        stream.close()
-        return 0
+        if (playlist.id == -1L) return ERROR // id -1 -> empty/null playlist
+        return savePlaylist(activity, uri, playlist)
     }
+
     /**
      * save Playlist as M3U
      * @param uri target file
      */
     fun savePlaylist(activity: Activity, uri: Uri, playlist: Playlist): Short {
 
-        val stream = activity.contentResolver.openOutputStream(uri) ?: return -1
-
-
         val songs: List<Song> =
             if (playlist is AbsCustomPlaylist) {
                 playlist.getSongs(activity)
@@ -72,23 +44,23 @@ object FileSaver {
                 PlaylistSongLoader.getPlaylistSongList(activity, playlist.id)
             }
 
-        if (songs.isEmpty()) return -1
+        if (songs.isEmpty()) return ERROR // no song? why save it?
 
-//        val bw = BufferedWriter
-        val b = StringBuffer()
-        b.append(FILE_HEADER)
+        val buffer = StringBuffer()
+        buffer.append(FILE_HEADER)
         for (song in songs) {
-            b.appendLine()
-            b.append(ENTRY_HEADER + song.duration + "," + song.artistName + " - " + song.title)
-            b.appendLine()
-            b.append(song.data)
+            buffer.appendLine()
+            buffer.append(ENTRY_HEADER + song.duration + "," + song.artistName + " - " + song.title)
+            buffer.appendLine()
+            buffer.append(song.data)
         }
 
-        stream.write(b.toString().encodeToByteArray())
-        stream.close()
-        return 0
+        return saveFile(activity, uri, buffer.toString())
     }
 
     private const val FILE_HEADER = "#EXTM3U"
     private const val ENTRY_HEADER = "#EXTINF:"
+
+    private const val SUCCESS: Short = 0
+    private const val ERROR: Short = (-1)
 }
