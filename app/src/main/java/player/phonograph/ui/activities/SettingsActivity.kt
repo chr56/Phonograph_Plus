714f3308a3647ede0f509a4e0d9e3f09afecb927
chr56 <chr0056@gmail.com>
2021-10-27 19:29:03 +0800

    convert SettingsActivity.java to kotlin

---
+++ app/src/main/java/player/phonograph/ui/activities/SettingsActivity.kt
@@ -0,0 +1,43 @@
+package player.phonograph.ui.activities
+
+import android.os.Bundle
+import android.view.MenuItem
+import androidx.appcompat.widget.Toolbar
+import chr_56.MDthemer.core.ThemeColor
+import player.phonograph.R
+import player.phonograph.ui.activities.base.AbsBaseActivity
+import player.phonograph.ui.fragments.SettingsFragment
+
+class SettingsActivity : AbsBaseActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_preferences)
+        setDrawUnderStatusbar()
+
+        setStatusbarColorAuto()
+        setNavigationbarColorAuto()
+        setTaskDescriptionColorAuto()
+
+        val toolbar = findViewById<Toolbar>(R.id.toolbar)
+        toolbar.setBackgroundColor(ThemeColor.primaryColor(this))
+        setSupportActionBar(toolbar)
+        supportActionBar!!.setDisplayHomeAsUpEnabled(true)
+
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.content_frame, SettingsFragment()).commit()
+        } else {
+            val frag =
+                supportFragmentManager.findFragmentById(R.id.content_frame) as SettingsFragment?
+            frag?.invalidateSettings()
+        }
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if (item.itemId == android.R.id.home) {
+            onBackPressed()
+            return true
+        }
+        return super.onOptionsItemSelected(item)
+    }
+}
