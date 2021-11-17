4d5adc83174e6d0eaed481c849f66de804f56526
chr56 <chr0056@gmail.com>
2021-11-17 12:29:52 +0800

    convert GenreDetailActivity.java into kotlin

--- app/src/main/java/player/phonograph/ui/activities/GenreDetailActivity.kt
+++ app/src/main/java/player/phonograph/ui/activities/GenreDetailActivity.kt
@@ -1,208 +1,165 @@
-package player.phonograph.ui.activities;
+package player.phonograph.ui.activities
 
-import android.content.Context;
-import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
-import android.view.View;
-import android.widget.TextView;
+import android.content.Context
+import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import android.view.View
+import android.widget.TextView
+import androidx.appcompat.widget.Toolbar
+import androidx.loader.app.LoaderManager
+import androidx.loader.content.Loader
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import chr_56.MDthemer.core.ThemeColor
+import com.afollestad.materialcab.MaterialCab
+import com.simplecityapps.recyclerview_fastscroll.views.FastScrollRecyclerView
+import player.phonograph.R
+import player.phonograph.adapter.song.SongAdapter
+import player.phonograph.helper.MusicPlayerRemote
+import player.phonograph.interfaces.CabHolder
+import player.phonograph.interfaces.LoaderIds
+import player.phonograph.loader.GenreLoader
+import player.phonograph.misc.WrappedAsyncTaskLoader
+import player.phonograph.model.Genre
+import player.phonograph.model.Song
+import player.phonograph.ui.activities.base.AbsSlidingMusicPanelActivity
+import player.phonograph.util.PhonographColorUtil
+import player.phonograph.util.ViewUtil
 
-import androidx.annotation.NonNull;
-import androidx.appcompat.widget.Toolbar;
-import androidx.loader.app.LoaderManager;
-import androidx.loader.content.Loader;
-import androidx.recyclerview.widget.LinearLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
+class GenreDetailActivity :
+    AbsSlidingMusicPanelActivity(), CabHolder, LoaderManager.LoaderCallbacks<List<Song>> {
 
-import com.afollestad.materialcab.MaterialCab;
-import com.h6ah4i.android.widget.advrecyclerview.utils.WrapperAdapterUtils;
-import player.phonograph.R;
-import player.phonograph.adapter.song.SongAdapter;
-import player.phonograph.helper.MusicPlayerRemote;
-import player.phonograph.interfaces.CabHolder;
-import player.phonograph.interfaces.LoaderIds;
-import player.phonograph.loader.GenreLoader;
-import player.phonograph.misc.WrappedAsyncTaskLoader;
-import player.phonograph.model.Genre;
-import player.phonograph.model.Song;
-import player.phonograph.ui.activities.base.AbsSlidingMusicPanelActivity;
-import player.phonograph.util.PhonographColorUtil;
-import player.phonograph.util.ViewUtil;
-import com.simplecityapps.recyclerview_fastscroll.views.FastScrollRecyclerView;
+    private lateinit var recyclerView: RecyclerView
+    private lateinit var mToolbar: Toolbar
+    private lateinit var empty: TextView
 
-import java.util.ArrayList;
-import java.util.List;
+    private lateinit var genre: Genre
 
-import butterknife.BindView;
-import butterknife.ButterKnife;
-import chr_56.MDthemer.core.ThemeColor;
+    private lateinit var adapter: SongAdapter
 
-public class GenreDetailActivity extends AbsSlidingMusicPanelActivity implements CabHolder, LoaderManager.LoaderCallbacks<List<Song>> {
+    private var cab: MaterialCab? = null
 
-    private static final int LOADER_ID = LoaderIds.GENRE_DETAIL_ACTIVITY;
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setDrawUnderStatusbar()
 
-    public static final String EXTRA_GENRE = "extra_genre";
+        // todo: viewBinding
+        recyclerView = findViewById(R.id.recycler_view)
+        mToolbar = findViewById(R.id.toolbar)
+        empty = findViewById(android.R.id.empty)
 
-    @BindView(R.id.recycler_view)
-    RecyclerView recyclerView;
-    @BindView(R.id.toolbar)
-    Toolbar toolbar;
-    @BindView(android.R.id.empty)
-    TextView empty;
+        setStatusbarColorAuto()
+        setNavigationbarColorAuto()
+        setTaskDescriptionColorAuto()
 
-    private Genre genre;
+        genre = intent.extras?.getParcelable(EXTRA_GENRE) ?: throw Exception("No genre in the intent!")
 
-    private MaterialCab cab;
-    private SongAdapter adapter;
+        setUpRecyclerView()
+        setUpToolBar()
 
-    private RecyclerView.Adapter wrappedAdapter;
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setDrawUnderStatusbar();
-        ButterKnife.bind(this);
-
-        setStatusbarColorAuto();
-        setNavigationbarColorAuto();
-        setTaskDescriptionColorAuto();
-
-        genre = getIntent().getExtras().getParcelable(EXTRA_GENRE);
-
-        setUpRecyclerView();
-
-        setUpToolBar();
-
-        getSupportLoaderManager().initLoader(LOADER_ID, null, this);
+        LoaderManager.getInstance(this).initLoader(LOADER_ID, null, this)
     }
 
-    @Override
-    protected View createContentView() {
-        return wrapSlidingMusicPanel(R.layout.activity_genre_detail);
+    override fun createContentView(): View {
+        return wrapSlidingMusicPanel(R.layout.activity_genre_detail)
     }
 
-    private void setUpRecyclerView() {
-        ViewUtil.setUpFastScrollRecyclerViewColor(this, ((FastScrollRecyclerView) recyclerView), ThemeColor.accentColor(this));
-        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+    private fun setUpRecyclerView() {
+        ViewUtil.setUpFastScrollRecyclerViewColor(
+            this, recyclerView as FastScrollRecyclerView, ThemeColor.accentColor(this)
+        )
 
-        adapter = new SongAdapter(this, new ArrayList<>(), R.layout.item_list, false, this);
-        recyclerView.setAdapter(adapter);
+        recyclerView.layoutManager = LinearLayoutManager(this)
+        adapter = SongAdapter(this, ArrayList(), R.layout.item_list, false, this)
+        recyclerView.adapter = adapter
 
-        adapter.registerAdapterDataObserver(new RecyclerView.AdapterDataObserver() {
-            @Override
-            public void onChanged() {
-                super.onChanged();
-                checkIsEmpty();
+        adapter.registerAdapterDataObserver(object : RecyclerView.AdapterDataObserver() {
+            override fun onChanged() {
+                super.onChanged()
+                checkIsEmpty()
             }
-        });
+        })
     }
 
-    private void setUpToolBar() {
-        toolbar.setBackgroundColor(ThemeColor.primaryColor(this));
-        setSupportActionBar(toolbar);
-        //noinspection ConstantConditions
-        getSupportActionBar().setTitle(genre.name);
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    private fun setUpToolBar() {
+        mToolbar.setBackgroundColor(ThemeColor.primaryColor(this))
+        setSupportActionBar(mToolbar)
+        supportActionBar!!.title = genre.name
+        supportActionBar!!.setDisplayHomeAsUpEnabled(true)
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.menu_genre_detail, menu);
-        return super.onCreateOptionsMenu(menu);
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_genre_detail, menu)
+        return super.onCreateOptionsMenu(menu)
     }
 
-    @Override
-    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
-        int id = item.getItemId();
-        switch (id) {
-            case R.id.action_shuffle_genre:
-                MusicPlayerRemote.openAndShuffleQueue(adapter.getDataSet(), true);
-                return true;
-            case android.R.id.home:
-                onBackPressed();
-                return true;
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.action_shuffle_genre -> {
+                MusicPlayerRemote.openAndShuffleQueue(adapter.dataSet, true)
+                return true
+            }
+            android.R.id.home -> {
+                onBackPressed()
+                return true
+            }
         }
-        return super.onOptionsItemSelected(item);
+        return super.onOptionsItemSelected(item)
     }
 
-    @NonNull
-    @Override
-    public MaterialCab openCab(final int menu, final MaterialCab.Callback callback) {
-        if (cab != null && cab.isActive()) cab.finish();
-        cab = new MaterialCab(this, R.id.cab_stub)
-                .setMenu(menu)
-                .setCloseDrawableRes(R.drawable.ic_close_white_24dp)
-                .setBackgroundColor(PhonographColorUtil.shiftBackgroundColorForLightText(ThemeColor.primaryColor(this)))
-                .start(callback);
-        return cab;
+    override fun openCab(menu: Int, callback: MaterialCab.Callback): MaterialCab {
+        cab?.let { if (it.isActive) it.finish() }
+        return MaterialCab(this, R.id.cab_stub)
+            .setMenu(menu)
+            .setCloseDrawableRes(R.drawable.ic_close_white_24dp)
+            .setBackgroundColor(PhonographColorUtil.shiftBackgroundColorForLightText(ThemeColor.primaryColor(this)))
+            .start(callback)
+            .also { cab = it /* set activity's cab */ }
     }
 
-    @Override
-    public void onBackPressed() {
-        if (cab != null && cab.isActive()) cab.finish();
-        else {
-            recyclerView.stopScroll();
-            super.onBackPressed();
+    override fun onBackPressed() {
+        if (cab != null && cab!!.isActive) cab!!.finish() else {
+            recyclerView.stopScroll()
+            super.onBackPressed()
         }
     }
 
-    @Override
-    public void onMediaStoreChanged() {
-        super.onMediaStoreChanged();
-        getSupportLoaderManager().restartLoader(LOADER_ID, null, this);
+    override fun onMediaStoreChanged() {
+        super.onMediaStoreChanged()
+        LoaderManager.getInstance(this).restartLoader(LOADER_ID, null, this)
     }
 
-    private void checkIsEmpty() {
-        empty.setVisibility(
-                adapter.getItemCount() == 0 ? View.VISIBLE : View.GONE
-        );
+    private fun checkIsEmpty() {
+        empty.visibility = if (adapter.itemCount == 0) View.VISIBLE else View.GONE
     }
 
-    @Override
-    protected void onDestroy() {
-        if (recyclerView != null) {
-            recyclerView.setAdapter(null);
-            recyclerView = null;
+    override fun onDestroy() {
+        recyclerView.adapter = null
+        super.onDestroy()
+    }
+
+    override fun onCreateLoader(id: Int, args: Bundle?): Loader<List<Song>> {
+        return AsyncGenreSongLoader(this, genre)
+    }
+
+    override fun onLoadFinished(loader: Loader<List<Song>>, data: List<Song>) {
+        adapter.swapDataSet(data)
+    }
+
+    override fun onLoaderReset(loader: Loader<List<Song>>) {
+        adapter.swapDataSet(ArrayList())
+    }
+
+    private class AsyncGenreSongLoader(context: Context, private val genre: Genre) :
+        WrappedAsyncTaskLoader<List<Song>>(context) {
+        override fun loadInBackground(): List<Song> {
+            return GenreLoader.getSongs(context, genre.id)
         }
-
-        if (wrappedAdapter != null) {
-            WrapperAdapterUtils.releaseAll(wrappedAdapter);
-            wrappedAdapter = null;
-        }
-        adapter = null;
-
-        super.onDestroy();
     }
 
-    @Override
-    @NonNull
-    public Loader<List<Song>> onCreateLoader(int id, Bundle args) {
-        return new GenreDetailActivity.AsyncGenreSongLoader(this, genre);
-    }
-
-    @Override
-    public void onLoadFinished(@NonNull Loader<List<Song>> loader, List<Song> data) {
-        if (adapter != null)
-            adapter.swapDataSet(data);
-    }
-
-    @Override
-    public void onLoaderReset(@NonNull Loader<List<Song>> loader) {
-        if (adapter != null)
-            adapter.swapDataSet(new ArrayList<>());
-    }
-
-    private static class AsyncGenreSongLoader extends WrappedAsyncTaskLoader<List<Song>> {
-        private final Genre genre;
-
-        public AsyncGenreSongLoader(Context context, Genre genre) {
-            super(context);
-            this.genre = genre;
-        }
-
-        @Override
-        public List<Song> loadInBackground() {
-            return GenreLoader.getSongs(getContext(), genre.id);
-        }
+    companion object {
+        private const val LOADER_ID = LoaderIds.GENRE_DETAIL_ACTIVITY
+        const val EXTRA_GENRE = "extra_genre"
     }
 }
