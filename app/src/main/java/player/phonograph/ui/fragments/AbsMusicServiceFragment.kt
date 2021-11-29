8926a42644f25129e39765b148a6fcbbcdedefd7
chr56 <chr0056@gmail.com>
2021-11-29 17:37:35 +0800

    convert AbsMusicServiceFragment.java into kotlin

---
+++ app/src/main/java/player/phonograph/ui/fragments/AbsMusicServiceFragment.kt
@@ -0,0 +1,46 @@
+package player.phonograph.ui.fragments
+
+import android.content.Context
+import android.os.Bundle
+import android.view.View
+import androidx.fragment.app.Fragment
+import player.phonograph.interfaces.MusicServiceEventListener
+import player.phonograph.ui.activities.base.AbsMusicServiceActivity
+
+/**
+ * @author Karim Abou Zeid (kabouzeid)
+ */
+open class AbsMusicServiceFragment : Fragment(), MusicServiceEventListener {
+    private var bindingActivity: AbsMusicServiceActivity? = null
+
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+        try {
+            bindingActivity = context as AbsMusicServiceActivity
+        } catch (e: ClassCastException) {
+            throw RuntimeException(context.javaClass.simpleName + " must be an instance of " + AbsMusicServiceActivity::class.java.simpleName)
+        }
+    }
+    override fun onDetach() {
+        super.onDetach()
+        bindingActivity = null
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        bindingActivity!!.addMusicServiceEventListener(this)
+    }
+    override fun onDestroyView() {
+        super.onDestroyView()
+        bindingActivity!!.removeMusicServiceEventListener(this)
+    }
+
+    override fun onPlayingMetaChanged() {}
+    override fun onServiceConnected() {}
+    override fun onServiceDisconnected() {}
+    override fun onQueueChanged() {}
+    override fun onPlayStateChanged() {}
+    override fun onRepeatModeChanged() {}
+    override fun onShuffleModeChanged() {}
+    override fun onMediaStoreChanged() {}
+}
