ec974c4f59d4a9406ffcb83f0ab6c425aec7109c
chr56 <chr0056@gmail.com>
2021-11-30 23:16:35 +0800

    extend LyricsUtil

--- app/src/main/java/player/phonograph/util/LyricsUtil.kt
+++ app/src/main/java/player/phonograph/util/LyricsUtil.kt
@@ -7,23 +7,118 @@ package player.phonograph.util
 import android.content.Context
 import android.content.Intent
 import android.os.Environment
+import org.jaudiotagger.audio.AudioFileIO
+import org.jaudiotagger.tag.FieldKey
 import org.json.JSONArray
 import player.phonograph.App
+import player.phonograph.BuildConfig
 import player.phonograph.R
 import player.phonograph.helper.MusicPlayerRemote
+import player.phonograph.model.Song
+import player.phonograph.model.lyrics.AbsLyrics
+import player.phonograph.model.lyrics.LyricsParsed
+import player.phonograph.model.lyrics.LyricsParsedSynchronized
+import java.io.File
 import java.io.FileOutputStream
+import java.util.regex.Pattern
 
 /**
- * Util for "MIUI StatusBar Lyrics" Xposed module
+ * Util for Lyrics
  */
 object LyricsUtil {
+
+    fun readRawLyrics(song: Song): String {
+
+        var rawLyrics: String? = null
+        val file = File(song.data)
+
+        // from song
+        try {
+            rawLyrics = AudioFileIO.read(file).tagOrCreateDefault.getFirst(FieldKey.LYRICS)
+        } catch (e: Exception) {
+            val buildType = BuildConfig.BUILD_TYPE
+            if (buildType != "release" || buildType != "preview") { e.printStackTrace() }
+        }
+
+        // from file
+        if (rawLyrics == null || rawLyrics.trim { it <= ' ' }.isEmpty()) {
+            val dir = file.absoluteFile.parentFile
+            if (dir != null && dir.exists() && dir.isDirectory) {
+                val format = ".*%s.*\\.(lrc|txt)"
+
+                val filename = Pattern.quote(FileUtil.stripExtension(file.name))
+                val songTitle = Pattern.quote(song.title)
+
+                val patterns = listOf<Pattern>(
+                    Pattern.compile(String.format(format, filename), Pattern.CASE_INSENSITIVE or Pattern.UNICODE_CASE),
+                    Pattern.compile(String.format(format, songTitle), Pattern.CASE_INSENSITIVE or Pattern.UNICODE_CASE)
+                )
+
+                val files = dir.listFiles { f: File ->
+                    for (pattern in patterns) {
+                        if (pattern.matcher(f.name).matches())
+                            return@listFiles true
+                    }
+                    false
+                }
+
+                if (files != null && files.isNotEmpty()) {
+                    for (f in files) {
+                        // todo Precise match
+                        try {
+                            rawLyrics = FileUtil.read(f)
+                            if (rawLyrics.trim { it <= ' ' }.isNotEmpty()) {
+                                return rawLyrics
+                            }
+                        } catch (e: Exception) {
+                            e.printStackTrace()
+                        }
+                    }
+                }
+            }
+        }
+
+        return rawLyrics ?: throw Exception("NO_LYRICS")
+    }
+
+    fun checkType(raw: String): Short {
+        val lrc = Pattern.compile("(\\[.+\\])+.*", Pattern.MULTILINE)
+        return if (lrc.matcher(raw).find()) {
+            AbsLyrics.LRC
+        } else {
+            AbsLyrics.TXT
+        }
+    }
+
+    fun loadLyrics(raw: String): AbsLyrics? {
+        return if (checkType(raw) == AbsLyrics.LRC) {
+            LyricsParsedSynchronized.parse(raw)
+        } else {
+            LyricsParsed.parse(raw)
+        }
+    }
+
+    fun fetchLyrics(song: Song): AbsLyrics? {
+        var raw: String? = null
+        try {
+            raw = readRawLyrics(song)
+        } catch (e: Exception) {
+            if (e.message == "NO_LYRICS") {
+                return null
+            } else {
+                e.printStackTrace()
+            }
+        }
+        return if (raw != null) loadLyrics(raw)
+        else null
+    }
 
     /**
      * broadcast for "MIUI StatusBar Lyrics" Xposed module
      * @param line the lyrics
      */
     fun broadcastLyrics(context: Context, line: String) {
-        if (!PreferenceUtil.getInstance(context).broadcastSynchronizedLyrics()) return;
+        if (!PreferenceUtil.getInstance(context).broadcastSynchronizedLyrics()) return
         // sending only when playing
         if (MusicPlayerRemote.isPlaying()) {
             if (line.isNotEmpty()) {
@@ -47,7 +142,7 @@ object LyricsUtil {
      * @param force send stop intent but ignoring preference
      */
     fun broadcastLyricsStop(context: Context, force: Boolean) {
-        if ((!PreferenceUtil.getInstance(context).broadcastSynchronizedLyrics()) && (!force)) return;
+        if ((!PreferenceUtil.getInstance(context).broadcastSynchronizedLyrics()) && (!force)) return
         context.sendBroadcast(
             Intent().setAction("Lyric_Server").putExtra("Lyric_Type", "app_stop")
         )
@@ -58,7 +153,7 @@ object LyricsUtil {
      * @param line the lyrics
      */
     fun writeLyricsFile(context: Context, line: String) {
-        if (!PreferenceUtil.getInstance(context).broadcastSynchronizedLyrics()) return;
+        if (!PreferenceUtil.getInstance(context).broadcastSynchronizedLyrics()) return
         // sending only when playing
         if (!MusicPlayerRemote.isPlaying()) return
         try {
@@ -83,7 +178,7 @@ object LyricsUtil {
      * write a file for "MIUI StatusBar Lyrics" Xposed module
      */
     fun writeLyricsFileStop() {
-        if (!PreferenceUtil.getInstance(App.instance).broadcastSynchronizedLyrics()) return;
+        if (!PreferenceUtil.getInstance(App.instance).broadcastSynchronizedLyrics()) return
         try {
             val outputStream = FileOutputStream(PATH)
             val jsonArray = JSONArray()
