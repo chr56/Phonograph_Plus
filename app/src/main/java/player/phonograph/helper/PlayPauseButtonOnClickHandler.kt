2856fd98fce0d37b9f194af9bd6c761f540435d7
chr56 <chr0056@gmail.com>
2021-12-02 19:18:30 +0800

    convert PlayPauseButtonOnClickHandler.java into kotlin

---
+++ app/src/main/java/player/phonograph/helper/PlayPauseButtonOnClickHandler.kt
@@ -0,0 +1,18 @@
+package player.phonograph.helper
+
+import android.view.View
+import androidx.annotation.Keep
+
+/**
+ * @author Karim Abou Zeid (kabouzeid)
+ */
+@Keep
+class PlayPauseButtonOnClickHandler : View.OnClickListener {
+    override fun onClick(v: View) {
+        if (MusicPlayerRemote.isPlaying()) {
+            MusicPlayerRemote.pauseSong()
+        } else {
+            MusicPlayerRemote.resumePlaying()
+        }
+    }
+}
