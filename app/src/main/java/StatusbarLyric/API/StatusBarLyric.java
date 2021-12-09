8801f9958ace903a8138b5d4aa2f6a1cc9daee94
chr56 <chr0056@gmail.com>
2021-12-09 10:13:47 +0800

    delete '\n' in icon BASE64

--- app/src/main/java/StatusbarLyric/API/StatusBarLyric.java
+++ app/src/main/java/StatusbarLyric/API/StatusBarLyric.java
@@ -88,7 +88,7 @@ public class StatusBarLyric {
         ByteArrayOutputStream baos = new ByteArrayOutputStream();
         bitmap.compress(Bitmap.CompressFormat.PNG,100,baos);
         byte[] bytes = baos.toByteArray();
-        return Base64.encodeToString(bytes, Base64.DEFAULT);
+        return Base64.encodeToString(bytes, Base64.DEFAULT).replace("\n","");
     }
 
 
