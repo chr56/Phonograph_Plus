1ff6c86401091d501bc583dbab2191c2d9e4249c
chr56 <chr0056@gmail.com>
2022-01-03 22:28:47 +0800

    SongLoader: convert into kotlin

--- app/src/main/java/player/phonograph/loader/SongLoader.kt
+++ app/src/main/java/player/phonograph/loader/SongLoader.kt
@@ -1,156 +1,53 @@
-package player.phonograph.loader;
+package player.phonograph.loader
 
-import android.content.Context;
-import android.database.Cursor;
-import android.provider.BaseColumns;
-import android.provider.MediaStore.Audio.AudioColumns;
-
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
-
-import java.util.List;
-
-import player.phonograph.model.Song;
-import player.phonograph.util.MediaStoreUtil;
+import android.content.Context
+import android.database.Cursor
+import android.provider.BaseColumns
+import android.provider.MediaStore
+import player.phonograph.model.Song
+import player.phonograph.util.MediaStoreUtil
+import player.phonograph.util.MediaStoreUtil.querySongs
 
 /**
  * @author Karim Abou Zeid (kabouzeid)
  */
-public class SongLoader {
-    protected static final String BASE_SELECTION = AudioColumns.IS_MUSIC + " =1" + " AND " + AudioColumns.TITLE + " != ''";
-    protected static final String[] BASE_PROJECTION = new String[]{
-            BaseColumns._ID,// 0
-            AudioColumns.TITLE,// 1
-            AudioColumns.TRACK,// 2
-            AudioColumns.YEAR,// 3
-            AudioColumns.DURATION,// 4
-            AudioColumns.DATA,// 5
-            AudioColumns.DATE_MODIFIED,// 6
-            AudioColumns.ALBUM_ID,// 7
-            AudioColumns.ALBUM,// 8
-            AudioColumns.ARTIST_ID,// 9
-            AudioColumns.ARTIST,// 10
-    };
+object SongLoader {
+    const val BASE_SELECTION =
+        MediaStore.Audio.AudioColumns.IS_MUSIC + " =1" + " AND " + MediaStore.Audio.AudioColumns.TITLE + " != ''"
+    internal val BASE_PROJECTION = arrayOf(
+        BaseColumns._ID, // 0
+        MediaStore.Audio.AudioColumns.TITLE, // 1
+        MediaStore.Audio.AudioColumns.TRACK, // 2
+        MediaStore.Audio.AudioColumns.YEAR, // 3
+        MediaStore.Audio.AudioColumns.DURATION, // 4
+        MediaStore.Audio.AudioColumns.DATA, // 5
+        MediaStore.Audio.AudioColumns.DATE_MODIFIED, // 6
+        MediaStore.Audio.AudioColumns.ALBUM_ID, // 7
+        MediaStore.Audio.AudioColumns.ALBUM, // 8
+        MediaStore.Audio.AudioColumns.ARTIST_ID, // 9
+        MediaStore.Audio.AudioColumns.ARTIST
+    )
 
-    @NonNull
-    public static List<Song> getAllSongs(@NonNull Context context) {
-//        Cursor cursor = makeSongCursor(context, null, null);
-//        return getSongs(cursor);
-        return MediaStoreUtil.INSTANCE.getAllSongs(context);
-    }
+    @JvmStatic
+    fun getAllSongs(context: Context): List<Song?> = MediaStoreUtil.getAllSongs(context)
 
-    @NonNull
-    public static List<Song> getSongs(@NonNull final Context context, final String query) {
-//        Cursor cursor = makeSongCursor(context, AudioColumns.TITLE + " LIKE ?", new String[]{"%" + query + "%"});
-//        return getSongs(cursor);
-        return MediaStoreUtil.INSTANCE.getSongs(context, query);
-    }
+    @JvmStatic
+    fun getSongs(context: Context, query: String): List<Song> = MediaStoreUtil.getSongs(context, query)
 
-    @NonNull
-    public static Song getSong(@NonNull final Context context, final long queryId) {
-//        Cursor cursor = makeSongCursor(context, AudioColumns._ID + "=?", new String[]{String.valueOf(queryId)});
-//        return getSong(cursor);
-        return MediaStoreUtil.INSTANCE.getSong(context, queryId);
-    }
+    @JvmStatic
+    fun getSong(context: Context, queryId: Long): Song = MediaStoreUtil.getSong(context, queryId)
 
-    @NonNull
-    public static List<Song> getSongs(@Nullable final Cursor cursor) {
-//        List<Song> songs = new ArrayList<>();
-//        if (cursor != null && cursor.moveToFirst()) {
-//            do {
-//                songs.add(getSongFromCursorImpl(cursor));
-//            } while (cursor.moveToNext());
-//        }
-//
-//        if (cursor != null)
-//            cursor.close();
-//        return songs;
-        return MediaStoreUtil.INSTANCE.getSongs(cursor);
-    }
+    @JvmStatic
+    fun getSongs(cursor: Cursor?): List<Song> = MediaStoreUtil.getSongs(cursor)
 
-    @NonNull
-    public static Song getSong(@Nullable Cursor cursor) {
-//        Song song;
-//        if (cursor != null && cursor.moveToFirst()) {
-//            song = getSongFromCursorImpl(cursor);
-//        } else {
-//            song = Song.EMPTY_SONG;
-//        }
-//        if (cursor != null) {
-//            cursor.close();
-//        }
-//        return song;
-        return MediaStoreUtil.INSTANCE.getSong(cursor);
-    }
-/*
-    @NonNull
-    private static Song getSongFromCursorImpl(@NonNull Cursor cursor) {
-        final long id = cursor.getLong(0);
-        final String title = cursor.getString(1);
-        final int trackNumber = cursor.getInt(2);
-        final int year = cursor.getInt(3);
-        final long duration = cursor.getLong(4);
-        final String data = cursor.getString(5);
-        final long dateModified = cursor.getLong(6);
-        final long albumId = cursor.getLong(7);
-        final String albumName = cursor.getString(8);
-        final long artistId = cursor.getLong(9);
-        final String artistName = cursor.getString(10);
+    @JvmStatic
+    fun getSong(cursor: Cursor?): Song = MediaStoreUtil.getSong(cursor)
 
-        return new Song(id, title, trackNumber, year, duration, data, dateModified, albumId, albumName, artistId, artistName);
-    }
-*/
-    @Nullable
-    public static Cursor makeSongCursor(@NonNull final Context context, @Nullable final String selection, final String[] selectionValues) {
-//        return makeSongCursor(context, selection, selectionValues, PreferenceUtil.getInstance(context).getSongSortOrder());
-        return MediaStoreUtil.INSTANCE.querySongs(context, selection, selectionValues);
-    }
+    @JvmStatic
+    fun makeSongCursor(context: Context, selection: String?, selectionValues: Array<String>?): Cursor? =
+        querySongs(context, selection, selectionValues)
 
-    @Nullable
-    public static Cursor makeSongCursor(@NonNull final Context context, @Nullable String selection, String[] selectionValues, final String sortOrder) {
-        /*
-        if (selection != null && !selection.trim().equals("")) {
-            selection = BASE_SELECTION + " AND " + selection;
-        } else {
-            selection = BASE_SELECTION;
-        }
-
-        // Blacklist
-        List<String> paths = BlacklistStore.getInstance(context).getPaths();
-        if (!paths.isEmpty()) {
-            selection = generateBlacklistSelection(selection, paths.size());
-            selectionValues = addBlacklistSelectionValues(selectionValues, paths);
-        }
-
-        try {
-            return context.getContentResolver().query(MediaStore.Audio.Media.EXTERNAL_CONTENT_URI,
-                    BASE_PROJECTION, selection, selectionValues, sortOrder);
-        } catch (SecurityException e) {
-            return null;
-        }
-
-         */
-        return MediaStoreUtil.INSTANCE.querySongs(context, selection, selectionValues, sortOrder);
-    }
-/*
-    private static String generateBlacklistSelection(String selection, int pathCount) {
-        String newSelection = selection != null && !selection.trim().equals("") ? selection + " AND " : "";
-        newSelection += AudioColumns.DATA + " NOT LIKE ?";
-        for (int i = 0; i < pathCount - 1; i++) {
-            newSelection += " AND " + AudioColumns.DATA + " NOT LIKE ?";
-        }
-        return newSelection;
-    }
-
-    private static String[] addBlacklistSelectionValues(String[] selectionValues, List<String> paths) {
-        if (selectionValues == null) selectionValues = new String[0];
-        String[] newSelectionValues = new String[selectionValues.length + paths.size()];
-        System.arraycopy(selectionValues, 0, newSelectionValues, 0, selectionValues.length);
-        for (int i = selectionValues.length; i < newSelectionValues.length; i++) {
-            newSelectionValues[i] = paths.get(i - selectionValues.length) + "%";
-        }
-        return newSelectionValues;
-    }
-
- */
+    @JvmStatic
+    fun makeSongCursor(context: Context, selection: String?, selectionValues: Array<String>?, sortOrder: String?): Cursor? =
+        querySongs(context, selection, selectionValues, sortOrder!!)
 }
