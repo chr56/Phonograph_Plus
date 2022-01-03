823d815457dd3d8b2f5c9ab9932548b2ffd40a97
chr56 <chr0056@gmail.com>
2022-01-03 22:14:22 +0800

    GenresFragment: convert into kotlin

--- app/src/main/java/player/phonograph/ui/fragments/mainactivity/library/pager/GenresFragment.kt
+++ app/src/main/java/player/phonograph/ui/fragments/mainactivity/library/pager/GenresFragment.kt
@@ -1,80 +1,58 @@
-package player.phonograph.ui.fragments.mainactivity.library.pager;
+package player.phonograph.ui.fragments.mainactivity.library.pager
 
-import android.content.Context;
-import android.os.Bundle;
+import android.content.Context
+import android.os.Bundle
+import androidx.loader.app.LoaderManager
+import androidx.loader.content.Loader
+import androidx.recyclerview.widget.LinearLayoutManager
+import player.phonograph.R
+import player.phonograph.adapter.GenreAdapter
+import player.phonograph.interfaces.LoaderIds
+import player.phonograph.loader.GenreLoader.getAllGenres
+import player.phonograph.misc.WrappedAsyncTaskLoader
+import player.phonograph.model.Genre
+import java.util.ArrayList
 
-import androidx.annotation.NonNull;
-import androidx.loader.app.LoaderManager;
-import androidx.loader.content.Loader;
-import androidx.recyclerview.widget.LinearLayoutManager;
+// todo null safety
+class GenresFragment : AbsLibraryPagerRecyclerViewFragment<GenreAdapter, LinearLayoutManager>(), LoaderManager.LoaderCallbacks<List<Genre>> {
 
-import player.phonograph.R;
-import player.phonograph.adapter.GenreAdapter;
-import player.phonograph.interfaces.LoaderIds;
-import player.phonograph.loader.GenreLoader;
-import player.phonograph.misc.WrappedAsyncTaskLoader;
-import player.phonograph.model.Genre;
-
-import java.util.ArrayList;
-import java.util.List;
-
-public class GenresFragment extends AbsLibraryPagerRecyclerViewFragment<GenreAdapter, LinearLayoutManager> implements LoaderManager.LoaderCallbacks<List<Genre>> {
-
-    private static final int LOADER_ID = LoaderIds.GENRES_FRAGMENT;
-
-    @Override
-    public void onActivityCreated(Bundle savedInstanceState) {
-        super.onActivityCreated(savedInstanceState);
-        getLoaderManager().initLoader(LOADER_ID, null, this);
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        loaderManager.initLoader(LOADER_ID, null, this)
     }
 
-    @NonNull
-    @Override
-    protected LinearLayoutManager createLayoutManager() {
-        return new LinearLayoutManager(getActivity());
+    override fun createLayoutManager(): LinearLayoutManager {
+        return LinearLayoutManager(activity)
     }
 
-    @NonNull
-    @Override
-    protected GenreAdapter createAdapter() {
-        List<Genre> dataSet = getAdapter() == null ? new ArrayList<>() : getAdapter().getDataSet();
-        return new GenreAdapter(getLibraryFragment().getMainActivity(), dataSet, R.layout.item_list_no_image);
+    override fun createAdapter(): GenreAdapter {
+        val dataSet = if (adapter == null) ArrayList() else adapter!!.dataSet
+        return GenreAdapter(libraryFragment!!.mainActivity, dataSet, R.layout.item_list_no_image)
     }
 
-    @Override
-    protected int getEmptyMessage() {
-        return R.string.no_genres;
+    override val emptyMessage: Int = R.string.no_genres
+
+    override fun onMediaStoreChanged() {
+        loaderManager.restartLoader(LOADER_ID, null, this)
     }
 
-    @Override
-    public void onMediaStoreChanged() {
-        getLoaderManager().restartLoader(LOADER_ID, null, this);
+    override fun onCreateLoader(id: Int, args: Bundle?): Loader<List<Genre>> {
+        return AsyncGenreLoader(libraryFragment!!.mainActivity)
     }
 
-    @Override
-    @NonNull
-    public Loader<List<Genre>> onCreateLoader(int id, Bundle args) {
-        return new GenresFragment.AsyncGenreLoader(getActivity());
+    override fun onLoadFinished(loader: Loader<List<Genre>>, data: List<Genre>) {
+        adapter!!.swapDataSet(data)
     }
 
-    @Override
-    public void onLoadFinished(@NonNull Loader<List<Genre>> loader, List<Genre> data) {
-        getAdapter().swapDataSet(data);
+    override fun onLoaderReset(loader: Loader<List<Genre>>) {
+        adapter!!.swapDataSet(ArrayList())
     }
 
-    @Override
-    public void onLoaderReset(@NonNull Loader<List<Genre>> loader) {
-        getAdapter().swapDataSet(new ArrayList<>());
+    private class AsyncGenreLoader(context: Context) : WrappedAsyncTaskLoader<List<Genre>>(context) {
+        override fun loadInBackground(): List<Genre> = getAllGenres(context)
     }
 
-    private static class AsyncGenreLoader extends WrappedAsyncTaskLoader<List<Genre>> {
-        public AsyncGenreLoader(Context context) {
-            super(context);
-        }
-
-        @Override
-        public List<Genre> loadInBackground() {
-            return GenreLoader.getAllGenres(getContext());
-        }
+    companion object {
+        private const val LOADER_ID = LoaderIds.GENRES_FRAGMENT
     }
 }
