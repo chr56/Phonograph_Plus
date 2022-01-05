bc9671bb535b024a46303dbcdf6bcab76c4414a3
chr56 <chr0056@gmail.com>
2022-01-05 11:58:37 +0800

    Glide: migrate PhonographGlideModule

--- app/src/main/java/player/phonograph/glide/PhonographGlideModule.java
+++ app/src/main/java/player/phonograph/glide/PhonographGlideModule.java
@@ -2,11 +2,16 @@ package player.phonograph.glide;
 
 import android.content.Context;
 
-import java.io.InputStream;
+import androidx.annotation.NonNull;
 
 import com.bumptech.glide.Glide;
 import com.bumptech.glide.GlideBuilder;
-import com.bumptech.glide.module.GlideModule;
+import com.bumptech.glide.Registry;
+import com.bumptech.glide.annotation.GlideModule;
+import com.bumptech.glide.module.AppGlideModule;
+
+import java.io.InputStream;
+
 import player.phonograph.glide.artistimage.ArtistImage;
 import player.phonograph.glide.artistimage.ArtistImageLoader;
 import player.phonograph.glide.audiocover.AudioFileCover;
@@ -15,15 +20,20 @@ import player.phonograph.glide.audiocover.AudioFileCoverLoader;
 /**
  * @author Karim Abou Zeid (kabouzeid)
  */
-public class PhonographGlideModule implements GlideModule {
+
+@GlideModule
+public class PhonographGlideModule extends AppGlideModule {
     @Override
-    public void applyOptions(Context context, GlideBuilder builder) {
+    public void applyOptions(@NonNull Context context, @NonNull GlideBuilder builder) {
 
     }
 
     @Override
-    public void registerComponents(Context context, Glide glide) {
-        glide.register(AudioFileCover.class, InputStream.class, new AudioFileCoverLoader.Factory());
-        glide.register(ArtistImage.class, InputStream.class, new ArtistImageLoader.Factory());
+    public void registerComponents(@NonNull Context context, @NonNull Glide glide, @NonNull Registry registry) {
+
+        registry.register(AudioFileCover.class, InputStream.class, new AudioFileCoverLoader.Factory());
+        registry.register(ArtistImage.class, InputStream.class, new ArtistImageLoader.Factory());
+
+        super.registerComponents(context, glide, registry);
     }
 }
