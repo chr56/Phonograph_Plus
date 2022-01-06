105ba6b3653aaeb7825c818c27131794e7d10bab
chr56 <chr0056@gmail.com>
2022-01-06 20:48:41 +0800

    Database: add method to refresh single song

--- app/src/main/java/player/phonograph/database/mediastore/Refresher.kt
+++ app/src/main/java/player/phonograph/database/mediastore/Refresher.kt
@@ -6,10 +6,13 @@ package player.phonograph.database.mediastore
 
 import android.content.Context
 import android.util.Log
+import player.phonograph.App
 import player.phonograph.helper.SortOrder
 import player.phonograph.util.MediaStoreUtil
 import player.phonograph.util.MediaStoreUtil.getSong
 import player.phonograph.util.MediaStoreUtil.querySongs
+import player.phonograph.database.mediastore.Song as SongEntity
+import player.phonograph.model.Song as SongModel
 
 object Refresher {
 
@@ -28,6 +31,20 @@ object Refresher {
 
         MusicDatabase.songsDataBase.lastUpdateTimestamp = getLastSong(context).dateModified
         Log.i("RoomDatabase", "End importing")
+    }
+
+    fun refreshSingleSong(context: Context?, songId: Long) {
+        refreshSingleSong(context, MediaStoreUtil.getSong(context ?: App.instance, songId))
+    }
+
+    fun refreshSingleSong(context: Context?, song: SongModel) {
+        val songDataBaseDao = MusicDatabase.songsDataBase.SongDao()
+        songDataBaseDao.update(SongConverter.fromSongModel(song))
+    }
+
+    fun refreshSingleSong(context: Context?, song: SongEntity) {
+        val songDataBaseDao = MusicDatabase.songsDataBase.SongDao()
+        songDataBaseDao.update(song)
     }
 
     fun getLastSong(context: Context): player.phonograph.model.Song {
