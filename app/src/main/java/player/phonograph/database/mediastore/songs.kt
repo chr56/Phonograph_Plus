2a3df39168b888b1a7fa2a60298d60813ac7e2a9
chr56 <chr0056@gmail.com>
2022-01-07 14:16:55 +0800

    Database: add more DAO functions

--- app/src/main/java/player/phonograph/database/mediastore/songs.kt
+++ app/src/main/java/player/phonograph/database/mediastore/songs.kt
@@ -10,7 +10,6 @@ import player.phonograph.util.PreferenceUtil
 
 // @Fts3
 @Entity(tableName = "songs")
-@TypeConverters(SongConverter::class)
 data class Song(
     @PrimaryKey // media store id
     var id: Long,
@@ -37,20 +36,33 @@ data class Song(
 )
 
 @Dao
+@TypeConverters(SongConverter::class)
 interface SongDao {
     @Query("SELECT * from songs")
     fun getAllSongs(): List<Song>
+    @Query("SELECT * from songs order by :sortOrder")
+    fun getAllSongs(sortOrder: String): List<Song>
 
     @Query("SELECT * from songs where id = :id")
     fun findSongById(id: Long): List<Song>
     @Query("SELECT * from songs where title = :title")
     fun findSongByTitle(title: String): List<Song>
-    @Query("SELECT * from songs where title like :title")
-    fun searchSongByTitle(title: String): List<Song>
+    @Query("SELECT * from songs where title like :title order by :sortOrder")
+    fun searchSongByTitle(title: String, sortOrder: String): List<Song>
     @Query("SELECT * from songs where album_name = :album")
     fun findSongByAlbum(album: String): List<Song>
+    @Query("SELECT * from songs where album_name like :album order by :sortOrder")
+    fun searchSongByAlbum(album: String, sortOrder: String): List<Song>
     @Query("SELECT * from songs where artist_name = :artist")
     fun findSongByArtist(artist: String): List<Song>
+    @Query("SELECT * from songs where artist_name like :artist order by :sortOrder")
+    fun searchSongByArtist(artist: String, sortOrder: String): List<Song>
+
+    @Query("SELECT * from songs where path in(:path) order by :sortOrder")
+    fun querySongByPath(path: Array<String>, sortOrder: String): List<Song>
+
+    @Query("SELECT * from songs where date_modified > :time order by :sortOrder")
+    fun queryLastAddedSongs(time: Long, sortOrder: String): List<Song>
 
     @Insert(onConflict = OnConflictStrategy.ABORT)
     fun insert(song: Song)
@@ -65,7 +77,7 @@ interface SongDao {
     fun delete(song: Song)
 }
 
-@Database(entities = arrayOf(Song::class), version = 1, exportSchema = false)
+@Database(entities = [Song::class], version = 1, exportSchema = false)
 abstract class SongDataBase : RoomDatabase() {
     abstract fun SongDao(): SongDao
     var lastUpdateTimestamp: Long = 0
