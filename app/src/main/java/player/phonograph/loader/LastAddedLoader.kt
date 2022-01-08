98b2ec708d7b2360742464ee38c931ad2e9f6fa1
chr56 <chr0056@gmail.com>
2022-01-08 12:09:38 +0800

    LastAddedLoader: use new database

--- app/src/main/java/player/phonograph/loader/LastAddedLoader.kt
+++ app/src/main/java/player/phonograph/loader/LastAddedLoader.kt
@@ -1,25 +1,22 @@
 package player.phonograph.loader
 
 import android.content.Context
-import android.database.Cursor
-import android.provider.MediaStore
-import player.phonograph.loader.SongLoader.getSongs
-import player.phonograph.loader.SongLoader.makeSongCursor
+import player.phonograph.database.mediastore.MusicDatabase
+import player.phonograph.database.mediastore.SongConverter
+import player.phonograph.helper.SortOrder
 import player.phonograph.model.Song
 import player.phonograph.util.PreferenceUtil.Companion.getInstance
 
 object LastAddedLoader {
     @JvmStatic
     fun getLastAddedSongs(context: Context): List<Song> {
-        return getSongs(makeLastAddedCursor(context))
-    }
 
-    private fun makeLastAddedCursor(context: Context): Cursor? {
-        val cutoff = getInstance(context).lastAddedCutoff
-        return makeSongCursor(
-            context,
-            MediaStore.Audio.Media.DATE_ADDED + ">?", arrayOf(cutoff.toString()),
-            MediaStore.Audio.Media.DATE_ADDED + " DESC"
-        )
+        val list = MusicDatabase.songsDataBase.SongDao()
+            .queryLastAddedSongs(
+                getInstance(context).lastAddedCutoff, SortOrder.SongSortOrder.SONG_DATE_MODIFIED_REVERT
+            )
+        return List<Song>(list.size) { index ->
+            SongConverter.toSongModel(list[index])
+        }
     }
 }
