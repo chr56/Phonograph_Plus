2a1d86fef2b589a82f83732a3bb69c2b6e3a1144
chr56 <chr0056@gmail.com>
2022-01-08 12:01:14 +0800

    LastAddedLoader: convert into kotlin

--- app/src/main/java/player/phonograph/loader/LastAddedLoader.kt
+++ app/src/main/java/player/phonograph/loader/LastAddedLoader.kt
@@ -1,29 +1,25 @@
-package player.phonograph.loader;
+package player.phonograph.loader
 
-import android.content.Context;
-import android.database.Cursor;
-import android.provider.MediaStore;
-import androidx.annotation.NonNull;
+import android.content.Context
+import android.database.Cursor
+import android.provider.MediaStore
+import player.phonograph.loader.SongLoader.getSongs
+import player.phonograph.loader.SongLoader.makeSongCursor
+import player.phonograph.model.Song
+import player.phonograph.util.PreferenceUtil.Companion.getInstance
 
-import player.phonograph.model.Song;
-import player.phonograph.util.PreferenceUtil;
-
-import java.util.List;
-
-public class LastAddedLoader {
-
-    @NonNull
-    public static List<Song> getLastAddedSongs(@NonNull Context context) {
-        return SongLoader.getSongs(makeLastAddedCursor(context));
+object LastAddedLoader {
+    @JvmStatic
+    fun getLastAddedSongs(context: Context): List<Song> {
+        return getSongs(makeLastAddedCursor(context))
     }
 
-    public static Cursor makeLastAddedCursor(@NonNull final Context context) {
-        long cutoff = PreferenceUtil.getInstance(context).getLastAddedCutoff();
-
-        return SongLoader.makeSongCursor(
-                context,
-                MediaStore.Audio.Media.DATE_ADDED + ">?",
-                new String[]{String.valueOf(cutoff)},
-                MediaStore.Audio.Media.DATE_ADDED + " DESC");
+    private fun makeLastAddedCursor(context: Context): Cursor? {
+        val cutoff = getInstance(context).lastAddedCutoff
+        return makeSongCursor(
+            context,
+            MediaStore.Audio.Media.DATE_ADDED + ">?", arrayOf(cutoff.toString()),
+            MediaStore.Audio.Media.DATE_ADDED + " DESC"
+        )
     }
 }
