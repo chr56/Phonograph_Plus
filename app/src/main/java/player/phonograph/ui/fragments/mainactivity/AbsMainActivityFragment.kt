b917568dedbc903851a550a08256c155f591a75e
chr56 <chr0056@gmail.com>
2022-01-09 19:24:24 +0800

    AbsMainActivityFragment: convert into kotlin

--- app/src/main/java/player/phonograph/ui/fragments/mainactivity/AbsMainActivityFragment.kt
+++ app/src/main/java/player/phonograph/ui/fragments/mainactivity/AbsMainActivityFragment.kt
@@ -1,23 +1,18 @@
-package player.phonograph.ui.fragments.mainactivity;
+package player.phonograph.ui.fragments.mainactivity
 
-import android.os.Bundle;
-
-import androidx.fragment.app.Fragment;
-
-import player.phonograph.ui.activities.MainActivity;
+import player.phonograph.ui.activities.MainActivity
+import android.os.Bundle
+import androidx.fragment.app.Fragment
 
 /**
  * @author Karim Abou Zeid (kabouzeid)
  */
-public abstract class AbsMainActivityFragment extends Fragment {
+abstract class AbsMainActivityFragment : Fragment() {
+    val mainActivity: MainActivity
+        get() = requireActivity() as MainActivity
 
-    public MainActivity getMainActivity() {
-        return (MainActivity) requireActivity();
-    }
-
-    @Override
-    public void onActivityCreated(Bundle savedInstanceState) {
-        super.onActivityCreated(savedInstanceState);
-        setHasOptionsMenu(true);
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        setHasOptionsMenu(true)
     } // Todo life-cycle
 }
