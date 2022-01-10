2274da6ace1f5c428c39ec27c31ce86354fe95d5
chr56 <chr0056@gmail.com>
2022-01-10 14:06:52 +0800

    SongPage: re-add

---
+++ app/src/main/java/player/phonograph/ui/fragments/mainactivity/library/new_ui/SongPage.kt
@@ -0,0 +1,19 @@
+/*
+ * Copyright (c) 2022 chr_56 & Abou Zeid (kabouzeid) (original author)
+ */
+
+package player.phonograph.ui.fragments.mainactivity.library.new_ui
+
+import androidx.recyclerview.widget.GridLayoutManager
+import player.phonograph.adapter.song.UniversalSongAdapter
+
+class SongPage : AbsDisplayPage<UniversalSongAdapter, GridLayoutManager>() {
+
+    override fun initLayoutManager(): GridLayoutManager {
+        TODO("Not yet implemented")
+    }
+
+    override fun initAdapter(): UniversalSongAdapter {
+        TODO("Not yet implemented")
+    }
+}
