3eddbe0285a0c4fe129fca2e0aa4472d57c94726
chr56 <chr0056@gmail.com>
2022-03-06 11:47:18 +0800

    [LoaderIds] convert into kotlin object & cleanup

---
+++ app/src/main/java/player/phonograph/interfaces/LoaderIds.kt
@@ -0,0 +1,13 @@
+package player.phonograph.interfaces
+
+/**
+ * @author Karim Abou Zeid (kabouzeid)
+ */
+object LoaderIds {
+    const val ALBUM_DETAIL_ACTIVITY = 1
+    const val ARTIST_DETAIL_ACTIVITY = 2
+    const val PLAYLIST_DETAIL_ACTIVITY = 3
+    const val GENRE_DETAIL_ACTIVITY = 4
+    const val SEARCH_ACTIVITY = 5
+    const val FOLDERS_FRAGMENT = 6
+}
