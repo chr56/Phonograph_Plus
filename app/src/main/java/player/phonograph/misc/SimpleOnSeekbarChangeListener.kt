3b1219a54f4f73cb28bb1020a3a6be6a49ebbc37
chr56 <chr0056@gmail.com>
2022-03-06 11:19:33 +0800

    [SimpleOnSeekbarChangeListener] convert into kotlin

---
+++ app/src/main/java/player/phonograph/misc/SimpleOnSeekbarChangeListener.kt
@@ -0,0 +1,13 @@
+package player.phonograph.misc
+
+import android.widget.SeekBar
+import android.widget.SeekBar.OnSeekBarChangeListener
+
+/**
+ * @author Karim Abou Zeid (kabouzeid)
+ */
+abstract class SimpleOnSeekbarChangeListener : OnSeekBarChangeListener {
+    override fun onProgressChanged(seekBar: SeekBar, progress: Int, fromUser: Boolean) {}
+    override fun onStartTrackingTouch(seekBar: SeekBar) {}
+    override fun onStopTrackingTouch(seekBar: SeekBar) {}
+}
