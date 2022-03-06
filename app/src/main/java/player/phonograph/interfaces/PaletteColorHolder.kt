b8bc8ec34ef26984c293a22056ab80d99418d48d
chr56 <chr0056@gmail.com>
2022-03-06 11:50:06 +0800

    [PaletteColorHolder] convert into kotlin

---
+++ app/src/main/java/player/phonograph/interfaces/PaletteColorHolder.kt
@@ -0,0 +1,11 @@
+package player.phonograph.interfaces
+
+import androidx.annotation.ColorInt
+
+/**
+ * @author Aidan Follestad (afollestad)
+ */
+interface PaletteColorHolder {
+    @get:ColorInt
+    val paletteColor: Int
+}
