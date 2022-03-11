4fc394ed1fc033bcae41935c959c4d7e409022f6
chr56 <chr0056@gmail.com>
2022-03-11 19:09:02 +0800

    [Setting] add a preference to switch song detail ui

--- app/src/main/java/player/phonograph/settings/Setting.kt
+++ app/src/main/java/player/phonograph/settings/Setting.kt
@@ -12,9 +12,6 @@ import android.os.Environment
 import android.util.Log
 import androidx.annotation.StyleRes
 import androidx.preference.PreferenceManager
-import java.io.File
-import kotlin.properties.ReadWriteProperty
-import kotlin.reflect.KProperty
 import org.json.JSONException
 import org.json.JSONObject
 import player.phonograph.App
@@ -27,6 +24,9 @@ import player.phonograph.mediastore.sort.SortRef
 import player.phonograph.ui.fragments.player.NowPlayingScreen
 import player.phonograph.util.CalendarUtil
 import player.phonograph.util.FileUtil
+import java.io.File
+import kotlin.properties.ReadWriteProperty
+import kotlin.reflect.KProperty
 
 class Setting(context: Context) {
 
@@ -246,6 +246,8 @@ class Setting(context: Context) {
     var useLegacyListFilesImpl: Boolean by BooleanPref(USE_LEGACY_LIST_FILES_IMPL, false)
     var playlistFilesOperationBehaviour: String by StringPref(PLAYLIST_FILES_OPERATION_BEHAVIOUR, PLAYLIST_OPS_BEHAVIOUR_AUTO)
 
+    var useLegacyDetailDialog: Boolean by BooleanPref(USE_LEGACY_DETAIL_DIALOG, false)
+
     // Changelog
     var lastChangeLogVersion: Int by IntPref(LAST_CHANGELOG_VERSION, -1)
     var introShown: Boolean by BooleanPref(INTRO_SHOWN, false)
@@ -326,6 +328,7 @@ class Setting(context: Context) {
         private const val USE_LEGACY_FAVORITE_PLAYLIST_IMPL = "use_legacy_favorite_playlist_impl"
         private const val USE_LEGACY_LIST_FILES_IMPL = "use_legacy_list_files_impl"
         private const val PLAYLIST_FILES_OPERATION_BEHAVIOUR = "playlist_files_operation_behaviour"
+        private const val USE_LEGACY_DETAIL_DIALOG = "use_legacy_detail_dialog"
 
         // Changelog
         private const val LAST_CHANGELOG_VERSION = "last_changelog_version"
