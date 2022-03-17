ca9c382207203506b1769a673fa13cae4473fc47
chr56 <chr0056@gmail.com>
2022-03-17 13:56:57 +0800

    [BlacklistUtil] improve performance

--- app/src/main/java/player/phonograph/util/BlacklistUtil.kt
+++ app/src/main/java/player/phonograph/util/BlacklistUtil.kt
@@ -7,6 +7,7 @@ package player.phonograph.util
 import android.content.Context
 import com.afollestad.materialdialogs.MaterialDialog
 import com.afollestad.materialdialogs.list.listItemsSingleChoice
+import kotlinx.coroutines.*
 import player.phonograph.App
 import player.phonograph.R
 import player.phonograph.model.Song
@@ -29,32 +30,40 @@ object BlacklistUtil {
     private fun addToBlacklistImpl(context: Context, string: String) {
         var path: String = string // parent folder
 
-        val candidatesPath = mutableListOf<String>()
-        while (path.isNotEmpty()) {
-            if (path.endsWith("/emulated/0", true)
-                or path.endsWith("/emulated", true)
-                or path.endsWith("/storage", true)
-            ) break // no junk paths
-            candidatesPath.add(path)
-            path = path.dropLastWhile { it != '/' }.dropLast(1) // last char is '/'
-        }
+        val coroutineScope = CoroutineScope(Dispatchers.IO)
 
-        MaterialDialog(context)
-            .title(R.string.label_file_path)
-            .noAutoDismiss()
-            .listItemsSingleChoice(items = candidatesPath) { dialog, _, pathText ->
-                if (pathText.isNotBlank()) {
-                    MaterialDialog(context)
-                        .title(R.string.add_blacklist)
-                        .message(text = pathText)
-                        .positiveButton(android.R.string.ok) {
-                            BlacklistStore.getInstance(App.instance).addPath(File(pathText as String))
-                            dialog.dismiss()
-                        }
-                        .negativeButton(android.R.string.cancel)
-                        .show()
-                }
+        coroutineScope.launch {
+            val candidatesPath = mutableListOf<String>()
+            while (path.isNotEmpty()) {
+                if (path.endsWith("/emulated/0", true)
+                    or path.endsWith("/emulated", true)
+                    or path.endsWith("/storage", true)
+                ) break // no junk paths
+                candidatesPath.add(path)
+                path = path.dropLastWhile { it != '/' }.dropLast(1) // last char is '/'
             }
-            .show()
+
+            withContext(Dispatchers.Main) {
+                MaterialDialog(context)
+                    .title(R.string.label_file_path)
+                    .noAutoDismiss()
+                    .listItemsSingleChoice(items = candidatesPath) { parentDialog, _, pathText ->
+                        if (pathText.isNotBlank()) {
+                            CoroutineScope(Dispatchers.Main).launch {
+                                MaterialDialog(context)
+                                    .title(R.string.add_blacklist)
+                                    .message(text = pathText)
+                                    .positiveButton(android.R.string.ok) {
+                                        CoroutineScope(Dispatchers.IO).launch(Dispatchers.IO) { BlacklistStore.getInstance(App.instance).addPath(File(pathText as String)) }
+                                        parentDialog.dismiss()
+                                    }
+                                    .negativeButton(android.R.string.cancel)
+                                    .show()
+                            }
+                        }
+                    }
+                    .show()
+            }
+        }
     }
 }
