6af4209dd6a28c5745a1b609c794f96a7512595f
chr56 <chr0056@gmail.com>
2022-03-17 20:40:50 +0800

    [AbsCustomPlaylist] convert into Kotlin

---
+++ app/src/main/java/player/phonograph/model/AbsCustomPlaylist.kt
@@ -0,0 +1,15 @@
+package player.phonograph.model
+
+import android.content.Context
+import android.os.Parcel
+
+/**
+ * @author Karim Abou Zeid (kabouzeid)
+ */
+abstract class AbsCustomPlaylist : Playlist {
+    constructor()
+    constructor(id: Long, name: String?) : super(id, name)
+    constructor(parcel: Parcel?) : super(parcel)
+
+    abstract fun getSongs(context: Context?): MutableList<Song>
+}
