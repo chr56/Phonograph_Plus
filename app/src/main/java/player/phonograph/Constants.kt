40dc9d0c47619d17c72c6b8bef4326ecd76e5e30
chr56 <chr0056@gmail.com>
2022-04-03 13:44:10 +0800

    [ErrorNotification] init

--- app/src/main/java/player/phonograph/Constants.kt
+++ app/src/main/java/player/phonograph/Constants.kt
@@ -19,4 +19,6 @@ const val UPGRADABLE = "upgradable"
 const val NOTIFICATION_CHANNEL_ID_UPGRADABLE = "upgrade_notification"
 const val NOTIFICATION_ID_UPGRADABLE = 8747233
 
+const val NOTIFICATION_CHANNEL_ID_ERROR = "error_notification"
+
 const val BROADCAST_PLAYLISTS_CHANGED = "${App.PACKAGE_NAME}.playlists_changed"
---
+++ app/src/main/java/player/phonograph/notification/ErrorNotification.kt
@@ -0,0 +1,81 @@
+/*
+ * Copyright (c) 2022 chr_56
+ */
+
+package player.phonograph.notification
+
+import android.app.Notification
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.app.PendingIntent
+import android.content.Context
+import android.content.Intent
+import android.os.Build
+import androidx.annotation.RequiresApi
+import androidx.core.app.NotificationCompat
+import player.phonograph.App
+import player.phonograph.NOTIFICATION_CHANNEL_ID_ERROR
+import player.phonograph.R
+import player.phonograph.ui.activities.CrashActivity
+
+object ErrorNotification {
+    private var notificationManager: NotificationManager? = null
+    private var isReady: Boolean = false
+
+    private var count = 0
+
+    fun init() {
+        notificationManager =
+            App.instance.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O && notificationManager != null) {
+            createNotificationChannel(notificationManager!!)
+        }
+        isReady = true
+    }
+
+    fun postErrorNotification(e: Throwable, note: String?) {
+        val msg = "$note\n${e::class.simpleName}\n${e.message}"
+        val context = App.instance
+        if (!isReady) init()
+
+        val action = Intent(context, CrashActivity::class.java).apply {
+            putExtra(App.KEY_STACK_TRACE, msg)
+        }
+
+        val clickIntent: PendingIntent = PendingIntent.getActivity(context, 0, action, PendingIntent.FLAG_IMMUTABLE)
+
+        notificationManager?.let { notificationManager ->
+            val notification: Notification =
+                NotificationCompat.Builder(context, NOTIFICATION_CHANNEL_ID_ERROR)
+                    .setSmallIcon(R.drawable.ic_notification)
+                    .setCategory(NotificationCompat.CATEGORY_ERROR)
+                    .setPriority(NotificationCompat.PRIORITY_HIGH)
+                    .setVisibility(NotificationCompat.VISIBILITY_PRIVATE)
+                    .setContentTitle(context.getString(R.string.error_notification_name))
+                    .setContentText(msg)
+                    .setContentIntent(clickIntent)
+                    .setAutoCancel(true)
+                    .build()
+
+            count++
+            notificationManager.notify(count, notification)
+        }
+    }
+
+    @RequiresApi(26)
+    private fun createNotificationChannel(notificationManager: NotificationManager) {
+
+        var notificationChannel: NotificationChannel? =
+            notificationManager.getNotificationChannel(NOTIFICATION_CHANNEL_ID_ERROR)
+        if (notificationChannel == null) {
+            notificationChannel = NotificationChannel(
+                NOTIFICATION_CHANNEL_ID_ERROR,
+                App.instance.getString(R.string.error_notification_name),
+                NotificationManager.IMPORTANCE_HIGH
+            )
+            notificationChannel.enableLights(false)
+            notificationChannel.enableVibration(false)
+            notificationManager.createNotificationChannel(notificationChannel)
+        }
+    }
+}
