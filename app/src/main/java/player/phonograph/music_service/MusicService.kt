cb018b9f97c1ed9a99abff9c291a529bbb406942
chr56 <chr0056@gmail.com>
2022-04-09 11:36:34 +0800

    [Service] register in manifest

---
+++ app/src/main/java/player/phonograph/music_service/MusicService.kt
@@ -0,0 +1,89 @@
+/*
+ * Copyright (c) 2022 chr_56
+ */
+
+package player.phonograph.music_service
+
+import android.app.PendingIntent
+import android.content.ComponentName
+import android.content.Intent
+import android.os.Bundle
+import android.support.v4.media.MediaBrowserCompat
+import android.support.v4.media.session.MediaSessionCompat
+import android.support.v4.media.session.PlaybackStateCompat
+import androidx.media.MediaBrowserServiceCompat
+import player.phonograph.App
+import player.phonograph.service.MediaButtonIntentReceiver // todo
+
+class MusicService : MediaBrowserServiceCompat() {
+    private lateinit var mediaSession: MediaSessionCompat
+    private lateinit var mediaSessionCallback: MediaSessionCallback
+
+    override fun onCreate() {
+        super.onCreate()
+        // init media session callback
+        mediaSessionCallback = MediaSessionCallback()
+        // setup media session
+        mediaSession = MediaSessionCompat(
+            this,
+            MEDIA_SESSION_TAG,
+            ComponentName(applicationContext, MediaButtonIntentReceiver::class.java),
+            PendingIntent.getBroadcast(
+                applicationContext,
+                1,
+                Intent(Intent.ACTION_MEDIA_BUTTON).apply { component = ComponentName(applicationContext, MediaButtonIntentReceiver::class.java) },
+                PendingIntent.FLAG_IMMUTABLE
+            )
+        ).apply {
+            setPlaybackState(
+                PlaybackStateCompat.Builder().setActions(
+                    PlaybackStateCompat.ACTION_PLAY or PlaybackStateCompat.ACTION_PAUSE or PlaybackStateCompat.ACTION_PLAY_PAUSE or PlaybackStateCompat.ACTION_SEEK_TO or PlaybackStateCompat.ACTION_SKIP_TO_NEXT or PlaybackStateCompat.ACTION_SKIP_TO_PREVIOUS
+                ).build()
+            )
+            setCallback(mediaSessionCallback)
+            // mediaSession.setFlags(MediaSessionCompat.FLAG_HANDLES_QUEUE_COMMANDS)
+        }
+
+        // setup notification
+        // todo
+        // setup session token
+        this.sessionToken = mediaSession.sessionToken
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        mediaSession.release()
+    }
+
+    override fun onGetRoot(clientPackageName: String, clientUid: Int, rootHints: Bundle?): BrowserRoot? {
+        // todo
+        return BrowserRoot("ROOT", null)
+    }
+
+    override fun onLoadChildren(parentId: String, result: Result<MutableList<MediaBrowserCompat.MediaItem>>) {
+        // todo
+        result.sendResult(null)
+    }
+
+    private inner class MediaSessionCallback : MediaSessionCompat.Callback() {
+        override fun onPlay() {
+            TODO()
+        }
+        override fun onPause() {
+            TODO()
+        }
+        override fun onSkipToNext() {
+            TODO()
+        }
+        override fun onSkipToPrevious() {
+            TODO()
+        }
+        override fun onSeekTo(pos: Long) {
+            TODO()
+        }
+    }
+
+    companion object {
+        private const val MEDIA_SESSION_TAG = "${App.ACTUAL_PACKAGE_NAME}.MediaSession"
+    }
+}
