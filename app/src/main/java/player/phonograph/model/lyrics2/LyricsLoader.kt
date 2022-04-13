5443cd5e808f2ba9ceb63590db4c3ced45cb85f1
chr56 <chr0056@gmail.com>
2022-04-13 10:51:04 +0800

    [LyricsLoader] use kotlin Regex api

--- app/src/main/java/player/phonograph/model/lyrics2/LyricsLoader.kt
+++ app/src/main/java/player/phonograph/model/lyrics2/LyricsLoader.kt
@@ -13,7 +13,6 @@ import org.jaudiotagger.tag.FieldKey
 import player.phonograph.BuildConfig
 import player.phonograph.util.FileUtil
 import java.io.File
-import java.util.regex.Pattern
 
 object LyricsLoader {
 
@@ -48,57 +47,54 @@ object LyricsLoader {
                     val songName = Regex.escape(songTitle)
 
                     // precise pattern
-                    val preciseRegex = "$filename\\.(lrc|txt)"
-                    val precisePattern = Pattern.compile(preciseRegex, Pattern.CASE_INSENSITIVE or Pattern.UNICODE_CASE)
-
+                    val preciseRegex = Regex("$filename\\.(lrc|txt)", RegexOption.IGNORE_CASE)
                     // vague pattern
-                    val vagueRegex1 = ".*[-;]?$filename[-;]?.*\\.(lrc|txt)"
-                    val vagueRegex2 = ".*[-;]?$songName[-;]?.*\\.(lrc|txt)"
-                    val vaguePatterns = listOf<Pattern>(
-                        Pattern.compile(vagueRegex1, Pattern.CASE_INSENSITIVE or Pattern.UNICODE_CASE),
-                        Pattern.compile(vagueRegex2, Pattern.CASE_INSENSITIVE or Pattern.UNICODE_CASE)
-                    )
+                    val vagueRegex1 = Regex(".*[-;]?$filename[-;]?.*\\.(lrc|txt)", RegexOption.IGNORE_CASE)
+                    val vagueRegex2 = Regex(".*[-;]?$songName[-;]?.*\\.(lrc|txt)", RegexOption.IGNORE_CASE)
+
                     val preciseFiles: MutableList<File> = ArrayList(2)
                     val vagueFiles: MutableList<File> = ArrayList(6)
 
                     // start list file under the same dir
-                    dir.listFiles { f: File ->
-                        // precise match
-                        if (precisePattern.matcher(f.name).matches()) {
-                            preciseFiles.add(f)
-                            Log.v(TAG, "add a precise file: ${f.path} for $songTitle")
-                            return@listFiles true
-                        }
-                        // vague match
-                        for (pattern in vaguePatterns) {
-                            if (pattern.matcher(f.name).matches()) {
+                    val result = dir.listFiles { f: File ->
+                        when {
+                            preciseRegex.matches(f.name) -> {
+                                preciseFiles.add(f)
+                                Log.v(TAG, "add a precise file: ${f.path} for $songTitle")
+                                return@listFiles true
+                            }
+                            vagueRegex1.matches(f.name) -> {
+                                vagueFiles.add(f)
+                                Log.v(TAG, "add a vague file: ${f.path} for $songTitle")
+                                return@listFiles true
+                            }
+                            vagueRegex2.matches(f.name) -> {
                                 vagueFiles.add(f)
                                 Log.v(TAG, "add a vague file: ${f.path} for $songTitle")
                                 return@listFiles true
                             }
                         }
                         false
-                    }?.also { allMatchedFiles ->
-
-                        Log.v(TAG, allMatchedFiles.map { it.path }.fold("All lyrics found:") { acc, str -> "$acc$str;" }.toString())
-
-                        try {
-                            // precise first
-                            for (f in preciseFiles) {
-                                FileUtil.read(f).also { str ->
-                                    if (str.isNotEmpty()) external = parse(str)
-                                }
-                            }
-                            // then vague
-                            for (f in vagueFiles) {
-                                FileUtil.read(f).also { str ->
-                                    if (str.isNotEmpty()) externalWithSuffix = parse(str)
-                                }
-                            }
-                        } catch (e: Exception) {
-                            Log.e(TAG, "Failed to read lyrics files\n${e.message}")
-                        }
                     }
+
+                    if (result.isNullOrEmpty()) return@let
+
+                    Log.v(TAG, result.map { it.path }.fold("All lyrics found:") { acc, str -> "$acc$str;" }.toString())
+
+                    try {
+                        // precise first
+                        for (f in preciseFiles) {
+                            FileUtil.read(f).also { str ->
+                                if (str.isNotEmpty()) external = parse(str)
+                            }
+                        }
+                        // then vague
+                        for (f in vagueFiles) {
+                            FileUtil.read(f).also { str ->
+                                if (str.isNotEmpty()) externalWithSuffix = parse(str)
+                            }
+                        }
+                    } catch (e: Exception) { Log.e(TAG, "Failed to read lyrics files\n${e.message}") }
                 }
             }
         }
@@ -111,22 +107,19 @@ object LyricsLoader {
         return LyricsPack(embedded, external, externalWithSuffix)
     }
 
-//    private val regex: Regex by lazy { "(\\[.+\\])+.*".toRegex(RegexOption.MULTILINE) }
-    private val pattern: Pattern by lazy { Pattern.compile("(\\[.+\\])+.*", Pattern.MULTILINE) }
     fun parse(raw: String): AbsLyrics {
+        val lines = raw.take(80).lines()
+        val regex = Regex("(\\[.+\\])+.*")
 
-        pattern.matcher(
-            raw.take(80) /* sample string */
-        ).find()
-            .also { result ->
-                return if (result) {
-                    Log.v(TAG, "using LrcLyrics")
-                    LrcLyrics.from(raw)
-                } else {
-                    Log.v(TAG, "using TextLyrics")
-                    TextLyrics.from(raw)
-                }
+        for (line in lines) {
+            if (regex.matches(line)) {
+                Log.v(TAG, "using LrcLyrics")
+                return LrcLyrics.from(raw)
             }
+        }
+        Log.v(TAG, "using TextLyrics")
+        return TextLyrics.from(raw)
     }
+
     private const val TAG = "LyricsLoader"
 }
