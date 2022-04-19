5c018bd067a8c03c0a399491161976656ac1e0a9
chr56 <chr0056@gmail.com>
2022-04-19 14:45:29 +0800

    [LoaderIds] cleanup

--- app/src/main/java/player/phonograph/interfaces/LoaderIds.kt
+++ app/src/main/java/player/phonograph/interfaces/LoaderIds.kt
@@ -4,7 +4,5 @@ package player.phonograph.interfaces
  * @author Karim Abou Zeid (kabouzeid)
  */
 object LoaderIds {
-    const val ALBUM_DETAIL_ACTIVITY = 100
-    const val ARTIST_DETAIL_ACTIVITY = 200
-    const val FOLDERS_FRAGMENT = 300
+    const val FOLDERS_FRAGMENT = 1000
 }
