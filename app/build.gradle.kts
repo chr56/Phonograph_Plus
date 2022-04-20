07ed9598d662b0afd269e3da871726eccee045ce
chr56 <chr0056@gmail.com>
2022-04-20 20:50:16 +0800

    [gradle] bump the version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -36,8 +36,8 @@ android {
         vectorDrawables.useSupportLibrary = true
 
         applicationId = "player.phonograph"
-        versionCode = 204
-        versionName = "0.2.2"
+        versionCode = 205
+        versionName = "0.2.3-RC1"
 
         // proguardFiles(File("proguard-rules-base.pro"), File("proguard-rules-app.pro"))
 
