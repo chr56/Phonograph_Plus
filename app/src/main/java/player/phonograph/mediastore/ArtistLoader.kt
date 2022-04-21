eda4e8d340c364cbd31b66e86b4f933920ea49c0
chr56 <chr0056@gmail.com>
2022-04-21 14:57:45 +0800

    [ArtistLoader] refactor

--- app/src/main/java/player/phonograph/mediastore/ArtistLoader.kt
+++ app/src/main/java/player/phonograph/mediastore/ArtistLoader.kt
@@ -6,10 +6,12 @@ package player.phonograph.mediastore
 
 import android.content.Context
 import android.provider.MediaStore.Audio.AudioColumns
+import android.util.ArrayMap
 import player.phonograph.mediastore.SongLoader.getSongs
 import player.phonograph.mediastore.SongLoader.makeSongCursor
 import player.phonograph.model.Album
 import player.phonograph.model.Artist
+import player.phonograph.model.Song
 import player.phonograph.settings.Setting
 
 /**
@@ -21,14 +23,14 @@ object ArtistLoader {
         val songs = getSongs(
             makeSongCursor(context, null, null, sortOrder)
         )
-        return splitIntoArtists(AlbumLoader.splitIntoAlbums(songs))
+        return splitIntoArtists(songs)
     }
 
     fun getArtists(context: Context, query: String): List<Artist> {
         val songs = getSongs(
             makeSongCursor(context, "${AudioColumns.ARTIST} LIKE ?", arrayOf("%$query%"), sortOrder)
         )
-        return splitIntoArtists(AlbumLoader.splitIntoAlbums(songs))
+        return splitIntoArtists(songs)
     }
 
     fun getArtist(context: Context, artistId: Long): Artist {
@@ -38,25 +40,30 @@ object ArtistLoader {
         return Artist(AlbumLoader.splitIntoAlbums(songs))
     }
 
-    fun splitIntoArtists(albums: List<Album>?): List<Artist> {
-        val artists: MutableList<Artist> = ArrayList()
-        if (albums != null) {
-            for (album in albums) {
-                getOrCreateArtist(artists, album.artistId).albums?.toMutableList()?.add(album)
-            }
-        }
-        return artists
-    }
+    fun splitIntoArtists(songs: List<Song>): List<Artist> {
+        if (songs.isEmpty()) return ArrayList()
 
-    private fun getOrCreateArtist(artists: MutableList<Artist>, artistId: Long): Artist {
-        for (artist in artists) {
-            if (artist.albums!!.isNotEmpty() && artist.albums[0].songs.isNotEmpty() && artist.albums[0].songs[0].artistId == artistId) {
-                return artist
+        // split artists
+
+        // artistID <-> List of song
+        val idMap: MutableMap<Long, MutableList<Song>> = ArrayMap()
+        for (song in songs) {
+            if (idMap[song.artistId] == null) {
+                // create new
+                idMap[song.artistId] = ArrayList<Song>(1).apply { add(song) }
+            } else {
+                // add to existed
+                idMap[song.artistId]!!.add(song)
             }
         }
-        val artist = Artist()
-        artists.add(artist)
-        return artist
+
+        // to albums
+        // list of every artists' list of albums
+        val artistAlbumsList: List<List<Album>> = idMap.values.map { artistSongs ->
+            AlbumLoader.splitIntoAlbums(artistSongs)
+        }
+
+        return artistAlbumsList.map { Artist(it) }
     }
 
     val sortOrder: String by lazy {
