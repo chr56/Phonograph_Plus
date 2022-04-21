4793e425a2dc62919b87cb0b326a3cdf23f413c3
chr56 <chr0056@gmail.com>
2022-04-21 19:53:53 +0800

    [GenreLoader] cleanup

--- app/src/main/java/player/phonograph/mediastore/GenreLoader.kt
+++ app/src/main/java/player/phonograph/mediastore/GenreLoader.kt
@@ -9,49 +9,36 @@ import android.content.Context
 import android.database.Cursor
 import android.os.Build
 import android.provider.MediaStore
-import java.lang.Exception
-import java.util.ArrayList
 import player.phonograph.model.Genre
 import player.phonograph.model.Song
-import player.phonograph.settings.Setting
 
 @SuppressLint("Recycle")
 object GenreLoader {
-    @JvmStatic
+
     fun getAllGenres(context: Context): List<Genre> {
         return getGenresFromCursor(context, makeGenreCursor(context))
     }
 
     fun getSongs(context: Context, genreId: Long): List<Song> {
-        return SongLoader.getSongs(makeGenreSongCursor(context, genreId))
+        return SongLoader.getSongs(makeSongCursor(context, genreId))
     }
 
     private fun getGenresFromCursor(context: Context, cursor: Cursor?): List<Genre> {
+        if (cursor == null)return ArrayList()
+
         val genres: MutableList<Genre> = ArrayList()
-        if (cursor != null) {
+
+        cursor.use {
             if (cursor.moveToFirst()) {
                 do {
                     val genre = getGenreFromCursor(context, cursor)
                     if (genre.songCount > 0) {
                         genres.add(genre)
                     } else {
-                        // try to remove the empty genre from the media store
-                        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.Q) {
-                            try {
-                                context.contentResolver.delete(
-                                    MediaStore.Audio.Genres.EXTERNAL_CONTENT_URI,
-                                    MediaStore.Audio.Genres._ID + " == " + genre.id,
-                                    null
-                                )
-                            } catch (e: Exception) {
-                                e.printStackTrace()
-                                // nothing we can do then
-                            }
-                        }
+                        removeEmptyGenre(context, genre)
                     }
                 } while (cursor.moveToNext())
             }
-            cursor.close()
         }
         return genres
     }
@@ -59,18 +46,17 @@ object GenreLoader {
     private fun getGenreFromCursor(context: Context, cursor: Cursor): Genre {
         val id = cursor.getLong(0)
         val name = cursor.getString(1)
-        val songs = getSongs(context, id).size
-        return Genre(id, name, songs)
+        val songCount = getSongs(context, id).size
+        return Genre(id, name, songCount)
     }
 
-    private fun makeGenreSongCursor(context: Context, genreId: Long): Cursor? {
+    private fun makeSongCursor(context: Context, genreId: Long): Cursor? {
         return try {
             context.contentResolver.query(
                 MediaStore.Audio.Genres.Members.getContentUri("external", genreId),
                 MediaStoreUtil.SongConst.BASE_PROJECTION,
                 MediaStoreUtil.SongConst.BASE_AUDIO_SELECTION,
-                null,
-                Setting.instance.songSortOrder
+                null, null
             )
         } catch (e: SecurityException) {
             null
@@ -84,10 +70,25 @@ object GenreLoader {
         return try {
             context.contentResolver.query(
                 MediaStore.Audio.Genres.EXTERNAL_CONTENT_URI,
-                projection, null, null, Setting.instance.genreSortOrder
+                projection, null, null, null
             )
         } catch (e: SecurityException) {
             null
+        }
+    }
+    private fun removeEmptyGenre(context: Context, genre: Genre) {
+        // try to remove the empty genre from the media store
+        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.Q) {
+            try {
+                context.contentResolver.delete(
+                    MediaStore.Audio.Genres.EXTERNAL_CONTENT_URI,
+                    MediaStore.Audio.Genres._ID + " == " + genre.id,
+                    null
+                )
+            } catch (e: Exception) {
+                e.printStackTrace()
+                // nothing we can do then
+            }
         }
     }
 }
