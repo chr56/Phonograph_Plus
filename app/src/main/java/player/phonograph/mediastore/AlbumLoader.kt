158d7aa42a064cb202af7a2c17d165a50b10432b
chr56 <chr0056@gmail.com>
2022-04-23 10:17:08 +0800

    [AlbumsLoader] improve splitIntoAlbums()

--- app/src/main/java/player/phonograph/mediastore/AlbumLoader.kt
+++ app/src/main/java/player/phonograph/mediastore/AlbumLoader.kt
@@ -7,14 +7,15 @@ package player.phonograph.mediastore
 import android.content.Context
 import android.provider.MediaStore.Audio.AudioColumns
 import android.util.ArrayMap
+import android.util.Log
 import kotlinx.coroutines.*
-import kotlinx.coroutines.flow.asFlow
-import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.flow.*
 import player.phonograph.mediastore.SongLoader.getSongs
 import player.phonograph.mediastore.SongLoader.makeSongCursor
 import player.phonograph.mediastore.sort.SortRef
 import player.phonograph.model.Album
 import player.phonograph.model.Song
+import player.phonograph.notification.ErrorNotification
 import player.phonograph.settings.Setting
 
 /**
@@ -47,48 +48,47 @@ object AlbumLoader {
         if (songs == null) return ArrayList()
 
         val albums = CoroutineScope(Dispatchers.Default).async {
-            val innerCoroutineScope = CoroutineScope(Dispatchers.Default)
 
-            val flow = songs.asFlow()
             var completed = false
+
+            val flow = flow {
+                for (song in songs) emit(song)
+            }.catch { e ->
+                Log.e("Loader", e.message.orEmpty())
+                ErrorNotification.postErrorNotification(e, "Fail to load albums")
+            }
 
             // AlbumID <-> List of song
             val table: MutableMap<Long, MutableList<Song>> = ArrayMap()
 
             // AlbumID <-> albumName
-            val albumName: MutableMap<Long, String?> = ArrayMap()
+            val albumNames: MutableMap<Long, String?> = ArrayMap()
 
-            innerCoroutineScope.launch {
-                delay(100)
-                try {
-                    flow.collect { song -> // check album
-                        if (table[song.albumId] == null) {
-                            // create new album
-                            albumName[song.albumId] = song.albumName
-                            table[song.albumId] = ArrayList<Song>(1).apply { add(song) }
-                        } else {
-                            // add to existed album
-                            table[song.albumId]!!.add(song)
-                        }
+            flow.onCompletion { completed = true }
+                .collect { song ->
+                    if (table[song.albumId] == null) {
+                        // create new album
+                        albumNames[song.albumId] = song.albumName
+                        table[song.albumId] = ArrayList<Song>(1).apply { add(song) }
+                    } else {
+                        // add to existed album
+                        table[song.albumId]!!.add(song)
                     }
-                } finally {
-                    completed = true
                 }
-            }
 
             while (!completed) yield() // wait until result is ready
 
             // handle result
-            // todo: use flow
-            val result: MutableList<Album> = ArrayList(table.size)
-            table.forEach { (albumId, songList) ->
-                // create album & add
-                result.add(
-                    Album(albumId, albumName[albumId], songList)
-                )
-            }
-
-            return@async result
+            return@async flow {
+                for ((id, list) in table) {
+                    emit(Pair(id, list))
+                }
+            }.map { (id, list) ->
+                Album(id, albumNames[id], list)
+            }.catch { e ->
+                Log.e("Loader", e.message.orEmpty())
+                ErrorNotification.postErrorNotification(e, "Fail to load albums")
+            }.toList()
         }
 
         return runBlocking {
