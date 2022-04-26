27e0769e9c54e06550ece5ba1cce887c52a8ecce
chr56 <chr0056@gmail.com>
2022-04-26 12:52:44 +0800

    [Build] fix add a wrong dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -157,7 +157,7 @@ dependencies {
 
     implementation("androidx.core:core-ktx:1.7.0")
     implementation("androidx.appcompat:appcompat:1.4.1")
-    implementation("androidx.activity:activity-compose:1.4.0")
+    implementation("androidx.activity:activity-ktx:1.4.0")
     implementation("androidx.fragment:fragment-ktx:1.4.1")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.4.1")
 
