dd615dada48c05f0a367c4c26167dfd1524278f6
chr56 <chr0056@gmail.com>
2022-06-03 20:41:01 +0800

    [SettingManager] add method to import Settings

--- app/src/main/java/player/phonograph/settings/SettingManager.kt
+++ app/src/main/java/player/phonograph/settings/SettingManager.kt
@@ -6,11 +6,11 @@ package player.phonograph.settings
 
 import android.content.Context
 import android.net.Uri
+import androidx.preference.PreferenceManager
+import java.io.*
 import org.json.JSONArray
 import org.json.JSONObject
 import player.phonograph.notification.ErrorNotification
-import java.io.FileOutputStream
-import java.io.IOException
 
 class SettingManager(var context: Context) {
 
@@ -35,8 +35,51 @@ class SettingManager(var context: Context) {
                 stream.bufferedWriter().apply {
                     write(content)
                     flush()
+                    close()
                 }
             }
+        }
+    }
+
+    fun importSetting(uri: Uri) {
+        context.contentResolver.openFileDescriptor(uri, "r")?.fileDescriptor?.let { fd ->
+            FileInputStream(fd).use {
+                loadSettings(it)
+            }
+        }
+    }
+
+    private fun loadSettings(fileInputStream: FileInputStream) {
+        runCatching {
+            val json: JSONObject
+            fileInputStream.use {
+                it.bufferedReader().apply {
+                    json = JSONObject(readText())
+                    close()
+                }
+            }
+            val editor = PreferenceManager.getDefaultSharedPreferences(context).edit()
+            for (key in json.keys()) {
+                when (val value = json.opt(key)) {
+                    is String -> editor.putString(key, value)
+                    is Int -> editor.putInt(key, value)
+                    is Long -> editor.putLong(key, value)
+                    is Boolean -> editor.putBoolean(key, value)
+                    is Float -> editor.putFloat(key, value)
+                    is Set<*> -> {
+                        if (value.isNotEmpty()) {
+                            editor.putStringSet(key, value.filterIsInstance<String>().toSet())
+                        }
+                    }
+                    else -> throw IllegalArgumentException("value ${value.javaClass} is unsupported!")
+                }
+            }
+            editor.apply()
+        }.let { r ->
+            if (r.isFailure) ErrorNotification.postErrorNotification(
+                r.exceptionOrNull() ?: Exception(),
+                "Failed to import Setting"
+            )
         }
     }
 
