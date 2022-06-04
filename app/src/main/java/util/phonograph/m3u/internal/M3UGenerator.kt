d347ad3fad58f5d6a221a2bec0655bb1835241e4
chr56 <chr0056@gmail.com>
2022-06-04 10:15:46 +0800

    [M3UGenerator] cleanup

--- app/src/main/java/util/phonograph/m3u/internal/M3UGenerator.kt
+++ app/src/main/java/util/phonograph/m3u/internal/M3UGenerator.kt
@@ -5,29 +5,26 @@
 package util.phonograph.m3u.internal
 
 import android.content.Context
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.GlobalScope
-import kotlinx.coroutines.launch
+import java.io.*
+import java.nio.charset.StandardCharsets
+import java.text.SimpleDateFormat
+import java.util.*
+import kotlinx.coroutines.*
 import player.phonograph.App
 import player.phonograph.R
 import player.phonograph.mediastore.PlaylistSongLoader
 import player.phonograph.model.Song
 import player.phonograph.model.playlist.Playlist
 import player.phonograph.model.playlist.SmartPlaylist
+import player.phonograph.notification.ErrorNotification
 import player.phonograph.util.Util
-import java.io.*
-import java.lang.StringBuilder
-import java.nio.charset.StandardCharsets
-import java.text.SimpleDateFormat
-import java.util.*
 
 object M3UGenerator {
 
-    @JvmStatic
     @Deprecated("don not use File")
     @Throws(IOException::class)
     fun writeFile(context: Context?, dir: File, playlist: Playlist): File {
-        if (!dir.exists()) dir.mkdirs() //noinspection ResultOfMethodCallIgnored
+        if (!dir.exists()) dir.mkdirs()
 
         val songs: List<Song> = playlist.getSongs(context ?: App.instance)
 
@@ -43,9 +40,7 @@ object M3UGenerator {
 
             bw.write(HEADER)
             for (song in songs) {
-                bw.write(
-                    createLine(song)
-                )
+                bw.write(createLine(song))
             }
 
             bw.close()
@@ -55,7 +50,12 @@ object M3UGenerator {
     }
 
     @JvmStatic
-    fun generate(outputStream: OutputStream, context: Context?, playlist: Playlist, addHeader: Boolean) {
+    fun generate(
+        outputStream: OutputStream,
+        context: Context?,
+        playlist: Playlist,
+        addHeader: Boolean
+    ) {
 
         val songs: List<Song> =
             if (playlist is SmartPlaylist) {
@@ -65,13 +65,15 @@ object M3UGenerator {
             }
 
         if (songs.isNotEmpty()) {
-            GlobalScope.launch(context = Dispatchers.IO) {
-                try {
-                    generate(outputStream, songs, addHeader)
-                } catch (e: IOException) {
-                    Util.coroutineToast(context ?: App.instance, R.string.failed)
+            CoroutineScope(Dispatchers.IO + SupervisorJob())
+                .launch(context = Dispatchers.IO) {
+                    try {
+                        generate(outputStream, songs, addHeader)
+                    } catch (e: IOException) {
+                        Util.coroutineToast(context ?: App.instance, R.string.failed)
+                        ErrorNotification.postErrorNotification(e, "Failed to write playlist!")
+                    }
                 }
-            }
         }
         return
     }
@@ -83,22 +85,14 @@ object M3UGenerator {
             val w = OutputStreamWriter(outputStream, StandardCharsets.UTF_8)
             if (addHeader) w.write(HEADER)
             for (song in songs) {
-                w.write(
-                    createLine(song)
-                )
+                w.write(createLine(song))
             }
             w.close()
         }
     }
 
-    private fun createLine(song: Song): String {
-        val b = StringBuilder("\n")
-        b.appendLine()
-        b.append("$ENTRY${song.duration}$DURATION_SEPARATOR${song.artistName} - ${song.title}")
-        b.appendLine()
-        b.append(song.data)
-        return b.toString()
-    }
+    private fun createLine(song: Song): String =
+        "\n$ENTRY${song.duration}$DURATION_SEPARATOR${song.artistName} - ${song.title}\n${song.data}"
 
     private const val EXTENSION = "m3u"
     private const val HEADER = "#EXTM3U"
