7ea93c3b9be40f5d94110d68a5104999c6865c35
chr56 <chr0056@gmail.com>
2022-06-05 13:37:54 +0800

    [SimpleStorage] add license

--- app/src/main/java/lib/phonograph/storage/UriExt.kt
+++ app/src/main/java/lib/phonograph/storage/UriExt.kt
@@ -1,6 +1,6 @@
 
 /*
- * Copyright (C) 2021 Anggrayudi H
+ * Copyright © 2020-2022 Anggrayudi Hardiannico A.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
