21e82748a556b8ea99e8fef2131c6ecad1219d00
chr56 <chr0056@gmail.com>
2022-06-10 14:23:35 +0800

    [UpdateToastMediaScannerCompletionListener] cleanup & refactor

--- app/src/main/java/player/phonograph/misc/UpdateToastMediaScannerCompletionListener.kt
+++ app/src/main/java/player/phonograph/misc/UpdateToastMediaScannerCompletionListener.kt
@@ -1,40 +1,55 @@
 package player.phonograph.misc
 
-import android.annotation.SuppressLint
 import android.app.Activity
 import android.media.MediaScannerConnection.OnScanCompletedListener
 import android.net.Uri
 import android.widget.Toast
-import player.phonograph.R
 import java.lang.ref.WeakReference
+import player.phonograph.R
+import player.phonograph.notification.BackgroundNotification
+import player.phonograph.notification.ErrorNotification
 
 /**
  * @author Karim Abou Zeid (kabouzeid)
  */
-class UpdateToastMediaScannerCompletionListener(activity: Activity, private val toBeScanned: Array<String>) :
+class UpdateToastMediaScannerCompletionListener(
+    activity: Activity,
+    paths: Array<String>
+) :
     OnScanCompletedListener {
-
-    private var scanned = 0
-    private var failed = 0
 
     private val activityWeakReference: WeakReference<Activity> = WeakReference(activity)
     private val toast = Toast.makeText(activity.applicationContext, "", Toast.LENGTH_SHORT)
 
+    private val notificationId = paths.hashCode()
     // Strings
     private val scannedFiles = activity.getString(R.string.scanned_files)
     private val couldNotScanFiles = activity.getString(R.string.could_not_scan_files)
 
+    private val title = activity.getString(R.string.background_notification_name)
+
+    val total: Int = paths.size
+    val rest = paths.toMutableList()
+    var fail = ArrayList<String>()
     override fun onScanCompleted(path: String, uri: Uri?) {
-        activityWeakReference.get() // activity
-            ?.runOnUiThread {
-                if (uri == null) { failed++ } else { scanned++ }
+        rest.remove(path)
+        if (uri == null) fail.add(path)
+        checkAndToast()
+    }
 
-                val text =
-                    " ${String.format(scannedFiles, scanned, toBeScanned.size)} " +
-                        if (failed > 0) String.format(couldNotScanFiles, failed) else ""
-
-                toast.setText(text)
+    private fun checkAndToast() {
+        BackgroundNotification.post(title, text, notificationId, total - rest.size, total)
+        if (rest.size == 0) {
+            BackgroundNotification.remove(notificationId)
+            if (fail.size > 0) {
+                ErrorNotification.postErrorNotification("Couldn't scan:\n${fail.reduce { acc, s -> "$acc\n$s" }}")
+            }
+            activityWeakReference.get()?.runOnUiThread {
+                toast.setText("$text $textFail")
                 toast.show()
             }
+        }
     }
+    private val text: String get() = " ${String.format(scannedFiles, total - fail.size, total)} "
+    private val textFail: String get() = if (fail.size > 0) String.format(couldNotScanFiles, fail.size) else ""
 }
