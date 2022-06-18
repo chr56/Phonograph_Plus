2095f73337a1eb8d4bc9cf7349f65f0407b4849f
chr56 <chr0056@gmail.com>
2022-06-18 15:55:26 +0800

    [gradle] bump the version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -56,7 +56,7 @@ android {
         vectorDrawables.useSupportLibrary = true
 
         applicationId = "player.phonograph"
-        versionCode = 252
+        versionCode = 253
         versionName = "0.3.x-alpha03"
 
         buildConfigField("String", "GIT_COMMIT_HASH", "\"${getGitHash(false)}\"")
