d0fcad935ab31cc041f226cea8cfe398f14bd548
chr56 <chr0056@gmail.com>
2022-06-18 16:49:31 +0800

    [FilesPageViewModel] add implement of legacy listFiles

--- app/src/main/java/player/phonograph/ui/fragments/mainactivity/home/FilesPageViewModel.kt
+++ app/src/main/java/player/phonograph/ui/fragments/mainactivity/home/FilesPageViewModel.kt
@@ -7,14 +7,19 @@ package player.phonograph.ui.fragments.mainactivity.home
 import android.content.Context
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import java.io.File
 import java.util.*
+import kotlin.collections.ArrayList
 import kotlinx.coroutines.*
 import player.phonograph.App
 import player.phonograph.mediastore.MediaStoreUtil.searchSongFiles
 import player.phonograph.model.FileEntity
 import player.phonograph.model.Location
+import player.phonograph.model.put
+import player.phonograph.ui.fragments.mainactivity.folders.FileScanner
 
 class FilesPageViewModel : ViewModel() {
+
     var currentLocation: Location = Location.HOME
 
     var currentFileList: MutableSet<FileEntity> = TreeSet<FileEntity>()
@@ -29,20 +34,65 @@ class FilesPageViewModel : ViewModel() {
         listFileJob?.cancel() // cancel current
         listFileJob =
             viewModelScope.launch(Dispatchers.IO + SupervisorJob()) {
-                listFiles(location, context, this)
+                if (useMediaStore)
+                    listFilesMediaStore(location, context, this)
+                else
+                    listFilesLegacy(location, context, this)
                 withContext(Dispatchers.Main) { onFinished() }
             }
     }
 
+    val useMediaStore = true // TODO
+
     @Synchronized
-    private fun listFiles(
+    private fun listFilesMediaStore(
         location: Location,
-        context: Context = App.instance,
+        context: Context,
         scope: CoroutineScope?,
     ) {
         currentFileList.clear()
         val set = searchSongFiles(context, location, scope) ?: return
         currentFileList.addAll(set)
+    }
+
+    @Synchronized
+    private fun listFilesLegacy(
+        location: Location,
+        context: Context,
+        scope: CoroutineScope?,
+    ) {
+        currentFileList.clear()
+        // todo
+        val directory = File(location.absolutePath).also { if (!it.isDirectory) return }
+        val files = directory.listFiles(FileScanner.audioFileFilter) ?: return
+        val list: MutableList<FileEntity> = ArrayList()
+        for (file in files) {
+            val l = Location.fromAbsolutePath(file.absolutePath)
+            if (scope?.isActive == false) break
+            val item =
+                when {
+                    file.isDirectory -> {
+                        FileEntity.Folder(
+                            location = l,
+                            name = file.name,
+                            dateAdded = file.lastModified(),
+                            dateModified = file.lastModified()
+                        )
+                    }
+                    file.isFile -> {
+                        FileEntity.File(
+                            location = l,
+                            name = file.name,
+                            size = file.length(),
+                            dateAdded = file.lastModified(),
+                            dateModified = file.lastModified()
+                        )
+                    }
+                    else -> null
+                }
+            item?.let { list.put(it) }
+        }
+        currentFileList.addAll(list)
     }
 
     override fun onCleared() {
