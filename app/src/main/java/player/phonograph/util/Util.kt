b730d801ff8afe5aa7fd701508e8400bbe840014
chr56 <chr0056@gmail.com>
2022-06-28 10:19:19 +0800

    [Util] remove the unused

--- app/src/main/java/player/phonograph/util/Util.kt
+++ app/src/main/java/player/phonograph/util/Util.kt
@@ -1,17 +1,13 @@
 package player.phonograph.util
 
-import android.annotation.TargetApi
 import android.app.Activity
 import android.content.Context
 import android.content.Intent
 import android.content.res.Configuration
 import android.content.res.Resources
 import android.graphics.Point
-import android.os.Build
 import android.text.format.DateFormat
 import android.util.TypedValue
-import android.view.View
-import android.view.Window
 import android.view.WindowManager
 import android.view.inputmethod.InputMethodManager
 import android.widget.Toast
@@ -79,19 +75,6 @@ object Util {
         val size = Point()
         display.getSize(size)
         return size
-    }
-
-    @TargetApi(Build.VERSION_CODES.KITKAT)
-    fun setStatusBarTranslucent(window: Window) = window.setFlags(
-        WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS, WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS
-    )
-
-    fun setAllowDrawUnderStatusBar(window: Window) {
-//        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
-//            window.setDecorFitsSystemWindows(false)
-//        } else
-        window.decorView.systemUiVisibility =
-            (View.SYSTEM_UI_FLAG_LAYOUT_STABLE or View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN)
     }
 
     @JvmStatic
