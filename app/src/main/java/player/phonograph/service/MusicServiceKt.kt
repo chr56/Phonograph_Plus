b3b506c6b0d939fc6116e6b305664830df73a747
chr56 <chr0056@gmail.com>
2022-07-06 20:08:54 +0800

    [MusicService] move MediaStoreObserver out

--- app/src/main/java/player/phonograph/service/MusicServiceKt.kt
+++ app/src/main/java/player/phonograph/service/MusicServiceKt.kt
@@ -8,7 +8,10 @@ import android.appwidget.AppWidgetManager
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
+import android.database.ContentObserver
 import android.net.Uri
+import android.os.Handler
+import android.provider.MediaStore
 import android.widget.Toast
 import player.phonograph.App
 import player.phonograph.R
@@ -24,10 +27,105 @@ import player.phonograph.util.MusicUtil.getSongFileUri
 
 class MusicServiceKt(val musicService: MusicService) {
 
+    var mediaStoreObserver: MediaStoreObserver? = null
+
+    fun setUpMediaStoreObserver(
+        context: Context,
+        playerHandler: Handler, // todo
+        handleAndSendChangeInternalCallback: (String) -> Unit
+    ) {
+        mediaStoreObserver = MediaStoreObserver(playerHandler, handleAndSendChangeInternalCallback)
+        with(context) {
+            contentResolver.registerContentObserver(
+                MediaStore.Audio.Media.EXTERNAL_CONTENT_URI,
+                true,
+                mediaStoreObserver!!
+            )
+            contentResolver.registerContentObserver(
+                MediaStore.Audio.Albums.EXTERNAL_CONTENT_URI,
+                true,
+                mediaStoreObserver!!
+            )
+            contentResolver.registerContentObserver(
+                MediaStore.Audio.Artists.EXTERNAL_CONTENT_URI,
+                true,
+                mediaStoreObserver!!
+            )
+            contentResolver.registerContentObserver(
+                MediaStore.Audio.Genres.EXTERNAL_CONTENT_URI,
+                true,
+                mediaStoreObserver!!
+            )
+            contentResolver.registerContentObserver(
+                MediaStore.Audio.Playlists.EXTERNAL_CONTENT_URI,
+                true,
+                mediaStoreObserver!!
+            )
+
+            contentResolver.registerContentObserver(
+                MediaStore.Audio.Media.INTERNAL_CONTENT_URI,
+                true,
+                mediaStoreObserver!!
+            )
+            contentResolver.registerContentObserver(
+                MediaStore.Audio.Albums.INTERNAL_CONTENT_URI,
+                true,
+                mediaStoreObserver!!
+            )
+            contentResolver.registerContentObserver(
+                MediaStore.Audio.Artists.INTERNAL_CONTENT_URI,
+                true,
+                mediaStoreObserver!!
+            )
+            contentResolver.registerContentObserver(
+                MediaStore.Audio.Genres.INTERNAL_CONTENT_URI,
+                true,
+                mediaStoreObserver!!
+            )
+            contentResolver.registerContentObserver(
+                MediaStore.Audio.Playlists.INTERNAL_CONTENT_URI,
+                true,
+                mediaStoreObserver!!
+            )
+        }
+    }
+
+    fun unregisterMediaStoreObserver(context: Context) {
+        context.contentResolver.unregisterContentObserver(mediaStoreObserver!!)
+    }
+
+    class MediaStoreObserver(
+        private val mHandler: Handler,
+        private val handleAndSendChangeInternalCallback: (String) -> Unit
+    ) : ContentObserver(mHandler), Runnable {
+
+        override fun onChange(selfChange: Boolean) {
+            // if a change is detected, remove any scheduled callback
+            // then post a new one. This is intended to prevent closely
+            // spaced events from generating multiple refresh calls
+            mHandler.removeCallbacks(this)
+            mHandler.postDelayed(this, REFRESH_DELAY)
+        }
+
+        override fun run() {
+            // actually call refresh when the delayed callback fires
+            // do not send a sticky broadcast here
+            handleAndSendChangeInternalCallback(MusicService.MEDIA_STORE_CHANGED)
+        }
+
+        companion object {
+            // milliseconds to delay before calling refresh to aggregate events
+            private const val REFRESH_DELAY: Long = 500
+        }
+    }
+
     @JvmField val appWidgetBig = AppWidgetBig.instance
-    @JvmField  val appWidgetClassic = AppWidgetClassic.instance
-    @JvmField  val appWidgetSmall = AppWidgetSmall.instance
-    @JvmField   val appWidgetCard = AppWidgetCard.instance
+
+    @JvmField val appWidgetClassic = AppWidgetClassic.instance
+
+    @JvmField val appWidgetSmall = AppWidgetSmall.instance
+
+    @JvmField val appWidgetCard = AppWidgetCard.instance
 
     @JvmField
     val widgetIntentReceiver: BroadcastReceiver =
