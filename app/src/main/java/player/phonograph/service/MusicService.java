b3021fa49e2fad9ea05fe56f6747fe664bc32abc
chr56 <chr0056@gmail.com>
2022-07-07 11:10:54 +0800

    [MultiPlayer] move to package .player

--- app/src/main/java/player/phonograph/service/MusicService.java
+++ app/src/main/java/player/phonograph/service/MusicService.java
@@ -54,6 +54,7 @@ import player.phonograph.notification.PlayingNotificationImpl;
 import player.phonograph.notification.PlayingNotificationImpl24;
 import player.phonograph.provider.HistoryStore;
 import player.phonograph.provider.SongPlayCountStore;
+import player.phonograph.service.player.MultiPlayer;
 import player.phonograph.service.player.Playback;
 import player.phonograph.service.queue.QueueChangeObserver;
 import player.phonograph.service.queue.QueueManager;
