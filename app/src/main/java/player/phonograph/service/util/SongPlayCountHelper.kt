e87c400129498ddc8ae6da830819bae8dc18bf4f
chr56 <chr0056@gmail.com>
2022-07-16 20:53:51 +0800

    [MusicService] cleanup handleChangeInternal

--- app/src/main/java/player/phonograph/service/util/SongPlayCountHelper.kt
+++ app/src/main/java/player/phonograph/service/util/SongPlayCountHelper.kt
@@ -1,24 +1,25 @@
 package player.phonograph.service.util
 
-import player.phonograph.helper.StopWatch
+import android.content.Context
+import java.util.*
 import player.phonograph.model.Song
+import player.phonograph.provider.SongPlayCountStore
 
 /**
  * @author Abou Zeid (kabouzeid)
  */
 class SongPlayCountHelper {
+
     private val stopWatch = StopWatch()
-    var song = Song.EMPTY_SONG
-        private set
-
-    fun shouldBumpPlayCount(): Boolean = song.duration * 0.5 < stopWatch.elapsedTime
-
-    fun notifySongChanged(song: Song) {
-        synchronized(this) {
-            stopWatch.reset()
-            this.song = song
+    var songMonitored = Song.EMPTY_SONG
+        set(song) {
+            synchronized(field) {
+                stopWatch.reset()
+                field = song
+            }
         }
-    }
+
+    private fun shouldBumpPlayCount(): Boolean = songMonitored.duration * 0.5 < stopWatch.elapsedTime
 
     fun notifyPlayStateChanged(isPlaying: Boolean) {
         synchronized(this) {
@@ -27,6 +28,92 @@ class SongPlayCountHelper {
             } else {
                 stopWatch.pause()
             }
+        }
+    }
+
+    fun checkForBumpingPlayCount(context: Context) {
+        if (shouldBumpPlayCount()) {
+            SongPlayCountStore.getInstance(context).bumpPlayCount(songMonitored.id)
+        }
+    }
+
+    /**
+     * Simple thread safe stop watch.
+     *
+     * @author Karim Abou Zeid (kabouzeid)
+     */
+    class StopWatch {
+        /**
+         * The time the stop watch was last started.
+         */
+        private var startTime: Long = 0
+
+        /**
+         * The time elapsed before the current [.startTime].
+         */
+        private var previousElapsedTime: Long = 0
+
+        /**
+         * Whether the stop watch is currently running or not.
+         */
+        private var isRunning = false
+
+        /**
+         * Starts or continues the stop watch.
+         *
+         * @see .pause
+         * @see .reset
+         */
+        fun start() {
+            synchronized(this) {
+                startTime = System.currentTimeMillis()
+                isRunning = true
+            }
+        }
+
+        /**
+         * Pauses the stop watch. It can be continued later from [.start].
+         *
+         * @see .start
+         * @see .reset
+         */
+        fun pause() {
+            synchronized(this) {
+                previousElapsedTime += System.currentTimeMillis() - startTime
+                isRunning = false
+            }
+        }
+
+        /**
+         * Stops and resets the stop watch to zero milliseconds.
+         *
+         * @see .start
+         * @see .pause
+         */
+        fun reset() {
+            synchronized(this) {
+                startTime = 0
+                previousElapsedTime = 0
+                isRunning = false
+            }
+        }
+
+        /**
+         * @return the total elapsed time in milliseconds
+         */
+        val elapsedTime: Long
+            get() {
+                synchronized(this) {
+                    var currentElapsedTime: Long = 0
+                    if (isRunning) {
+                        currentElapsedTime = System.currentTimeMillis() - startTime
+                    }
+                    return previousElapsedTime + currentElapsedTime
+                }
+            }
+
+        override fun toString(): String {
+            return String.format(Locale.getDefault(), "%d millis", elapsedTime)
         }
     }
 }
