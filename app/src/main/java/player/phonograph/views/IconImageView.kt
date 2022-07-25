d940ba25aec1b5ee6fbeca64d002b252da32462b
chr56 <chr0056@gmail.com>
2022-07-25 22:01:43 +0800

    [IconImageView] convert into kotlin

---
+++ app/src/main/java/player/phonograph/views/IconImageView.kt
@@ -0,0 +1,31 @@
+package player.phonograph.views
+
+import android.content.Context
+import android.graphics.PorterDuff
+import android.util.AttributeSet
+import androidx.appcompat.widget.AppCompatImageView
+import player.phonograph.R
+import util.mddesign.util.Util
+
+/**
+ * @author Karim Abou Zeid (kabouzeid)
+ */
+class IconImageView : AppCompatImageView {
+
+    constructor(context: Context?) : super(context!!) {
+        init(context)
+    }
+
+    constructor(context: Context?, attrs: AttributeSet?) : super(context!!, attrs) {
+        init(context)
+    }
+
+    constructor(context: Context?, attrs: AttributeSet?, defStyleAttr: Int) : super(context!!, attrs, defStyleAttr) {
+        init(context)
+    }
+
+    private fun init(context: Context?) {
+        if (context == null) return
+        setColorFilter(Util.resolveColor(context, R.attr.iconColor), PorterDuff.Mode.SRC_IN)
+    }
+}
