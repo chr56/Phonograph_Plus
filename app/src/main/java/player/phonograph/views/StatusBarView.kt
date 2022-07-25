1f1f499b9fe5d23d16ced3beae3edbd503e3172f
chr56 <chr0056@gmail.com>
2022-07-25 17:13:25 +0800

    [StatusBarView] convert into kotlin

---
+++ app/src/main/java/player/phonograph/views/StatusBarView.kt
@@ -0,0 +1,21 @@
+package player.phonograph.views
+
+import android.content.Context
+import android.os.Build
+import android.util.AttributeSet
+import android.view.View
+import android.view.WindowInsets
+
+class StatusBarView : View {
+
+    constructor(context: Context?) : super(context) {}
+    constructor(context: Context?, attrs: AttributeSet?) : super(context, attrs) {}
+    constructor(context: Context?, attrs: AttributeSet?, defStyleAttr: Int) : super(context, attrs, defStyleAttr) {}
+
+    override fun onApplyWindowInsets(insets: WindowInsets): WindowInsets {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            layoutParams = layoutParams.apply { height = insets.systemWindowInsetTop }
+        }
+        return super.onApplyWindowInsets(insets)
+    }
+}
