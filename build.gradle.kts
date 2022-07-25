3d1c341c3753f4c2cedc8d852192569f8c47c9da
chr56 <chr0056@gmail.com>
2022-07-25 13:46:01 +0800

    [Gradle] add tasks encodeReleaseNoteToUrl

--- build.gradle.kts
+++ build.gradle.kts
@@ -22,3 +22,20 @@ allprojects {
         maven(url = "https://jitpack.io")
     }
 }
+
+tasks.create("encodeReleaseNoteToUrl") {
+
+    val inputFile = File("ReleaseNote.md")
+    val outputFile = File("ReleaseNote.url.txt")
+
+    if (inputFile.exists()) {
+        java.io.FileInputStream(inputFile).use { input ->
+            val content = String(input.readAllBytes())
+            val result = java.net.URLEncoder.encode(content, "UTF-8")
+            java.io.FileOutputStream(outputFile).use { output ->
+                output.write(result.toByteArray())
+                output.flush()
+            }
+        }
+    }
+}
