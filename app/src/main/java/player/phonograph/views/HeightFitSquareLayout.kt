ccc1661224946f950c88f292a33b0b986d8e4fdd
chr56 <chr0056@gmail.com>
2022-07-25 17:10:15 +0800

    [SquareLayouts] convert into kotlin

---
+++ app/src/main/java/player/phonograph/views/HeightFitSquareLayout.kt
@@ -0,0 +1,32 @@
+package player.phonograph.views
+
+import android.widget.FrameLayout
+import android.annotation.TargetApi
+import android.content.Context
+import android.os.Build
+import android.util.AttributeSet
+
+/**
+ * @author Karim Abou Zeid (kabouzeid)
+ */
+class HeightFitSquareLayout : FrameLayout {
+    private var forceSquare = true
+
+    constructor(context: Context?) : super(context!!) {}
+    constructor(context: Context?, attrs: AttributeSet?) : super(context!!, attrs) {}
+    constructor(context: Context?, attrs: AttributeSet?, defStyleAttr: Int) : super(context!!, attrs, defStyleAttr) {}
+
+    @TargetApi(Build.VERSION_CODES.LOLLIPOP)
+    constructor(context: Context?, attrs: AttributeSet?, defStyleAttr: Int, defStyleRes: Int) : super(
+        context!!, attrs, defStyleAttr, defStyleRes) {
+    }
+
+    override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
+        super.onMeasure(if (forceSquare) heightMeasureSpec else widthMeasureSpec, heightMeasureSpec)
+    }
+
+    fun forceSquare(forceSquare: Boolean) {
+        this.forceSquare = forceSquare
+        requestLayout()
+    }
+}
