93ec3c97e052456a0ce2254ba20129a97f24a7ba
chr56 <chr0056@gmail.com>
2022-07-26 13:06:44 +0800

    [ColoredTarget] init

---
+++ app/src/main/java/player/phonograph/coil/target/ColoredTarget.kt
@@ -0,0 +1,23 @@
+/*
+ * Copyright (c) 2022 chr_56
+ */
+
+package player.phonograph.coil.target
+
+import android.graphics.drawable.Drawable
+import androidx.palette.graphics.Palette
+
+abstract class ColoredTarget : PaletteTarget() {
+
+    override fun onSuccess(result: Drawable) {
+        super.onSuccess(result)
+        onReady(drawable = result, palette = palette)
+    }
+
+    /**
+     * call only when success
+     * @param drawable fetched result by coil
+     * @param palette null if drawable can not generate to palette
+     */
+    abstract fun onReady(drawable: Drawable, palette: Palette?)
+}
