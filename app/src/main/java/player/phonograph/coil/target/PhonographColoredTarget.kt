8333b355d257abe7653159516a33f8541d88bbd0
chr56 <chr0056@gmail.com>
2022-07-26 13:14:15 +0800

    [PhonographColoredTarget] init

---
+++ app/src/main/java/player/phonograph/coil/target/PhonographColoredTarget.kt
@@ -0,0 +1,27 @@
+/*
+ * Copyright (c) 2022 chr_56
+ */
+
+package player.phonograph.coil.target
+
+import android.graphics.drawable.Drawable
+import androidx.palette.graphics.Palette
+import player.phonograph.App
+import player.phonograph.R
+import player.phonograph.util.PhonographColorUtil
+import util.mddesign.util.Util
+
+abstract class PhonographColoredTarget : ColoredTarget() {
+
+    override fun onReady(drawable: Drawable, palette: Palette?) {
+        onReady(drawable, PhonographColorUtil.getColor(palette, defaultFooterColor))
+    }
+
+    /**
+     * more specific
+     */
+    abstract fun onReady(drawable: Drawable, color: Int)
+
+    protected open val defaultFooterColor: Int =
+        Util.resolveColor(App.instance, R.attr.defaultFooterColor)
+}
