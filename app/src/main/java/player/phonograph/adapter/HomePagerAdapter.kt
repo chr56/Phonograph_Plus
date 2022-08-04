42b693fe2d6c6c7e2fd1abebb84d75fe1eb3a2d5
chr56 <chr0056@gmail.com>
2022-08-04 11:00:19 +0800

    [PageConfig] make PAGER `object` & add getTintedIcon

--- app/src/main/java/player/phonograph/adapter/HomePagerAdapter.kt
+++ app/src/main/java/player/phonograph/adapter/HomePagerAdapter.kt
@@ -5,7 +5,9 @@
 package player.phonograph.adapter
 
 import android.content.Context
+import android.graphics.drawable.Drawable
 import android.util.ArrayMap
+import androidx.core.graphics.BlendModeCompat
 import androidx.fragment.app.Fragment
 import androidx.viewpager2.adapter.FragmentStateAdapter
 import java.lang.ref.WeakReference
@@ -15,6 +17,7 @@ import org.json.JSONException
 import org.json.JSONObject
 import player.phonograph.R
 import player.phonograph.ui.fragments.mainactivity.home.*
+import player.phonograph.util.ImageUtil.getTintedDrawable
 
 class HomePagerAdapter(fragment: Fragment, var cfg: PageConfig) : FragmentStateAdapter(fragment) {
     val map: MutableMap<Int, WeakReference<AbsPage>> = ArrayMap(cfg.getSize())
@@ -66,28 +69,44 @@ class PageConfig(var tabMap: MutableMap<Int, String>) : Iterable<String> {
         }
 }
 
-interface PAGERS {
-    companion object {
-        const val EMPTY = "EMPTY"
-        const val SONG = "SONG"
-        const val ALBUM = "ALBUM"
-        const val ARTIST = "ARTIST"
-        const val PLAYLIST = "PLAYLIST"
-        const val GENRE = "GENRE"
-        const val FOLDER = "FOLDER"
+object PAGERS {
+    const val EMPTY = "EMPTY"
+    const val SONG = "SONG"
+    const val ALBUM = "ALBUM"
+    const val ARTIST = "ARTIST"
+    const val PLAYLIST = "PLAYLIST"
+    const val GENRE = "GENRE"
+    const val FOLDER = "FOLDER"
 
-        fun getDisplayName(pager: String?, context: Context): String {
-            return when (pager) {
-                SONG -> context.getString(R.string.songs)
-                ALBUM -> context.getString(R.string.albums)
-                ARTIST -> context.getString(R.string.artists)
-                PLAYLIST -> context.getString(R.string.playlists)
-                GENRE -> context.getString(R.string.genres)
-                FOLDER -> context.getString(R.string.folders)
-                EMPTY -> context.getString(R.string.empty)
-                else -> "UNKNOWN"
-            }
+    fun getDisplayName(pager: String?, context: Context): String {
+        return when (pager) {
+            SONG -> context.getString(R.string.songs)
+            ALBUM -> context.getString(R.string.albums)
+            ARTIST -> context.getString(R.string.artists)
+            PLAYLIST -> context.getString(R.string.playlists)
+            GENRE -> context.getString(R.string.genres)
+            FOLDER -> context.getString(R.string.folders)
+            EMPTY -> context.getString(R.string.empty)
+            else -> "UNKNOWN"
         }
+    }
+
+    fun getTintedIcon(
+        pager: String?,
+        color: Int,
+        context: Context,
+        mode: BlendModeCompat = BlendModeCompat.SRC_IN
+    ): Drawable? {
+        val id = when (pager) {
+            SONG -> R.drawable.ic_music_note_white_24dp
+            ALBUM -> R.drawable.ic_album_white_24dp
+            ARTIST -> R.drawable.ic_person_white_24dp
+            PLAYLIST -> R.drawable.ic_queue_music_white_24dp
+            GENRE -> R.drawable.ic_bookmark_music_white_24dp
+            FOLDER -> R.drawable.ic_folder_white_24dp
+            else -> R.drawable.ic_library_music_white_24dp
+        }
+        return context.getTintedDrawable(id, color, mode)
     }
 }
 
