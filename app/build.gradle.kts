69d2e307eaa37c6e6f15b8be107c1fa47c4e3a8e
chr56 <chr0056@gmail.com>
2022-08-04 12:20:52 +0800

    [Gradle] bump version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -56,8 +56,8 @@ android {
         vectorDrawables.useSupportLibrary = true
 
         applicationId = "player.phonograph"
-        versionCode = 261
-        versionName = "0.3-alpha10"
+        versionCode = 262
+        versionName = "0.3-beta01"
 
         buildConfigField("String", "GIT_COMMIT_HASH", "\"${getGitHash(false)}\"")
         setProperty("archivesBaseName", "PhonographPlus_$versionName")
