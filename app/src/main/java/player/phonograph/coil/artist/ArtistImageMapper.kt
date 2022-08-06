18ed81f1a93a95b0d0cea46ba7652ae5c3ad5411
chr56 <chr0056@gmail.com>
2022-08-06 17:42:09 +0800

    [ArtistImageMapper] init

---
+++ app/src/main/java/player/phonograph/coil/artist/ArtistImageMapper.kt
@@ -0,0 +1,17 @@
+/*
+ * Copyright (c) 2022 chr_56
+ */
+
+package player.phonograph.coil.artist
+
+import coil.map.Mapper
+import coil.request.Options
+import player.phonograph.model.Artist
+
+class ArtistImageMapper : Mapper<Artist, ArtistImage> {
+    override fun map(data: Artist, options: Options): ArtistImage? =
+        ArtistImage(
+            data.name,
+            data.albums.map { ArtistImage.AlbumCover(it.year, it.safeGetFirstSong().data) }
+        ).takeIf { it.albumCovers.isNotEmpty() }
+}
