c2ecd2f0f50fb5cf05add860c7db4f5e4e828ef8
chr56 <chr0056@gmail.com>
2022-08-06 17:29:16 +0800

    [ArtistKeyer] init

---
+++ app/src/main/java/player/phonograph/coil/artist/ArtistKeyer.kt
@@ -0,0 +1,15 @@
+/*
+ * Copyright (c) 2022 chr_56
+ */
+
+package player.phonograph.coil.artist
+
+import coil.key.Keyer
+import coil.request.Options
+import player.phonograph.model.Artist
+
+class ArtistKeyer : Keyer<Artist> {
+    override fun key(data: Artist, options: Options): String {
+        return with(data) { "$name/$id" }
+    }
+}
