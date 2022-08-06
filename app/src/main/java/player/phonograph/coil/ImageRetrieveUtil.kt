42d4210f845a0bad18e7499e8ab516539a3da366
chr56 <chr0056@gmail.com>
2022-08-06 18:23:10 +0800

    [ImageRetrieveUtil] extract relative method from AudioFileFetcher

---
+++ app/src/main/java/player/phonograph/coil/ImageRetrieveUtil.kt
@@ -0,0 +1,112 @@
+/*
+ * Copyright (c) 2022 chr_56
+ */
+@file:JvmName("ImageRetrieveUtil")
+
+package player.phonograph.coil
+
+import android.content.ContentResolver
+import android.content.Context
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
+import android.graphics.Point
+import android.media.MediaMetadataRetriever
+import android.net.Uri
+import android.os.Build
+import android.os.Bundle
+import coil.annotation.ExperimentalCoilApi
+import coil.decode.ContentMetadata
+import coil.decode.DataSource
+import coil.decode.ImageSource
+import coil.fetch.SourceResult
+import coil.size.Dimension
+import coil.size.Size
+import java.io.File
+import java.io.InputStream
+import okio.buffer
+import okio.source
+import org.jaudiotagger.audio.mp3.MP3File
+import player.phonograph.util.MusicUtil.getMediaStoreAlbumCoverUri
+
+internal fun readFromMediaStore(albumId: Long, context: Context, size: Size): SourceResult? {
+    return runCatching {
+        val uri = getMediaStoreAlbumCoverUri(albumId)
+        readFromMediaStore(uri, context, size)
+    }.getOrNull()
+}
+
+internal fun retrieveFromMediaMetadataRetriever(filepath: String): Bitmap? {
+    val retriever = MediaMetadataRetriever()
+    val embeddedPicture: ByteArray? =
+        runCatching {
+            retriever.setDataSource(filepath)
+            retriever.embeddedPicture
+        }.getOrNull()
+    return embeddedPicture?.toBitmap()
+}
+
+internal fun retrieveFromJAudioTagger(filepath: String): Bitmap? {
+    return runCatching {
+        val mp3File = MP3File(filepath)
+        /* return@runCatching */ if (mp3File.hasID3v2Tag()) {
+            mp3File.tag.firstArtwork?.binaryData?.toBitmap() ?: return@runCatching null
+        } else {
+            null
+        }
+    }.getOrNull()
+}
+
+internal fun retrieveFromExternalFile(filepath: String): Bitmap? {
+    val parent = File(filepath).parentFile ?: return null
+    for (fallback in fallbackCoverFiles) {
+        val coverFile = File(parent, fallback)
+        return if (coverFile.exists()) {
+            BitmapFactory.decodeFile(coverFile.absolutePath)
+        } else {
+            continue
+        }
+    }
+    return null
+}
+
+internal val fallbackCoverFiles = arrayOf(
+    "cover.jpg",
+    "album.jpg",
+    "folder.jpg",
+    "cover.png",
+    "album.png",
+    "folder.png"
+)
+
+@OptIn(ExperimentalCoilApi::class)
+fun readFromMediaStore(uri: Uri, context: Context, size: Size): SourceResult? {
+    val contentResolver = context.contentResolver
+    val inputStream: InputStream? =
+        if (Build.VERSION.SDK_INT >= 29) {
+            val bundle: Bundle? =
+                run {
+                    val width = (size.width as? Dimension.Pixels)?.px ?: return@run null
+                    val height = (size.height as? Dimension.Pixels)?.px ?: return@run null
+                    Bundle(1).apply {
+                        putParcelable(
+                            ContentResolver.EXTRA_SIZE,
+                            Point(width, height)
+                        )
+                    }
+                }
+            contentResolver.openTypedAssetFile(uri, "image/*", bundle, null)?.createInputStream()
+        } else {
+            contentResolver.openInputStream(uri)
+        }
+    return if (inputStream != null) SourceResult(
+        source = ImageSource(
+            source = inputStream.source().buffer(),
+            context = context,
+            metadata = ContentMetadata(uri)
+        ),
+        mimeType = contentResolver.getType(uri),
+        dataSource = DataSource.DISK
+    ) else null
+}
+
+fun ByteArray.toBitmap(): Bitmap = BitmapFactory.decodeByteArray(this, 0, this.size)
--- app/src/main/java/player/phonograph/coil/audiofile/AudioFileFetcher.kt
+++ app/src/main/java/player/phonograph/coil/audiofile/AudioFileFetcher.kt
@@ -4,42 +4,24 @@
 
 package player.phonograph.coil.audiofile
 
-import android.content.ContentResolver
 import android.content.Context
 import android.graphics.Bitmap
-import android.graphics.BitmapFactory
-import android.graphics.Point
 import android.graphics.drawable.BitmapDrawable
-import android.media.MediaMetadataRetriever
-import android.net.Uri
-import android.os.Build
-import android.os.Bundle
 import android.util.Log
 import coil.ImageLoader
-import coil.annotation.ExperimentalCoilApi
-import coil.decode.ContentMetadata
 import coil.decode.DataSource
-import coil.decode.ImageSource
 import coil.fetch.DrawableResult
 import coil.fetch.FetchResult
 import coil.fetch.Fetcher
-import coil.fetch.SourceResult
 import coil.request.Options
-import coil.size.Dimension
 import coil.size.Size
-import java.io.File
-import java.io.InputStream
-import okio.buffer
-import okio.source
-import org.jaudiotagger.audio.mp3.MP3File
-import player.phonograph.BuildConfig
-import player.phonograph.coil.IgnoreMediaStorePreference
-import player.phonograph.util.MusicUtil
+import player.phonograph.BuildConfig.DEBUG
+import player.phonograph.coil.*
 
 class AudioFileFetcher private constructor(
-    val audioFile: AudioFile,
-    val context: Context,
-    val size: Size
+    private val audioFile: AudioFile,
+    private val context: Context,
+    private val size: Size
 ) : Fetcher {
 
     class Factory : Fetcher.Factory<AudioFile> {
@@ -56,68 +38,41 @@ class AudioFileFetcher private constructor(
         // 0: lookup for MediaStore
         //
         if (!IgnoreMediaStorePreference.ignoreMediaStore) {
-            val uri = MusicUtil.getMediaStoreAlbumCoverUri(audioFile.albumId)
-            runCatching {
-                val result = readFromMediaStore(uri)
-                if (result != null) return result
+            val result = readFromMediaStore(audioFile.albumId, context, size)
+            if (result != null) {
+                return result
+            } else {
+                if (DEBUG) {
+                    Log.v(TAG, "No cover for $audioFile from Android MediaStore")
+                }
             }
         }
 
         //
         // 1: From Android MediaMetadataRetriever
         //
-        val retriever = MediaMetadataRetriever()
-        val embeddedPicture: ByteArray? =
-            try {
-                retriever.setDataSource(audioFile.path)
-                retriever.embeddedPicture
-            } catch (ignored: Exception) {
-                null
-            }
-        bitmap =
-            if (embeddedPicture != null) {
-                // success
-                embeddedPicture.toBitmap()
-            } else {
-                if (BuildConfig.DEBUG) {
-                    Log.v(TAG, "No cover for $audioFile from Android naive MediaMetadataRetriever")
-                }
-                null
-            }
+        bitmap = retrieveFromMediaMetadataRetriever(audioFile.path)
+        if (DEBUG && bitmap == null) {
+            Log.v(TAG, "No cover for $audioFile from Android naive MediaMetadataRetriever")
+        }
 
         //
         // 2: Use JAudioTagger to get embedded high resolution album art if there is any
         //
         if (bitmap == null) {
-            bitmap =
-                runCatching {
-                    val mp3File = MP3File(audioFile.path)
-                    /* return@runCatching */ if (mp3File.hasID3v2Tag()) {
-                        mp3File.tag.firstArtwork?.binaryData?.toBitmap() ?: return@runCatching null
-                    } else {
-                        if (BuildConfig.DEBUG) {
-                            Log.v(TAG, "No tag containing imagine for $audioFile")
-                        }
-                        null
-                    }
-                }.getOrNull()
+            bitmap = retrieveFromJAudioTagger(audioFile.path)
+            if (DEBUG && bitmap == null) {
+                Log.v(TAG, "No cover for $audioFile in tags")
+            }
         }
 
         //
         // 3: Look for album art in external files
         //
         if (bitmap == null) {
-            val parent = File(audioFile.path).parentFile ?: return null
-            for (fallback in fallbackFiles) {
-                val coverFile = File(parent, fallback)
-                bitmap = if (coverFile.exists()) {
-                    BitmapFactory.decodeFile(coverFile.absolutePath)
-                } else {
-                    if (BuildConfig.DEBUG) {
-                        Log.v(TAG, "No cover file along with $audioFile in folder")
-                    }
-                    null
-                }
+            bitmap = retrieveFromExternalFile(audioFile.path)
+            if (DEBUG && bitmap == null) {
+                Log.v(TAG, "No cover file along with $audioFile in folder")
             }
         }
 
@@ -131,55 +86,14 @@ class AudioFileFetcher private constructor(
                 DataSource.DISK
             )
         } else {
-            if (BuildConfig.DEBUG) {
+            if (DEBUG) {
                 Log.v(TAG, "No any cover for $audioFile")
             }
             null
         }
     }
 
-    @OptIn(ExperimentalCoilApi::class)
-    private fun readFromMediaStore(uri: Uri): SourceResult? {
-        val contentResolver = context.contentResolver
-        val inputStream: InputStream? =
-            if (Build.VERSION.SDK_INT >= 29) {
-                val bundle: Bundle? =
-                    run {
-                        val width = (size.width as? Dimension.Pixels)?.px ?: return@run null
-                        val height = (size.height as? Dimension.Pixels)?.px ?: return@run null
-                        Bundle(1).apply {
-                            putParcelable(
-                                ContentResolver.EXTRA_SIZE,
-                                Point(width, height)
-                            )
-                        }
-                    }
-                contentResolver.openTypedAssetFile(uri, "image/*", bundle, null)?.createInputStream()
-            } else {
-                contentResolver.openInputStream(uri)
-            }
-        return if (inputStream != null) SourceResult(
-            source = ImageSource(
-                source = inputStream.source().buffer(),
-                context = context,
-                metadata = ContentMetadata(uri)
-            ),
-            mimeType = contentResolver.getType(uri),
-            dataSource = DataSource.DISK
-        ) else null
-    }
-
     companion object {
         private const val TAG = "AudioFileFetcher"
-        val fallbackFiles = arrayOf(
-            "cover.jpg",
-            "album.jpg",
-            "folder.jpg",
-            "cover.png",
-            "album.png",
-            "folder.png"
-        )
-
-        fun ByteArray.toBitmap(): Bitmap = BitmapFactory.decodeByteArray(this, 0, this.size)
     }
 }
