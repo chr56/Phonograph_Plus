7bb09f1e3b7aec0c31cfb4343d51bff61614b9bf
chr56 <chr0056@gmail.com>
2022-08-07 21:23:26 +0800

    [Gradle] add plugin 'gradle-versions-plugin'

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,13 +3,19 @@ buildscript {
     repositories {
         mavenCentral()
         google()
+        gradlePluginPortal()
         jcenter()
         maven(url = "https://jitpack.io")
     }
     dependencies {
         classpath("com.android.tools.build:gradle:7.2.1")
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.6.21")
+        classpath ("com.github.ben-manes:gradle-versions-plugin:0.39.0")
     }
+}
+
+plugins {
+    id("com.github.ben-manes.versions") version "0.39.0"
 }
 
 allprojects {
