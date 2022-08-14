a3d69c5fc207885da8194680f76a7ffc3941076f
chr56 <chr0056@gmail.com>
2022-08-14 13:14:30 +0800

    [ShuffleHelper] fix IndexOutOfBoundsException in rare circumstance

--- app/src/main/java/player/phonograph/helper/ShuffleHelper.kt
+++ app/src/main/java/player/phonograph/helper/ShuffleHelper.kt
@@ -10,7 +10,7 @@ object ShuffleHelper {
     fun MutableList<Song>.shuffleAt(current: Int) {
         if (isEmpty()) return
 
-        if (current >= 0) {
+        if (current in 0 until size) {
             val song: Song = removeAt(current)
             shuffle()
             add(0, song)
