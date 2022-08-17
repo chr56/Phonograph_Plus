1da50f97c03eb93f138403cbe26fe38aa9128988
chr56 <chr0056@gmail.com>
2022-08-17 19:13:35 +0800

    [Localization] split file & always updateResources

--- app/src/main/java/lib/phonograph/localization/Localization.kt
+++ app/src/main/java/lib/phonograph/localization/Localization.kt
@@ -6,8 +6,6 @@ package lib.phonograph.localization
 
 import android.app.Activity
 import android.content.Context
-import android.content.res.Configuration
-import android.content.res.Resources
 import java.util.*
 import lib.phonograph.localization.LocalizationStore.Companion.startUpLocale
 
@@ -54,37 +52,4 @@ object Localization {
      */
     fun defaultLocale(context: Context?): Locale =
         if (context != null) context.resources.configuration.locales[0] else startUpLocale
-}
-
-object LocalizationUtil {
-    @JvmOverloads
-    fun updateResources(
-        resources: Resources,
-        newLocale: Locale = Locale.getDefault()
-    ) {
-        if (resources.configuration.locales[0] == newLocale) return
-        resources.updateConfiguration(
-            amendConfiguration(resources.configuration, newLocale),
-            resources.displayMetrics
-        )
-    }
-
-    @JvmOverloads
-    fun createNewConfigurationContext(
-        context: Context,
-        newLocale: Locale = Localization.currentLocale(context)
-    ): Context =
-        context.createConfigurationContext(
-            amendConfiguration(context.resources.configuration, newLocale)
-        )
-
-    @JvmOverloads
-    fun amendConfiguration(
-        configuration: Configuration,
-        newLocale: Locale = Locale.getDefault()
-    ): Configuration =
-        configuration.apply {
-            setLocale(newLocale)
-            setLayoutDirection(newLocale)
-        }
 }
---
+++ app/src/main/java/lib/phonograph/localization/LocalizationUtil.kt
@@ -0,0 +1,42 @@
+/*
+ * Copyright (c) 2022 chr_56
+ */
+
+package lib.phonograph.localization
+
+import android.content.Context
+import android.content.res.Configuration
+import android.content.res.Resources
+import java.util.*
+
+object LocalizationUtil {
+    @JvmOverloads
+    fun updateResources(
+        resources: Resources,
+        newLocale: Locale = Locale.getDefault()
+    ) {
+        resources.updateConfiguration(
+            amendConfiguration(resources.configuration, newLocale),
+            resources.displayMetrics
+        )
+    }
+
+    @JvmOverloads
+    fun createNewConfigurationContext(
+        context: Context,
+        newLocale: Locale = Localization.currentLocale(context)
+    ): Context =
+        context.createConfigurationContext(
+            amendConfiguration(context.resources.configuration, newLocale)
+        )
+
+    @JvmOverloads
+    fun amendConfiguration(
+        configuration: Configuration,
+        newLocale: Locale = Locale.getDefault()
+    ): Configuration =
+        configuration.apply {
+            setLocale(newLocale)
+            setLayoutDirection(newLocale)
+        }
+}
