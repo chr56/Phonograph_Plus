b08f2f27eec7b85d2a5d12cc996d7b7c5bdfb088
chr56 <chr0056@gmail.com>
2022-08-18 15:36:26 +0800

    [coil] enable loader for artist

--- app/src/main/java/player/phonograph/coil/LoaderSingleton.kt
+++ app/src/main/java/player/phonograph/coil/LoaderSingleton.kt
@@ -6,6 +6,9 @@ package player.phonograph.coil
 
 import android.content.Context
 import coil.ImageLoader
+import player.phonograph.coil.artist.ArtistImageFetcher
+import player.phonograph.coil.artist.ArtistImageMapper
+import player.phonograph.coil.artist.ArtistKeyer
 import player.phonograph.coil.audiofile.AudioFileFetcher
 import player.phonograph.coil.audiofile.AudioFileKeyer
 import player.phonograph.coil.audiofile.AudioFileMapper
@@ -14,9 +17,14 @@ fun createPhonographImageLoader(context: Context): ImageLoader {
     return ImageLoader.Builder(context)
         .allowHardware(false)
         .components {
+            // song files
             add(AudioFileKeyer())
             add(AudioFileMapper())
             add(AudioFileFetcher.Factory())
+            // artist
+            add(ArtistKeyer())
+            add(ArtistImageMapper())
+            add(ArtistImageFetcher.Factory())
         }
         .crossfade(true)
         .build()
