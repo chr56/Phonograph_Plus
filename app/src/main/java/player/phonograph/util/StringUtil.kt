1d5bf66b3513aeea4eab88ff7d4b133cf836a930
chr56 <chr0056@gmail.com>
2022-08-19 15:26:50 +0800

    [StringUtil] extract buildSomeMessage()

--- app/src/main/java/player/phonograph/util/StringUtil.kt
+++ app/src/main/java/player/phonograph/util/StringUtil.kt
@@ -12,29 +12,43 @@ import java.lang.StringBuilder
 
 object StringUtil {
     fun buildDeletionMessage(context: Context, itemSize: Int, extraSuffix: CharSequence?, vararg data: ItemGroup): Spanned {
-        val content = StringBuilder()
-
-        for (group in data) {
-            if (group.items.isNotEmpty()) {
-                content.append("${group.typeName}<br/>")
-                for (i in group.items) {
-                    content.append("* <b>$i</b><br/>")
-                }
-            }
-        }
-
-        val message = """
-           <b>${context.resources.getQuantityString(R.plurals.msg_header_delete_items, itemSize)}</b><br />
-           $content
-           <br/>
-           <b> ${context.getString(R.string.warning_can_not_retract)}</b><br/>
-           <br/>$extraSuffix
-        """.trimIndent()
-        return Html.fromHtml(message, Html.FROM_HTML_MODE_LEGACY)
+        return buildSomeMessage(
+            header = context.resources.getQuantityString(R.plurals.msg_header_delete_items, itemSize),
+            content = data,
+            end = extraSuffix,
+            warning = context.getString(R.string.warning_can_not_retract),
+        )
     }
 
     class ItemGroup(
         val typeName: String,
         val items: List<String>,
     )
+
+    fun buildSomeMessage(
+        header: CharSequence,
+        content: Array<out ItemGroup>,
+        end: CharSequence?,
+        warning: CharSequence?,
+    ): Spanned {
+        val s = StringBuilder()
+
+        for (group in content) {
+            if (group.items.isNotEmpty()) {
+                s.append("${group.typeName}<br/>")
+                for (i in group.items) {
+                    s.append("* <b>$i</b><br/>")
+                }
+            }
+        }
+
+        val message = """
+           <b>$header</b><br />
+           $s
+           <br/>
+           <b>$warning</b><br/>
+           <br/>$end
+        """.trimIndent()
+        return Html.fromHtml(message, Html.FROM_HTML_MODE_LEGACY)
+    }
 }
