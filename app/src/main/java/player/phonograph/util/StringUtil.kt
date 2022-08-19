765c3f64548bb4936eec353090527d99be760431
chr56 <chr0056@gmail.com>
2022-08-19 15:32:46 +0800

    [StringUtil] add buildRemovalMessage

--- app/src/main/java/player/phonograph/util/StringUtil.kt
+++ app/src/main/java/player/phonograph/util/StringUtil.kt
@@ -8,28 +8,52 @@ import android.content.Context
 import android.text.Html
 import android.text.Spanned
 import player.phonograph.R
-import java.lang.StringBuilder
 
 object StringUtil {
-    fun buildDeletionMessage(context: Context, itemSize: Int, extraSuffix: CharSequence?, vararg data: ItemGroup): Spanned {
+
+    fun buildRemovalMessage(
+        context: Context,
+        itemSize: Int,
+        where: CharSequence,
+        suffix: CharSequence?,
+        vararg data: ItemGroup
+    ): Spanned {
+        val res = context.resources
         return buildSomeMessage(
-            header = context.resources.getQuantityString(R.plurals.msg_header_delete_items, itemSize),
+            header =
+            res.getQuantityString(R.plurals.msg_header_remove_items, itemSize, where),
+            content = data,
+            end = suffix,
+            warning = res.getString(R.string.warning_can_not_retract)
+        )
+    }
+
+    fun buildDeletionMessage(
+        context: Context,
+        itemSize: Int,
+        extraSuffix: CharSequence?,
+        vararg data: ItemGroup
+    ): Spanned {
+        val res = context.resources
+        return buildSomeMessage(
+            header =
+            res.getQuantityString(R.plurals.msg_header_delete_items, itemSize),
             content = data,
             end = extraSuffix,
-            warning = context.getString(R.string.warning_can_not_retract),
+            warning = res.getString(R.string.warning_can_not_retract)
         )
     }
 
     class ItemGroup(
         val typeName: String,
-        val items: List<String>,
+        val items: List<String>
     )
 
     fun buildSomeMessage(
         header: CharSequence,
         content: Array<out ItemGroup>,
         end: CharSequence?,
-        warning: CharSequence?,
+        warning: CharSequence?
     ): Spanned {
         val s = StringBuilder()
 
