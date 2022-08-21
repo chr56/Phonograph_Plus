fe67fb21430c39a9eea722394a53e2454d026e57
chr56 <chr0056@gmail.com>
2022-08-21 14:12:55 +0800

    [Gradle] upgrade gradle-versions-plugin to 0.42.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,12 +8,11 @@ buildscript {
     dependencies {
         classpath("com.android.tools.build:gradle:7.2.1")
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.6.21")
-        classpath ("com.github.ben-manes:gradle-versions-plugin:0.39.0")
     }
 }
 
 plugins {
-    id("com.github.ben-manes.versions") version "0.39.0"
+    id("com.github.ben-manes.versions") version "0.42.0"
 }
 
 allprojects {
