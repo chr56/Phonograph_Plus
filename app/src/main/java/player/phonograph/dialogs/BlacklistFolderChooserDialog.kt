5e7b0453f19d103c7ef3627fa3d38d90cf6c640d
chr56 <chr0056@gmail.com>
2022-09-07 14:40:47 +0800

    [BlacklistFolderChooserDialog] use FileChooserDialog

--- app/src/main/java/player/phonograph/dialogs/BlacklistFolderChooserDialog.kt
+++ app/src/main/java/player/phonograph/dialogs/BlacklistFolderChooserDialog.kt
@@ -1,80 +1,40 @@
 package player.phonograph.dialogs
 
-import android.Manifest
-import android.annotation.SuppressLint
-import android.app.Dialog
-import android.content.Intent
-import android.content.pm.PackageManager
-import android.net.Uri
-import android.os.Build
-import android.os.Bundle
-import android.provider.Settings
-import androidx.core.app.ActivityCompat
-import androidx.fragment.app.DialogFragment
+import android.view.View
 import com.afollestad.materialdialogs.MaterialDialog
 import com.afollestad.materialdialogs.WhichButton
 import com.afollestad.materialdialogs.actions.getActionButton
-import com.afollestad.materialdialogs.files.folderChooser
 import mt.pref.ThemeColor
 import player.phonograph.App
 import player.phonograph.R
+import player.phonograph.model.file.Location
 import player.phonograph.preferences.BlacklistPreferenceDialog
 import player.phonograph.provider.BlacklistStore
-import player.phonograph.util.Util
 import java.io.File
 
-class BlacklistFolderChooserDialog : DialogFragment() {
+class BlacklistFolderChooserDialog : FileChooserDialog() {
 
-    @SuppressLint("ObsoleteSdkInt")
-    override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
-        // Storage permission check
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M &&
-            ActivityCompat.checkSelfPermission(
-                requireActivity(), Manifest.permission.READ_EXTERNAL_STORAGE
-            )
-            != PackageManager.PERMISSION_GRANTED
-        ) {
-            return MaterialDialog(requireContext())
-                .title(R.string.Permission_denied)
-                .message(R.string.err_permission_storage)
-                .neutralButton(R.string.grant_permission) {
-                    Util.navigateToStorageSetting(requireActivity())
-                }
-                .positiveButton(android.R.string.ok)
-        }
+    override fun affirmative(view: View, currentLocation: Location) {
+        val file = File(currentLocation.absolutePath)
+        MaterialDialog(requireContext())
+            .title(R.string.add_blacklist)
+            .message(text = file.absolutePath)
+            .positiveButton(android.R.string.ok) {
+                BlacklistStore.getInstance(App.instance).addPath(file)
 
-        // FileChooser
-        val dialog = MaterialDialog(requireContext())
-            .folderChooser(context = requireContext(),
-                waitForPositiveButton = true,
-                emptyTextRes = R.string.empty,
-                initialDirectory = File("/storage/emulated/0")) { _, file ->
-                val alertDialog = MaterialDialog(requireContext())
-                    .title(R.string.add_blacklist)
-                    .message(text = file.absolutePath)
+                it.dismiss() // dismiss this alert dialog
+                this.dismiss() // dismiss Folder Chooser
 
-                alertDialog
-                    .positiveButton(android.R.string.ok) {
-                        BlacklistStore.getInstance(App.instance).addPath(file)
-                        alertDialog.dismiss() // dismiss this alert dialog
-
-                        dismiss() // dismiss Folder Chooser
-                        BlacklistPreferenceDialog.newInstance()
-                            .show(parentFragmentManager, "Blacklist_Preference_Dialog") // then reopen BlacklistPreferenceDialog
-                    }
-                    .negativeButton(android.R.string.cancel) {
-                        alertDialog.dismiss() // dismiss this alert dialog
-                    }
-                    .show()
-                // todo tint
+                BlacklistPreferenceDialog.newInstance()
+                    .show(parentFragmentManager, "Blacklist_Preference_Dialog") // then reopen BlacklistPreferenceDialog
             }
-            .noAutoDismiss()
-            .positiveButton(R.string.add_action)
-            .negativeButton(android.R.string.cancel) { dismiss() }
-        // set button color
-        dialog.getActionButton(WhichButton.POSITIVE).updateTextColor(ThemeColor.accentColor(requireActivity()))
-        dialog.getActionButton(WhichButton.NEGATIVE).updateTextColor(ThemeColor.accentColor(requireActivity()))
-
-        return dialog
+            .negativeButton(android.R.string.cancel) {
+                it.dismiss() // dismiss this alert dialog
+            }
+            .apply {
+                getActionButton(WhichButton.POSITIVE).updateTextColor(ThemeColor.accentColor(requireActivity()))
+                getActionButton(WhichButton.NEGATIVE).updateTextColor(ThemeColor.accentColor(requireActivity()))
+            }
+            .show()
     }
 }
