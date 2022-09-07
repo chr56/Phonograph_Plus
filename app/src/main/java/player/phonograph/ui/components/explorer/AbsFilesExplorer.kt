18884ccad2e06b917a1daa989f0a98c2fa408188
chr56 <chr0056@gmail.com>
2022-09-07 14:23:31 +0800

    [AbsFilesExplorer] pull up all common methods

--- app/src/main/java/player/phonograph/ui/components/explorer/AbsFilesExplorer.kt
+++ app/src/main/java/player/phonograph/ui/components/explorer/AbsFilesExplorer.kt
@@ -5,11 +5,28 @@
 package player.phonograph.ui.components.explorer
 
 import android.content.Context
+import android.graphics.drawable.Drawable
+import android.os.Environment
+import android.os.storage.StorageManager
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.annotation.DrawableRes
+import androidx.core.content.getSystemService
+import androidx.recyclerview.widget.RecyclerView
+import com.afollestad.materialdialogs.MaterialDialog
+import com.afollestad.materialdialogs.list.listItemsSingleChoice
 import com.google.android.material.appbar.AppBarLayout
+import com.google.android.material.snackbar.Snackbar
+import lib.phonograph.storage.root
+import mt.util.color.primaryTextColor
+import player.phonograph.R
 import player.phonograph.databinding.FragmentFolderPageBinding
+import player.phonograph.model.file.Location
+import player.phonograph.notification.ErrorNotification
 import player.phonograph.ui.components.ViewComponent
+import player.phonograph.util.ImageUtil.getTintedDrawable
+import player.phonograph.util.PhonographColorUtil.nightMode
 
 sealed class AbsFilesExplorer<M : AbsFileViewModel>(protected val context: Context) : ViewComponent<ViewGroup, M> {
 
@@ -53,7 +70,88 @@ sealed class AbsFilesExplorer<M : AbsFileViewModel>(protected val context: Conte
             )
         }
 
-    abstract fun reload()
-    abstract fun changeVolume(): Boolean
-    abstract fun gotoTopLevel(allowToChangeVolume: Boolean): Boolean
+    /**
+     * reload all files (determined by [AbsFileViewModel.currentLocation])
+     */
+    protected fun reload() {
+        binding.container.isRefreshing = true
+        model.loadFiles(context) {
+            updateFilesDisplayed()
+            binding.header.apply {
+                location = model.currentLocation
+                layoutManager.scrollHorizontallyBy(
+                    binding.header.width / 4,
+                    recyclerView.Recycler(),
+                    RecyclerView.State()
+                )
+            }
+            binding.buttonBack.setImageDrawable(
+                if (model.currentLocation.parent == null) {
+                    context.getThemedDrawable(R.drawable.ic_library_music_white_24dp)
+                } else {
+                    context.getThemedDrawable(R.drawable.icon_back_white)
+                }
+            )
+            binding.container.isRefreshing = false
+        }
+    }
+    abstract fun updateFilesDisplayed()
+
+    /**
+     * open a dialog to ask change storage volume (disk)
+     */
+    internal fun requireChangeVolume(): Boolean {
+        val storageManager = context.getSystemService<StorageManager>()
+        val volumes = storageManager?.storageVolumes
+            ?.filter { it.state == Environment.MEDIA_MOUNTED || it.state == Environment.MEDIA_MOUNTED_READ_ONLY }
+            ?: emptyList()
+        if (volumes.isEmpty()) {
+            ErrorNotification.postErrorNotification("No volumes found! Your system might be not supported!")
+            return false
+        }
+        MaterialDialog(context)
+            .listItemsSingleChoice(
+                items = volumes.map { "${it.getDescription(context)}\n(${it.root()?.path ?: "N/A"})" },
+                initialSelection = volumes.indexOf(model.currentLocation.storageVolume),
+                waitForPositiveButton = true,
+            ) { materialDialog: MaterialDialog, i: Int, _: CharSequence ->
+                materialDialog.dismiss()
+                val path = volumes[i].root()?.absolutePath
+                if (path == null) {
+                    Toast.makeText(context, "Unmounted volume", Toast.LENGTH_SHORT).show()
+                } else { // todo
+                    model.currentLocation = Location.fromAbsolutePath("$path/")
+                    reload()
+                }
+            }
+            .title(R.string.folders)
+            .positiveButton(android.R.string.ok)
+            .show()
+        return true
+    }
+
+    /**
+     * @param allowToChangeVolume false if do not intend to change volume
+     * @return success or not
+     */
+    internal fun gotoTopLevel(allowToChangeVolume: Boolean): Boolean {
+        val parent = model.currentLocation.parent
+        return if (parent != null) {
+            model.currentLocation = parent
+            reload()
+            true
+        } else {
+            Snackbar.make(binding.root, context.getString(R.string.reached_to_root), Snackbar.LENGTH_SHORT).show()
+            if (!allowToChangeVolume) {
+                false
+            } else {
+                requireChangeVolume()
+            }
+        }
+    }
+
+    companion object {
+        internal fun Context.getThemedDrawable(@DrawableRes resId: Int): Drawable? =
+            getTintedDrawable(resId, primaryTextColor(resources.nightMode))
+    }
 }
--- app/src/main/java/player/phonograph/ui/components/explorer/FilesChooserExplorer.kt
+++ app/src/main/java/player/phonograph/ui/components/explorer/FilesChooserExplorer.kt
@@ -4,31 +4,16 @@
 
 package player.phonograph.ui.components.explorer
 
-import android.graphics.drawable.Drawable
-import android.os.Environment
-import android.os.storage.StorageManager
 import android.view.View.GONE
-import android.widget.Toast
-import androidx.annotation.DrawableRes
 import androidx.core.app.ComponentActivity
-import androidx.core.content.getSystemService
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
-import com.afollestad.materialdialogs.MaterialDialog
-import com.afollestad.materialdialogs.list.listItemsSingleChoice
-import com.google.android.material.snackbar.Snackbar
-import lib.phonograph.storage.root
 import mt.pref.ThemeColor
-import mt.util.color.primaryTextColor
 import player.phonograph.App
 import player.phonograph.R
 import player.phonograph.adapter.file.FileChooserAdapter
 import player.phonograph.model.file.FileEntity
 import player.phonograph.model.file.Location
-import player.phonograph.notification.ErrorNotification
-import player.phonograph.ui.fragments.HomeFragment
-import player.phonograph.util.ImageUtil.getTintedDrawable
-import player.phonograph.util.PhonographColorUtil.nightMode
 import player.phonograph.util.ViewUtil.setUpFastScrollRecyclerViewColor
 
 class FilesChooserExplorer(
@@ -42,7 +27,7 @@ class FilesChooserExplorer(
     override fun initModel(model: FilesChooserViewModel) {
         fileModel = model
         binding.buttonPageHeader.visibility = GONE
-        binding.buttonBack.setImageDrawable(getDrawable(R.drawable.md_nav_back))
+        binding.buttonBack.setImageDrawable(activity.getThemedDrawable(R.drawable.md_nav_back))
         binding.buttonBack.setOnClickListener { gotoTopLevel(true) }
         binding.buttonBack.setOnLongClickListener {
             model.currentLocation = Location.HOME
@@ -91,79 +76,7 @@ class FilesChooserExplorer(
         binding.innerAppBar.setExpanded(true)
     }
 
-    /**
-     * @param allowToChangeVolume false if do not intend to change volume
-     * @return success or not
-     */
-    override fun gotoTopLevel(allowToChangeVolume: Boolean): Boolean {
-        val parent = fileModel.currentLocation.parent
-        return if (parent != null) {
-            fileModel.currentLocation = parent
-            reload()
-            true
-        } else {
-            Snackbar.make(binding.root, context.getString(R.string.reached_to_root), Snackbar.LENGTH_SHORT).show()
-            if (!allowToChangeVolume) {
-                false
-            } else {
-                changeVolume()
-            }
-        }
+    override fun updateFilesDisplayed() {
+        adapter.dataSet = fileModel.currentFileList.toMutableList()
     }
-
-    override fun changeVolume(): Boolean {
-        val storageManager = activity.getSystemService<StorageManager>()
-        val volumes = storageManager?.storageVolumes
-            ?.filter { it.state == Environment.MEDIA_MOUNTED || it.state == Environment.MEDIA_MOUNTED_READ_ONLY }
-            ?: emptyList()
-        if (volumes.isEmpty()) {
-            ErrorNotification.postErrorNotification("No volumes found! Your system might be not supported!")
-            return false
-        }
-        MaterialDialog(activity)
-            .listItemsSingleChoice(
-                items = volumes.map { "${it.getDescription(context)}\n(${it.root()?.path ?: "N/A"})" },
-                initialSelection = volumes.indexOf(fileModel.currentLocation.storageVolume),
-                waitForPositiveButton = true,
-            ) { materialDialog: MaterialDialog, i: Int, _: CharSequence ->
-                materialDialog.dismiss()
-                val path = volumes[i].root()?.absolutePath
-                if (path == null) {
-                    Toast.makeText(activity, "Unmounted volume", Toast.LENGTH_SHORT).show()
-                } else { // todo
-                    fileModel.currentLocation = Location.fromAbsolutePath("$path/")
-                    reload()
-                }
-            }
-            .title(R.string.folders)
-            .positiveButton(android.R.string.ok)
-            .show()
-        return true
-    }
-
-    override fun reload() {
-        binding.container.isRefreshing = true
-        fileModel.loadFiles(activity) {
-            adapter.dataSet = fileModel.currentFileList.toMutableList()
-            binding.header.apply {
-                location = fileModel.currentLocation
-                layoutManager.scrollHorizontallyBy(
-                    binding.header.width / 4,
-                    recyclerView.Recycler(),
-                    RecyclerView.State()
-                )
-            }
-            binding.buttonBack.setImageDrawable(
-                if (fileModel.currentLocation.parent == null) {
-                    getDrawable(R.drawable.ic_library_music_white_24dp)
-                } else {
-                    getDrawable(R.drawable.icon_back_white)
-                }
-            )
-            binding.container.isRefreshing = false
-        }
-    }
-
-    private fun getDrawable(@DrawableRes resId: Int): Drawable? =
-        activity.getTintedDrawable(resId, activity.primaryTextColor(activity.resources.nightMode))
 }
