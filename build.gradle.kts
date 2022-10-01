dde58f50838466c9d4d10239e8406ee97e8e379e
chr56 <chr0056@gmail.com>
2022-10-01 17:05:54 +0800

    [Gradle] update AGP to `7.3.0`

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     id("com.github.ben-manes.versions") version "0.42.0" apply false // enable if needed
-    id("com.android.application") version "7.2.2" apply false
+    id("com.android.application") version "7.3.0" apply false
+    // id("com.android.library") version "7.3.0" apply false
     id("org.jetbrains.kotlin.android") version "1.7.10" apply false
     id("org.jetbrains.kotlin.plugin.serialization") version "1.7.10" apply false
 }
