095836e70b8fc7cfbca3379517f940546bc69be6
chr56 <chr0056@gmail.com>
2022-10-08 20:46:18 +0800

    [Gradle] increase versions

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -40,8 +40,8 @@ android {
         vectorDrawables.useSupportLibrary = true
 
         applicationId = "player.phonograph"
-        versionCode = 313
-        versionName = "0.3.5"
+        versionCode = 350
+        versionName = "0.4-dev"
 
         buildConfigField("String", "GIT_COMMIT_HASH", "\"${getGitHash(false)}\"")
         setProperty("archivesBaseName", "PhonographPlus_$versionName")
