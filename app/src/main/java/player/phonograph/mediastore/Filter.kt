7ea55cef8b777233ee1bb55a3c6145413c573c3d
chr56 <chr0056@gmail.com>
2022-10-09 19:36:52 +0800

    [Filter] enable new filter

--- app/src/main/java/player/phonograph/mediastore/Filter.kt
+++ app/src/main/java/player/phonograph/mediastore/Filter.kt
@@ -7,24 +7,26 @@ package player.phonograph.mediastore
 import android.content.Context
 import android.provider.MediaStore
 import player.phonograph.provider.PathFilterStore
+import player.phonograph.settings.Setting
 
 class SQLWhereClause(val selection: String, val selectionValues: Array<String>)
 
 /**
  *  amend path with path filter SQL to SQLWhereClause
- *  @param mode whitelist mode
  */
-fun withPathFilter(context: Context, mode: Boolean = false, block: () -> SQLWhereClause): SQLWhereClause {
+fun withPathFilter(context: Context, block: () -> SQLWhereClause): SQLWhereClause {
+    val includeMode = !Setting.instance().pathFilterExcludeMode
+
     val paths =
-        if (mode)
-            emptyList() // todo: not implemented yet
+        if (includeMode)
+            PathFilterStore.getInstance(context).whitelistPaths.map { "$it%" }
         else
             PathFilterStore.getInstance(context).blacklistPaths.map { "$it%" }
 
     val target = block()
 
     val pattern =
-        if (mode)
+        if (includeMode)
             "${MediaStore.Audio.AudioColumns.DATA} LIKE ?"
         else
             "${MediaStore.Audio.AudioColumns.DATA} NOT LIKE ?"
@@ -32,7 +34,7 @@ fun withPathFilter(context: Context, mode: Boolean = false, block: () -> SQLWher
 
     return if (paths.isNotEmpty()) {
         SQLWhereClause(
-            plusSelectionCondition(target.selection, mode, pattern, paths.size),
+            plusSelectionCondition(target.selection, includeMode, pattern, paths.size),
             target.selectionValues.plus(paths)
         )
     } else {
@@ -42,13 +44,13 @@ fun withPathFilter(context: Context, mode: Boolean = false, block: () -> SQLWher
 
 /**
  * create duplicated string for selection
- * @param mode whitelist mode
+ * @param includeMode "whitelist" mode
  * @param what the string to duplicate
  * @param count count of the duplicated [what]
  */
-private fun plusSelectionCondition(selection: String, mode: Boolean, what: String, count: Int): String {
+private fun plusSelectionCondition(selection: String, includeMode: Boolean, what: String, count: Int): String {
     if (count <= 0) return selection // do nothing
-    val separator = if (mode) "OR" else " AND"
+    val separator = if (includeMode) "OR" else " AND"
     var accumulator = selection
     // first
     accumulator +=
