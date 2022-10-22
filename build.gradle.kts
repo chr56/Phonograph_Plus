cc0dc1ec61cf53fb3ed54efd2be95153e43d4b2f
chr56 <chr0056@gmail.com>
2022-10-22 13:24:24 +0800

    [Gradle] cleanup for `build.gradle.kts` in root project

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,11 +1,13 @@
 plugins {
-    // id("com.github.ben-manes.versions") version "0.42.0" apply false // enable if needed
-    id("com.android.application") version "7.3.0" apply false
-    // id("com.android.library") version "7.3.0" apply false
+    val AGPVersion = "7.3.0"
+    id("com.android.application") version AGPVersion apply false
+    // id("com.android.library") version AGPVersion apply false
     val kotlinVersion = "1.7.10"
     id("org.jetbrains.kotlin.android") version kotlinVersion apply false
     id("org.jetbrains.kotlin.plugin.serialization") version kotlinVersion apply false
     id("org.jetbrains.kotlin.plugin.parcelize") version kotlinVersion apply false
+    // enable if needed
+    // id("com.github.ben-manes.versions") version "0.42.0"
 }
 repositories {
     gradlePluginPortal()
