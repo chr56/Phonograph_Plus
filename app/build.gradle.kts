6498d620d969d517f0ac54655d5fe5d1905d6551
chr56 <chr0056@gmail.com>
2023-01-07 13:45:02 +0800

    [Gradle] use java 11

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -164,8 +164,8 @@ android {
     }
 
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
     }
 
     composeOptions {
@@ -173,7 +173,7 @@ android {
     }
 
     kotlinOptions {
-        jvmTarget = "1.8"
+        jvmTarget = "11"
         freeCompilerArgs = listOf(
             "-P",
             "plugin:androidx.compose.compiler.plugins.kotlin:suppressKotlinVersionCompatibilityCheck=true"
