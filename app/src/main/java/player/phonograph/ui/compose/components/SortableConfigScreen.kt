42273cadc37248fa1e206d6f52a492b030ec8a47
chr56 <chr0056@gmail.com>
2023-01-10 17:11:17 +0800

    [SortableConfigScreen] refactor

--- app/src/main/java/player/phonograph/ui/compose/components/SortableConfigScreen.kt
+++ app/src/main/java/player/phonograph/ui/compose/components/SortableConfigScreen.kt
@@ -4,12 +4,10 @@
 
 package player.phonograph.ui.compose.components
 
-import org.burnoutcrew.reorderable.ItemPosition
 import org.burnoutcrew.reorderable.ReorderableItem
 import org.burnoutcrew.reorderable.detectReorderAfterLongPress
 import org.burnoutcrew.reorderable.rememberReorderableLazyListState
 import org.burnoutcrew.reorderable.reorderable
-import player.phonograph.model.SortableConfigModel
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
@@ -17,24 +15,39 @@ import androidx.compose.foundation.lazy.items
 import androidx.compose.material.Checkbox
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 
+
+interface SortableConfigContract {
+    fun size(): Int
+    fun itemsAt(position: Int): Item
+    fun allItems(): List<Item>
+    fun move(from: Int, to: Int)
+    fun toggle(position: Int, newValue: Boolean)
+    fun toggle(item: Item, newValue: Boolean)
+    interface Item {
+        fun text(): String
+        fun enabled(): Boolean
+        fun id(): Int
+    }
+}
+
 @Composable
-fun SortableConfigScreen(model: SortableConfigModel) {
-    val data = remember { mutableStateOf(model.items.toList()) }
-    val onMove: (ItemPosition, ItemPosition) -> Unit =
-        { from, to ->
-            model.move(from.index, to.index)
-            data.value = data.value.toMutableList().apply { add(to.index, removeAt(from.index)) }
-        }
-    val onToggleEnabled: (SortableConfigModel.Item, Boolean) -> Unit =
-        { item, enabled -> model.toggle(item, enabled) }
-    val state = rememberReorderableLazyListState(onMove = onMove)
+fun SortableConfigScreen(adapter: SortableConfigContract) {
+    var data by remember { mutableStateOf(adapter.allItems()) }
+    val state = rememberReorderableLazyListState(
+        onMove = { from, to ->
+            adapter.move(from.index, to.index)
+            data = data.toMutableList().apply { add(to.index, removeAt(from.index)) }
+        })
     LazyColumn(
         modifier = Modifier
             .padding(16.dp)
@@ -42,9 +55,14 @@ fun SortableConfigScreen(model: SortableConfigModel) {
             .detectReorderAfterLongPress(state),
         state = state.listState
     ) {
-        items(data.value, { it.id() }) { item ->
-            ReorderableItem(state, key = item.id()) {
-                Item(sortableConfigItem = item) { onToggleEnabled(item, it) }
+        items(data/* */) { item ->
+            ReorderableItem(reorderableState = state, key = item.id()) {
+                val enabled = remember { mutableStateOf(item.enabled()) }
+                val onToggle = { newValue: Boolean ->
+                    enabled.value = newValue
+                    adapter.toggle(item, newValue)
+                }
+                Item(itemName = item.text(), enabled = enabled, onCheckedChange = onToggle)
             }
         }
     }
@@ -52,24 +70,48 @@ fun SortableConfigScreen(model: SortableConfigModel) {
 
 @Composable
 private fun Item(
-    sortableConfigItem: SortableConfigModel.Item,
+    itemName: String,
+    enabled: MutableState<Boolean>,
     onCheckedChange: (Boolean) -> Unit
 ) {
-    val enabled = remember { mutableStateOf(sortableConfigItem.enabled) }
     Row(
         Modifier.padding(horizontal = 8.dp)
     ) {
         Checkbox(
             checked = enabled.value,
-            onCheckedChange = {
-                onCheckedChange(it)
-                enabled.value = it
-            }
+            onCheckedChange = onCheckedChange
         )
         Text(
-            text = sortableConfigItem.name(),
+            text = itemName,
             modifier = Modifier.align(Alignment.CenterVertically),
             textAlign = TextAlign.Start
         )
+    }
+}
+
+class SortableConfigAdapter(val data: MutableList<Item>) : SortableConfigContract {
+
+    override fun size(): Int = data.size
+
+    override fun itemsAt(position: Int): Item = data[position]
+
+    override fun allItems(): List<Item> = data
+
+    override fun move(from: Int, to: Int) {
+        data.add(to, data.removeAt(from))
+    }
+
+    override fun toggle(position: Int, newValue: Boolean) {
+        data[position].enabled = newValue
+    }
+
+    override fun toggle(item: SortableConfigContract.Item, newValue: Boolean) {
+        data.find { item.id() == it.id }?.enabled = newValue
+    }
+
+    class Item(val text: String, var enabled: Boolean, val id: Int) : SortableConfigContract.Item {
+        override fun text(): String = text
+        override fun enabled(): Boolean = enabled
+        override fun id(): Int = id
     }
 }
