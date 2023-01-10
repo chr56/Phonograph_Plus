c626e4fba16546a1d259725a7b4d38f06d063eec
chr56 <chr0056@gmail.com>
2023-01-10 15:42:14 +0800

    [ImageSourceConfig] implement json encode/decode

--- app/src/main/java/player/phonograph/model/config/ImageSourceConfig.kt
+++ app/src/main/java/player/phonograph/model/config/ImageSourceConfig.kt
@@ -4,6 +4,10 @@
 
 package player.phonograph.model.config
 
+import player.phonograph.model.IMAGE_SOURCE_EXTERNAL_FILE
+import player.phonograph.model.IMAGE_SOURCE_J_AUDIO_TAGGER
+import player.phonograph.model.IMAGE_SOURCE_MEDIA_METADATA_RETRIEVER
+import player.phonograph.model.IMAGE_SOURCE_MEDIA_STORE
 import player.phonograph.model.ImageSource
 import androidx.annotation.Keep
 import android.os.Parcelable
@@ -26,4 +30,19 @@ data class ImageSourceConfig(
         val imageSource: ImageSource
             get() = ImageSource.fromKey(name)
     }
+
+    companion object {
+        @Suppress("PropertyName")
+        val DEFAULT: ImageSourceConfig
+            get() = ImageSourceConfig(
+                listOf(
+                    Item(IMAGE_SOURCE_MEDIA_STORE, true),
+                    Item(IMAGE_SOURCE_MEDIA_METADATA_RETRIEVER, true),
+                    Item(IMAGE_SOURCE_J_AUDIO_TAGGER, true),
+                    Item(IMAGE_SOURCE_EXTERNAL_FILE, true),
+                ), VERSION
+            )
+        const val VERSION = 1
+    }
 }
+
