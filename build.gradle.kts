eae45286c78d12c84ab86d128175df507f346311
chr56 <chr0056@gmail.com>
2023-01-27 12:31:37 +0800

    [Gradle] update AGP to `7.4.1`
    with Android Studio `Electric Eel` (Patch 1)

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
-    val AGPVersion = "7.3.0"
+    val AGPVersion = "7.4.1"
     id("com.android.application") version AGPVersion apply false
     // id("com.android.library") version AGPVersion apply false
     val kotlinVersion = "1.7.10"
@@ -7,7 +7,7 @@ plugins {
     id("org.jetbrains.kotlin.plugin.serialization") version kotlinVersion apply false
     id("org.jetbrains.kotlin.plugin.parcelize") version kotlinVersion apply false
     // enable if needed
-    // id("com.github.ben-manes.versions") version "0.42.0"
+    // id("com.github.ben-manes.versions") version "0.44.0"
 }
 repositories {
     gradlePluginPortal()
