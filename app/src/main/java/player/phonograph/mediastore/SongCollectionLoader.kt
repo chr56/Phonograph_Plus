36148e1ffe0d138c65ea78a9b03ac188b89ff516
chr56 <chr0056@gmail.com>
2023-02-22 10:52:36 +0800

    [SongCollection] add SongCollection & its loader

---
+++ app/src/main/java/player/phonograph/mediastore/SongCollectionLoader.kt
@@ -0,0 +1,82 @@
+/*
+ * Copyright (c) 2022~2023 chr_56
+ */
+
+package player.phonograph.mediastore
+
+import player.phonograph.model.Song
+import player.phonograph.model.SongCollection
+import android.content.Context
+import android.util.Log
+import kotlinx.coroutines.flow.asFlow
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.flow.onEach
+import kotlinx.coroutines.runBlocking
+
+object SongCollectionLoader {
+
+
+    fun getAllSongCollection(context: Context): List<SongCollection> = runBlocking {
+        val allSongs = querySongs(context).getSongs()
+        getAllSongCollectionImpl(context, allSongs)
+    }
+
+    private suspend fun getAllSongCollectionImpl(
+        context: Context,
+        allSongs: List<Song>
+    ): List<SongCollection> {
+
+        val sorted = mutableMapOf<String, MutableList<Song>>()
+
+        allSongs.asFlow().onEach { song: Song ->
+            val folder =
+                song.data.substringBeforeLast('/', "(root-directory)")
+            sorted.getOrPut(folder) { mutableListOf() }.add(song)
+        }.collect()
+
+
+        //
+        // collect short names
+        //
+        val shortNames = mutableMapOf<String, String>()
+        sorted.forEach { (folder, _) ->
+            val short = folder.substringAfterLast('/')
+            shortNames[folder] = short
+        }
+
+
+        // todo: improve `solve name conflicts`
+
+        //
+        // solve name conflicts
+        //
+        val uniques = shortNames.values.toSet()
+        if (uniques.size != shortNames.size) {
+            // name conflict!
+            val conflictNames = mutableListOf<String>().let { list ->
+                shortNames.values.toCollection(list).also {
+                    val result = it.removeAll(uniques)
+                    if (!result) {
+                        Log.w("SongCollection", "can not solve conflicts")
+                        it.clear()
+                    }
+                }
+            }
+            conflictNames.forEach { name ->
+                val items = shortNames.filter { (_, value) -> value == name }
+                items.forEach { item ->
+                    shortNames[item.key] = item.key //todo: make different
+                }
+            }
+        }
+
+        // convert
+        return sorted.map { (folder, songs) ->
+            SongCollection(
+                shortNames.getOrDefault(folder, "Unknown"),
+                songs,
+                folder
+            )
+        }
+    }
+}
---
+++ app/src/main/java/player/phonograph/model/SongCollection.kt
@@ -0,0 +1,67 @@
+/*
+ * Copyright (c) 2022~2023 chr_56
+ */
+
+package player.phonograph.model
+
+import androidx.annotation.Keep
+import android.content.Context
+import android.os.Parcel
+import android.os.Parcelable
+
+class SongCollection(
+    val name: String,
+    val songs: List<Song>,
+    val detail: String? = null,
+) : Parcelable, Displayable {
+
+
+    override fun getItemID(): Long = hashCode().toLong()
+    override fun getDisplayTitle(context: Context) = name
+    override fun getDescription(context: Context) = songCountString(context, songs.size)
+
+
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (other !is SongCollection) return false
+
+        if (name != other.name) return false
+        if (songs != other.songs) return false
+        if (detail != other.detail) return false
+
+        return true
+    }
+
+    override fun hashCode(): Int = name.hashCode() * 31 + songs.hashCode()
+
+    override fun toString(): String = "SongCollection(name='$name', songs=$songs)"
+
+    constructor(parcel: Parcel) : this(
+        parcel.readString()!!,
+        parcel.createTypedArrayList(Song.CREATOR)!!,
+        parcel.readString()
+    )
+
+    override fun writeToParcel(parcel: Parcel, flags: Int) {
+        parcel.writeString(name)
+        parcel.writeTypedList(songs)
+        parcel.writeString(detail)
+    }
+
+    override fun describeContents(): Int = 0
+
+    companion object {
+        @Keep
+        @JvmField
+        val CREATOR = object : Parcelable.Creator<SongCollection> {
+            override fun createFromParcel(parcel: Parcel): SongCollection {
+                return SongCollection(parcel)
+            }
+
+            override fun newArray(size: Int): Array<SongCollection?> {
+                return arrayOfNulls(size)
+            }
+        }
+    }
+
+}
