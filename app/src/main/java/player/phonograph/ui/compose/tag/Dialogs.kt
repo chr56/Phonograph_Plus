5bc5afc4d4f71d1304a32dd2bdace8c2c416a8b1
chr56 <chr0056@gmail.com>
2023-02-25 00:29:15 +0800

    [TagEdit] Unify Dialog Code

--- app/src/main/java/player/phonograph/ui/compose/tag/Dialogs.kt
+++ app/src/main/java/player/phonograph/ui/compose/tag/Dialogs.kt
@@ -9,7 +9,19 @@ import com.vanpra.composematerialdialogs.MaterialDialogState
 import com.vanpra.composematerialdialogs.customView
 import com.vanpra.composematerialdialogs.title
 import player.phonograph.R
+import androidx.annotation.StringRes
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.requiredHeight
+import androidx.compose.foundation.layout.wrapContentWidth
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 
 
@@ -61,3 +73,48 @@ internal fun ExitWithoutSavingDialog(
         title(res = R.string.exit_without_saving)
     }
 }
+
+@Composable
+internal fun CoverImageDetailDialog(
+    state: MaterialDialogState,
+    artworkExist: Boolean,
+    onSave: () -> Unit,
+    onDelete: () -> Unit,
+    onUpdate: () -> Unit,
+    editMode: Boolean
+) = MaterialDialog(
+    dialogState = state,
+    buttons = {
+        positiveButton(res = android.R.string.ok) { state.hide() }
+    }
+) {
+    title(res = R.string.label_details)
+    Column(
+        modifier = Modifier
+            .padding(bottom = 28.dp, start = 24.dp, end = 24.dp)
+            .wrapContentWidth()
+    ) {
+        if (artworkExist) {
+            MenuItem(textRes = R.string.save, onSave)
+            if (editMode) {
+                MenuItem(textRes = R.string.remove_cover, onDelete)
+            }
+        }
+        if (editMode) {
+            MenuItem(textRes = R.string.update_image, onUpdate)
+        }
+    }
+}
+
+@Composable
+private fun MenuItem(@StringRes textRes: Int, onClick: () -> Unit) =
+    Text(
+        text = stringResource(textRes),
+        color = MaterialTheme.colors.primary,
+        textAlign = TextAlign.Start,
+        style = MaterialTheme.typography.button,
+        modifier = Modifier
+            .fillMaxWidth()
+            .requiredHeight(32.dp)
+            .clickable(onClick = onClick),
+    )
