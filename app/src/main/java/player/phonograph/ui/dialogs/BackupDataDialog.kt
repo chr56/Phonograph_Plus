704711c61f7e619536d47857a74ed75ce557fd32
chr56 <chr0056@gmail.com>
2023-02-27 12:05:30 +0800

    [BackupDataDialog] cleanup & refactor

--- app/src/main/java/player/phonograph/ui/dialogs/BackupDataDialog.kt
+++ app/src/main/java/player/phonograph/ui/dialogs/BackupDataDialog.kt
@@ -4,16 +4,23 @@
 
 package player.phonograph.ui.dialogs
 
+import player.phonograph.App
+import player.phonograph.R
 import player.phonograph.migrate.DatabaseBackupManger
 import player.phonograph.misc.ICreateFileStorageAccess
 import player.phonograph.misc.IOpenFileStorageAccess
 import player.phonograph.misc.OpenDocumentContract
+import player.phonograph.util.CoroutineUtil
 import player.phonograph.util.TimeUtil
 import androidx.appcompat.app.AlertDialog
 import androidx.fragment.app.DialogFragment
+import androidx.lifecycle.lifecycleScope
+import android.app.Activity
 import android.app.Dialog
 import android.net.Uri
 import android.os.Bundle
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 
 class BackupDataDialog : DialogFragment() {
     override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
@@ -21,53 +28,76 @@ class BackupDataDialog : DialogFragment() {
         require(activity is IOpenFileStorageAccess && activity is ICreateFileStorageAccess) {
             "Unsupported activity (SAF unavailable)"
         }
-        val items = mapOf(
-            0 to "export path filter",
-            1 to "import path filter",
-            2 to "export playing queues",
-            3 to "import playing queues",
+        val menu = listOf(
+            "export path filter" to {
+                export(
+                    requireActivity(),
+                    "phonograph_plus_path_filter_${TimeUtil.currentDateTime()}.json"
+                ) { uri ->
+                    DatabaseBackupManger.exportPathFilter(activity, uri)
+                    true
+                }
+            },
+            "import path filter" to {
+                import(requireActivity(), "application/json") { uri ->
+                    DatabaseBackupManger.importPathFilter(activity, uri)
+                    true
+                }
+            },
+            "export playing queues" to {
+                export(
+                    requireActivity(),
+                    "phonograph_plus_playing_queues_${TimeUtil.currentDateTime()}.json"
+                ) { uri ->
+                    DatabaseBackupManger.exportPlayingQueues(activity, uri)
+                    true
+                }
+            },
+            "import playing queues" to {
+                import(requireActivity(), "application/json") { uri ->
+                    DatabaseBackupManger.importPlayingQueues(activity, uri)
+                    true
+                }
+            },
         )
         return AlertDialog.Builder(activity)
             .setTitle("Backup")
-            .setItems(items.values.toTypedArray()) { dialog, i ->
+            .setItems(menu.map { it.first }.toTypedArray()) { dialog, i ->
                 dialog.dismiss()
-                when (i) {
-                    0 -> {
-                        activity.createFileStorageAccessTool.launch(
-                            "phonograph_plus_path_filter_${TimeUtil.currentDateTime()}.json"
-                        ) { uri: Uri? ->
-                            uri ?: return@launch
-                            DatabaseBackupManger.exportPathFilter(activity, uri)
-                        }
-                    }
-                    1 -> {
-                        activity.openFileStorageAccessTool.launch(
-                            OpenDocumentContract.Cfg(null, arrayOf("application/json"))
-                        ) { uri: Uri? ->
-                            uri ?: return@launch
-                            DatabaseBackupManger.importPathFilter(activity, uri)
-                        }
-                    }
-                    2 -> {
-                        activity.createFileStorageAccessTool.launch(
-                            "phonograph_plus_playing_queues_${TimeUtil.currentDateTime()}.json"
-                        ) { uri: Uri? ->
-                            uri ?: return@launch
-                            DatabaseBackupManger.exportPlayingQueues(activity, uri)
-                        }
-                    }
-                    3 -> {
-                        activity.openFileStorageAccessTool.launch(
-                            OpenDocumentContract.Cfg(null, arrayOf("application/json"))
-                        ) { uri: Uri? ->
-                            uri ?: return@launch
-                            DatabaseBackupManger.importPlayingQueues(activity, uri)
-                        }
-                    }
-                }
+                menu[i].second.invoke()
             }
             .setNegativeButton(android.R.string.cancel) { dialog, _ -> dialog.dismiss() }
             .setCancelable(true)
             .create()
+    }
+
+    private fun import(activity: Activity, mimeType: String, callback: (Uri) -> Boolean) {
+        require(activity is IOpenFileStorageAccess && activity is ICreateFileStorageAccess) {
+            "Unsupported activity (SAF unavailable)"
+        }
+        activity.openFileStorageAccessTool.launch(
+            OpenDocumentContract.Cfg(null, arrayOf(mimeType))
+        ) { uri: Uri? ->
+            uri ?: return@launch
+            lifecycleScope.launch(Dispatchers.IO) {
+                callback(uri).andReport()
+            }
+        }
+    }
+
+    private fun export(activity: Activity, fileName: String, callback: (Uri) -> Boolean) {
+        require(activity is IOpenFileStorageAccess && activity is ICreateFileStorageAccess) {
+            "Unsupported activity (SAF unavailable)"
+        }
+        activity.createFileStorageAccessTool.launch(fileName) { uri: Uri? ->
+            uri ?: return@launch
+            lifecycleScope.launch(Dispatchers.IO) {
+                callback(uri).andReport()
+            }
+        }
+    }
+
+    private suspend fun Boolean.andReport() {
+        CoroutineUtil.coroutineToast(App.instance, if (this) R.string.success else R.string.failed)
     }
 }
