a5b7fd25e80713abd005fff3c638159b31edb901
chr56 <chr0056@gmail.com>
2023-02-27 10:35:42 +0800

    [BackupDataDialog] add PathFilter backup

--- app/src/main/java/player/phonograph/ui/dialogs/BackupDataDialog.kt
+++ app/src/main/java/player/phonograph/ui/dialogs/BackupDataDialog.kt
@@ -4,11 +4,15 @@
 
 package player.phonograph.ui.dialogs
 
+import player.phonograph.migrate.DatabaseBackupManger
 import player.phonograph.misc.ICreateFileStorageAccess
 import player.phonograph.misc.IOpenFileStorageAccess
+import player.phonograph.misc.OpenDocumentContract
+import player.phonograph.util.TimeUtil
 import androidx.appcompat.app.AlertDialog
 import androidx.fragment.app.DialogFragment
 import android.app.Dialog
+import android.net.Uri
 import android.os.Bundle
 
 class BackupDataDialog : DialogFragment() {
@@ -18,13 +22,30 @@ class BackupDataDialog : DialogFragment() {
             "Unsupported activity (SAF unavailable)"
         }
         val items = mapOf(
-            -1 to ""
+            0 to "export path filter",
+            1 to "import path filter",
         )
         return AlertDialog.Builder(activity)
             .setTitle("Backup")
             .setItems(items.values.toTypedArray()) { dialog, i ->
                 dialog.dismiss()
                 when (i) {
+                    0 -> {
+                        activity.createFileStorageAccessTool.launch(
+                            "phonograph_plus_path_filter_${TimeUtil.currentDateTime()}.json"
+                        ) { uri: Uri? ->
+                            uri ?: return@launch
+                            DatabaseBackupManger.exportPathFilter(activity, uri)
+                        }
+                    }
+                    1 -> {
+                        activity.openFileStorageAccessTool.launch(
+                            OpenDocumentContract.Cfg(null, arrayOf("application/json"))
+                        ) { uri: Uri? ->
+                            uri ?: return@launch
+                            DatabaseBackupManger.importPathFilter(activity, uri)
+                        }
+                    }
                 }
             }
             .setNegativeButton(android.R.string.cancel) { dialog, _ -> dialog.dismiss() }
