db2bc6389ef7096a52e08c551eb48d3a379fa892
chr56 <chr0056@gmail.com>
2023-03-14 19:05:04 +0800

    [PhonographIntroActivity] add initial slide

--- app/src/main/java/player/phonograph/ui/activities/PhonographIntroActivity.kt
+++ app/src/main/java/player/phonograph/ui/activities/PhonographIntroActivity.kt
@@ -5,12 +5,31 @@
 package player.phonograph.ui.activities
 
 import com.github.appintro.AppIntro
+import com.github.appintro.AppIntroFragment
+import player.phonograph.R
 import androidx.fragment.app.Fragment
 import android.os.Bundle
 
 class PhonographIntroActivity : AppIntro() {
+
+    private fun config() {
+        showStatusBar(true)
+        isColorTransitionsEnabled = true
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        config()
+
+        addSlide(
+            AppIntroFragment.createInstance(
+                title = getString(R.string.app_name),
+                description = getString(R.string.welcome_to_phonograph),
+                imageDrawable = R.drawable.icon_web,
+                backgroundColorRes = R.color.md_blue_900
+            )
+        )
     }
 
     override fun onSkipPressed(currentFragment: Fragment?) {
