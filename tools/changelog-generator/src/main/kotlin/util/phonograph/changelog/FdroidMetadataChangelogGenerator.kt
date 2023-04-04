b1c2ee2b11fc1959168b09bfd739a2b0854d2e12
chr56 <chr0056@gmail.com>
2023-04-04 17:36:37 +0800

    [changelog-generator] cut off overflowed froid changelog

--- tools/changelog-generator/src/main/kotlin/util/phonograph/changelog/FdroidMetadataChangelogGenerator.kt
+++ tools/changelog-generator/src/main/kotlin/util/phonograph/changelog/FdroidMetadataChangelogGenerator.kt
@@ -18,7 +18,7 @@ fun generateFdroidMetadataChangelogText(model: ReleaseNoteModel, lang: String): 
         for (line in note) {
             appendLine("- $line")
         }
-    }
+    }.cutOff()
 
 fun writeFdroidMetadataChangelogText(model: ReleaseNoteModel, rootPath: String, lang: String) {
     val text = generateFdroidMetadataChangelogText(model, lang)
@@ -65,3 +65,36 @@ private const val FDROID_METADATA_VERSION_INFO = "fdroid.properties"
 
 private const val LATEST_VERSION_NAME = "latest_version_name"
 private const val LATEST_VERSION_CODE = "latest_version_code"
+
+
+/**
+ * take first bytes of [text]
+ * @param overflowMessage suffix message if [text] was cut off
+ * @param limit size in BYTE
+ */
+private fun cutOffOverflowedText(text: String, overflowMessage: String = OVERFLOWED_MESSAGE, limit: Int = 500): String {
+    val textBytes = text.encodeToByteArray()
+    val textSize = textBytes.size
+    return if (textSize > limit) {
+        val overflowMessageBytes = overflowMessage.encodeToByteArray()
+
+        // taken size of cut-offed text
+        var validSize = limit - overflowMessageBytes.size - 1
+        while (textBytes[validSize].toInt() < 0) {
+            // make sure not cut a character longer than 2 (whose msb is not 0)
+            validSize--
+        }
+
+        // remaining String
+        val cutOff = ByteArray(validSize) { textBytes[it] }
+
+        "${String(cutOff)}\n$overflowMessage"
+    } else {
+        text
+    }
+}
+
+private fun String.cutOff(overflowMessage: String = OVERFLOWED_MESSAGE, limit: Int = 500): String =
+    cutOffOverflowedText(this, overflowMessage, limit)
+
+private const val OVERFLOWED_MESSAGE = "...(Visit project homepage to see full changelogs)"
