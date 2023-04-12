7b1b8d2fc44a63b70b447d64db6cd9834dc5ddde
chr56 <chr0056@gmail.com>
2023-04-12 13:12:25 +0800

    [SettingsActivity] use `BackupExportDialog`

--- app/src/main/java/player/phonograph/ui/activities/SettingsActivity.kt
+++ app/src/main/java/player/phonograph/ui/activities/SettingsActivity.kt
@@ -21,6 +21,7 @@ import lib.phonograph.misc.OpenFileStorageAccessTool
 import player.phonograph.migrate.backup.Backup
 import player.phonograph.misc.menuProvider
 import player.phonograph.ui.dialogs.BackupDataDialog
+import player.phonograph.ui.dialogs.BackupExportDialog
 import player.phonograph.ui.fragments.SettingsFragment
 import player.phonograph.util.coroutineToast
 import player.phonograph.util.text.currentDateTime
@@ -165,17 +166,7 @@ class SettingsActivity : ToolbarActivity(), ICreateFileStorageAccess, IOpenFileS
                 title = getString(R.string.action_export, getString(R.string.action_backup))
                 showAsActionFlag = SHOW_AS_ACTION_NEVER
                 onClick {
-                    createFileStorageAccessTool.launch(
-                        "phonograph_plus_backup_${currentDateTime()}.zip"
-                    ) { uri ->
-                        uri ?: return@launch
-                        lifecycleScope.launch(Dispatchers.IO) {
-                            context.contentResolver.openOutputStream(uri, "wt")?.use {
-                                Backup.exportBackupToArchive(context = this@SettingsActivity, targetOutputStream = it)
-                            }
-                        }
-                    }
-
+                    BackupExportDialog().show(supportFragmentManager, "EXPORT")
                     true
                 }
             }
