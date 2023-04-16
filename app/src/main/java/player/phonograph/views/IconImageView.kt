c9da3a03e3a6d99023abaec3194c92c240752947
chr56 <chr0056@gmail.com>
2023-04-16 12:26:04 +0800

    [IconImageView] fallback to accent color

--- app/src/main/java/player/phonograph/views/IconImageView.kt
+++ app/src/main/java/player/phonograph/views/IconImageView.kt
@@ -1,31 +1,34 @@
 package player.phonograph.views
 
+import mt.pref.ThemeColor
+import mt.util.color.resolveColor
+import player.phonograph.R
+import androidx.appcompat.widget.AppCompatImageView
 import android.content.Context
 import android.graphics.PorterDuff
 import android.util.AttributeSet
-import androidx.appcompat.widget.AppCompatImageView
-import player.phonograph.R
-import mt.util.color.resolveColor
 
 /**
  * @author Karim Abou Zeid (kabouzeid)
  */
 class IconImageView : AppCompatImageView {
 
-    constructor(context: Context?) : super(context!!) {
+    constructor(context: Context) : super(context) {
         init(context)
     }
 
-    constructor(context: Context?, attrs: AttributeSet?) : super(context!!, attrs) {
+    constructor(context: Context, attrs: AttributeSet?) : super(context, attrs) {
         init(context)
     }
 
-    constructor(context: Context?, attrs: AttributeSet?, defStyleAttr: Int) : super(context!!, attrs, defStyleAttr) {
+    constructor(context: Context, attrs: AttributeSet?, defStyleAttr: Int) : super(context, attrs, defStyleAttr) {
         init(context)
     }
 
-    private fun init(context: Context?) {
-        if (context == null) return
-        setColorFilter(resolveColor(context, R.attr.iconColor), PorterDuff.Mode.SRC_IN)
+    private fun init(context: Context) {
+        setColorFilter(
+            resolveColor(context, R.attr.iconColor, ThemeColor.accentColor(context)),
+            PorterDuff.Mode.SRC_IN
+        )
     }
 }
