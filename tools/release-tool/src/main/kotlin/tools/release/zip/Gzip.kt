1937bd4140b0c8b5cccfb5466561ba264da9345e
chr56 <chr0056@gmail.com>
2023-04-18 18:40:52 +0800

    [release-tool] add gzip function

---
+++ tools/release-tool/src/main/kotlin/tools/release/zip/Gzip.kt
@@ -0,0 +1,40 @@
+/*
+ *  Copyright (c) 2022~2023 chr_56
+ */
+
+package tools.release.zip
+
+import java.io.File
+import java.io.FileInputStream
+import java.io.FileOutputStream
+import java.util.zip.GZIPOutputStream
+
+
+fun File.gzip(target: File? = null): File = gzipImpl(this, target)
+
+fun gzipImpl(file: File, target: File? = null): File {
+    require(file.exists()) { "File(${file.absolutePath}) is not available" }
+    require(file.isFile) { "File(${file.absolutePath}) is not a file!" }
+    val zippedFile = (target ?: File("${file.absolutePath}.gz")).assure()
+    FileOutputStream(zippedFile).use { outputStream ->
+        GZIPOutputStream(outputStream, 4096).use { gzipOutputStream ->
+            FileInputStream(file).use { inputStream ->
+                val buffer = ByteArray(4096)
+                var len: Int
+                while (inputStream.read(buffer).also { len = it } != -1) {
+                    gzipOutputStream.write(buffer, 0, len)
+                }
+                gzipOutputStream.flush()
+            }
+        }
+    }
+    return zippedFile
+}
+
+private fun File.assure(): File {
+    if (!this.exists()) {
+        createNewFile()
+    }
+    require(this.isFile) { "File($absolutePath) is not a file" }
+    return this
+}
