c64aa0b089d1fb49d8484ed6d3cdc48c831f6350
chr56 <chr0056@gmail.com>
2023-05-04 17:46:21 +0800

    [AlbumCoverPagerAdapter] move to PlayerAlbumCoverFragment.kt

--- app/src/main/java/player/phonograph/ui/fragments/player/PlayerAlbumCoverFragment.kt
+++ app/src/main/java/player/phonograph/ui/fragments/player/PlayerAlbumCoverFragment.kt
@@ -1,9 +1,9 @@
 package player.phonograph.ui.fragments.player
 
 import lib.phonograph.misc.SimpleAnimatorListener
-import player.phonograph.adapter.AlbumCoverPagerAdapter
 import player.phonograph.coil.loadImage
 import player.phonograph.coil.target.PaletteBitmap
+import player.phonograph.databinding.FragmentAlbumCoverBinding
 import player.phonograph.databinding.FragmentPlayerAlbumCoverBinding
 import player.phonograph.misc.MusicProgressViewUpdateHelperDelegate
 import player.phonograph.model.Song
@@ -14,6 +14,7 @@ import player.phonograph.settings.Setting
 import player.phonograph.ui.fragments.AbsMusicServiceFragment
 import player.phonograph.util.ui.PHONOGRAPH_ANIM_TIME
 import androidx.collection.LruCache
+import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.ViewModel
@@ -21,6 +22,7 @@ import androidx.lifecycle.lifecycleScope
 import androidx.lifecycle.repeatOnLifecycle
 import androidx.lifecycle.whenResumed
 import androidx.lifecycle.whenStarted
+import androidx.viewpager2.adapter.FragmentStateAdapter
 import androidx.viewpager2.widget.ViewPager2
 import android.animation.Animator
 import android.annotation.SuppressLint
@@ -36,6 +38,7 @@ import android.view.View.OnTouchListener
 import android.view.ViewGroup
 import android.view.animation.AccelerateInterpolator
 import android.view.animation.DecelerateInterpolator
+import android.widget.ImageView
 import kotlinx.coroutines.*
 
 /**
@@ -360,3 +363,76 @@ class AlbumCoverViewModel : ViewModel() {
 
 }
 
+class AlbumCoverPagerAdapter(
+    val fragment: Fragment,
+    dataSet: List<Song>,
+) : FragmentStateAdapter(fragment) {
+
+    var dataSet: List<Song> = dataSet
+        @SuppressLint("NotifyDataSetChanged")
+        set(value) {
+            field = value
+            notifyDataSetChanged()
+        }
+
+
+    override fun createFragment(position: Int): Fragment = coverFragment(dataSet[position])
+
+    override fun getItemCount(): Int = dataSet.size
+
+    private fun coverFragment(song: Song): AlbumCoverFragment =
+        AlbumCoverFragment().apply {
+            arguments = Bundle().apply {
+                putParcelable(SONG_ARG, song)
+            }
+        }
+
+    class AlbumCoverFragment : Fragment() {
+
+        private var _binding: FragmentAlbumCoverBinding? = null
+        val binding get() = _binding!!
+
+        private val viewModel: AlbumCoverViewModel by viewModels({ requireParentFragment() })
+
+        private lateinit var song: Song
+
+        override fun onCreate(savedInstanceState: Bundle?) {
+            super.onCreate(savedInstanceState)
+            song = requireArguments().getParcelable(SONG_ARG)!!
+        }
+
+        override fun onCreateView(
+            inflater: LayoutInflater,
+            container: ViewGroup?,
+            savedInstanceState: Bundle?,
+        ): View {
+            _binding = FragmentAlbumCoverBinding.inflate(LayoutInflater.from(context))
+            return binding.root
+        }
+
+        override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+            super.onViewCreated(view, savedInstanceState)
+            forceSquareAlbumCover(false)
+            lifecycleScope.launch {
+                val bitmap = viewModel.getImage(requireContext(), song)
+                withContext(Dispatchers.Main) {
+                    binding.playerImage.setImageBitmap(bitmap)
+                }
+            }
+        }
+
+        override fun onDestroyView() {
+            super.onDestroyView()
+            _binding = null
+        }
+
+        private fun forceSquareAlbumCover(forceSquareAlbumCover: Boolean) {
+            binding.playerImage.scaleType =
+                if (forceSquareAlbumCover) ImageView.ScaleType.FIT_CENTER else ImageView.ScaleType.CENTER_CROP
+        }
+    }
+
+    companion object {
+        private const val SONG_ARG = "song"
+    }
+}
