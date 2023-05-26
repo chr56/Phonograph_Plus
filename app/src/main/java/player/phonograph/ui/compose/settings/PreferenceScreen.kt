551c9d2ca5694090bddcb4679f1f4ab4c8ab062b
chr56 <chr0056@gmail.com>
2023-05-26 21:40:41 +0800

    [PreferenceScreen] add Compose ListPref & OptionGroup

--- app/src/main/java/player/phonograph/ui/compose/settings/PreferenceScreen.kt
+++ app/src/main/java/player/phonograph/ui/compose/settings/PreferenceScreen.kt
@@ -4,8 +4,11 @@
 
 package player.phonograph.ui.compose.settings
 
+import com.alorma.compose.settings.storage.base.SettingValueState
 import com.alorma.compose.settings.storage.base.rememberBooleanSettingState
+import com.alorma.compose.settings.storage.base.rememberIntSettingState
 import com.alorma.compose.settings.storage.datastore.rememberPreferenceDataStoreBooleanSettingState
+import com.alorma.compose.settings.ui.SettingsListDropdown
 import com.alorma.compose.settings.ui.SettingsMenuLink
 import com.alorma.compose.settings.ui.SettingsSwitch
 import player.phonograph.settings.dataStore
@@ -21,8 +24,13 @@ import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalInspectionMode
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
+import androidx.datastore.preferences.core.edit
+import androidx.datastore.preferences.core.stringPreferencesKey
 import androidx.fragment.app.DialogFragment
 import androidx.fragment.app.FragmentActivity
+import android.content.Context
+import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.runBlocking
 
 
 
@@ -74,6 +82,87 @@ private fun DialogPref(
         }
     }
 }
+
+
+@Composable
+private fun ListPref(
+    optionGroup: OptionGroup,
+    @StringRes titleRes: Int,
+    @StringRes summaryRes: Int = 0,
+    enabled: Boolean = true,
+) {
+    val context = LocalContext.current
+    val state =
+        if (LocalInspectionMode.current) {
+            rememberIntSettingState(0)
+        } else {
+            rememberIntSettingState(optionGroup.selected(context))
+        }
+    val items =
+        if (LocalInspectionMode.current) {
+            optionGroup.optionsValue
+        } else {
+            optionGroup.options(context)
+        }
+    val onItemSelected: (Int, String) -> Unit =
+        if (LocalInspectionMode.current) {
+            { _, _ -> }
+        } else {
+            { index, _ ->
+                optionGroup.onSelect(context, index)
+            }
+        }
+
+    ListPrefImpl(
+        state = state,
+        items = items,
+        onItemSelected = onItemSelected,
+        titleRes = titleRes
+    )
+}
+
+@Composable
+private fun ListPrefImpl(
+    items: List<String>,
+    state: SettingValueState<Int>,
+    onItemSelected: (Int, String) -> Unit,
+    @StringRes titleRes: Int,
+    @StringRes summaryRes: Int = 0,
+    enabled: Boolean = true,
+) {
+    SettingsListDropdown(
+        state = state,
+        enabled = enabled,
+        title = title(titleRes),
+        subtitle = subtitle(summaryRes),
+        items = items,
+        onItemSelected = onItemSelected
+    )
+}
+
+internal class OptionGroup(
+    val key: String,
+    val optionsValue: List<String>,
+    val optionsStringRes: List<Int>,
+) {
+
+    fun options(context: Context): List<String> {
+        return optionsStringRes.map { context.getString(it) }
+    }
+
+    fun selected(context: Context): Int = runBlocking {//todo no runBlocking
+        val value = context.dataStore.data.first()[stringPreferencesKey(key)]
+        optionsValue.indexOf(value)
+    }
+
+    fun onSelect(context: Context, index: Int) = runBlocking {
+        context.dataStore.edit { preferences ->
+            val newValue = optionsValue.getOrElse(index) { optionsValue.first() }
+            preferences[stringPreferencesKey(key)] = newValue
+        }
+    }
+}
+
 
 @Composable
 private fun dependOn(key: String, required: Boolean = true): Boolean {
