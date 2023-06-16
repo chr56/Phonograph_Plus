47a4dae9bb183fd3afcd29b2fe8ca1ea2b0d964f
chr56 <chr0056@gmail.com>
2023-06-16 21:19:38 +0800

    [ResolverUtil] read `ALBUM_ARTIST` & `COMPOSER`

--- app/src/main/java/player/phonograph/mediastore/ResolverUtil.kt
+++ app/src/main/java/player/phonograph/mediastore/ResolverUtil.kt
@@ -57,6 +57,8 @@ fun readSong(cursor: Cursor): Song {
     val albumName = cursor.getString(9)
     val artistId = cursor.getLong(10)
     val artistName = cursor.getString(11)
+    val albumArtist = cursor.getString(12)
+    val composer = cursor.getString(13)
     return Song(
         id = id,
         title = title,
