9ae70626351951156a85eff02dce43169e401218
chr56 <chr0056@gmail.com>
2023-06-17 11:39:15 +0800

    [PlaylistsManagement] cleanup

--- app/src/main/java/player/phonograph/mechanism/PlaylistsManagement.kt
+++ app/src/main/java/player/phonograph/mechanism/PlaylistsManagement.kt
@@ -15,7 +15,6 @@ import player.phonograph.model.playlist.FilePlaylist
 import player.phonograph.model.sort.SortRef
 import player.phonograph.settings.Setting
 import androidx.documentfile.provider.DocumentFile
-import android.annotation.SuppressLint
 import android.content.ContentUris
 import android.content.Context
 import android.database.Cursor
@@ -26,75 +25,66 @@ import android.provider.MediaStore
 import android.util.Log
 
 object PlaylistsManagement {
-    private const val TAG: String = "PlaylistUtil"
+    private const val TAG: String = "PlaylistsManagement"
 
-    fun getAllPlaylists(context: Context): List<FilePlaylist> {
-        return getAllPlaylists(
-            queryPlaylists(context, null, null)
-        ).sortAll()
-    }
+    fun getAllPlaylists(context: Context): List<FilePlaylist> =
+        queryPlaylists(context, null, null).intoPlaylists().sortAll()
 
-    fun getAllPlaylists(cursor: Cursor?): List<FilePlaylist> {
-        val filePlaylists: MutableList<FilePlaylist> = ArrayList()
-        if (cursor != null && cursor.moveToFirst()) {
-            do {
-                filePlaylists.add(
-                    FilePlaylist(
-                        id = cursor.getLong(0),
-                        name = cursor.getString(1),
-                        path = cursor.getString(2),
-                        dateAdded = cursor.getLong(3),
-                        dateModified = cursor.getLong(4),
+    /**
+     * consume cursor (read & close) and convert into FilePlaylist list
+     */
+    private fun Cursor?.intoPlaylists(): List<FilePlaylist> =
+        this?.use {
+            val filePlaylists = mutableListOf<FilePlaylist>()
+            if (moveToFirst()) {
+                do {
+                    filePlaylists.add(
+                        FilePlaylist(
+                            id = getLong(0),
+                            name = getString(1),
+                            path = getString(2),
+                            dateAdded = getLong(3),
+                            dateModified = getLong(4),
+                        )
                     )
-                )
-            } while (cursor.moveToNext())
-        }
-        cursor?.close()
-        return filePlaylists
-    }
+                } while (moveToNext())
+            }
+            filePlaylists
+        } ?: emptyList()
 
-    fun getPlaylist(context: Context, playlistId: Long): FilePlaylist {
-        return getPlaylist(
-            queryPlaylists(
-                context, BaseColumns._ID + "=?", arrayOf(playlistId.toString())
-            )
-        )
-    }
+    fun getPlaylist(context: Context, playlistId: Long): FilePlaylist =
+        queryPlaylists(
+            context, BaseColumns._ID + "=?", arrayOf(playlistId.toString())
+        ).intoFirstPlaylist()
 
-    fun getPlaylist(context: Context, playlistName: String): FilePlaylist {
-        return getPlaylist(
-            queryPlaylists(
-                context, PlaylistsColumns.NAME + "=?", arrayOf(playlistName)
-            )
-        )
-    }
+    fun getPlaylist(context: Context, playlistName: String): FilePlaylist =
+        queryPlaylists(
+            context, PlaylistsColumns.NAME + "=?", arrayOf(playlistName)
+        ).intoFirstPlaylist()
 
-    fun getPlaylist(cursor: Cursor?): FilePlaylist {
-        val playlist =
-            if (cursor != null && cursor.moveToFirst())
+    fun Cursor?.intoFirstPlaylist(): FilePlaylist {
+        return this?.use {
+            if (moveToFirst())
                 FilePlaylist(
-                    id = cursor.getLong(0),
-                    name = cursor.getString(1),
-                    path = cursor.getString(2),
-                    dateAdded = cursor.getLong(3),
-                    dateModified = cursor.getLong(4),
+                    id = getLong(0),
+                    name = getString(1),
+                    path = getString(2),
+                    dateAdded = getLong(3),
+                    dateModified = getLong(4),
                 )
             else
                 FilePlaylist.EMPTY_PLAYLIST
-        cursor?.close()
-        return playlist
+        } ?: FilePlaylist.EMPTY_PLAYLIST
     }
 
     /**
      * query playlist file via MediaStore
      */
-    @SuppressLint("Recycle")
     fun queryPlaylists(
         context: Context,
         selection: String?,
         selectionValues: Array<String>?,
     ): Cursor? {
-
         val actual =
             withPathFilter(context) {
                 SQLWhereClause(
@@ -102,25 +92,22 @@ object PlaylistsManagement {
                     selectionValues = selectionValues ?: emptyArray()
                 )
             }
-
-
-        val cursor: Cursor? = try {
-            context.contentResolver.query(
-                Playlists.EXTERNAL_CONTENT_URI,
-                arrayOf(
-                    BaseColumns._ID, // 0
-                    PlaylistsColumns.NAME, // 1
-                    PlaylistsColumns.DATA, // 2
-                    PlaylistsColumns.DATE_ADDED, // 3
-                    PlaylistsColumns.DATE_MODIFIED, // 4
-                ),
-                actual.selection, actual.selectionValues, Playlists.DEFAULT_SORT_ORDER
-            )
-        } catch (e: SecurityException) {
-            null
-        }
-        return cursor
+        return context.contentResolver.query(
+            Playlists.EXTERNAL_CONTENT_URI,
+            BASE_PLAYLIST_PROJECTION,
+            actual.selection,
+            actual.selectionValues,
+            Playlists.DEFAULT_SORT_ORDER
+        )
     }
+
+    private val BASE_PLAYLIST_PROJECTION = arrayOf(
+        BaseColumns._ID, // 0
+        PlaylistsColumns.NAME, // 1
+        PlaylistsColumns.DATA, // 2
+        PlaylistsColumns.DATE_ADDED, // 3
+        PlaylistsColumns.DATE_MODIFIED, // 4
+    )
 
     fun getPlaylistPath(context: Context, filePlaylist: FilePlaylist): String {
         val cursor = context.contentResolver.query(
@@ -133,11 +120,10 @@ object PlaylistsManagement {
             "${BaseColumns._ID} = ? AND ${PlaylistsColumns.NAME} = ?",
             arrayOf(filePlaylist.id.toString(), filePlaylist.name), null
         )
-        var path: String = "-"
-        cursor?.let {
+        var path = "-"
+        cursor?.use {
             it.moveToFirst()
             path = it.getString(2)
-            it.close()
         }
         return path
     }
@@ -149,29 +135,21 @@ object PlaylistsManagement {
     fun doesPlaylistExist(context: Context, playlistId: Long): Boolean =
         playlistId != -1L && doesPlaylistExistImp(context, Playlists._ID + "=?", arrayOf(playlistId.toString()))
 
-    private fun doesPlaylistExistImp(context: Context, selection: String, values: Array<String>): Boolean {
-        val cursor = context.contentResolver
+    private fun doesPlaylistExistImp(context: Context, selection: String, values: Array<String>): Boolean =
+        context.contentResolver
             .query(Playlists.EXTERNAL_CONTENT_URI, arrayOf(), selection, values, null)
-        var exists = false
-        if (cursor != null) {
-            exists = cursor.count != 0
-            cursor.close()
-        }
-        return exists
-    }
+            ?.use { it.count >= 0 } ?: false
 
     fun getNameForPlaylist(context: Context, id: Long): String {
-        try {
-            val cursor = context.contentResolver.query(
-                ContentUris.withAppendedId(Playlists.EXTERNAL_CONTENT_URI, id),
-                arrayOf(PlaylistsColumns.NAME), null, null, null
-            )
-            if (cursor != null && cursor.moveToFirst()) {
-                cursor.use { return it.getString(0).orEmpty() }
-            }
-        } catch (ignored: SecurityException) {
+        val cursor = context.contentResolver.query(
+            ContentUris.withAppendedId(Playlists.EXTERNAL_CONTENT_URI, id),
+            arrayOf(PlaylistsColumns.NAME), null, null, null
+        ) ?: return ""
+        return if (cursor.moveToFirst()) {
+            cursor.use { it.getString(0).orEmpty() }
+        } else {
+            ""
         }
-        return ""
     }
 
     /**
@@ -190,19 +168,20 @@ object PlaylistsManagement {
             null, null, null
         )
 
-        val displayNames: MutableList<String> = ArrayList()
-        if (cursor != null && cursor.moveToFirst()) {
-            do {
-                ids.forEach { id ->
-                    if (cursor.getLong(0) == id) {
-                        displayNames.add(cursor.getString(1))
+        val displayNames = mutableListOf<String>()
+        cursor?.use {
+            if (cursor.moveToFirst()) {
+                do {
+                    ids.forEach { id ->
+                        if (cursor.getLong(0) == id) {
+                            displayNames.add(cursor.getString(1))
+                        }
                     }
-                }
-            } while (cursor.moveToNext())
-            cursor.close()
+                } while (cursor.moveToNext())
+            }
         }
 
-        return if (displayNames.isNotEmpty()) displayNames else ArrayList()
+        return displayNames.ifEmpty { emptyList() }
     }
 
     fun getPlaylistUris(filePlaylist: FilePlaylist): Uri = getPlaylistUris(filePlaylist.id)
@@ -216,43 +195,32 @@ object PlaylistsManagement {
 
     fun doesPlaylistContain(context: Context, playlistId: Long, songId: Long): Boolean {
         if (playlistId != -1L) {
-            try {
-                val c = context.contentResolver.query(
-                    getPlaylistUris(playlistId),
-                    arrayOf(Playlists.Members.AUDIO_ID),
-                    Playlists.Members.AUDIO_ID + "=?", arrayOf(songId.toString()), null
-                )
-                var count = 0
-                if (c != null) {
-                    count = c.count
-                    c.close()
-                }
-                return count > 0
-            } catch (ignored: SecurityException) {
-            }
+            val cursor = context.contentResolver.query(
+                getPlaylistUris(playlistId),
+                arrayOf(Playlists.Members.AUDIO_ID),
+                Playlists.Members.AUDIO_ID + "=?", arrayOf(songId.toString()), null
+            ) ?: return false
+            return cursor.use { it.count > 0 }
         }
         return false
     }
 
 
-    fun searchPlaylist(context: Context, path: String): FilePlaylist? {
-        return getPlaylist(
-            queryPlaylists(context, "${PlaylistsColumns.DATA} = ?", arrayOf(path))
-        ).takeIf { it.id > 0 }
-    }
+    fun searchPlaylist(context: Context, path: String): FilePlaylist? =
+        queryPlaylists(context, "${PlaylistsColumns.DATA} = ?", arrayOf(path)).intoFirstPlaylist().takeIf { it.id > 0 }
 
     fun searchPlaylist(context: Context, dir: DocumentFile, filePlaylists: List<FilePlaylist>): List<DocumentFile> {
         val fileNames = getPlaylistFileNames(context, filePlaylists)
         if (fileNames.isEmpty()) {
             Log.w(TAG, "No playlist display name?")
-            return ArrayList()
+            return mutableListOf()
         }
         return searchFiles(context, dir, fileNames)
     }
 
     fun searchFiles(context: Context, dir: DocumentFile, filenames: List<String>): List<DocumentFile> {
-        if (dir.isFile or dir.isVirtual) return ArrayList()
-        val result: MutableList<DocumentFile> = ArrayList(1)
+        if (dir.isFile or dir.isVirtual) return mutableListOf()
+        val result = mutableListOf<DocumentFile>()
         if (dir.isDirectory) {
             dir.listFiles().forEach { sub ->
                 if (sub.isFile) {
