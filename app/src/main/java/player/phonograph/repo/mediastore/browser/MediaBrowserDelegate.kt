0c32e07ac4dd1d4c0c4af6864718ad0ab4f1306f
chr56 <chr0056@gmail.com>
2023-07-17 22:21:39 +0800

    [MediaBrowserDelegate] handle parsing mediaId

--- app/src/main/java/player/phonograph/repo/mediastore/browser/MediaBrowserDelegate.kt
+++ app/src/main/java/player/phonograph/repo/mediastore/browser/MediaBrowserDelegate.kt
@@ -4,6 +4,10 @@
 
 package player.phonograph.repo.mediastore.browser
 
+import player.phonograph.model.Song
+import player.phonograph.repo.mediastore.loaders.AlbumLoader
+import player.phonograph.repo.mediastore.loaders.ArtistLoader
+import player.phonograph.repo.mediastore.loaders.SongLoader
 import androidx.media.MediaBrowserServiceCompat.BrowserRoot
 import android.content.Context
 import android.os.Bundle
@@ -39,12 +43,25 @@ object MediaBrowserDelegate {
         }
     }
 
-    fun playFromMediaId(mediaId: String?, extras: Bundle?): Any? {
-        if (mediaId == null) return null
+    fun playFromMediaId(context: Context, mediaId: String, extras: Bundle?): List<Song> {
 
         val fragments = mediaId.split(MEDIA_BROWSER_SEPARATOR, limit = 2)
 
+        if (fragments.size != 2) {
+            Log.e(TAG, "Failed to parse: $mediaId")
+            return emptyList()
+        }
 
-        return Any()
+        val type = fragments[0]
+        val id = fragments[1].toLongOrNull() ?: return emptyList()
+
+        val songs = when (type) {
+            MEDIA_BROWSER_SONGS   -> listOf(SongLoader.id(context, id))
+            MEDIA_BROWSER_ALBUMS  -> AlbumLoader.id(context, id).songs
+            MEDIA_BROWSER_ARTISTS -> ArtistLoader.id(context, id).songs
+            else                  -> emptyList()
+        }
+
+        return songs
     }
 }
