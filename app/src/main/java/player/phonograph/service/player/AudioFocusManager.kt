3d4e57d8257f2041ac0eb5b8a4624d17ba03bb31
chr56 <chr0056@gmail.com>
2023-07-31 11:45:53 +0800

    [AudioFocusManager] cleanup

--- app/src/main/java/player/phonograph/service/player/AudioFocusManager.kt
+++ app/src/main/java/player/phonograph/service/player/AudioFocusManager.kt
@@ -1,54 +1,55 @@
 package player.phonograph.service.player
 
-import android.media.AudioFocusRequest
-import android.media.AudioManager
-import android.os.Build
-import androidx.media.MediaBrowserServiceCompat
-import kotlin.LazyThreadSafetyMode.NONE
-import player.phonograph.App
 import player.phonograph.service.player.PlayerController.Companion.PAUSE_FOR_TRANSIENT_LOSS_OF_FOCUS
 import player.phonograph.service.player.PlayerController.ControllerHandler.Companion.DUCK
 import player.phonograph.service.player.PlayerController.ControllerHandler.Companion.UNDUCK
+import androidx.annotation.RequiresApi
+import android.content.Context.AUDIO_SERVICE
+import android.media.AudioFocusRequest
+import android.media.AudioManager
+import android.os.Build.VERSION.SDK_INT
+import android.os.Build.VERSION_CODES.O
+import kotlin.LazyThreadSafetyMode.NONE
 
 /**
  * @author chr_56 & Abou Zeid (kabouzeid) (original author)
  */
 @Suppress("DEPRECATION")
 class AudioFocusManager(private val controller: PlayerController) {
-    private val audioManager: AudioManager by lazy {
-        App.instance.getSystemService(MediaBrowserServiceCompat.AUDIO_SERVICE) as AudioManager
+
+    private val audioManager: AudioManager by lazy(NONE) {
+        controller.service.getSystemService(AUDIO_SERVICE) as AudioManager
     }
+
     fun requestAudioFocus(): Boolean {
-        val result = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-            audioManager.requestAudioFocus(audioFocusRequest!!)
+        val result = if (SDK_INT >= O) {
+            audioManager.requestAudioFocus(audioFocusRequest)
         } else {
-            audioManager.requestAudioFocus(
-                audioFocusListener,
-                AudioManager.STREAM_MUSIC,
-                AudioManager.AUDIOFOCUS_GAIN
-            )
+            audioManager.requestAudioFocus(audioFocusListener, AudioManager.STREAM_MUSIC, AudioManager.AUDIOFOCUS_GAIN)
         }
         return result == AudioManager.AUDIOFOCUS_GAIN
     }
+
     fun abandonAudioFocus() {
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-            audioManager.abandonAudioFocusRequest(audioFocusRequest!!)
+        if (SDK_INT >= O) {
+            audioManager.abandonAudioFocusRequest(audioFocusRequest)
         } else {
             audioManager.abandonAudioFocus(audioFocusListener)
         }
     }
-    private val audioFocusRequest: AudioFocusRequest? by lazy(NONE) {
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-            AudioFocusRequest.Builder(AudioManager.AUDIOFOCUS_GAIN).setOnAudioFocusChangeListener(
-                audioFocusListener
-            ).build()
-        } else { null }
+
+    @delegate:RequiresApi(O)
+    private val audioFocusRequest: AudioFocusRequest by lazy(NONE) {
+        AudioFocusRequest.Builder(AudioManager.AUDIOFOCUS_GAIN)
+            .setOnAudioFocusChangeListener(audioFocusListener)
+            .build()
     }
+
 
     private val audioFocusListener: AudioManager.OnAudioFocusChangeListener by lazy(NONE) {
         AudioManager.OnAudioFocusChangeListener { focusChange ->
             when (focusChange) {
-                AudioManager.AUDIOFOCUS_GAIN -> {
+                AudioManager.AUDIOFOCUS_GAIN                    -> {
                     if (!controller.isPlaying() && controller.pauseReason == PAUSE_FOR_TRANSIENT_LOSS_OF_FOCUS) {
                         // restore playing
                         controller.play()
@@ -56,11 +57,13 @@ class AudioFocusManager(private val controller: PlayerController) {
                     controller.handler.removeMessages(DUCK)
                     controller.handler.sendEmptyMessage(UNDUCK)
                 }
-                AudioManager.AUDIOFOCUS_LOSS -> {
+
+                AudioManager.AUDIOFOCUS_LOSS                    -> {
                     // Lost focus for an unbounded amount of time: stop playback and release media playback
                     controller.pause()
                 }
-                AudioManager.AUDIOFOCUS_LOSS_TRANSIENT -> {
+
+                AudioManager.AUDIOFOCUS_LOSS_TRANSIENT          -> {
                     // Lost focus for a short time, but we have to stop
                     // playback. We don't release the media playback because playback
                     // is likely to resume
@@ -69,6 +72,7 @@ class AudioFocusManager(private val controller: PlayerController) {
                         controller.pauseReason = PAUSE_FOR_TRANSIENT_LOSS_OF_FOCUS
                     }
                 }
+
                 AudioManager.AUDIOFOCUS_LOSS_TRANSIENT_CAN_DUCK -> {
                     // Lost focus for a short time, but it's ok to keep playing
                     // at an attenuated level
