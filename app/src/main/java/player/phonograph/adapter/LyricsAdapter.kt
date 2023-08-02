f506159a6e643495065fd7894ba5d8d95c650423
chr56 <chr0056@gmail.com>
2023-08-02 10:23:26 +0800

    [LyricsAdapter] cleanup

--- app/src/main/java/player/phonograph/adapter/LyricsAdapter.kt
+++ app/src/main/java/player/phonograph/adapter/LyricsAdapter.kt
@@ -4,69 +4,87 @@ import player.phonograph.R
 import player.phonograph.service.MusicPlayerRemote
 import player.phonograph.settings.Setting
 import androidx.recyclerview.widget.RecyclerView
+import android.annotation.SuppressLint
 import android.app.Activity
 import android.app.Dialog
+import android.content.Context
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
 import java.util.regex.Pattern
 
-class LyricsAdapter(private val context: Activity, stamps: IntArray, lines: Array<String>, private val callbackDialog: Dialog? = null) :
-    RecyclerView.Adapter<LyricsAdapter.VH>() {
+class LyricsAdapter(
+    private val context: Activity,
+    stamps: IntArray,
+    lines: Array<String>,
+    private val callbackDialog: Dialog? = null,
+) : RecyclerView.Adapter<LyricsAdapter.ViewHolder>() {
+
     private var lyrics = lines
     private var timeStamps = stamps
 
-    inner class VH(itemView: View) : RecyclerView.ViewHolder(itemView) {
-        val line: TextView = itemView.findViewById(R.id.dialog_lyrics_line)
-        val time: TextView = itemView.findViewById(R.id.dialog_lyrics_times)
-    }
-
+    @SuppressLint("NotifyDataSetChanged")
     fun update(stamps: IntArray, lines: Array<String>) {
         lyrics = lines
         timeStamps = stamps
         notifyDataSetChanged()
     }
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): VH {
-        return VH(LayoutInflater.from(parent.context).inflate(R.layout.item_lyrics, parent, false))
-    }
+    class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        val line: TextView = itemView.findViewById(R.id.dialog_lyrics_line)
+        val time: TextView = itemView.findViewById(R.id.dialog_lyrics_times)
 
-    override fun onBindViewHolder(holder: VH, position: Int) {
-        // parse line feed
-        val b = StringBuffer()
-        lyrics[position].split(Pattern.compile("\\\\[nNrR]")).forEach {
-            b.append(it).appendLine()
+        fun bind(context: Context, lyrics: Array<String>, timeStamps: IntArray, dismiss: (() -> Unit)?) {
+            // parse line feed
+            val b = StringBuffer()
+            lyrics[bindingAdapterPosition].split(Pattern.compile("\\\\[nNrR]")).forEach {
+                b.append(it).appendLine()
+            }
+
+            time.text = parseTimeStamp(timeStamps[bindingAdapterPosition])
+            time.setTextColor(context.getColor(R.color.dividerColor))
+            if (timeStamps[bindingAdapterPosition] < 0 || !Setting.instance.displaySynchronizedLyricsTimeAxis)
+                time.visibility = View.GONE
+
+            line.text = b.trim().toString()
+
+            line.setOnLongClickListener {
+                MusicPlayerRemote.seekTo(timeStamps[bindingAdapterPosition])
+                dismiss?.invoke()
+                true
+            }
         }
 
-        holder.time.text = parseTimeStamp(timeStamps[position])
-        holder.time.setTextColor(context.resources.getColor(R.color.dividerColor))
-        if (timeStamps[position] < 0 || !Setting.instance.displaySynchronizedLyricsTimeAxis)
-            holder.time.visibility = View.GONE
 
-        holder.line.text = b.trim().toString()
+        companion object {
+            fun inflate(context: Context, parent: ViewGroup) =
+                ViewHolder(LayoutInflater.from(context).inflate(R.layout.item_lyrics, parent, false))
+        }
+    }
 
-        holder.line.setOnLongClickListener {
-            MusicPlayerRemote.seekTo(timeStamps[position])
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder = ViewHolder.inflate(context, parent)
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.bind(context, lyrics, timeStamps) {
             callbackDialog?.dismiss()
-            true
         }
     }
 
-    override fun getItemCount(): Int {
-        return lyrics.size
-    }
+    override fun getItemCount(): Int = lyrics.size
 
-    /**
-     * convert a timestamp to a readable String
-     *
-     * @param t timeStamp to parse (Unit: milliseconds)
-     * @return human-friendly time
-     */
-    private fun parseTimeStamp(t: Int): String {
-        val ms = (t % 1000).toLong()
-        val s = (t % (1000 * 60) / 1000).toLong()
-        val m = (t - s * 1000 - ms) / (1000 * 60)
-        return String.format("%d:%02d.%03d", m, s, ms)
+    companion object {
+        /**
+         * convert a timestamp to a readable String
+         *
+         * @param t timeStamp to parse (Unit: milliseconds)
+         * @return human-friendly time
+         */
+        private fun parseTimeStamp(t: Int): String {
+            val ms = (t % 1000).toLong()
+            val s = (t % (1000 * 60) / 1000).toLong()
+            val m = (t - s * 1000 - ms) / (1000 * 60)
+            return String.format("%d:%02d.%03d", m, s, ms)
+        }
     }
 }
