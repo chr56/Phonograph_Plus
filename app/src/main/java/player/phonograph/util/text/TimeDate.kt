217bf5f03f1397f34970e88f5a1f0e2d8ef2c115
chr56 <chr0056@gmail.com>
2023-08-02 21:34:19 +0800

    [TimeDateUtil] cleanup

--- app/src/main/java/player/phonograph/util/text/TimeDate.kt
+++ app/src/main/java/player/phonograph/util/text/TimeDate.kt
@@ -16,15 +16,27 @@ fun currentTimestamp(): Long = currentDate().time
 
 fun currentDateTime(): CharSequence = DateFormat.format("yyMMdd_HHmmss", currentDate())
 
+private fun lazyFormatter(pattern: String, locale: Locale = Locale.getDefault()) =
+    lazy(LazyThreadSafetyMode.NONE) { SimpleDateFormat(pattern, locale) }
+
 fun date(stamp: Long) = Date(stamp * 1000)
-fun dateText(stamp: Long) = SimpleDateFormat("yyyy.MM.dd", Locale.getDefault()).format(date(stamp))
-fun timeText(stamp: Long) = SimpleDateFormat("HH:mm:ss", Locale.getDefault()).format(date(stamp))
 
-fun shortDateText(timestamp: Long): String = SimpleDateFormat("yy.MM.dd", Locale.getDefault()).format(date(timestamp))
-fun fullDateText(timestamp: Long): String = SimpleDateFormat("yyyy.MM.dd HH:mm", Locale.getDefault()).format(date(timestamp))
+private val formatterDate by lazyFormatter("yyyy.MM.dd")
+private val formatterTime by lazyFormatter("HH:mm:ss")
+private val formatterDateShort by lazyFormatter("yy.MM.dd")
+private val formatterDateTime by lazyFormatter("yyyy.MM.dd HH:mm")
+private val formatterFileSuffix by lazyFormatter("_yy-MM-dd_HH-mm", Locale.US)
+private val formatterFileSuffixCompat by lazyFormatter("yyMMdd_HHmmss", Locale.US)
 
-fun datetimeSuffix(date: Date): CharSequence = SimpleDateFormat("_yy-MM-dd_HH-mm", Locale.US).format(date)
+fun dateText(timestamp: Long): String = formatterDate.format(timestamp * 1000)
+fun timeText(timestamp: Long): String = formatterTime.format(timestamp * 1000)
+
+fun shortDateText(timestamp: Long): String = formatterDateShort.format(timestamp * 1000)
+fun fullDateText(timestamp: Long): String = formatterDateTime.format(timestamp * 1000)
+
+fun datetimeSuffix(date: Date): String = formatterFileSuffix.format(date)
 fun withDatetimeSuffix(string: String, date: Date): String = string + datetimeSuffix(date)
+fun datetimeSuffixCompat(timestamp: Long): CharSequence = formatterFileSuffixCompat.format(timestamp)
 
 /**
  * convert a timestamp to a readable String
