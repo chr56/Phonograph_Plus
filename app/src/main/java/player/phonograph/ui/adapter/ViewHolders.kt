47bea03260f570838e21e235852148084d15b5f7
chr56 <chr0056@gmail.com>
2023-08-07 18:10:42 +0800

    [ViewHolders] exposure views from binding

--- app/src/main/java/player/phonograph/ui/adapter/ViewHolders.kt
+++ app/src/main/java/player/phonograph/ui/adapter/ViewHolders.kt
@@ -13,28 +13,102 @@ import androidx.recyclerview.widget.RecyclerView
 import androidx.viewbinding.ViewBinding
 import android.content.Context
 import android.view.LayoutInflater
+import android.view.View
+import android.widget.ImageView
+import android.widget.TextView
 
 abstract class BindingViewHolder<VB : ViewBinding>
 protected constructor(val binding: VB) : RecyclerView.ViewHolder(binding.root) {
     protected val context: Context = itemView.context
+
+    protected open var image: ImageView? = null
+    protected open var imageText: TextView? = null
+    protected open var title: TextView? = null
+    protected open var text: TextView? = null
+    protected open var menu: View? = null
+    protected open var separator: View? = null
+    protected open var shortSeparator: View? = null
+    protected open var dragView: View? = null
+    protected open var paletteColorContainer: View? = null
 }
 
 class ListViewHolder(context: Context) : BindingViewHolder<ItemListBinding>(
     ItemListBinding.inflate(LayoutInflater.from(context))
-) {}
+) {
+    init {
+        image = binding.image
+        imageText = binding.imageText
+        title = binding.title
+        text = binding.text
+        menu = binding.menu
+        separator = binding.separator
+        shortSeparator = binding.shortSeparator
+        dragView = binding.dragView
+        // paletteColorContainer = binding.paletteColorContainer
+    }
+}
 
 class SingleRowListViewHolder(context: Context) : BindingViewHolder<ItemListSingleRowBinding>(
     ItemListSingleRowBinding.inflate(LayoutInflater.from(context))
-) {}
+) {
+    init {
+        image = binding.image
+        imageText = binding.imageText
+        title = binding.title
+        // text = binding.text
+        menu = binding.menu
+        separator = binding.separator
+        shortSeparator = binding.shortSeparator
+        dragView = binding.dragView
+        // paletteColorContainer = binding.paletteColorContainer
+    }
+}
 
 class NoImageListViewHolder(context: Context) : BindingViewHolder<ItemListNoImageBinding>(
     ItemListNoImageBinding.inflate(LayoutInflater.from(context))
-) {}
+) {
+    init {
+        // image = binding.image
+        // imageText = binding.imageText
+        title = binding.title
+        text = binding.text
+        menu = binding.menu
+        separator = binding.separator
+        shortSeparator = binding.shortSeparator
+        dragView = binding.dragView
+        // paletteColorContainer = binding.paletteColorContainer
+    }
+}
 
 class GridViewHolder(context: Context) : BindingViewHolder<ItemGridBinding>(
     ItemGridBinding.inflate(LayoutInflater.from(context))
-) {}
+) {
+    init {
+        image = binding.image
+        // imageText = binding.imageText
+        title = binding.title
+        text = binding.text
+        // menu = binding.menu
+        // separator = binding.separator
+        // shortSeparator = binding.shortSeparator
+        // dragView = binding.dragView
+        paletteColorContainer = binding.paletteColorContainer
+    }
+}
 
 class GridCardHorizontalViewHolder(context: Context) : BindingViewHolder<ItemGridCardHorizontalBinding>(
     ItemGridCardHorizontalBinding.inflate(LayoutInflater.from(context))
-) {}
+) {
+    init {
+        image = binding.image
+        // imageText = binding.imageText
+        title = binding.title
+        text = binding.text
+        // menu = binding.menu
+        // separator = binding.separator
+        // shortSeparator = binding.shortSeparator
+        // dragView = binding.dragView
+        // paletteColorContainer = binding.paletteColorContainer
+    }
+
+}
