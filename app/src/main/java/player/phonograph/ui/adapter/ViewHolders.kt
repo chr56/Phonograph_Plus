5685bfb3ed4a11459f170f05bc13a176a72b4a01
chr56 <chr0056@gmail.com>
2023-08-07 18:15:36 +0800

    [ViewHolders] exposure using methods

--- app/src/main/java/player/phonograph/ui/adapter/ViewHolders.kt
+++ app/src/main/java/player/phonograph/ui/adapter/ViewHolders.kt
@@ -9,6 +9,7 @@ import player.phonograph.databinding.ItemGridCardHorizontalBinding
 import player.phonograph.databinding.ItemListBinding
 import player.phonograph.databinding.ItemListNoImageBinding
 import player.phonograph.databinding.ItemListSingleRowBinding
+import player.phonograph.ui.views.IconImageView
 import androidx.recyclerview.widget.RecyclerView
 import androidx.viewbinding.ViewBinding
 import android.content.Context
@@ -21,94 +22,83 @@ abstract class BindingViewHolder<VB : ViewBinding>
 protected constructor(val binding: VB) : RecyclerView.ViewHolder(binding.root) {
     protected val context: Context = itemView.context
 
-    protected open var image: ImageView? = null
-    protected open var imageText: TextView? = null
-    protected open var title: TextView? = null
-    protected open var text: TextView? = null
-    protected open var menu: View? = null
-    protected open var separator: View? = null
-    protected open var shortSeparator: View? = null
-    protected open var dragView: View? = null
-    protected open var paletteColorContainer: View? = null
+    protected open val image: ImageView? get() = null
+    protected open val imageText: TextView? get() = null
+    protected open val title: TextView? get() = null
+    protected open val text: TextView? get() = null
+    protected open val menu: View? get() = null
+    protected open val separator: View? get() = null
+    protected open val shortSeparator: View? get() = null
+    protected open val dragView: View? get() = null
+    protected open val paletteColorContainer: View? get() = null
 }
 
 class ListViewHolder(context: Context) : BindingViewHolder<ItemListBinding>(
     ItemListBinding.inflate(LayoutInflater.from(context))
 ) {
-    init {
-        image = binding.image
-        imageText = binding.imageText
-        title = binding.title
-        text = binding.text
-        menu = binding.menu
-        separator = binding.separator
-        shortSeparator = binding.shortSeparator
-        dragView = binding.dragView
-        // paletteColorContainer = binding.paletteColorContainer
-    }
+    override val image: ImageView get() = binding.image
+    override val imageText: TextView get() = binding.imageText
+    override val title: TextView get() = binding.title
+    override val text: TextView get() = binding.text
+    override val menu: IconImageView get() = binding.menu
+    override val separator: View get() = binding.separator
+    override val shortSeparator: View get() = binding.shortSeparator
+    override val dragView: IconImageView get() = binding.dragView
+    // override val paletteColorContainer : View get() = binding.paletteColorContainer
 }
 
 class SingleRowListViewHolder(context: Context) : BindingViewHolder<ItemListSingleRowBinding>(
     ItemListSingleRowBinding.inflate(LayoutInflater.from(context))
 ) {
-    init {
-        image = binding.image
-        imageText = binding.imageText
-        title = binding.title
-        // text = binding.text
-        menu = binding.menu
-        separator = binding.separator
-        shortSeparator = binding.shortSeparator
-        dragView = binding.dragView
-        // paletteColorContainer = binding.paletteColorContainer
-    }
+    override val image: ImageView get() = binding.image
+    override val imageText: TextView get() = binding.imageText
+    override val title: TextView get() = binding.title
+    // override val text: TextView get() = binding.text
+    override val menu: IconImageView get() = binding.menu
+    override val separator: View get() = binding.separator
+    override val shortSeparator: View get() = binding.shortSeparator
+    override val dragView: IconImageView get() = binding.dragView
+    // override val paletteColorContainer : View get() = binding.paletteColorContainer
 }
 
 class NoImageListViewHolder(context: Context) : BindingViewHolder<ItemListNoImageBinding>(
     ItemListNoImageBinding.inflate(LayoutInflater.from(context))
 ) {
-    init {
-        // image = binding.image
-        // imageText = binding.imageText
-        title = binding.title
-        text = binding.text
-        menu = binding.menu
-        separator = binding.separator
-        shortSeparator = binding.shortSeparator
-        dragView = binding.dragView
-        // paletteColorContainer = binding.paletteColorContainer
-    }
+    // override val image: ImageView get() = binding.image
+    // override val imageText: TextView get() = binding.imageText
+    override val title: TextView get() = binding.title
+    override val text: TextView get() = binding.text
+    override val menu: IconImageView get() = binding.menu
+    override val separator: View get() = binding.separator
+    override val shortSeparator: View get() = binding.shortSeparator
+    override val dragView: IconImageView get() = binding.dragView
+    // override val paletteColorContainer : View get() = binding.paletteColorContainer
 }
 
 class GridViewHolder(context: Context) : BindingViewHolder<ItemGridBinding>(
     ItemGridBinding.inflate(LayoutInflater.from(context))
 ) {
-    init {
-        image = binding.image
-        // imageText = binding.imageText
-        title = binding.title
-        text = binding.text
-        // menu = binding.menu
-        // separator = binding.separator
-        // shortSeparator = binding.shortSeparator
-        // dragView = binding.dragView
-        paletteColorContainer = binding.paletteColorContainer
-    }
+    override val image: ImageView get() = binding.image
+    // override val imageText: TextView get() = binding.imageText
+    override val title: TextView get() = binding.title
+    override val text: TextView get() = binding.text
+    // override val menu: IconImageView get() = binding.menu
+    // override val separator: View get() = binding.separator
+    // override val shortSeparator: View get() = binding.shortSeparator
+    // override val dragView: IconImageView get() = binding.dragView
+    override val paletteColorContainer: View get() = binding.paletteColorContainer
 }
 
 class GridCardHorizontalViewHolder(context: Context) : BindingViewHolder<ItemGridCardHorizontalBinding>(
     ItemGridCardHorizontalBinding.inflate(LayoutInflater.from(context))
 ) {
-    init {
-        image = binding.image
-        // imageText = binding.imageText
-        title = binding.title
-        text = binding.text
-        // menu = binding.menu
-        // separator = binding.separator
-        // shortSeparator = binding.shortSeparator
-        // dragView = binding.dragView
-        // paletteColorContainer = binding.paletteColorContainer
-    }
-
+    override val image: ImageView get() = binding.image
+    // override val imageText: TextView get() = binding.imageText
+    override val title: TextView get() = binding.title
+    override val text: TextView get() = binding.text
+    // override val menu: IconImageView get() = binding.menu
+    // override val separator: View get() = binding.separator
+    // override val shortSeparator: View get() = binding.shortSeparator
+    // override val dragView: IconImageView get() = binding.dragView
+    // override val paletteColorContainer : View get() = binding.paletteColorContainer
 }
