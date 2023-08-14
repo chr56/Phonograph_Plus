b89f856242691554dfad57be1046212d328b44d2
chr56 <chr0056@gmail.com>
2023-08-14 17:37:00 +0800

    [SearchResultPageAdapter] setup recyclerview in fragment

--- app/src/main/java/player/phonograph/ui/activities/SearchResultPageAdapter.kt
+++ app/src/main/java/player/phonograph/ui/activities/SearchResultPageAdapter.kt
@@ -9,11 +9,14 @@ import player.phonograph.databinding.FragmentMainActivityRecyclerViewBinding
 import androidx.annotation.StringRes
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import androidx.lifecycle.lifecycleScope
+import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.viewpager2.adapter.FragmentStateAdapter
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import kotlinx.coroutines.launch
 
 class SearchResultPageAdapter(
     searchActivity: SearchActivity,
@@ -32,10 +35,43 @@ class SearchResultPageAdapter(
 
         val viewModel: SearchActivityViewModel by viewModels(ownerProducer = { requireActivity() })
 
+        private lateinit var adapter: SearchResultAdapter
+
         override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
             _viewBinding = FragmentMainActivityRecyclerViewBinding.inflate(inflater, container, false)
             return binding.root
         }
+
+        override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+            super.onViewCreated(view, savedInstanceState)
+            val activity = requireActivity()
+            adapter = SearchResultAdapter(activity)
+            with(binding) {
+                recyclerView.layoutManager = LinearLayoutManager(activity)
+                recyclerView.adapter = adapter
+                // noinspection ClickableViewAccessibility
+                recyclerView.setOnTouchListener { _, _ ->
+                    // hideSoftKeyboard() //todo
+                    false
+                }
+            }
+            val flow = flow()
+            lifecycleScope.launch {
+                flow.collect { data ->
+                    binding.empty.visibility = if (data.isEmpty()) View.VISIBLE else View.GONE
+                    adapter.dataSet = data
+                }
+            }
+        }
+
+        private fun flow() =
+            when (type) {
+                TabType.SONG     -> viewModel.songs
+                TabType.ALBUM    -> viewModel.albums
+                TabType.ARTIST   -> viewModel.artists
+                TabType.PLAYLIST -> viewModel.playlists
+                TabType.QUEUE    -> viewModel.songsInQueue
+            }
 
         override fun onDestroyView() {
             super.onDestroyView()
