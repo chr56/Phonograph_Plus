c3193d7444df3766975c0ba063245f4376cf5958
chr56 <chr0056@gmail.com>
2023-08-15 12:28:20 +0800

    [QueueSong] add `fromQueue`

--- app/src/main/java/player/phonograph/model/QueueSong.kt
+++ app/src/main/java/player/phonograph/model/QueueSong.kt
@@ -8,4 +8,9 @@ import android.os.Parcelable
 import kotlinx.parcelize.Parcelize
 
 @Parcelize
-data class QueueSong(val song: Song, val index: Int) : Displayable by song, Parcelable
+data class QueueSong(val song: Song, val index: Int) : Displayable by song, Parcelable {
+    companion object {
+        fun fromQueue(songs: List<Song>): List<QueueSong> =
+            songs.mapIndexed { index, song -> QueueSong(song, index) }
+    }
+}
