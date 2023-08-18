7794f9f7444b70b90185e53191a38afeb81d087c
chr56 <chr0056@gmail.com>
2023-08-18 13:40:09 +0800

    [MainActivity] add `refresh database`

--- app/src/main/java/player/phonograph/ui/activities/MainActivity.kt
+++ app/src/main/java/player/phonograph/ui/activities/MainActivity.kt
@@ -33,6 +33,7 @@ import player.phonograph.model.pages.Pages
 import player.phonograph.model.version.VersionCatalog
 import player.phonograph.notification.UpgradeNotification
 import player.phonograph.repo.loader.Songs
+import player.phonograph.repo.room.Refresher
 import player.phonograph.service.MusicPlayerRemote
 import player.phonograph.service.queue.CurrentQueueState
 import player.phonograph.service.queue.ShuffleMode
@@ -280,6 +281,9 @@ class MainActivity : AbsSlidingMusicPanelActivity(),
                 titleRes(R.string.more_actions)
                 onClick {
                     val items = listOf(
+                        context.getString(R.string.refresh_database) to {
+                            Refresher.refreshDatabase(context)
+                        },
                         context.getString(R.string.action_grant_storage_permission) to {
                             navigateToStorageSetting(context)
                         },
