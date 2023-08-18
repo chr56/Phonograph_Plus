5b45e51285cf69b95779c94cd182c779497ab093
chr56 <chr0056@gmail.com>
2023-08-18 13:30:10 +0800

    [MusicDatabase] add `Metadata`

--- app/src/main/java/player/phonograph/repo/room/MusicDatabase.kt
+++ app/src/main/java/player/phonograph/repo/room/MusicDatabase.kt
@@ -4,10 +4,12 @@
 
 package player.phonograph.repo.room
 
+import player.phonograph.App
 import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
-import player.phonograph.App
+import android.content.Context
+import android.content.SharedPreferences
 
 object MusicDatabase {
     private var innerSongsDataBase: SongDataBase? = null
@@ -18,27 +20,45 @@ object MusicDatabase {
             App.instance,
             SongDataBase::class.java,
             "musicV1.db"
-        )
-            .allowMainThreadQueries().build()
+        ).allowMainThreadQueries().build()
     }
 
     val songsDataBase: SongDataBase
         get() {
-            if (innerSongsDataBase == null) { initSongDataBase() }
+            if (innerSongsDataBase == null) initSongDataBase()
             return innerSongsDataBase!!
         }
+
+
+    object Metadata {
+        private const val PREFERENCE_NAME = "database_meta"
+        private val sharedPreferences: SharedPreferences =
+            App.instance.getSharedPreferences(PREFERENCE_NAME, Context.MODE_PRIVATE)
+
+        var lastUpdateTimestamp: Long
+            get() = sharedPreferences.getLong(LAST_MUSIC_DATABASE_UPDATE_TIMESTAMP, -1L)
+            set(value) {
+                sharedPreferences.edit().putLong(LAST_MUSIC_DATABASE_UPDATE_TIMESTAMP, value).apply()
+            }
+        var lastAccessTimestamp: Long
+            get() = sharedPreferences.getLong(LAST_MUSIC_DATABASE_ACCESS_TIMESTAMP, -1L)
+            set(value) {
+                sharedPreferences.edit().putLong(LAST_MUSIC_DATABASE_ACCESS_TIMESTAMP, value).apply()
+            }
+
+        private const val LAST_MUSIC_DATABASE_UPDATE_TIMESTAMP = "last_music_database_update_timestamp"
+        private const val LAST_MUSIC_DATABASE_ACCESS_TIMESTAMP = "last_music_database_access_timestamp"
+    }
 }
 
-@Database(entities = [Song::class, Album::class, Artist::class, SongAndArtistLinkage::class], version = 1, exportSchema = false)
+@Database(
+    entities = [Song::class, Album::class, Artist::class, SongAndArtistLinkage::class],
+    version = 1,
+    exportSchema = false
+)
 abstract class SongDataBase : RoomDatabase() {
     abstract fun SongDao(): SongDao
     abstract fun AlbumDao(): AlbumDAO
     abstract fun ArtistDao(): ArtistDAO
     abstract fun ArtistSongsDao(): ArtistSongDAO
-//    var lastUpdateTimestamp: Long = -1L
-//        get() = Setting.instance.lastMusicDatabaseUpdateTimestamp
-//        set(value) { field = value; Setting.instance.lastMusicDatabaseUpdateTimestamp = value }
-//    var lastAccessTimestamp: Long = -1L
-//        get() = Setting.instance.lastMusicDatabaseAccessTimestamp
-//        set(value) { field = value; Setting.instance.lastMusicDatabaseAccessTimestamp = value }
 }
