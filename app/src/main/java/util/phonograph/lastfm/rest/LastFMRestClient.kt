0755f75e2984030f1ee1a85590a93776799346e6
chr56 <chr0056@gmail.com>
2023-08-21 12:36:02 +0800

    [LastFMRestClient] refactor

--- app/src/main/java/util/phonograph/lastfm/rest/LastFMRestClient.kt
+++ app/src/main/java/util/phonograph/lastfm/rest/LastFMRestClient.kt
@@ -1,72 +1,56 @@
+/*
+ *  Copyright (c) 2022~2023 chr_56, Karim Abou Zeid (kabouzeid)
+ */
+
 package util.phonograph.lastfm.rest
 
-import android.content.Context
 import lib.phonograph.serialization.KtSerializationRetrofitConverterFactory
-import okhttp3.internal.userAgent
 import okhttp3.Cache
+import okhttp3.Call
 import okhttp3.Interceptor
 import okhttp3.MediaType.Companion.toMediaType
 import okhttp3.OkHttpClient
+import okhttp3.internal.format
+import okhttp3.internal.userAgent
 import player.phonograph.BuildConfig
 import retrofit2.Retrofit
 import util.phonograph.lastfm.rest.service.LastFMService
+import android.content.Context
 import java.io.File
-import java.util.*
 
-/**
- * @author Karim Abou Zeid (kabouzeid)
- */
-class LastFMRestClient {
+class LastFMRestClient private constructor(val apiService: LastFMService) {
 
-    val apiService: LastFMService
+    constructor(context: Context) : this(lastFMOkHttpClient(context).build())
 
-    constructor(f: okhttp3.Call.Factory) {
-        val restAdapter = Retrofit.Builder()
-            .baseUrl(BASE_URL)
-            .callFactory(f)
-            .addConverterFactory(KtSerializationRetrofitConverterFactory("application/json".toMediaType()))
-            .build()
-        apiService = restAdapter.create(LastFMService::class.java)
-    }
-    constructor(context: Context) : this(createDefaultOkHttpClientBuilder(context).build())
+    private constructor(client: Call.Factory) : this(restRetrofitAdapter(client))
+
+    private constructor(retrofit: Retrofit) : this(retrofit.create(LastFMService::class.java))
 
     companion object {
 
         private const val BASE_URL = "https://ws.audioscrobbler.com/2.0/"
         private const val USER_AGENT = "$userAgent PhonographPlus/${BuildConfig.VERSION_NAME}"
 
-        fun createDefaultCache(context: Context): Cache? {
-            val cacheDir = File(context.cacheDir.absolutePath, "/okhttp-lastfm/")
-            return if (cacheDir.mkdirs() || cacheDir.isDirectory) {
-                Cache(cacheDir, 1024 * 1024 * 10)
-            } else null
-        }
+        private fun restRetrofitAdapter(client: Call.Factory): Retrofit = Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .callFactory(client)
+            .addConverterFactory(KtSerializationRetrofitConverterFactory("application/json".toMediaType()))
+            .build()
 
-        fun createCacheControlInterceptor(): Interceptor {
-            return Interceptor { chain: Interceptor.Chain ->
+        private fun lastFMOkHttpClient(context: Context): OkHttpClient.Builder = OkHttpClient.Builder()
+            .cache(defaultCache(context))
+            .addInterceptor { chain: Interceptor.Chain ->
                 val modifiedRequest = chain.request().newBuilder()
-                    .addHeader("Cache-Control", String.format(Locale.getDefault(), "max-age=%d, max-stale=%d", 31536000, 31536000))
-                    .build()
-                chain.proceed(modifiedRequest)
-            }
-        }
-
-        fun createUserAgentInterceptor(): Interceptor {
-            return Interceptor { chain: Interceptor.Chain ->
-                val modifiedRequest = chain.request()
-                    .newBuilder()
+                    .addHeader("Cache-Control", format("max-age=%d, max-stale=%d", 31536000, 31536000))
                     .removeHeader("User-Agent")
                     .addHeader("User-Agent", USER_AGENT)
                     .build()
                 chain.proceed(modifiedRequest)
             }
-        }
 
-        fun createDefaultOkHttpClientBuilder(context: Context): OkHttpClient.Builder {
-            return OkHttpClient.Builder()
-                .cache(createDefaultCache(context))
-                .addInterceptor(createCacheControlInterceptor())
-                .addInterceptor(createUserAgentInterceptor())
+        private fun defaultCache(context: Context): Cache? {
+            val cacheDir = File(context.cacheDir.absolutePath, "/okhttp-lastfm/")
+            return if (cacheDir.mkdirs() || cacheDir.isDirectory) Cache(cacheDir, 1024 * 1024 * 10) else null
         }
     }
 }
