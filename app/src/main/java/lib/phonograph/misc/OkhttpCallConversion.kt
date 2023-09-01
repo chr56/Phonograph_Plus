9deb7bc028d2450ef9833146fa4c469eca9f7441
chr56 <chr0056@gmail.com>
2023-09-01 15:46:03 +0800

    [OkhttpCallConversion] use Result

--- app/src/main/java/lib/phonograph/misc/OkhttpCallConversion.kt
+++ app/src/main/java/lib/phonograph/misc/OkhttpCallConversion.kt
@@ -4,29 +4,27 @@
 
 package lib.phonograph.misc
 
-import java.io.IOException
-import kotlin.coroutines.resumeWithException
-import kotlin.jvm.Throws
-import kotlinx.coroutines.ExperimentalCoroutinesApi
-import kotlinx.coroutines.suspendCancellableCoroutine
 import okhttp3.Call
 import okhttp3.Callback
 import okhttp3.Response
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.suspendCancellableCoroutine
+import java.io.IOException
 
 @OptIn(ExperimentalCoroutinesApi::class)
 @Throws(IOException::class)
-suspend fun Call.emit(reportFailure: Boolean = true): Response =
+suspend fun Call.emit(reportFailure: Boolean = true): Result<Response> =
     suspendCancellableCoroutine { continuation ->
         enqueue(object : Callback {
             override fun onResponse(call: Call, response: Response) {
-                continuation.resume(response) { }
+                continuation.resume(Result.success(response)) { }
             }
 
             override fun onFailure(call: Call, e: IOException) {
                 if (!reportFailure || continuation.isCancelled) {
                     continuation.cancel()
                 } else {
-                    continuation.resumeWithException(e)
+                    continuation.resume(Result.failure(e)) { }
                 }
             }
         })
