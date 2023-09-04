6b9df378b2b7babc65536f4aa1602ba5163e4435
chr56 <chr0056@gmail.com>
2023-09-04 19:20:25 +0800

    [WebSearchActivity] rename `parseIntent` -> `checkCommand`

--- app/src/main/java/player/phonograph/ui/compose/web/WebSearchActivity.kt
+++ app/src/main/java/player/phonograph/ui/compose/web/WebSearchActivity.kt
@@ -36,7 +36,7 @@ class WebSearchActivity : ThemeActivity() {
         super.useCustomStatusBar = false
         super.onCreate(savedInstanceState)
 
-        val query = parseIntent(this, intent)
+        val query = checkCommand(this, intent)
         viewModel.prepareQuery(this, query)
 
         setContent {
@@ -75,38 +75,38 @@ class WebSearchActivity : ThemeActivity() {
         }
     }
 
-    private fun parseIntent(context: Context, intent: Intent): Query<*>? {
+    private fun checkCommand(context: Context, intent: Intent): Query<*>? {
         val factory = viewModel.queryFactory
-        return when (intent.getStringExtra(EXTRA_TYPE)) {
-            EXTRA_ALBUM -> intent.parcelableExtra<PhonographAlbum>(EXTRA_DATA)?.let { factory.from(context, it) }
-            EXTRA_ARTIST -> intent.parcelableExtra<PhonographArtist>(EXTRA_DATA)?.let { factory.from(context, it) }
-            EXTRA_SONG -> intent.parcelableExtra<PhonographSong>(EXTRA_DATA)?.let { factory.from(context, it) }
-            else -> null
+        return when (intent.getStringExtra(EXTRA_COMMAND)) {
+            EXTRA_QUERY_ALBUM  -> intent.parcelableExtra<PhonographAlbum>(EXTRA_DATA)?.let { factory.from(context, it) }
+            EXTRA_QUERY_ARTIST -> intent.parcelableExtra<PhonographArtist>(EXTRA_DATA)?.let { factory.from(context, it) }
+            EXTRA_QUERY_SONG   -> intent.parcelableExtra<PhonographSong>(EXTRA_DATA)?.let { factory.from(context, it) }
+            else             -> null
         }
     }
 
     companion object {
-        const val EXTRA_TYPE = "TYPE"
-        const val EXTRA_SONG = "Song"
-        const val EXTRA_ARTIST = "Artist"
-        const val EXTRA_ALBUM = "Album"
+        const val EXTRA_COMMAND = "COMMAND"
+        const val EXTRA_QUERY_SONG = "Query:Song"
+        const val EXTRA_QUERY_ARTIST = "Query:Artist"
+        const val EXTRA_QUERY_ALBUM = "Query:Album"
         const val EXTRA_DATA = "DATA"
 
         fun launchIntent(context: Context, data: PhonographAlbum?): Intent =
             launchIntent(context).apply {
-                putExtra(EXTRA_TYPE, EXTRA_ALBUM)
+                putExtra(EXTRA_COMMAND, EXTRA_QUERY_ALBUM)
                 putExtra(EXTRA_DATA, data)
             }
 
         fun launchIntent(context: Context, data: PhonographArtist?): Intent =
             launchIntent(context).apply {
-                putExtra(EXTRA_TYPE, EXTRA_ARTIST)
+                putExtra(EXTRA_COMMAND, EXTRA_QUERY_ARTIST)
                 putExtra(EXTRA_DATA, data)
             }
 
         fun launchIntent(context: Context, data: PhonographSong?): Intent =
             launchIntent(context).apply {
-                putExtra(EXTRA_TYPE, EXTRA_SONG)
+                putExtra(EXTRA_COMMAND, EXTRA_QUERY_SONG)
                 putExtra(EXTRA_DATA, data)
             }
 
