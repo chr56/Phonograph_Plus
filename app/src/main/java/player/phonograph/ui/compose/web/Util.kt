ae3abea0ce5021341074bb83127b3a41dd8893eb
chr56 <chr0056@gmail.com>
2023-09-05 15:08:17 +0800

    [JumpMusicBrainz] correctly navigate

--- app/src/main/java/player/phonograph/ui/compose/web/Util.kt
+++ app/src/main/java/player/phonograph/ui/compose/web/Util.kt
@@ -5,6 +5,10 @@
 package player.phonograph.ui.compose.web
 
 import player.phonograph.R
+import player.phonograph.ui.compose.web.MusicBrainzQuery.QueryAction.ViewArtist
+import player.phonograph.ui.compose.web.MusicBrainzQuery.QueryAction.ViewRecording
+import player.phonograph.ui.compose.web.MusicBrainzQuery.QueryAction.ViewRelease
+import player.phonograph.ui.compose.web.MusicBrainzQuery.QueryAction.ViewReleaseGroup
 import player.phonograph.ui.compose.web.MusicBrainzQuery.Target
 import player.phonograph.ui.compose.web.WebSearchActivity.Companion.launchIntent
 import player.phonograph.ui.compose.web.WebSearchActivity.Companion.launchIntentMusicBrainzArtist
@@ -16,24 +20,44 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
+import androidx.lifecycle.lifecycleScope
 import android.content.Context
 import android.content.Intent
 import android.net.Uri
+import kotlinx.coroutines.launch
 
 @Composable
 fun JumpMusicBrainz(modifier: Modifier, type: Target, mbid: String?) {
     if (!mbid.isNullOrEmpty()) {
         val context = LocalContext.current
+        val navigator = LocalPageNavigator.current
         TextButton(
             onClick = {
-                context.startActivity(
-                    when (type) {
-                        Target.Artist -> launchIntentMusicBrainzArtist(context, mbid)
-                        Target.Recording -> launchIntentMusicBrainzRecording(context, mbid)
-                        Target.Release -> launchIntentMusicBrainzRelease(context, mbid)
-                        else -> launchIntent(context)
+                if (context is WebSearchActivity && navigator != null) {
+                    context.lifecycleScope.launch {
+                        val query = context.queryFactory.musicBrainzQuery(context)
+                        val result = when (type) {
+                            Target.ReleaseGroup -> query.query(context, ViewReleaseGroup(mbid))
+                            Target.Release      -> query.query(context, ViewRelease(mbid))
+                            Target.Artist       -> query.query(context, ViewArtist(mbid))
+                            Target.Recording    -> query.query(context, ViewRecording(mbid))
+                        }
+                        val page = Page.Detail.MusicBrainzDetail(
+                            result.await() ?: Any()
+                        )
+                        navigator.navigateTo(page)
                     }
-                )
+
+                } else {
+                    context.startActivity(
+                        when (type) {
+                            Target.Artist -> launchIntentMusicBrainzArtist(context, mbid)
+                            Target.Recording -> launchIntentMusicBrainzRecording(context, mbid)
+                            Target.Release -> launchIntentMusicBrainzRelease(context, mbid)
+                            else -> launchIntent(context)
+                        }
+                    )
+                }
             },
             modifier = modifier
         ) {
