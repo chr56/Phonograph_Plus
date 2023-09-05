3ffd9e4d5373251814a2ae4f961168a057350968
chr56 <chr0056@gmail.com>
2023-09-05 12:30:13 +0800

    [WebSearch] reorganize files

--- app/src/main/java/player/phonograph/ui/compose/web/WebSearch.kt
+++ app/src/main/java/player/phonograph/ui/compose/web/WebSearch.kt
@@ -9,28 +9,43 @@ import player.phonograph.ui.compose.web.WebSearchViewModel.Page.Search.LastFmSea
 import player.phonograph.ui.compose.web.WebSearchViewModel.Page.Search.MusicBrainzSearch
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.ColumnScope
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.DrawerState
+import androidx.compose.material.Icon
+import androidx.compose.material.LocalTextStyle
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material.icons.filled.Menu
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
+import kotlinx.coroutines.launch
 
 @Composable
 fun Home(viewModel: WebSearchViewModel, pageState: Page) {
     val navigator = viewModel.navigator
     val context = LocalContext.current
     Column(Modifier.fillMaxSize()) {
-        Item(navigator, LastFmSearch(viewModel.queryFactory.lastFm(context)))
-        Item(navigator, MusicBrainzSearch(viewModel.queryFactory.musicBrainzQuery(context)))
+        HomeItem(navigator, LastFmSearch(viewModel.queryFactory.lastFm(context)))
+        HomeItem(navigator, MusicBrainzSearch(viewModel.queryFactory.musicBrainzQuery(context)))
     }
 }
 
 @Composable
-private fun Item(navigator: WebSearchViewModel.Navigator, page: Page.Search<*>) {
+private fun HomeItem(navigator: WebSearchViewModel.Navigator, page: Page.Search<*>) {
     Text(
         page.source,
         modifier = Modifier
@@ -42,3 +57,73 @@ private fun Item(navigator: WebSearchViewModel.Navigator, page: Page.Search<*>) 
         style = MaterialTheme.typography.h4
     )
 }
+@Composable
+fun NavigateButton(drawerState: DrawerState, navigator: WebSearchViewModel.Navigator) {
+    val pageState by navigator.currentPage.collectAsState()
+    if (pageState.isRoot()) {
+        val coroutineScope = rememberCoroutineScope()
+        Icon(
+            Icons.Default.Menu, null,
+            Modifier.clickable {
+                coroutineScope.launch {
+                    drawerState.open()
+                }
+            }
+        )
+    } else {
+        Icon(
+            Icons.Default.ArrowBack, null,
+            Modifier.clickable {
+                navigator.navigateUp()
+            }
+        )
+    }
+}
+
+@Composable
+fun ColumnScope.Drawer(navigator: WebSearchViewModel.Navigator, viewModel: WebSearchViewModel) {
+    val pageState by navigator.currentPage.collectAsState()
+    CompositionLocalProvider(
+        LocalTextStyle provides MaterialTheme.typography.h6
+    ) {
+        Spacer(
+            modifier = Modifier
+                .height(32.dp)
+                .weight(WEIGHT_SPACE_TOP)
+        )
+        Text(
+            text = stringResource(pageState.nameRes),
+            Modifier
+                .padding(12.dp)
+                .weight(WEIGHT_CURRENT)
+        )
+        Spacer(
+            modifier = Modifier
+                .height(32.dp)
+                .weight(WEIGHT_SPACE_GAP)
+        )
+        val context = LocalContext.current
+        Switcher(navigator, LastFmSearch(viewModel.queryFactory.lastFm(context)))
+        Switcher(navigator, MusicBrainzSearch(viewModel.queryFactory.musicBrainzQuery(context)))
+        Switcher(navigator, Page.Home)
+    }
+}
+
+@Composable
+private fun ColumnScope.Switcher(navigator: WebSearchViewModel.Navigator, page: Page) {
+    val text = "${stringResource(page.nameRes)} ${if (page is Page.Search<*>) page.source else ""}"
+    Text(
+        text,
+        Modifier
+            .clickable {
+                navigator.navigateTo(page)
+            }
+            .padding(12.dp)
+            .weight(WEIGHT_SWITCHER)
+    )
+}
+
+private const val WEIGHT_CURRENT = 2f
+private const val WEIGHT_SWITCHER = 2f
+private const val WEIGHT_SPACE_TOP = 3f
+private const val WEIGHT_SPACE_GAP = 20f
