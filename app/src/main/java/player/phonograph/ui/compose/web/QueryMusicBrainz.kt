a60f6ec4006b89700ee713c2108e3962b0a380d1
chr56 <chr0056@gmail.com>
2023-09-05 15:33:08 +0800

    [MusicBrainzQuery] implement search backend

--- app/src/main/java/player/phonograph/ui/compose/web/QueryMusicBrainz.kt
+++ app/src/main/java/player/phonograph/ui/compose/web/QueryMusicBrainz.kt
@@ -9,6 +9,11 @@ import util.phonograph.tagsources.musicbrainz.MusicBrainzRecording
 import util.phonograph.tagsources.musicbrainz.MusicBrainzRelease
 import util.phonograph.tagsources.musicbrainz.MusicBrainzReleaseGroup
 import util.phonograph.tagsources.musicbrainz.MusicBrainzRestClient
+import util.phonograph.tagsources.musicbrainz.MusicBrainzSearchResult
+import util.phonograph.tagsources.musicbrainz.MusicBrainzSearchResultArtists
+import util.phonograph.tagsources.musicbrainz.MusicBrainzSearchResultRecording
+import util.phonograph.tagsources.musicbrainz.MusicBrainzSearchResultReleases
+import util.phonograph.tagsources.musicbrainz.MusicBrainzSearchResultReleasesGroup
 import util.phonograph.tagsources.musicbrainz.MusicBrainzService
 import android.content.Context
 import kotlinx.coroutines.CoroutineScope
@@ -33,31 +38,83 @@ class MusicBrainzQuery(
         ;
     }
 
-    private val _queryParameter: MutableStateFlow<QueryParameter> = MutableStateFlow(QueryParameter())
+    private val _queryParameter: MutableStateFlow<QueryParameter> = MutableStateFlow(QueryParameter(Target.Release, ""))
     override val queryParameter = _queryParameter.asStateFlow()
     override fun updateQueryParameter(update: (QueryParameter) -> QueryParameter) {
         _queryParameter.update(update)
     }
 
-    class QueryParameter() : Parameter
+    data class QueryParameter(var target: Target, var query: String) : Parameter {
+        fun searchAction(): QueryAction = when (target) {
+            Target.ReleaseGroup -> QueryAction.SearchReleaseGroup(query)
+            Target.Release      -> QueryAction.SearchRelease(query)
+            Target.Artist       -> QueryAction.SearchArtist(query)
+            Target.Recording    -> QueryAction.SearchRecording(query)
+        }
+    }
 
     sealed class QueryAction : Action {
+        data class SearchArtist(val query: String) : QueryAction()
+        data class SearchRelease(val query: String) : QueryAction()
+        data class SearchReleaseGroup(val query: String) : QueryAction()
+        data class SearchRecording(val query: String) : QueryAction()
         data class ViewArtist(val mbid: String) : QueryAction()
         data class ViewRelease(val mbid: String) : QueryAction()
         data class ViewReleaseGroup(val mbid: String) : QueryAction()
         data class ViewRecording(val mbid: String) : QueryAction()
     }
 
+
+    private val _result: MutableStateFlow<MusicBrainzSearchResult?> = MutableStateFlow(null)
+    val result get() = _result.asStateFlow()
+
     //region Query Implementations
     override fun query(context: Context, action: QueryAction): Deferred<*> {
         return musicBrainzQuery { service ->
             when (action) {
-                is QueryAction.ViewReleaseGroup -> viewMusicBrainzReleaseGroup(service, action.mbid)
-                is QueryAction.ViewRelease      -> viewMusicBrainzRelease(service, action.mbid)
-                is QueryAction.ViewArtist       -> viewMusicBrainzArtist(service, action.mbid)
-                is QueryAction.ViewRecording    -> viewMusicBrainzRecording(service, action.mbid)
+                is QueryAction.ViewReleaseGroup   -> viewMusicBrainzReleaseGroup(service, action.mbid)
+                is QueryAction.ViewRelease        -> viewMusicBrainzRelease(service, action.mbid)
+                is QueryAction.ViewArtist         -> viewMusicBrainzArtist(service, action.mbid)
+                is QueryAction.ViewRecording      -> viewMusicBrainzRecording(service, action.mbid)
+                is QueryAction.SearchReleaseGroup -> searchMusicBrainzReleaseGroup(service, action.query)
+                is QueryAction.SearchRelease      -> searchMusicBrainzRelease(service, action.query)
+                is QueryAction.SearchArtist       -> searchMusicBrainzArtists(service, action.query)
+                is QueryAction.SearchRecording    -> searchMusicBrainzRecordings(service, action.query)
             }
         }
+    }
+
+
+    private suspend fun searchMusicBrainzReleaseGroup(
+        service: MusicBrainzService,
+        query: String,
+    ): MusicBrainzSearchResultReleasesGroup? {
+        val call = service.searchReleaseGroup(query, 0)
+        return call.tryExecute().also { _result.tryEmit(it) }
+    }
+
+    private suspend fun searchMusicBrainzRelease(
+        service: MusicBrainzService,
+        query: String,
+    ): MusicBrainzSearchResultReleases? {
+        val call = service.searchRelease(query, 0)
+        return call.tryExecute().also { _result.tryEmit(it) }
+    }
+
+    private suspend fun searchMusicBrainzArtists(
+        service: MusicBrainzService,
+        query: String,
+    ): MusicBrainzSearchResultArtists? {
+        val call = service.searchArtist(query, 0)
+        return call.tryExecute().also { _result.tryEmit(it) }
+    }
+
+    private suspend fun searchMusicBrainzRecordings(
+        service: MusicBrainzService,
+        query: String,
+    ): MusicBrainzSearchResultRecording? {
+        val call = service.searchRecording(query, 0)
+        return call.tryExecute().also { _result.tryEmit(it) }
     }
 
     private suspend fun viewMusicBrainzReleaseGroup(
@@ -91,6 +148,7 @@ class MusicBrainzQuery(
             block.invoke(this, service)
         }
     }
+
     private val musicBrainzRestClient: MusicBrainzRestClient = MusicBrainzRestClient(context)
     private var musicBrainzQueryJob: Job? = null
     //endregion
