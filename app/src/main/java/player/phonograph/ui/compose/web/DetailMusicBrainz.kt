e02e801fe2eb5022959d768e353034cb4c41e3d8
chr56 <chr0056@gmail.com>
2023-09-07 16:12:03 +0800

    [DetailMusicBrainz] make `CascadeItem` collapsible

--- app/src/main/java/player/phonograph/ui/compose/web/DetailMusicBrainz.kt
+++ app/src/main/java/player/phonograph/ui/compose/web/DetailMusicBrainz.kt
@@ -26,7 +26,9 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.ColumnScope
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.RowScope
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.foundation.rememberScrollState
@@ -36,11 +38,16 @@ import androidx.compose.material.Icon
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.KeyboardArrowDown
+import androidx.compose.material.icons.filled.KeyboardArrowRight
 import androidx.compose.material.icons.outlined.ArrowForward
 import androidx.compose.material.icons.outlined.Info
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.painter.Painter
@@ -487,9 +494,11 @@ private fun CascadeVerticalItem(
     modifier: Modifier = Modifier,
     textStyle: TextStyle = LabeledItemLayoutDefault.titleStyle,
     innerColumnModifier: Modifier = Modifier,
+    collapsible: Boolean = true,
+    collapsed: Boolean = false,
     content: @Composable ColumnScope.() -> Unit,
 ) {
-    CascadeItem(modifier.padding(vertical = 8.dp), title, textStyle, Modifier) {
+    CascadeItem(modifier.padding(vertical = 8.dp), title, textStyle, Modifier, collapsible, collapsed) {
         Column(innerColumnModifier.padding(start = 8.dp)) {
             content()
         }
@@ -504,7 +513,7 @@ private fun CascadeHorizontalItem(
     innerRowModifier: Modifier = Modifier,
     content: @Composable RowScope.() -> Unit,
 ) {
-    CascadeItem(modifier.padding(vertical = 4.dp), title, textStyle, Modifier) {
+    CascadeItem(modifier.padding(vertical = 4.dp), title, textStyle, Modifier, collapsible = false, collapsed = false) {
         Row(
             innerRowModifier
                 .padding(horizontal = 8.dp)
@@ -521,18 +530,33 @@ private fun CascadeItem(
     title: String,
     textStyle: TextStyle = LabeledItemLayoutDefault.titleStyle,
     textModifier: Modifier,
+    collapsible: Boolean,
+    collapsed: Boolean,
     content: @Composable () -> Unit,
 ) {
     Column(
         modifier, verticalArrangement = Arrangement.SpaceEvenly
     ) {
-        Text(
-            title,
-            modifier = textModifier
-                .align(Alignment.Start),
-            style = textStyle,
-        )
-        content()
+        var collapseState by remember { mutableStateOf(collapsed) }
+        Row {
+            if (collapsible) {
+                Icon(
+                    if (collapseState) Icons.Default.KeyboardArrowRight else Icons.Default.KeyboardArrowDown,
+                    "Collapse",
+                    Modifier
+                        .align(Alignment.CenterVertically)
+                        .clickable {
+                            collapseState = !collapseState
+                        }
+                )
+            }
+            Text(
+                title,
+                modifier = textModifier.align(Alignment.Top),
+                style = textStyle,
+            )
+        }
+        if (!collapsible || !collapseState) content() else Spacer(Modifier.height(4.dp))
     }
 }
 
