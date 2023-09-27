012c47a6391a6165ba61d412dc6eda6253f8324d
chr56 <chr0056@gmail.com>
2023-09-27 17:40:05 +0800

    [SongAndArtistLinkage] store `role`

--- app/src/main/java/player/phonograph/repo/room/entity/SongAndArtistLinkage.kt
+++ app/src/main/java/player/phonograph/repo/room/entity/SongAndArtistLinkage.kt
@@ -4,6 +4,7 @@
 
 package player.phonograph.repo.room.entity
 
+import androidx.annotation.StringDef
 import androidx.room.ColumnInfo
 import androidx.room.Entity
 import androidx.room.Index
@@ -18,4 +19,18 @@ data class SongAndArtistLinkage(
     var songId: Long,
     @ColumnInfo(name = "artist_id")
     var artistId: Long,
-)
+    @ColumnInfo(name = "role", defaultValue = ROLE_ARTIST)
+    @ArtistRole
+    var role: String,
+) {
+
+    companion object {
+        const val ROLE_ARTIST = "artist"
+        const val ROLE_COMPOSER = "composer"
+        const val ROLE_ALBUM_ARTIST = "album_artist"
+    }
+
+    @StringDef(ROLE_ARTIST, ROLE_COMPOSER, ROLE_ALBUM_ARTIST)
+    @Retention(AnnotationRetention.SOURCE)
+    annotation class ArtistRole
+}
