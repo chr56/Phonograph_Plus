035fd9d97b422a72d28b10c89ad8e12efcc7b5f3
chr56 <chr0056@gmail.com>
2023-09-29 13:03:29 +0800

    [Converters] extend

--- app/src/main/java/player/phonograph/repo/room/Converters.kt
+++ app/src/main/java/player/phonograph/repo/room/Converters.kt
@@ -4,6 +4,10 @@
 
 package player.phonograph.repo.room
 
+import player.phonograph.model.Album
+import player.phonograph.model.Artist
+import player.phonograph.repo.room.entity.AlbumEntity
+import player.phonograph.repo.room.entity.ArtistEntity
 import player.phonograph.repo.room.entity.SongEntity
 import androidx.room.TypeConverter
 import player.phonograph.model.Song as SongModel
@@ -11,43 +15,57 @@ import player.phonograph.model.Song as SongModel
 object Converters {
 
     @TypeConverter
-    fun fromSongModel(song: SongModel): SongEntity {
-        // todo
-        return SongEntity(
-            id = song.id,
-            path = song.data,
-            dateAdded = song.dateModified,
-            dateModified = song.dateModified,
-            title = song.title,
-            albumId = song.albumId,
-            albumName = song.albumName,
-            rawArtistId = song.artistId,
-            rawArtistName = song.artistName,
-            albumArtistName = song.albumArtistName,
-            composer = song.composer,
-            year = song.year,
-            duration = song.duration,
-            trackNumber = song.trackNumber
-        )
-    }
+    fun fromSongModel(song: SongModel) = SongEntity(
+        id = song.id,
+        path = song.data,
+        dateAdded = song.dateModified,
+        dateModified = song.dateModified,
+        title = song.title,
+        albumId = song.albumId,
+        albumName = song.albumName,
+        rawArtistId = song.artistId,
+        rawArtistName = song.artistName,
+        albumArtistName = song.albumArtistName,
+        composer = song.composer,
+        year = song.year,
+        duration = song.duration,
+        trackNumber = song.trackNumber
+    )
 
     @TypeConverter
-    fun toSongModel(songEntity: SongEntity): SongModel {
-        return SongModel(
-            id = songEntity.id,
-            title = songEntity.title ?: "UNKNOWN",
-            trackNumber = songEntity.trackNumber,
-            year = songEntity.year,
-            duration = songEntity.duration,
-            data = songEntity.path,
-            dateAdded = songEntity.dateModified,
-            dateModified = songEntity.dateModified,
-            albumId = songEntity.albumId,
-            albumName = songEntity.albumName,
-            artistId = songEntity.rawArtistId,
-            artistName = songEntity.rawArtistName,
-            albumArtistName = songEntity.albumArtistName,
-            composer = songEntity.composer,
-        )
-    }
+    fun toSongModel(songEntity: SongEntity) = SongModel(
+        id = songEntity.id,
+        title = songEntity.title ?: "UNKNOWN",
+        trackNumber = songEntity.trackNumber,
+        year = songEntity.year,
+        duration = songEntity.duration,
+        data = songEntity.path,
+        dateAdded = songEntity.dateModified,
+        dateModified = songEntity.dateModified,
+        albumId = songEntity.albumId,
+        albumName = songEntity.albumName,
+        artistId = songEntity.rawArtistId,
+        artistName = songEntity.rawArtistName,
+        albumArtistName = songEntity.albumArtistName,
+        composer = songEntity.composer,
+    )
+
+    @TypeConverter
+    fun toAlbumModel(albumEntity: AlbumEntity) = Album(
+        id = albumEntity.albumId,
+        title = albumEntity.albumName,
+        songCount = albumEntity.songCount,
+        artistId = albumEntity.artistId,
+        artistName = albumEntity.albumArtistName,
+        year = albumEntity.year,
+        dateModified = albumEntity.dateModified,
+    )
+
+    @TypeConverter
+    fun toArtistModel(artistEntity: ArtistEntity) = Artist(
+        id = artistEntity.artistId,
+        name = artistEntity.artistName,
+        albumCount = artistEntity.albumCount,
+        songCount = artistEntity.songCount,
+    )
 }
