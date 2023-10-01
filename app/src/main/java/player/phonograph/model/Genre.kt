067c6fba5c3dbbbd141cb56f2f6a902c7df198c8
chr56 <chr0056@gmail.com>
2023-10-01 22:36:30 +0800

    [Genre] use `@Parcelize`

--- app/src/main/java/player/phonograph/model/Genre.kt
+++ app/src/main/java/player/phonograph/model/Genre.kt
@@ -1,44 +1,16 @@
 package player.phonograph.model
 
 import android.content.Context
-import android.os.Parcel
 import android.os.Parcelable
-import androidx.annotation.Keep
+import kotlinx.parcelize.Parcelize
 
-class Genre : Parcelable, Displayable {
+@Parcelize
+data class Genre(
+    @JvmField val id: Long,
+    @JvmField val name: String?,
+    @JvmField val songCount: Int,
+) : Parcelable, Displayable {
 
-    @JvmField val id: Long
-    @JvmField val name: String?
-    @JvmField val songCount: Int
-
-    constructor(id: Long, name: String?, songCount: Int) {
-        this.id = id
-        this.name = name
-        this.songCount = songCount
-    }
-
-    override fun equals(other: Any?): Boolean {
-        if (this === other) return true
-        if (other == null || javaClass != other.javaClass) return false
-        val genre = other as Genre
-        if (id != genre.id) return false
-        return if (name != genre.name) false else songCount == genre.songCount
-    }
-
-    override fun hashCode(): Int = name.hashCode() + id.toInt() * 31 + songCount * 31
-    override fun toString(): String = "Genre{id=$id, name='$name', songCount=$songCount'}"
-
-    override fun describeContents(): Int = 0
-    override fun writeToParcel(dest: Parcel, flags: Int) {
-        dest.writeLong(id)
-        dest.writeString(name)
-        dest.writeInt(songCount)
-    }
-    protected constructor(parcel: Parcel) {
-        id = parcel.readLong()
-        name = parcel.readString()!!
-        songCount = parcel.readInt()
-    }
 
     override fun getItemID(): Long = id
 
@@ -46,17 +18,4 @@ class Genre : Parcelable, Displayable {
 
     override fun getDescription(context: Context): CharSequence = infoString(context)
 
-    companion object {
-        @Keep
-        @JvmField
-        val CREATOR: Parcelable.Creator<Genre> = object : Parcelable.Creator<Genre> {
-            override fun createFromParcel(source: Parcel): Genre {
-                return Genre(source)
-            }
-
-            override fun newArray(size: Int): Array<Genre?> {
-                return arrayOfNulls(size)
-            }
-        }
-    }
 }
