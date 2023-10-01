213a408724afce61ba619811945c6d815126b5a0
chr56 <chr0056@gmail.com>
2023-10-01 22:30:24 +0800

    [SongCollection] use `@Parcelize`

--- app/src/main/java/player/phonograph/model/SongCollection.kt
+++ app/src/main/java/player/phonograph/model/SongCollection.kt
@@ -6,14 +6,14 @@ package player.phonograph.model
 
 import lib.phonograph.storage.root
 import player.phonograph.App
-import androidx.annotation.Keep
 import androidx.core.content.getSystemService
 import android.content.Context
-import android.os.Parcel
 import android.os.Parcelable
 import android.os.storage.StorageManager
+import kotlinx.parcelize.Parcelize
 
-class SongCollection(
+@Parcelize
+data class SongCollection(
     val name: String,
     val songs: List<Song>,
     val detail: String? = null,
@@ -25,48 +25,7 @@ class SongCollection(
     override fun getDescription(context: Context) =
         "${songCountString(context, songs.size)} ...${stripStorageVolume(detail.orEmpty())}"
 
-
-    override fun equals(other: Any?): Boolean {
-        if (this === other) return true
-        if (other !is SongCollection) return false
-
-        if (name != other.name) return false
-        if (songs != other.songs) return false
-        if (detail != other.detail) return false
-
-        return true
-    }
-
-    override fun hashCode(): Int = name.hashCode() * 31 + songs.hashCode()
-
-    override fun toString(): String = "SongCollection(name='$name', songs=$songs)"
-
-    constructor(parcel: Parcel) : this(
-        parcel.readString()!!,
-        parcel.createTypedArrayList(Song.CREATOR)!!,
-        parcel.readString()
-    )
-
-    override fun writeToParcel(parcel: Parcel, flags: Int) {
-        parcel.writeString(name)
-        parcel.writeTypedList(songs)
-        parcel.writeString(detail)
-    }
-
-    override fun describeContents(): Int = 0
-
     companion object {
-        @Keep
-        @JvmField
-        val CREATOR = object : Parcelable.Creator<SongCollection> {
-            override fun createFromParcel(parcel: Parcel): SongCollection {
-                return SongCollection(parcel)
-            }
-
-            override fun newArray(size: Int): Array<SongCollection?> {
-                return arrayOfNulls(size)
-            }
-        }
 
         private fun stripStorageVolume(str: String): String {
             return str.removePrefix(internalStorageRootPath).removePrefix("/storage")
