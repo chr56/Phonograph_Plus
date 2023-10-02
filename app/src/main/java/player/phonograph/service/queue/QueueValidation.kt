0e2070cb8f6a9c0be1053ddd8579bb4ea866db76
chr56 <chr0056@gmail.com>
2023-10-02 12:48:05 +0800

    [QueueValidation] improve marking of deleted song

--- app/src/main/java/player/phonograph/service/queue/QueueValidation.kt
+++ app/src/main/java/player/phonograph/service/queue/QueueValidation.kt
@@ -4,16 +4,24 @@
 
 package player.phonograph.service.queue
 
+import player.phonograph.R
 import player.phonograph.model.Song
 import player.phonograph.repo.loader.Songs
 import android.content.Context
 
 fun validSongs(context: Context, songs: List<Song>): List<Song> {
     return songs.map { song ->
-        findSong(context, song) ?: song.copy(title = "[Deleted]" + song.title)
+        findSong(context, song) ?: markDeleted(context, song)
     }
 }
 
 private fun findSong(context: Context, song: Song): Song? =
     Songs.id(context, song.id).takeIf { Song.EMPTY_SONG != it }
         ?: Songs.path(context, song.data).takeIf { Song.EMPTY_SONG != it }
+
+private fun markDeleted(context: Context, song: Song): Song {
+    val prefix = "[${context.getString(R.string.deleted)}]"
+    val title = song.title
+    val new = title.takeIf { title.startsWith(prefix) } ?: (prefix + title)
+    return song.copy(title = new)
+}
