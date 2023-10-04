d43ac567e6b03a2b8db7b9da5e4242f3caf87d03
chr56 <chr0056@gmail.com>
2023-10-04 20:23:28 +0800

    [res] use svg drawable for NowPlayingScreen Preview

--- app/src/main/java/player/phonograph/model/NowPlayingScreen.kt
+++ app/src/main/java/player/phonograph/model/NowPlayingScreen.kt
@@ -1,14 +1,14 @@
 package player.phonograph.model
 
+import player.phonograph.R
 import androidx.annotation.DrawableRes
 import androidx.annotation.StringRes
-import player.phonograph.R
 
 enum class NowPlayingScreen(
     @field:StringRes @param:StringRes val titleRes: Int,
     @field:DrawableRes @param:DrawableRes val drawableResId: Int,
     val id: Int
 ) {
-    CARD(R.string.card, R.drawable.np_card, 0),
-    FLAT(R.string.flat, R.drawable.np_flat, 1);
+    CARD(R.string.card, R.drawable.player_card, 0),
+    FLAT(R.string.flat, R.drawable.player_flat, 1);
 }
