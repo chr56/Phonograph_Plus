d7da068223105a2b43005cded94c4a06946508c4
chr56 <chr0056@gmail.com>
2023-10-09 12:22:51 +0800

    [ViewHolderLayout] add `LIST_EXTENDED`

--- app/src/main/java/player/phonograph/ui/adapter/ViewHolderLayout.kt
+++ app/src/main/java/player/phonograph/ui/adapter/ViewHolderLayout.kt
@@ -12,6 +12,7 @@ import androidx.annotation.LayoutRes
 @IntDef(
     ViewHolderLayout.TYPE_CUSTOM,
     ViewHolderLayout.TYPE_LIST,
+    ViewHolderLayout.TYPE_LIST_EXTENDED,
     ViewHolderLayout.TYPE_LIST_SINGLE_ROW,
     ViewHolderLayout.TYPE_LIST_NO_IMAGE,
     ViewHolderLayout.TYPE_LIST_3L,
@@ -35,6 +36,7 @@ value class ViewHolderLayout private constructor(@ViewHolderType val ordinal: In
     @LayoutRes
     fun layout(): Int = when (ordinal) {
         TYPE_LIST                 -> R.layout.item_list
+        TYPE_LIST_EXTENDED        -> R.layout.item_list_extended
         TYPE_LIST_SINGLE_ROW      -> R.layout.item_list_single_row
         TYPE_LIST_NO_IMAGE        -> R.layout.item_list_no_image
         TYPE_LIST_3L              -> R.layout.item_list_3l
@@ -47,6 +49,7 @@ value class ViewHolderLayout private constructor(@ViewHolderType val ordinal: In
 
 
         const val TYPE_LIST = 1
+        const val TYPE_LIST_EXTENDED = 7
         const val TYPE_LIST_SINGLE_ROW = 2
         const val TYPE_LIST_NO_IMAGE = 3
         const val TYPE_LIST_3L = 4
@@ -57,6 +60,8 @@ value class ViewHolderLayout private constructor(@ViewHolderType val ordinal: In
 
         val LIST: ViewHolderLayout
             get() = ViewHolderLayout(TYPE_LIST)
+        val LIST_EXTENDED: ViewHolderLayout
+            get() = ViewHolderLayout(TYPE_LIST_EXTENDED)
         val LIST_SINGLE_ROW: ViewHolderLayout
             get() = ViewHolderLayout(TYPE_LIST_SINGLE_ROW)
         val LIST_NO_IMAGE: ViewHolderLayout
@@ -70,6 +75,7 @@ value class ViewHolderLayout private constructor(@ViewHolderType val ordinal: In
 
         fun from(@ViewHolderType type: Int): ViewHolderLayout = when (type) {
             TYPE_LIST                 -> LIST
+            TYPE_LIST_EXTENDED        -> LIST_EXTENDED
             TYPE_LIST_SINGLE_ROW      -> LIST_SINGLE_ROW
             TYPE_LIST_NO_IMAGE        -> LIST_NO_IMAGE
             TYPE_LIST_3L              -> LIST_3L
