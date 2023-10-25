0a4b3d615b4f07783f2ac26153d46d96a0db60cc
chr56 <chr0056@gmail.com>
2023-10-25 13:01:02 +0800

    [menu] remove legacy implementation of item action menu

--- app/src/main/java/player/phonograph/ui/adapter/DisplayableItemRegistry.kt
+++ app/src/main/java/player/phonograph/ui/adapter/DisplayableItemRegistry.kt
@@ -6,35 +6,14 @@
 
 package player.phonograph.ui.adapter
 
-import player.phonograph.actions.menu.playlistPopupMenu
-import player.phonograph.actions.menu.songPopupMenu
 import player.phonograph.model.Album
 import player.phonograph.model.Artist
 import player.phonograph.model.Displayable
 import player.phonograph.model.Genre
 import player.phonograph.model.Song
 import player.phonograph.model.playlist.Playlist
-import android.content.Context
-import android.view.Menu
-import android.view.View
 
 fun Displayable.hasMenu(): Boolean = this is Song || this is Playlist
-
-/**
- * setup three-dot menu for [Song]
- */
-fun Displayable.initMenu(
-    context: Context,
-    menu: Menu,
-    showPlay: Boolean = false,
-    index: Int = Int.MIN_VALUE,
-    transitionView: View? = null,
-) =
-    when (this) {
-        is Song     -> songPopupMenu(context, menu, this, showPlay, index, transitionView)
-        is Playlist -> playlistPopupMenu(menu, context, this)
-        else        -> menu.clear()
-    }
 
 /**
  * for fast-scroll recycler-view's bar hint
