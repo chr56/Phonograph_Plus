368465ef0b294b3975a7d5c85b868c1009725b06
chr56 <chr0056@gmail.com>
2023-11-07 12:12:33 +0800

    [ScanMediaFolderDialog] cleanup

--- app/src/main/java/player/phonograph/ui/dialogs/ScanMediaFolderDialog.kt
+++ app/src/main/java/player/phonograph/ui/dialogs/ScanMediaFolderDialog.kt
@@ -1,58 +1,52 @@
 package player.phonograph.ui.dialogs
 
-import android.app.Activity
-import android.media.MediaScannerConnection
-import android.util.Log
-import android.view.View
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.SupervisorJob
-import kotlinx.coroutines.launch
-import kotlinx.coroutines.withContext
 import player.phonograph.App
 import player.phonograph.R
 import player.phonograph.misc.UpdateToastMediaScannerCompletionListener
 import player.phonograph.model.file.Location
-import player.phonograph.notification.ErrorNotification
-import player.phonograph.util.coroutineToast
 import player.phonograph.util.FileUtil.DirectoryInfo
 import player.phonograph.util.FileUtil.FileScanner
+import player.phonograph.util.coroutineToast
+import player.phonograph.util.reportError
+import android.app.Activity
+import android.content.Context
+import android.media.MediaScannerConnection
+import android.view.View
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 import java.io.File
 
 class ScanMediaFolderDialog : FileChooserDialog() {
 
     override fun affirmative(view: View, currentLocation: Location) {
-        CoroutineScope(Dispatchers.IO + SupervisorJob()).launch {
-            runCatching {
+        CoroutineScope(Dispatchers.IO).launch {
+            try {
                 val paths = FileScanner.listPaths(
                     DirectoryInfo(File(currentLocation.absolutePath), FileScanner.audioFileFilter),
                     this
                 )
                 if (!paths.isNullOrEmpty()) {
-                    scanFile(activity, paths)
+                    scanFile(activity ?: requireContext(), paths)
                 } else {
                     coroutineToast(requireContext().applicationContext, R.string.nothing_to_scan)
                 }
-            }.apply {
-                if (isFailure) {
-                    exceptionOrNull()?.let { e ->
-                        ErrorNotification.postErrorNotification(e, "Scan Fail")
-                        Log.w("ScanMediaFolderDialog", e)
-                    }
-                }
+            } catch (e: Exception) {
+                reportError(e, "ScanMediaFolderDialog", getString(R.string.failed))
             }
         }
         // dismiss()
     }
 
     companion object {
-        suspend fun scanFile(activity: Activity?, paths: Array<String>) {
+        suspend fun scanFile(context: Context, paths: Array<String>) {
             withContext(Dispatchers.Main) {
                 MediaScannerConnection.scanFile(
-                    activity?.applicationContext ?: App.instance,
+                    context.applicationContext ?: App.instance,
                     paths,
                     arrayOf("audio/*", "application/ogg", "application/x-ogg", "application/itunes"),
-                    if (activity != null) UpdateToastMediaScannerCompletionListener(activity, paths) else null
+                    if (context is Activity) UpdateToastMediaScannerCompletionListener(context, paths) else null
                 )
             }
         }
