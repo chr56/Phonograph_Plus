9f7da38d0e3fef41bf2bf5ac25a11a89072d0921
chr56 <chr0056@gmail.com>
2023-11-19 11:37:43 +0800

    [coil] enable cache for `AlbumImage`

--- app/src/main/java/player/phonograph/coil/album/AlbumImageFetcher.kt
+++ app/src/main/java/player/phonograph/coil/album/AlbumImageFetcher.kt
@@ -9,6 +9,7 @@ import coil.fetch.FetchResult
 import coil.fetch.Fetcher
 import coil.request.Options
 import coil.size.Size
+import player.phonograph.coil.retriever.CacheStore
 import player.phonograph.coil.retriever.ImageRetriever
 import player.phonograph.coil.retriever.raw
 import player.phonograph.coil.retriever.retrieveAudioFile
@@ -16,6 +17,9 @@ import player.phonograph.coil.retriever.retrieverFromConfig
 import player.phonograph.util.debug
 import android.content.Context
 import android.util.Log
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 
 class AlbumImageFetcher(
     val data: AlbumImage,
@@ -38,17 +42,37 @@ class AlbumImageFetcher(
 
     private fun retrieve(
         retrievers: List<ImageRetriever>,
-        data: AlbumImage,
+        album: AlbumImage,
         context: Context,
         size: Size,
     ): FetchResult? {
-        for (file in data.files) {
+        val noImage = CacheStore.AlbumImages(context).isNoImage(album)
+        if (noImage) return null // skipping
+
+        for (file in album.files) {
+            val cached = CacheStore.AlbumImages(context).get(album, file.songId.toString())
+            if (cached != null) {
+                debug {
+                    Log.v(TAG, "Image was read from cache of file($file) for album $album")
+                }
+                return cached
+            }
+            val noSpecificImage = CacheStore.AlbumImages(context).isNoImage(album, file.songId.toString())
+            if (noSpecificImage) continue
             val result = retrieveAudioFile(retrievers, file, context, size, raw)
-            if (result != null) return result
+            if (result != null) {
+                CoroutineScope(Dispatchers.IO).launch {
+                    CacheStore.AlbumImages(context).set(album, result, file.songId.toString())
+                }
+                return result
+            } else {
+                CacheStore.AlbumImages(context).markNoImage(album, file.songId.toString())
+            }
         }
         debug {
-            Log.v(TAG, "No any image for album ${data.name}")
+            Log.v(TAG, "No any image for album ${album.name}")
         }
+        CacheStore.AlbumImages(context).markNoImage(album)
         return null
     }
 
