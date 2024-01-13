88719ff02daadcf906e19b3859a15dccd01c84f5
chr56 <chr0056@gmail.com>
2024-01-13 19:16:48 +0800

    [view] use `WindowInsets.getInsets` on R and above

--- app/src/main/java/player/phonograph/ui/views/StatusBarView.kt
+++ app/src/main/java/player/phonograph/ui/views/StatusBarView.kt
@@ -2,6 +2,7 @@ package player.phonograph.ui.views
 
 import android.content.Context
 import android.os.Build
+import android.os.Build.VERSION.SDK_INT
 import android.util.AttributeSet
 import android.view.View
 import android.view.WindowInsets
@@ -13,9 +14,16 @@ class StatusBarView : View {
     constructor(context: Context?, attrs: AttributeSet?, defStyleAttr: Int) : super(context, attrs, defStyleAttr) {}
 
     override fun onApplyWindowInsets(insets: WindowInsets): WindowInsets {
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
-            layoutParams = layoutParams.apply { height = insets.systemWindowInsetTop }
-        }
+        layoutParams =
+            layoutParams.apply {
+                height =
+                    if (SDK_INT >= Build.VERSION_CODES.R) {
+                        insets.getInsets(WindowInsets.Type.statusBars()).top
+                    } else {
+                        @Suppress("DEPRECATION")
+                        insets.systemWindowInsetTop
+                    }
+            }
         return super.onApplyWindowInsets(insets)
     }
 }
