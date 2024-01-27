5a2f331ee4506241ce4fbbd60f3fdcb95c5aa6b0
chr56 <chr0056@gmail.com>
2024-01-27 09:56:14 +0800

    [QueueManager] avoid crash due to modifying observer list in concurrent

--- app/src/main/java/player/phonograph/service/queue/QueueManager.kt
+++ app/src/main/java/player/phonograph/service/queue/QueueManager.kt
@@ -323,8 +323,8 @@ class QueueManager(val context: Application) {
 
     private inner class ObserverManager {
         private val observers: MutableList<QueueObserver> = ArrayList()
-        fun addObserver(observer: QueueObserver) = observers.add(observer)
-        fun removeObserver(observer: QueueObserver): Boolean = observers.remove(observer)
+        fun addObserver(observer: QueueObserver) = synchronized(observers) { observers.add(observer) }
+        fun removeObserver(observer: QueueObserver): Boolean = synchronized(observers) { observers.remove(observer) }
 
         fun notifyQueueChanged(newPlayingQueue: List<Song>, newOriginalQueue: List<Song>) =
             notifyAllObservers {
@@ -347,8 +347,10 @@ class QueueManager(val context: Application) {
             }
 
         private inline fun notifyAllObservers(block: QueueObserver.() -> Unit) {
-            for (observer in observers) {
-                block(observer)
+            synchronized(observers) {
+                for (observer in observers) {
+                    block(observer)
+                }
             }
         }
     }
