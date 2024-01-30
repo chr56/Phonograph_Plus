f023fd0d7c2d50196b941c9aa11e3ec6636244a2
chr56 <chr0056@gmail.com>
2024-01-30 11:07:39 +0800

    [Release] release v0.7.2.4

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -42,8 +42,8 @@ android {
         vectorDrawables.useSupportLibrary = true
 
         applicationId = "player.phonograph.plus"
-        versionCode = 623
-        versionName = "0.7.2.3"
+        versionCode = 624
+        versionName = "0.7.2.4"
 
 
         setProperty("archivesBaseName", "PhonographPlus_$versionName")
