6a6195c202f4f163baa7ed75bef9a12bb35302aa
chr56 <chr0056@gmail.com>
2024-02-02 11:43:46 +0800

    [AbsPage] remove `setHasOptionsMenu` call

--- app/src/main/java/player/phonograph/ui/fragments/pages/AbsPage.kt
+++ app/src/main/java/player/phonograph/ui/fragments/pages/AbsPage.kt
@@ -4,9 +4,6 @@
 
 package player.phonograph.ui.fragments.pages
 
-import android.os.Bundle
-import androidx.lifecycle.DefaultLifecycleObserver
-import androidx.lifecycle.LifecycleOwner
 import player.phonograph.ui.fragments.AbsMusicServiceFragment
 import player.phonograph.ui.fragments.MainFragment
 
@@ -15,20 +12,5 @@ abstract class AbsPage : AbsMusicServiceFragment() {
 
     protected val hostFragment: MainFragment
         get() = parentFragment?.let { it as MainFragment } ?: throw IllegalStateException("${this::class.simpleName} hasn't attach to MainFragment")
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-
-        hostFragment.lifecycle.addObserver(
-            object : DefaultLifecycleObserver {
-                override fun onCreate(owner: LifecycleOwner) {
-                    super.onCreate(owner)
-                    @Suppress("DEPRECATION") //todo: replace `setHasOptionsMenu` with MenuProvider
-                    setHasOptionsMenu(true)
-                }
-            }
-        )
-    }
-
     open fun onBackPress(): Boolean = false
 }
