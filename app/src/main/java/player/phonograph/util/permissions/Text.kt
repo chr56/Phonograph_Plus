8f3cb280a284e88c6b7f95f778fbe436d6450873
chr56 <chr0056@gmail.com>
2024-02-16 11:29:36 +0800

    [permission-util] use custom names and descriptions

--- app/src/main/java/player/phonograph/util/permissions/Text.kt
+++ app/src/main/java/player/phonograph/util/permissions/Text.kt
@@ -4,6 +4,12 @@
 
 package player.phonograph.util.permissions
 
+import player.phonograph.R
+import android.Manifest.permission.MANAGE_EXTERNAL_STORAGE
+import android.Manifest.permission.POST_NOTIFICATIONS
+import android.Manifest.permission.READ_EXTERNAL_STORAGE
+import android.Manifest.permission.READ_MEDIA_AUDIO
+import android.Manifest.permission.WRITE_EXTERNAL_STORAGE
 import android.content.Context
 import android.content.pm.PermissionInfo
 
@@ -11,11 +17,33 @@ fun permissionInfo(context: Context, permissionId: String): PermissionInfo =
     context.packageManager.getPermissionInfo(permissionId, 0)
 
 fun permissionName(context: Context, permissionId: String): CharSequence {
-    val info = permissionInfo(context, permissionId)
-    return info.loadLabel(context.packageManager)
+    val stringRes = when (permissionId) {
+        POST_NOTIFICATIONS      -> R.string.permission_name_post_notifications
+        READ_MEDIA_AUDIO        -> R.string.permission_name_read_media_audio
+        READ_EXTERNAL_STORAGE   -> R.string.permission_name_read_external_storage
+        WRITE_EXTERNAL_STORAGE  -> R.string.permission_name_write_external_storage
+        MANAGE_EXTERNAL_STORAGE -> R.string.permission_name_manage_external_storage
+        else                    -> 0
+    }
+    return if (stringRes > 0) {
+        context.getString(stringRes)
+    } else {
+        permissionInfo(context, permissionId).loadLabel(context.packageManager)
+    }
 }
 
 fun permissionDescription(context: Context, permissionId: String): CharSequence? {
-    val info = permissionInfo(context, permissionId)
-    return info.loadDescription(context.packageManager)
+    val stringRes = when (permissionId) {
+        POST_NOTIFICATIONS      -> R.string.permission_desc_post_notifications
+        READ_MEDIA_AUDIO        -> R.string.permission_desc_read_media_audio
+        READ_EXTERNAL_STORAGE   -> R.string.permission_desc_read_external_storage
+        WRITE_EXTERNAL_STORAGE  -> R.string.permission_desc_write_external_storage
+        MANAGE_EXTERNAL_STORAGE -> R.string.permission_desc_manage_external_storage
+        else                    -> 0
+    }
+    return if (stringRes > 0) {
+        context.getString(stringRes)
+    } else {
+        permissionInfo(context, permissionId).loadDescription(context.packageManager)
+    }
 }
