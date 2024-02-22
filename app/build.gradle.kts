980d8b5c5054cadf889c954ef9aff1c21efcdce6
chr56 <chr0056@gmail.com>
2024-02-22 09:31:46 +0800

    [Release] release v1.5.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -39,8 +39,8 @@ android {
         targetSdk = 34
 
         applicationId = "player.phonograph.plus"
-        versionCode = 1050
-        versionName = "1.5.0"
+        versionCode = 1051
+        versionName = "1.5.1"
 
 
         setProperty("archivesBaseName", "PhonographPlus_$versionName")
