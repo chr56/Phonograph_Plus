f6b955de51534b1441a5e20e8cb5a9f604f1a2bd
chr56 <chr0056@gmail.com>
2024-03-07 20:18:01 +0800

    [M3UParser] cleanup

--- app/src/main/java/util/phonograph/playlist/m3u/M3UParser.kt
+++ app/src/main/java/util/phonograph/playlist/m3u/M3UParser.kt
@@ -4,40 +4,16 @@
 
 package util.phonograph.playlist.m3u
 
-import player.phonograph.model.Song
-import player.phonograph.repo.loader.Songs
-import android.content.Context
 import java.io.InputStream
-import java.io.InputStreamReader
 import java.nio.charset.StandardCharsets
 
 object M3UParser {
 
-    fun readForSongs(inputStream: InputStream, context: Context): List<Song> {
-        val songFiles = readForPaths(inputStream)
-        val songs: MutableList<Song> = ArrayList(15)
-        for (file in songFiles) {
-            songs.add(
-                Songs.searchByPath(context, file, true).firstOrNull() ?: Song.EMPTY_SONG
-            )
-        }
-        return if (songs.isNotEmpty()) songs else ArrayList(1)
-    }
-
     fun readForPaths(inputStream: InputStream): List<String> {
-        val inputStreamReader = InputStreamReader(inputStream, StandardCharsets.UTF_8)
-
-        val songFiles: MutableList<String> = ArrayList(15)
-        val pathRegex = Regex("(/.)*/?.*\\..*")
-        inputStreamReader.use { reader ->
-            val lines = reader.readLines()
-            for (line in lines) {
-                if (line[0] == '#') continue // ignore comment
-                if (line.startsWith("http")) continue // ignore stream
-                if (pathRegex.matches(line)) songFiles.add(line)
-            }
+        return inputStream.reader(StandardCharsets.UTF_8).use { reader ->
+            val pathRegex = Regex("(/.+)*/?.+")
+            reader.readLines().filterNot { it.startsWith('#') }.filter { line -> pathRegex.matches(line) }
         }
-        return songFiles
     }
 
 }
