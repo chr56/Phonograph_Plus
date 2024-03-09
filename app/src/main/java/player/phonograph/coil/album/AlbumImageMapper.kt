4e5a0343cfc89d64eeb567c4931fd7c18e793337
chr56 <chr0056@gmail.com>
2024-03-09 13:35:59 +0800

    [coil-mappers] make query of Song suspend

--- app/src/main/java/player/phonograph/coil/album/AlbumImageMapper.kt
+++ app/src/main/java/player/phonograph/coil/album/AlbumImageMapper.kt
@@ -6,11 +6,12 @@ import player.phonograph.coil.model.SongImage
 import player.phonograph.coil.model.AlbumImage
 import player.phonograph.model.Album
 import player.phonograph.repo.loader.Songs
+import kotlinx.coroutines.runBlocking
 
 class AlbumImageMapper : Mapper<Album, AlbumImage> {
     override fun map(data: Album, options: Options): AlbumImage? = AlbumImage(
         data.id,
         data.title,
-        Songs.album(options.context, data.id).map { SongImage.from(it) }
+        runBlocking{ Songs.album(options.context, data.id).map { SongImage.from(it) } }
     ).takeIf { it.files.isNotEmpty() }
 }
--- app/src/main/java/player/phonograph/coil/artist/ArtistImageMapper.kt
+++ app/src/main/java/player/phonograph/coil/artist/ArtistImageMapper.kt
@@ -6,16 +6,17 @@ package player.phonograph.coil.artist
 
 import coil.map.Mapper
 import coil.request.Options
-import player.phonograph.coil.model.SongImage
 import player.phonograph.coil.model.ArtistImage
+import player.phonograph.coil.model.SongImage
 import player.phonograph.model.Artist
 import player.phonograph.repo.loader.Songs
+import kotlinx.coroutines.runBlocking
 
 class ArtistImageMapper : Mapper<Artist, ArtistImage> {
     override fun map(data: Artist, options: Options): ArtistImage? =
         ArtistImage(
             data.id,
             data.name,
-            Songs.artist(options.context, data.id).map { SongImage.from(it) }
+            runBlocking { Songs.artist(options.context, data.id).map { SongImage.from(it) } }
         ).takeIf { it.files.isNotEmpty() }
 }
