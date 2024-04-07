31deab1fb6284df1cef557036936c5cdf9e9ae10
chr56 <chr0056@gmail.com>
2024-04-07 19:45:16 +0800

    [ClickModeSettingDialog] improve paddings

--- app/src/main/java/player/phonograph/ui/dialogs/ClickModeSettingDialog.kt
+++ app/src/main/java/player/phonograph/ui/dialogs/ClickModeSettingDialog.kt
@@ -5,7 +5,6 @@
 package player.phonograph.ui.dialogs
 
 import com.vanpra.composematerialdialogs.MaterialDialog
-import com.vanpra.composematerialdialogs.customView
 import com.vanpra.composematerialdialogs.rememberMaterialDialogState
 import com.vanpra.composematerialdialogs.title
 import player.phonograph.R
@@ -14,7 +13,6 @@ import player.phonograph.settings.Keys
 import player.phonograph.settings.Setting
 import player.phonograph.ui.compose.ComposeViewDialogFragment
 import player.phonograph.ui.compose.PhonographTheme
-import player.phonograph.ui.compose.components.DialogContent
 import player.phonograph.ui.compose.components.FlagCheckBox
 import player.phonograph.ui.compose.components.ModeRadioBox
 import player.phonograph.util.setBit
@@ -23,9 +21,12 @@ import player.phonograph.util.unsetBit
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.MaterialTheme
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
@@ -51,11 +52,7 @@ class ClickModeSettingDialog : ComposeViewDialogFragment() {
             ) {
                 Spacer(modifier = Modifier.height(16.dp))
                 title(res = R.string.pref_title_click_behavior)
-                customView {
-                    DialogContent {
-                        ClickModeSettingDialogContent(requireContext())
-                    }
-                }
+                ClickModeSettingDialogContent(requireContext())
             }
         }
     }
@@ -63,12 +60,16 @@ class ClickModeSettingDialog : ComposeViewDialogFragment() {
 
 @Composable
 private fun ClickModeSettingDialogContent(context: Context) {
-    Column {
+    Column(
+        modifier = Modifier
+            .padding(horizontal = 24.dp, vertical = 12.dp)
+            .verticalScroll(rememberScrollState())
+    ) {
         val currentMode = remember {
-            mutableStateOf(Setting(context)[Keys.songItemClickMode].data)
+            mutableIntStateOf(Setting(context)[Keys.songItemClickMode].data)
         }
         val setCurrentMode = { new: Int ->
-            currentMode.value = new
+            currentMode.intValue = new
             Setting(context)[Keys.songItemClickMode].data = new
         }
         for (id in SongClickMode.baseModes) {
@@ -83,15 +84,15 @@ private fun ClickModeSettingDialogContent(context: Context) {
         Spacer(Modifier.height(8.dp))
 
         val currentExtraFlag = remember {
-            mutableStateOf(Setting(context)[Keys.songItemClickExtraFlag].data)
+            mutableIntStateOf(Setting(context)[Keys.songItemClickExtraFlag].data)
         }
         val flipExtraFlagBit = { mask: Int ->
-            val new = if (currentExtraFlag.value.testBit(mask)) {
-                currentExtraFlag.value.unsetBit(mask)
+            val new = if (currentExtraFlag.intValue.testBit(mask)) {
+                currentExtraFlag.intValue.unsetBit(mask)
             } else {
-                currentExtraFlag.value.setBit(mask)
+                currentExtraFlag.intValue.setBit(mask)
             }
-            currentExtraFlag.value = new
+            currentExtraFlag.intValue = new
             Setting(context)[Keys.songItemClickExtraFlag].data = new
         }
 
