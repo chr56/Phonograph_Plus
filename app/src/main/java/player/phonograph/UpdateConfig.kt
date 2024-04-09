b9cb7be8c6983619871b2dc8980d3ec2bdb433f0
chr56 <chr0056@gmail.com>
2024-04-09 11:51:55 +0800

    [UpgradeInfoDialog] refactor `UpgradeDialog` use Compose

--- app/src/main/java/player/phonograph/UpdateConfig.kt
+++ app/src/main/java/player/phonograph/UpdateConfig.kt
@@ -18,10 +18,16 @@ object UpdateConfig {
 
     const val GITHUB_REPO = "$OWNER/$REPO"
 
+    const val DOMAIN_GITHUB = "github.com"
+    const val DOMAIN_TG_LINK = "t.me"
+
     const val requestUriGitHub = "https://raw.githubusercontent.com/$GITHUB_REPO/$BRANCH/$FILE"
     const val requestUriCodeberg = "https://codeberg.org/$ORGANIZATION2/$REPO/raw/branch/$BRANCH/$FILE"
     const val requestUriBitBucket = "https://bitbucket.org/$ORGANIZATION/$REPO/raw/$BRANCH/$FILE"
 
     const val requestUriJsdelivr = "https://cdn.jsdelivr.net/gh/$GITHUB_REPO@$BRANCH/$FILE"
     const val requestUriFastGit = "https://endpoint.fastgit.org/https://github.com/$GITHUB_REPO/blob/$BRANCH/$FILE"
+
+    const val CHANNEL_NAME = "Phonograph_Plus"
+
 }
---
+++ app/src/main/java/player/phonograph/ui/dialogs/UpgradeInfoDialog.kt
@@ -0,0 +1,252 @@
+/*
+ * Copyright (c) 2021 chr_56 & Abou Zeid (kabouzeid) (original author)
+ */
+
+package player.phonograph.ui.dialogs
+
+import com.aghajari.compose.text.AnnotatedText
+import com.aghajari.compose.text.asAnnotatedString
+import com.vanpra.composematerialdialogs.MaterialDialog
+import com.vanpra.composematerialdialogs.rememberMaterialDialogState
+import com.vanpra.composematerialdialogs.title
+import lib.phonograph.dialog.alertDialog
+import mt.color.MaterialColor
+import player.phonograph.R
+import player.phonograph.UpdateConfig.CHANNEL_NAME
+import player.phonograph.UpdateConfig.DOMAIN_GITHUB
+import player.phonograph.UpdateConfig.DOMAIN_TG_LINK
+import player.phonograph.UpdateConfig.GITHUB_REPO
+import player.phonograph.mechanism.canAccessGitHub
+import player.phonograph.model.version.Version
+import player.phonograph.model.version.VersionCatalog
+import player.phonograph.settings.Keys
+import player.phonograph.settings.Setting
+import player.phonograph.ui.compose.ComposeViewDialogFragment
+import player.phonograph.ui.compose.PhonographTheme
+import player.phonograph.ui.compose.components.TempPopupContent
+import player.phonograph.util.parcelable
+import player.phonograph.util.text.dateText
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.Card
+import androidx.compose.material.Icon
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
+import androidx.compose.material.TextButton
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Home
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.window.Popup
+import android.content.Context
+import android.content.DialogInterface
+import android.content.Intent
+import android.net.Uri
+import android.os.Bundle
+import android.widget.Toast
+
+class UpgradeInfoDialog : ComposeViewDialogFragment() {
+
+    private lateinit var versionCatalog: VersionCatalog
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        versionCatalog = requireArguments().parcelable(VERSION_CATALOG)
+            ?: throw IllegalStateException("VersionCatalog non-exist")
+    }
+
+    @Composable
+    override fun Content() {
+        MainContent(versionCatalog, ::dismiss)
+    }
+
+    companion object {
+
+        private const val VERSION_CATALOG = "VERSION_CATALOG"
+        fun create(versionCatalog: VersionCatalog): UpgradeInfoDialog =
+            UpgradeInfoDialog().apply {
+                arguments = Bundle().also {
+                    it.putParcelable(VERSION_CATALOG, versionCatalog)
+                }
+            }
+
+    }
+
+}
+@Composable
+private fun MainContent(versionCatalog: VersionCatalog, dismiss: () -> Unit) {
+    PhonographTheme {
+        val context = LocalContext.current
+        MaterialDialog(
+            dialogState = rememberMaterialDialogState(true),
+            onCloseRequest = { dismiss() },
+            buttons = {
+                button(
+                    res = R.string.ignore_once,
+                    textStyle = MaterialTheme.typography.button.copy(color = MaterialTheme.colors.secondary),
+                ) {
+                    dismiss()
+                    actionIgnore(context, versionCatalog)
+                }
+                positiveButton(
+                    res = R.string.more_actions,
+                    textStyle = MaterialTheme.typography.button.copy(color = MaterialTheme.colors.secondary),
+                    disableDismiss = true
+                ) {
+                    actionMore(context)
+                }
+            }
+        ) {
+            title(res = R.string.new_version)
+            Column(
+                Modifier
+                    .padding(16.dp)
+                    .verticalScroll(rememberScrollState())
+            ) {
+                for (version in versionCatalog.versions) {
+                    Card(modifier = Modifier.padding(vertical = 8.dp), elevation = 2.dp) {
+                        Version(version)
+                    }
+                }
+            }
+        }
+    }
+}
+
+@Composable
+private fun Version(version: Version) {
+    var showPopup: Boolean by remember { mutableStateOf(false) }
+    val dismissPopup = { showPopup = false }
+    Box(Modifier.padding(8.dp)) {
+        VersionInfo(version) { showPopup = true }
+        if (showPopup) Popup(Alignment.Center, onDismissRequest = dismissPopup) {
+            VersionPopupContent(version, dismissPopup)
+        }
+    }
+}
+
+@Composable
+private fun VersionInfo(version: Version, onClick: () -> Unit) {
+    Column(Modifier.clickable(onClick = onClick)) {
+        val context = LocalContext.current
+        VersionTitle(version)
+        AnnotatedText(
+            version.releaseNote.parsed(context.resources).asAnnotatedString(),
+            Modifier.padding(horizontal = 12.dp, vertical = 8.dp)
+        )
+    }
+}
+
+@Composable
+private fun VersionTitle(version: Version, modifier: Modifier = Modifier) {
+    Row(modifier) {
+        Column(
+            Modifier
+                .padding(horizontal = 8.dp)
+                .weight(6f)
+        ) {
+            Text(
+                version.versionName,
+                fontWeight = FontWeight.Bold,
+                color = MaterialTheme.colors.secondary
+            )
+            Text(
+                version.channel,
+                fontWeight = FontWeight.Bold,
+                color = channelColor(version.channel)
+            )
+        }
+        Spacer(Modifier.weight(1f))
+        Text(
+            dateText(version.date),
+            Modifier.weight(5f),
+            fontWeight = FontWeight.Bold,
+            maxLines = 1
+        )
+    }
+}
+
+private fun channelColor(channel: String) =
+    Color(
+        when (channel.lowercase()) {
+            "stable" -> MaterialColor.Blue._A200.asColor
+            "preview" -> MaterialColor.DeepOrange._A200.asColor
+            "lts" -> MaterialColor.Lime._A700.asColor
+            else -> MaterialColor.BlueGrey._700.asColor
+        }
+    )
+
+
+@Composable
+private fun VersionPopupContent(version: Version, dismissPopup: () -> Unit) {
+    val context = LocalContext.current
+    TempPopupContent(dismissPopup = dismissPopup, onClick = dismissPopup) {
+        Column {
+            for (link in version.link) {
+                TextButton(onClick = { context.open(link.uri) }) {
+                    Icon(
+                        Icons.Default.Home,
+                        null,
+                        Modifier
+                            .padding(4.dp)
+                            .align(Alignment.CenterVertically)
+                    )
+                    Text(
+                        link.name,
+                        Modifier.align(Alignment.CenterVertically),
+                        style = MaterialTheme.typography.button
+                    )
+                }
+            }
+        }
+    }
+}
+
+private fun actionIgnore(context: Context, versionCatalog: VersionCatalog) {
+    Setting(context)[Keys.ignoreUpgradeDate].data = versionCatalog.currentLatestChannelVersionBy { it.date }.date
+    Toast.makeText(context, R.string.upgrade_ignored, Toast.LENGTH_SHORT).show()
+}
+
+private fun actionMore(context: Context) {
+    val map = mutableListOf(
+        Pair("${context.getString(R.string.git_hub)} (Release Page)") { _: DialogInterface ->
+            context.open(GITHUB_RELEASE_URL)
+        }
+    )
+    if (canAccessGitHub) {
+        map += Pair(context.getString(R.string.tg_channel)) { _: DialogInterface -> context.open(TG_CHANNEL) }
+    }
+    alertDialog(context) {
+        title(R.string.download)
+        positiveButton(android.R.string.ok) { dialog -> dialog.dismiss() }
+        singleChoiceItems(map, -1, true)
+    }.show()
+}
+
+
+private fun Context.open(uri: String) {
+    startActivity(
+        Intent(Intent.ACTION_VIEW).apply {
+            data = Uri.parse(if (uri.matches(Regex("[a-zA-Z]*://.+"))) uri else "http://$uri")
+            flags = Intent.FLAG_ACTIVITY_NEW_TASK
+        }
+    )
+}
+
+private const val GITHUB_RELEASE_URL = "$DOMAIN_GITHUB/$GITHUB_REPO/releases"
+private const val TG_CHANNEL = "$DOMAIN_TG_LINK/$CHANNEL_NAME"
