ab4ff012b92ff2ff5400719d0891e25a375ae7e7
chr56 <chr0056@gmail.com>
2024-04-11 11:46:44 +0800

    [dependency] remove `AnnotatedText`

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -211,7 +211,6 @@ dependencies {
     implementation(libs.advrecyclerview)
     implementation(libs.recyclerviewFastscroll)
     implementation(libs.composeReorderable)
-    implementation(libs.composeAnnotatedText)
     implementation(libs.bundles.composeSettings) {
         val uiTooling = libs.compose.ui.tooling.get().module
         exclude(group = uiTooling.group, module = uiTooling.name)
