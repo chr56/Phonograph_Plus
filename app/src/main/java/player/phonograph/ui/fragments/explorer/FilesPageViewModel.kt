9cc55c76b0dc922e6d995bf7f590c5951f48768e
chr56 <chr0056@gmail.com>
2024-04-29 11:33:02 +0800

    [file-explorer] cleanup view-models

--- app/src/main/java/player/phonograph/ui/fragments/explorer/FilesPageViewModel.kt
+++ app/src/main/java/player/phonograph/ui/fragments/explorer/FilesPageViewModel.kt
@@ -27,11 +27,11 @@ class FilesPageViewModel : AbsFileViewModel() {
             Setting(App.instance)[Keys.showFileImages].data = value
         }
 
-    override suspend fun listFiles(context: Context, location: Location, scope: CoroutineScope?): Set<FileEntity> {
-        return if (useLegacyListFile) {
+    override suspend fun listFiles(context: Context, location: Location, scope: CoroutineScope?): Set<FileEntity> =
+        if (useLegacyListFile) {
             FileEntityLoader.listFilesLegacy(location, scope)
-        } else
+        } else {
             FileEntityLoader.listFilesMediaStore(location, context, scope)
-    }
+        }
 
 }
