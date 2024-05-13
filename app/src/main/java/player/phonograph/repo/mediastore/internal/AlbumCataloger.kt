c140a62297de9447453971ebe2500ec1b937400e
chr56 <chr0056@gmail.com>
2024-05-13 12:30:38 +0800

    [AlbumLoader] fix ArtistAlbums sorting

--- app/src/main/java/player/phonograph/repo/mediastore/internal/AlbumCataloger.kt
+++ app/src/main/java/player/phonograph/repo/mediastore/internal/AlbumCataloger.kt
@@ -4,12 +4,10 @@
 
 package player.phonograph.repo.mediastore.internal
 
-import player.phonograph.App
 import player.phonograph.model.Album
 import player.phonograph.model.Song
+import player.phonograph.model.sort.SortMode
 import player.phonograph.model.sort.SortRef
-import player.phonograph.settings.Keys
-import player.phonograph.settings.Setting
 import player.phonograph.util.reportError
 import player.phonograph.util.sort
 import android.util.ArrayMap
@@ -91,12 +89,11 @@ suspend fun catalogAlbums(songs: List<Song>): Deferred<List<Album>> = coroutineS
             createAlbum(id, list)
         }.catch { e ->
             reportError(e, TAG_ALBUM, "Fail to load albums")
-        }.toList().sortAllAlbums()
+        }.toList().sortAllAlbums(SortMode(SortRef.YEAR, true))
     }
 }
 
-internal fun List<Album>.sortAllAlbums(): List<Album> {
-    val sortMode = Setting(App.instance).Composites[Keys.albumSortMode].data
+private fun List<Album>.sortAllAlbums(sortMode: SortMode): List<Album> {
     val revert = sortMode.revert
     return when (sortMode.sortRef) {
         SortRef.ALBUM_NAME  -> this.sort(revert) { it.title }
