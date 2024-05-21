664665f87086826cc016b1fcfb22ff3c9757276e
chr56 <chr0056@gmail.com>
2024-05-21 11:41:57 +0800

    [Setting] remove legacy `SharedPreferencesMigration`

--- app/src/main/java/player/phonograph/settings/Setting.kt
+++ app/src/main/java/player/phonograph/settings/Setting.kt
@@ -5,10 +5,8 @@
 package player.phonograph.settings
 
 import androidx.datastore.core.DataStore
-import androidx.datastore.preferences.SharedPreferencesMigration
 import androidx.datastore.preferences.core.Preferences
 import androidx.datastore.preferences.preferencesDataStore
-import androidx.preference.PreferenceManager
 import android.content.Context
 
 
@@ -37,9 +35,4 @@ private const val PREFERENCE_NAME = "setting_main"
 private val Context.settingsDatastore: DataStore<Preferences>
         by preferencesDataStore(
             name = PREFERENCE_NAME,
-            produceMigrations = {
-                listOf(
-                    SharedPreferencesMigration({ PreferenceManager.getDefaultSharedPreferences(it) })
-                )
-            }
         )
