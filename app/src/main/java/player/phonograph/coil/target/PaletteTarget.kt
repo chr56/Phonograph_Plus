2c1e742f966d6a4fc2dc97b8f35b20b12af4fda5
chr56 <chr0056@gmail.com>
2024-06-05 23:59:34 +0800

    [PaletteDelegateTarget] extract `Callbacks` to `PaletteTarget`

--- app/src/main/java/player/phonograph/coil/target/PaletteTarget.kt
+++ app/src/main/java/player/phonograph/coil/target/PaletteTarget.kt
@@ -12,40 +12,39 @@ import coil.target.Target
 import kotlinx.coroutines.Deferred
 import player.phonograph.coil.target.PaletteUtil.toPaletteAsync
 
-class PaletteDelegateTarget internal constructor(private val callbacks: Callbacks) : Target {
+interface PaletteTarget {
+    fun onStart(placeholder: Drawable?) {}
+
+    fun onError(error: Drawable?) {}
+
+    fun onSuccess(result: Drawable, palette: Deferred<Palette>?) {}
+}
+
+class PaletteDelegateTarget(private val delegate: PaletteTarget) : Target {
 
     override fun onStart(placeholder: Drawable?) {
-        callbacks.onStart(placeholder)
+        delegate.onStart(placeholder)
     }
 
     override fun onSuccess(result: Drawable) {
         if (result is BitmapDrawable) {
-            callbacks.onSuccess(result, result.bitmap.toPaletteAsync())
+            delegate.onSuccess(result, result.bitmap.toPaletteAsync())
         } else {
             Log.w("PaletteTarget", "Not A Bitmap drawable: $result")
-            callbacks.onSuccess(result, null)
+            delegate.onSuccess(result, null)
         }
     }
 
     override fun onError(error: Drawable?) {
-        callbacks.onError(error)
-    }
-
-    interface Callbacks {
-        fun onStart(placeholder: Drawable?) {}
-
-        fun onError(error: Drawable?) {}
-
-        fun onSuccess(result: Drawable, palette: Deferred<Palette>?) {}
+        delegate.onError(error)
     }
 }
 
-internal fun createBasePaletteTarget(t: PaletteDelegateTarget.Callbacks) = PaletteDelegateTarget(t)
 internal inline fun createBasePaletteTarget(
     crossinline onStart: (placeholder: Drawable?) -> Unit = {},
     crossinline onError: (error: Drawable?) -> Unit = {},
     crossinline onSuccess: (result: Drawable, palette: Deferred<Palette>?) -> Unit = { _, _ -> },
-) = PaletteDelegateTarget(object : PaletteDelegateTarget.Callbacks {
+) = PaletteDelegateTarget(object : PaletteTarget {
     override fun onStart(placeholder: Drawable?) = onStart(placeholder)
     override fun onError(error: Drawable?) = onError(error)
     override fun onSuccess(result: Drawable, palette: Deferred<Palette>?) =
