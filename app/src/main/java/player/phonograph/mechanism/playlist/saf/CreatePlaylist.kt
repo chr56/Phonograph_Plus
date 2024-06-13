d08aea6960906da3cbae23c2d8f19cde0c2a77e1
chr56 <chr0056@gmail.com>
2024-06-13 19:09:57 +0800

    [CreatePlaylist] reimplement `createPlaylistsViaSAF`

--- app/src/main/java/player/phonograph/mechanism/playlist/saf/CreatePlaylist.kt
+++ app/src/main/java/player/phonograph/mechanism/playlist/saf/CreatePlaylist.kt
@@ -19,6 +19,7 @@ import player.phonograph.util.text.currentDate
 import player.phonograph.util.text.dateTimeSuffix
 import player.phonograph.util.warning
 import player.phonograph.mechanism.playlist.m3u.M3UWriter
+import player.phonograph.util.text.dateTimeSuffixCompat
 import android.content.Context
 import android.net.Uri
 import android.provider.DocumentsContract
@@ -83,7 +84,7 @@ suspend fun createPlaylistsViaSAF(
                     context.contentResolver,
                     parentDocumentUri,
                     PLAYLIST_MIME_TYPE,
-                    "${playlist.name}${dateTimeSuffix(currentDate())}"
+                    "${playlist.name}_${dateTimeSuffixCompat(currentDate())}"
                 )
             } catch (e: Exception) {
                 e.printStackTrace()
@@ -119,6 +120,74 @@ suspend fun createPlaylistsViaSAF(
     } else {
         coroutineToast(context, R.string.success)
     }
+}
+
+/**
+ * @param context must be [IOpenFileStorageAccessible]
+ */
+suspend fun createPlaylistsViaSAF(
+    context: Context,
+    songBatches: List<List<Song>>,
+    names: List<String>,
+    location: String,
+) = withContext(Dispatchers.IO) {
+    // check
+    if (songBatches.isEmpty()) return@withContext
+    require(context is IOpenFileStorageAccessible)
+    while (context.openFileStorageAccessDelegate.busy) yield()
+    // launch
+    val treeUri = chooseDirViaSAF(context, location)
+
+    val parentDocumentUri =
+        DocumentsContract.buildDocumentUriUsingTree(treeUri, DocumentsContract.getTreeDocumentId(treeUri))
+
+    // execute
+    val failedToCreate: MutableList<String> = mutableListOf()
+    val failedToWrite: MutableList<String> = mutableListOf()
+
+    for ((index, songBatch) in songBatches.withIndex()) {
+        val childUri: Uri? =
+            try {
+                DocumentsContract.createDocument(
+                    context.contentResolver,
+                    parentDocumentUri,
+                    PLAYLIST_MIME_TYPE,
+                    "${names[index]}${dateTimeSuffix(currentDate())}"
+                )
+            } catch (e: Exception) {
+                e.printStackTrace()
+                failedToCreate.add(names[index])
+                null
+            }
+        if (childUri != null) {
+            openOutputStreamSafe(context, childUri, "rwt")?.use { outputStream ->
+                try {
+                    M3UWriter.write(outputStream, songBatch, true)
+                } catch (e: IOException) {
+                    failedToWrite.add(names[index])
+                    e.printStackTrace()
+                }
+            }
+        }
+    }
+
+    if (failedToCreate.isNotEmpty() || failedToWrite.isNotEmpty()) {
+        coroutineToast(context, R.string.failed)
+        val message = buildString {
+            append("Tree     Uri: ${treeUri.path} \n")
+            append("Document Uri: ${parentDocumentUri.path} \n")
+            for (playlistName in failedToCreate) {
+                append("Failed to create playlist ${playlistName}\n")
+            }
+            for (playlistName in failedToWrite) {
+                append("Failed to write playlist ${playlistName}\n")
+            }
+        }
+        warning(TAG, message)
+    } else {
+        coroutineToast(context, R.string.success)
+    }
+
 }
 
 
