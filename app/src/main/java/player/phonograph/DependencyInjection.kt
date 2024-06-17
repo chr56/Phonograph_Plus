f0855067fcfb7f5e934689e8ff6b3de449dfeb97
chr56 <chr0056@gmail.com>
2024-06-17 22:02:17 +0800

    [LegacyPlaylistModel] remove legacy Playlist model

--- app/src/main/java/player/phonograph/DependencyInjection.kt
+++ app/src/main/java/player/phonograph/DependencyInjection.kt
@@ -8,30 +8,16 @@ import org.koin.dsl.module
 import player.phonograph.mechanism.FavoriteDatabaseImpl
 import player.phonograph.mechanism.FavoritePlaylistImpl
 import player.phonograph.mechanism.event.MediaStoreTracker
-import player.phonograph.model.playlist.FavoriteSongsPlaylist
-import player.phonograph.model.playlist.FilePlaylist
-import player.phonograph.model.playlist.HistoryPlaylist
-import player.phonograph.model.playlist.LastAddedPlaylist
-import player.phonograph.model.playlist.MyTopTracksPlaylist
-import player.phonograph.model.playlist.ShuffleAllPlaylist
 import player.phonograph.repo.database.FavoritesStore
 import player.phonograph.repo.database.HistoryStore
 import player.phonograph.repo.database.PathFilterStore
 import player.phonograph.repo.database.SongPlayCountStore
 import player.phonograph.repo.mediastore.loaders.RecentlyPlayedTracksLoader
 import player.phonograph.repo.mediastore.loaders.TopTracksLoader
-import player.phonograph.repo.mediastore.playlist.FavoriteSongsPlaylistImpl
-import player.phonograph.repo.mediastore.playlist.FilePlaylistImpl
-import player.phonograph.repo.mediastore.playlist.HistoryPlaylistImpl
-import player.phonograph.repo.mediastore.playlist.LastAddedPlaylistImpl
-import player.phonograph.repo.mediastore.playlist.MyTopTracksPlaylistImpl
-import player.phonograph.repo.mediastore.playlist.ShuffleAllPlaylistImpl
 import player.phonograph.service.queue.MusicPlaybackQueueStore
 import player.phonograph.service.queue.QueueManager
 import player.phonograph.settings.Keys
 import player.phonograph.settings.Setting
-import android.content.Context
-import android.os.Parcel
 
 val moduleStatus = module {
     single { QueueManager(get()) }
@@ -53,14 +39,4 @@ val moduleLoaders = module {
     factory { TopTracksLoader(get()) }
     factory { RecentlyPlayedTracksLoader(get()) }
 
-    factory<FavoriteSongsPlaylist> { FavoriteSongsPlaylistImpl(get<Context>()) }
-    factory<HistoryPlaylist> { HistoryPlaylistImpl(get<Context>()) }
-    factory<LastAddedPlaylist> { LastAddedPlaylistImpl(get<Context>()) }
-    factory<MyTopTracksPlaylist> { MyTopTracksPlaylistImpl(get<Context>()) }
-    factory<ShuffleAllPlaylist> { ShuffleAllPlaylistImpl(get<Context>()) }
-
-    // factory<FilePlaylist> { (id: Long, name: String?, path: String, dateAdded: Long, dateModified: Long) ->
-    //     FilePlaylistImpl(id, name, path, dateAdded, dateModified)
-    // }
-    factory<FilePlaylist> { (parcel: Parcel) -> FilePlaylistImpl(parcel) }
 }
