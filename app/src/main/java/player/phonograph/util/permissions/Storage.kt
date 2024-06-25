2e78fc2e103b6732329dfe2bc033df9aa6bebee6
chr56 <chr0056@gmail.com>
2024-06-25 14:05:35 +0800

    [util] split storage permission checker implementations

--- app/src/main/java/player/phonograph/util/permissions/Storage.kt
+++ app/src/main/java/player/phonograph/util/permissions/Storage.kt
@@ -4,10 +4,12 @@
 
 package player.phonograph.util.permissions
 
+import androidx.annotation.RequiresApi
 import android.Manifest.permission.MANAGE_EXTERNAL_STORAGE
 import android.Manifest.permission.READ_EXTERNAL_STORAGE
 import android.Manifest.permission.READ_MEDIA_AUDIO
 import android.Manifest.permission.WRITE_EXTERNAL_STORAGE
+import android.annotation.SuppressLint
 import android.content.Context
 import android.os.Build.VERSION.SDK_INT
 import android.os.Build.VERSION_CODES
@@ -21,19 +23,38 @@ interface IStoragePermissionChecker {
     fun hasStorageWritePermission(context: Context): Boolean
 }
 
-object StoragePermissionChecker : IStoragePermissionChecker {
+val StoragePermissionChecker: IStoragePermissionChecker
+    get() = when {
+        SDK_INT >= VERSION_CODES.R -> StoragePermissionCheckerR
+        else                       -> StoragePermissionCheckerM
+    }
+
+@RequiresApi(VERSION_CODES.R)
+object StoragePermissionCheckerR : IStoragePermissionChecker {
 
     override val necessaryStorageReadPermission: String
         get() = if (SDK_INT >= VERSION_CODES.TIRAMISU) READ_MEDIA_AUDIO else READ_EXTERNAL_STORAGE
     override val necessaryStorageWritePermission: String
-        get() = if (SDK_INT >= VERSION_CODES.R) MANAGE_EXTERNAL_STORAGE else WRITE_EXTERNAL_STORAGE
+        get() = MANAGE_EXTERNAL_STORAGE
 
     override fun hasStorageReadPermission(context: Context): Boolean =
         hasPermission(context, necessaryStorageReadPermission)
 
-    override fun hasStorageWritePermission(context: Context): Boolean = when {
-        /** check [MANAGE_EXTERNAL_STORAGE] on Android R and above **/
-        SDK_INT >= VERSION_CODES.R -> Environment.isExternalStorageManager()
-        else                       -> hasPermission(context, WRITE_EXTERNAL_STORAGE)
-    }
+    /** check [MANAGE_EXTERNAL_STORAGE] on Android R and above **/
+    override fun hasStorageWritePermission(context: Context): Boolean =
+        Environment.isExternalStorageManager()
+}
+
+@SuppressLint("ObsoleteSdkInt")
+@RequiresApi(VERSION_CODES.M)
+object StoragePermissionCheckerM : IStoragePermissionChecker {
+
+    override val necessaryStorageReadPermission: String = READ_EXTERNAL_STORAGE
+    override val necessaryStorageWritePermission: String = WRITE_EXTERNAL_STORAGE
+
+    override fun hasStorageReadPermission(context: Context): Boolean =
+        hasPermission(context, necessaryStorageReadPermission)
+
+    override fun hasStorageWritePermission(context: Context): Boolean =
+        hasPermission(context, necessaryStorageWritePermission)
 }
