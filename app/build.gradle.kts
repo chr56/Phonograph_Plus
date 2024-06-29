122e6212f744481e3b059ef1196e4fafe19b0cf0
chr56 <chr0056@gmail.com>
2024-06-29 14:59:43 +0800

    [gradle] remove Compose compiler version check suppression
    `suppressKotlinVersionCompatibilityCheck`

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -147,10 +147,6 @@ android {
 
     kotlinOptions {
         jvmTarget = "17"
-        freeCompilerArgs = listOf(
-            "-P",
-            "plugin:androidx.compose.compiler.plugins.kotlin:suppressKotlinVersionCompatibilityCheck=true"
-        )
     }
 
 }
