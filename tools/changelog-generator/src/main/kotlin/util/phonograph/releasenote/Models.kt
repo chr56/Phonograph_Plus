cbc92ab97500d77d3c5fc0ad58d52dc010bb2275
chr56 <chr0056@gmail.com>
2024-07-04 14:53:58 +0800

    [changelog-generator] model: improve & enhance

--- tools/changelog-generator/src/main/kotlin/util/phonograph/releasenote/Models.kt
+++ tools/changelog-generator/src/main/kotlin/util/phonograph/releasenote/Models.kt
@@ -18,16 +18,49 @@ data class ReleaseNote(
     val previousChannel: ReleaseChannel,
 ) {
 
-    val tag: String
-        get() = when (channel) {
-            ReleaseChannel.PREVIEW -> "preview_$version"
-            ReleaseChannel.STABLE  -> "v$version"
-            ReleaseChannel.LTS     -> "v$version"
-        }
+    val tag: String get() = tagOf(version, channel)
+    val previousTag: String get() = tagOf(previousVersion, previousChannel)
+
+    fun variant(variant: TargetVariant): String = variantOf(variant, channel)
 
     fun language(lang: Language): Notes.Note = when (lang) {
         Language.EN -> notes.en
         Language.ZH -> notes.zh
+    }
+
+    companion object {
+        private fun tagOf(version: String, channel: ReleaseChannel): String =
+            when (channel) {
+                ReleaseChannel.PREVIEW -> "preview_$version"
+                ReleaseChannel.STABLE  -> "v$version"
+                ReleaseChannel.LTS     -> "v$version"
+            }
+
+        private fun variantOf(
+            variant: TargetVariant,
+            channel: ReleaseChannel,
+            buildType: BuildType = BuildType.RELEASE,
+        ): String =
+            buildString {
+                append(
+                    when (variant) {
+                        TargetVariant.MODERN -> "Modern"
+                        TargetVariant.LEGACY -> "Legacy"
+                    }
+                )
+                append(
+                    when (channel) {
+                        ReleaseChannel.PREVIEW -> "Preview"
+                        else                   -> "Stable"
+                    }
+                )
+                append(
+                    when (buildType) {
+                        BuildType.RELEASE -> "Release"
+                        BuildType.DEBUG   -> "Debug"
+                    }
+                )
+            }
     }
 }
 
@@ -50,6 +83,20 @@ enum class ReleaseChannel {
     STABLE,
     PREVIEW,
     LTS,
+    ;
+}
+
+@Serializable
+enum class TargetVariant {
+    MODERN,
+    LEGACY,
+    ;
+}
+
+@Serializable
+enum class BuildType {
+    RELEASE,
+    DEBUG,
     ;
 }
 
