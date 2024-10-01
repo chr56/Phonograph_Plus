851ee734c9fa81e08ec689fd534b47aa89981655
chr56 <chr0056@gmail.com>
2024-10-01 20:28:35 +0800

    [MusicDatabase] add test options of `MusicDatabase`

--- app/src/main/java/player/phonograph/ui/modules/setting/SettingsActivity.kt
+++ app/src/main/java/player/phonograph/ui/modules/setting/SettingsActivity.kt
@@ -18,6 +18,8 @@ import lib.storage.launcher.OpenFileStorageAccessDelegate
 import player.phonograph.R
 import player.phonograph.mechanism.SettingDataManager
 import player.phonograph.mechanism.backup.Backup
+import player.phonograph.repo.room.DatabaseUtil
+import player.phonograph.repo.room.MusicDatabase
 import player.phonograph.ui.compose.ComposeThemeActivity
 import player.phonograph.ui.compose.PhonographTheme
 import player.phonograph.ui.compose.components.DropDownMenuContent
@@ -130,6 +132,7 @@ class SettingsActivity : ComposeThemeActivity(),
                 menuItemClearAll(context),
                 menuItemImport(context),
                 menuItemExport(),
+                menuItemDeleteDatabase(),
             )
         )
     }
@@ -177,6 +180,14 @@ class SettingsActivity : ComposeThemeActivity(),
     private fun menuItemExport(): Pair<String, Function0<Unit>> =
         stringResource(id = R.string.action_export).format(stringResource(id = R.string.action_backup)) to {
             BackupExportDialog().show(supportFragmentManager, "EXPORT")
+        }
+
+    @Composable
+    private fun menuItemDeleteDatabase(): Pair<String, Function0<Unit>> =
+        "Delete Database" to {
+            lifecycleScope.launch(Dispatchers.IO) {
+                DatabaseUtil.deleteEntireDatabase(this@SettingsActivity, MusicDatabase.koinInstance)
+            }
         }
 
 
