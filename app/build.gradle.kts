08b507a87ca72cb01bd0eb251a6d7ec379579387
chr56 <chr0056@gmail.com>
2024-10-17 16:58:35 +0800

    [toolchain] compile with SDK 35

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -23,8 +23,8 @@ if (isSigningFileExist) {
 }
 
 android {
-    compileSdk = 34
-    buildToolsVersion = "34.0.0"
+    compileSdk = 35
+    buildToolsVersion = "35.0.0"
     namespace = "player.phonograph"
 
     val appName = "Phonograph Plus"
