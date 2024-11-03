1969e07eb1b01da0c6d7a9e492cb3032a29641b9
chr56 <chr0056@gmail.com>
2024-11-03 12:39:46 +0800

    [GestureExclusiveLayout] using GestureExclusiveLayout

---
+++ app/src/main/java/player/phonograph/ui/views/GestureExclusiveFrameLayout.kt
@@ -0,0 +1,40 @@
+/*
+ *  Copyright (c) 2022~2024 chr_56
+ */
+
+package player.phonograph.ui.views
+
+import android.content.Context
+import android.graphics.Canvas
+import android.graphics.Rect
+import android.os.Build.VERSION.SDK_INT
+import android.os.Build.VERSION_CODES
+import android.util.AttributeSet
+import android.util.Log
+import android.widget.FrameLayout
+
+class GestureExclusiveFrameLayout : FrameLayout {
+
+    constructor(context: Context) : super(context)
+
+    constructor(context: Context, attrs: AttributeSet?) :
+            super(context, attrs)
+
+    constructor(context: Context, attrs: AttributeSet?, defStyleAttr: Int) :
+            super(context, attrs, defStyleAttr)
+
+
+    private val rect: Rect = Rect()
+    private val exclusionAreaWidth: Int = 400
+
+    override fun onDraw(canvas: Canvas) {
+        super.onDraw(canvas)
+        if (SDK_INT >= VERSION_CODES.Q) {
+            rect.set(0, 0, exclusionAreaWidth, height)
+            systemGestureExclusionRects = systemGestureExclusionRects + listOf(rect)
+            player.phonograph.util.debug {
+                Log.v("GestureExclusiveLayout", "Exclude: $rect")
+            }
+        }
+    }
+}
