67fd025f8df8140352de729c967a5c45e52867a8
chr56 <chr0056@gmail.com>
2024-11-09 11:46:51 +0800

    [theme] handle system bar color manually

--- app/src/main/java/player/phonograph/util/theme/SystemUI.kt
+++ app/src/main/java/player/phonograph/util/theme/SystemUI.kt
@@ -8,10 +8,10 @@ import player.phonograph.App
 import player.phonograph.R
 import player.phonograph.settings.Keys
 import player.phonograph.settings.Setting
-import util.theme.activity.setNavigationBarColor
-import util.theme.activity.setStatusbarColor
 import util.theme.activity.setTaskDescriptionColor
 import util.theme.color.darkenColor
+import util.theme.color.isColorLight
+import androidx.core.view.WindowInsetsControllerCompat
 import android.app.Activity
 import android.graphics.Color
 import android.view.View
@@ -20,15 +20,22 @@ import android.view.View
  * update Statusbar Color following current setting
  */
 fun Activity.updateStatusbarColor(color: Int = darkenColor(primaryColor())) {
-    setStatusbarColor(darkenColor(color), R.id.status_bar)
+    val backgroundColor = darkenColor(color)
+    @Suppress("DEPRECATION")
+    window.statusBarColor = backgroundColor
+    window.decorView.rootView.findViewById<View>(R.id.status_bar)?.setBackgroundColor(backgroundColor)
+    WindowInsetsControllerCompat(window, window.decorView).isAppearanceLightStatusBars = isColorLight(backgroundColor)
 }
 
 /**
  * update Navigation Color following current setting
  */
 fun Activity.updateNavigationbarColor(color: Int = darkenColor(primaryColor())) {
-    val targetColor = if (coloredNavigationBar) color else Color.BLACK
-    setNavigationBarColor(targetColor)
+    val backgroundColor = if (coloredNavigationBar) color else Color.BLACK
+    @Suppress("DEPRECATION")
+    window.navigationBarColor = backgroundColor
+    // window.decorView.rootView.findViewById<View>(R.id.navigation_bar)?.setBackgroundColor(backgroundColor)
+    WindowInsetsControllerCompat(window, window.decorView).isAppearanceLightNavigationBars = isColorLight(backgroundColor)
 }
 
 fun Activity.updateTaskDescriptionColor(color: Int = darkenColor(primaryColor())) = setTaskDescriptionColor(color)
