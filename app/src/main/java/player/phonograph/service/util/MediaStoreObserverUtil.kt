5de09f110900a81919c26e29128fff3247ebf059
chr56 <chr0056@gmail.com>
2024-11-11 10:52:20 +0800

    [MediaStoreObserverUtil] cleanup

--- app/src/main/java/player/phonograph/service/util/MediaStoreObserverUtil.kt
+++ app/src/main/java/player/phonograph/service/util/MediaStoreObserverUtil.kt
@@ -1,97 +1,59 @@
 /*
- * Copyright (c) 2022 chr_56 & Abou Zeid (kabouzeid) (original author)
+ * Copyright (c) 2022-2024 chr_56 & Abou Zeid (kabouzeid) (original author)
  */
 
 package player.phonograph.service.util
 
-import legacy.phonograph.MediaStoreCompat
+import legacy.phonograph.MediaStoreCompat.Audio.Playlists
+import player.phonograph.MusicServiceMsgConst
 import android.content.Context
 import android.database.ContentObserver
 import android.os.Handler
-import android.provider.MediaStore
-import player.phonograph.MusicServiceMsgConst
-import player.phonograph.service.MusicService
+import android.provider.MediaStore.Audio.Albums
+import android.provider.MediaStore.Audio.Artists
+import android.provider.MediaStore.Audio.Genres
+import android.provider.MediaStore.Audio.Media
 
 class MediaStoreObserverUtil {
 
-    var mediaStoreObserver: MediaStoreObserver? = null
+    lateinit var mediaStoreObserver: MediaStoreObserver
 
     fun setUpMediaStoreObserver(
         context: Context,
-        playerHandler: Handler, // todo
-        handleAndSendChangeInternalCallback: (String) -> Unit
+        playerHandler: Handler,
+        handleAndSendChangeInternalCallback: (String) -> Unit,
     ) {
         mediaStoreObserver = MediaStoreObserver(playerHandler, handleAndSendChangeInternalCallback)
-        with(context) {
-            contentResolver.registerContentObserver(
-                MediaStore.Audio.Media.EXTERNAL_CONTENT_URI,
-                true,
-                mediaStoreObserver!!
-            )
-            contentResolver.registerContentObserver(
-                MediaStore.Audio.Albums.EXTERNAL_CONTENT_URI,
-                true,
-                mediaStoreObserver!!
-            )
-            contentResolver.registerContentObserver(
-                MediaStore.Audio.Artists.EXTERNAL_CONTENT_URI,
-                true,
-                mediaStoreObserver!!
-            )
-            contentResolver.registerContentObserver(
-                MediaStore.Audio.Genres.EXTERNAL_CONTENT_URI,
-                true,
-                mediaStoreObserver!!
-            )
-            contentResolver.registerContentObserver(
-                MediaStoreCompat.Audio.Playlists.EXTERNAL_CONTENT_URI,
-                true,
-                mediaStoreObserver!!
-            )
+        with(context.contentResolver) {
+            registerContentObserver(Media.EXTERNAL_CONTENT_URI, true, mediaStoreObserver)
+            registerContentObserver(Albums.EXTERNAL_CONTENT_URI, true, mediaStoreObserver)
+            registerContentObserver(Artists.EXTERNAL_CONTENT_URI, true, mediaStoreObserver)
+            registerContentObserver(Genres.EXTERNAL_CONTENT_URI, true, mediaStoreObserver)
+            registerContentObserver(Playlists.EXTERNAL_CONTENT_URI, true, mediaStoreObserver)
 
-            contentResolver.registerContentObserver(
-                MediaStore.Audio.Media.INTERNAL_CONTENT_URI,
-                true,
-                mediaStoreObserver!!
-            )
-            contentResolver.registerContentObserver(
-                MediaStore.Audio.Albums.INTERNAL_CONTENT_URI,
-                true,
-                mediaStoreObserver!!
-            )
-            contentResolver.registerContentObserver(
-                MediaStore.Audio.Artists.INTERNAL_CONTENT_URI,
-                true,
-                mediaStoreObserver!!
-            )
-            contentResolver.registerContentObserver(
-                MediaStore.Audio.Genres.INTERNAL_CONTENT_URI,
-                true,
-                mediaStoreObserver!!
-            )
-            contentResolver.registerContentObserver(
-                MediaStoreCompat.Audio.Playlists.INTERNAL_CONTENT_URI,
-                true,
-                mediaStoreObserver!!
-            )
+            registerContentObserver(Media.INTERNAL_CONTENT_URI, true, mediaStoreObserver)
+            registerContentObserver(Albums.INTERNAL_CONTENT_URI, true, mediaStoreObserver)
+            registerContentObserver(Artists.INTERNAL_CONTENT_URI, true, mediaStoreObserver)
+            registerContentObserver(Genres.INTERNAL_CONTENT_URI, true, mediaStoreObserver)
+            registerContentObserver(Playlists.INTERNAL_CONTENT_URI, true, mediaStoreObserver)
         }
     }
 
     fun unregisterMediaStoreObserver(context: Context) {
-        context.contentResolver.unregisterContentObserver(mediaStoreObserver!!)
+        context.contentResolver.unregisterContentObserver(mediaStoreObserver)
     }
 
     class MediaStoreObserver(
-        private val mHandler: Handler,
-        private val handleAndSendChangeInternalCallback: (String) -> Unit
-    ) : ContentObserver(mHandler), Runnable {
+        private val playerHandler: Handler,
+        private val handleAndSendChangeInternalCallback: (String) -> Unit,
+    ) : ContentObserver(playerHandler), Runnable {
 
         override fun onChange(selfChange: Boolean) {
             // if a change is detected, remove any scheduled callback
             // then post a new one. This is intended to prevent closely
             // spaced events from generating multiple refresh calls
-            mHandler.removeCallbacks(this)
-            mHandler.postDelayed(this, REFRESH_DELAY)
+            playerHandler.removeCallbacks(this)
+            playerHandler.postDelayed(this, REFRESH_DELAY)
         }
 
         override fun run() {
