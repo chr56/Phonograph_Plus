dbb373d4dc1e7616f987a66283978caefc5cc307
chr56 <chr0056@gmail.com>
2025-02-12 15:04:23 +0800

    [PlaylistToolbarMenuProvider] add `Clean` action for favorite playlist

--- app/src/main/java/player/phonograph/ui/modules/playlist/PlaylistToolbarMenuProvider.kt
+++ app/src/main/java/player/phonograph/ui/modules/playlist/PlaylistToolbarMenuProvider.kt
@@ -6,20 +6,27 @@ package player.phonograph.ui.modules.playlist
 
 import com.github.chr56.android.menu_dsl.attach
 import com.github.chr56.android.menu_dsl.menuItem
+import com.google.android.material.dialog.MaterialAlertDialogBuilder
+import org.koin.core.context.GlobalContext
 import player.phonograph.R
+import player.phonograph.mechanism.IFavorite
 import player.phonograph.mechanism.actions.DetailToolbarMenuProviders
 import player.phonograph.model.UIMode
+import player.phonograph.model.playlist.PLAYLIST_TYPE_FAVORITE
 import player.phonograph.model.playlist.PLAYLIST_TYPE_LAST_ADDED
 import player.phonograph.model.playlist.Playlist
 import player.phonograph.model.playlist.VirtualPlaylistLocation
 import player.phonograph.ui.dialogs.LastAddedPlaylistIntervalDialog
 import player.phonograph.util.fragmentActivity
 import player.phonograph.util.theme.getTintedDrawable
+import player.phonograph.util.theme.tintButtons
 import androidx.activity.ComponentActivity
 import androidx.lifecycle.DefaultLifecycleObserver
 import androidx.lifecycle.LifecycleOwner
+import androidx.lifecycle.lifecycleScope
 import android.view.Menu
 import android.view.MenuItem
+import kotlinx.coroutines.launch
 
 class PlaylistToolbarMenuProvider(
     private val onAction: (PlaylistAction) -> Boolean,
@@ -58,25 +65,62 @@ class PlaylistToolbarMenuProvider(
                     }
                 } else {
                     val location = item.location
-                    if (location is VirtualPlaylistLocation && location.type == PLAYLIST_TYPE_LAST_ADDED) {
-                        menuItem {
-                            itemId = R.id.action_setting_last_added_interval
-                            title = getString(R.string.pref_title_last_added_interval)
-                            icon = getTintedDrawable(R.drawable.ic_timer_white_24dp, iconColor)
-                            onClick {
-                                fragmentActivity(context) { activity ->
-                                    val dialog = LastAddedPlaylistIntervalDialog()
-                                    dialog.show(activity.supportFragmentManager, "LAST_ADDED")
-                                    dialog.lifecycle.addObserver(object : DefaultLifecycleObserver {
-                                        override fun onDestroy(owner: LifecycleOwner) {
-                                            onAction(Refresh(fetch = true))
+                    if (location is VirtualPlaylistLocation) {
+                        when (location.type) {
+                            PLAYLIST_TYPE_LAST_ADDED -> {
+                                menuItem {
+                                    itemId = R.id.action_setting_last_added_interval
+                                    title = getString(R.string.pref_title_last_added_interval)
+                                    icon = getTintedDrawable(R.drawable.ic_timer_white_24dp, iconColor)
+                                    onClick {
+                                        fragmentActivity(context) { activity ->
+                                            val dialog = LastAddedPlaylistIntervalDialog()
+                                            dialog.show(activity.supportFragmentManager, "LAST_ADDED")
+                                            dialog.lifecycle.addObserver(object : DefaultLifecycleObserver {
+                                                override fun onDestroy(owner: LifecycleOwner) {
+                                                    onAction(Refresh(fetch = true))
+                                                }
+                                            })
+                                            true
                                         }
-                                    })
-                                    true
+                                        true
+                                    }
                                 }
-                                true
                             }
+
+                            PLAYLIST_TYPE_FAVORITE   -> {
+                                menuItem {
+                                    title = getString(R.string.action_clean)
+                                    icon = getTintedDrawable(R.drawable.ic_cleaning_bucket_24dp, iconColor)
+                                    onClick {
+                                        fragmentActivity(context) { activity ->
+                                            MaterialAlertDialogBuilder(context)
+                                                .setTitle(R.string.action_clean)
+                                                .setMessage(R.string.action_clean_missing_items)
+                                                .setPositiveButton(getString(android.R.string.ok)) { dialog, _ ->
+                                                    context.lifecycleScope.launch {
+                                                        val favorite = GlobalContext.get().get<IFavorite>()
+                                                        favorite.cleanMissed(context)
+                                                    }
+                                                    dialog.dismiss()
+                                                }
+                                                .setNegativeButton(getString(android.R.string.cancel)) { dialog, _ ->
+                                                    dialog.dismiss()
+                                                }
+                                                .create()
+                                                .tintButtons()
+                                                .show()
+                                            true
+                                        }
+                                        true
+                                    }
+                                }
+                            }
+
+
+                            else                     -> {}
                         }
+
                     }
                 }
             }
