313ad035e9c2cdf5888021584592ddf96b1e65ee
chr56 <chr0056@gmail.com>
2025-02-27 16:57:17 +0800

    [MusicTagFormat] better name

--- app/src/main/java/player/phonograph/model/metadata/MusicTagFormat.kt
+++ app/src/main/java/player/phonograph/model/metadata/MusicTagFormat.kt
@@ -9,19 +9,19 @@ import androidx.annotation.Keep
 @Keep
 enum class MusicTagFormat(val id: String) {
 
-    ID3v1("ID3v1Tag"),
-    ID3v11("ID3v11Tag"),
-    ID3v24("ID3v24Tag"),
-    ID3v22("ID3v22Tag"),
-    ID3v23("ID3v23Tag"),
-    Mp4("Mp4Tag"),
-    VorbisComment("VorbisCommentTag"),
-    Flac("FlacTag"),
-    Aiff("AiffTag"),
-    Asf("AsfTag"),
-    Real("RealTag"),
-    Wav("WavTag"),
-    WavInfo("WavInfoTag"),
+    Mp4("Mpeg-4 Metadata"),
+    ID3v24("ID3 v2.4"),
+    ID3v23("ID3 v2.3"),
+    ID3v22("ID3 v2.2"),
+    ID3v11("ID3 v1.1"),
+    ID3v1("ID3 v1"),
+    Flac("Vorbis Comment for FLAC"),
+    Aiff("ID3 for Aiff"),
+    Asf("ASF for WMA"),
+    Real("Real Metadata"),
+    Wav("ID3 for WAV"),
+    WavInfo("WAV List Info"),
+    VorbisComment("Vorbis Comment"),
     Unknown("N/A"),
     ;
 }
