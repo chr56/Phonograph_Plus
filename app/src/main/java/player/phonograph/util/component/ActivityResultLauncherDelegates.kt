39f414d69766023c8ef851652bc86b8810fceb87
chr56 <chr0056@gmail.com>
2025-03-03 09:58:49 +0800

    [util] move `ActivityResultLauncherDelegates`

--- app/src/main/java/player/phonograph/util/component/ActivityResultLauncherDelegates.kt
+++ app/src/main/java/player/phonograph/util/component/ActivityResultLauncherDelegates.kt
@@ -1,8 +1,8 @@
 /*
- *  Copyright (c) 2022~2024 chr_56
+ *  Copyright (c) 2022~2025 chr_56
  */
 
-package player.phonograph.util
+package player.phonograph.util.component
 
 import lib.activityresultcontract.ActivityResultLauncherDelegate
 import androidx.activity.result.contract.ActivityResultContract
