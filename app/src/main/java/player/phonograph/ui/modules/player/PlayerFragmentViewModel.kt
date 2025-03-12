1ce459e89d46e05f0d7dfcd72e3c25d952eda3b0
chr56 <chr0056@gmail.com>
2025-03-12 20:39:09 +0800

    [PlayerAlbumCover] simplify image loading

--- app/src/main/java/player/phonograph/ui/modules/player/PlayerFragmentViewModel.kt
+++ app/src/main/java/player/phonograph/ui/modules/player/PlayerFragmentViewModel.kt
@@ -4,39 +4,31 @@
 
 package player.phonograph.ui.modules.player
 
+import coil.request.Disposable
+import coil.request.Parameters
+import coil.target.Target
 import org.koin.core.context.GlobalContext
 import player.phonograph.R
-import player.phonograph.coil.cache.AbsPreloadImageCache
+import player.phonograph.coil.PARAMETERS_KEY_PALETTE
+import player.phonograph.coil.PARAMETERS_KEY_QUICK_CACHE
 import player.phonograph.coil.loadImage
 import player.phonograph.coil.palette.PaletteColorTarget
 import player.phonograph.mechanism.IFavorite
-import player.phonograph.model.PaletteBitmap
 import player.phonograph.model.Song
 import player.phonograph.service.MusicPlayerRemote
-import player.phonograph.util.concurrent.withTimeoutOrNot
 import player.phonograph.util.text.buildInfoString
 import player.phonograph.util.text.readableDuration
 import player.phonograph.util.theme.themeFooterColor
 import androidx.annotation.ColorInt
-import androidx.appcompat.content.res.AppCompatResources
-import androidx.core.graphics.drawable.toBitmap
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import android.content.Context
 import android.content.res.Resources
-import android.graphics.Bitmap
 import android.graphics.Color
-import android.graphics.drawable.BitmapDrawable
-import kotlinx.coroutines.Deferred
-import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.Job
-import kotlinx.coroutines.TimeoutCancellationException
-import kotlinx.coroutines.async
-import kotlinx.coroutines.cancel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.launch
-import kotlinx.coroutines.suspendCancellableCoroutine
 
 class PlayerFragmentViewModel : ViewModel() {
 
@@ -87,21 +79,29 @@ class PlayerFragmentViewModel : ViewModel() {
     private val _paletteColor: MutableStateFlow<Int> = MutableStateFlow(Color.GRAY)
     val paletteColor get() = _paletteColor.asStateFlow()
 
-    private var fetcherDeferred: Deferred<Int>? = null
+    private var disposable: Disposable? = null
     fun refreshPaletteColor(context: Context, song: Song) {
-        fetcherDeferred?.cancel()
-        fetcherDeferred = viewModelScope.async {
-            preloadImageCache.fetch(context, song).paletteColor
-        }
-        viewModelScope.launch {
-            val current = fetcherDeferred
-            if (current != null && !current.isCancelled) {
-                val color = current.await()
-                if (current == fetcherDeferred) {
-                    _paletteColor.emit(color)
-                }
-            }
-        }
+        disposable?.dispose()
+        disposable = loadImage(context)
+            .from(song)
+            .parameters(
+                Parameters.Builder()
+                    .set(PARAMETERS_KEY_PALETTE, true)
+                    .set(PARAMETERS_KEY_QUICK_CACHE, true)
+                    .build()
+            )
+            .into(
+                PaletteColorTarget(
+                    start = { _, color ->
+                        _paletteColor.value = color
+                    },
+                    success = { _, color ->
+                        _paletteColor.value = color
+                    },
+                    defaultColor = themeFooterColor(context),
+                )
+            )
+            .enqueue()
     }
 
     fun refreshPaletteColor(@ColorInt color: Int) {
@@ -110,51 +110,6 @@ class PlayerFragmentViewModel : ViewModel() {
         }
     }
 
-    private val preloadImageCache: PaletteBitmapPreloadImageCache = PaletteBitmapPreloadImageCache(6)
-
-    class PaletteBitmapPreloadImageCache(size: Int) :
-            AbsPreloadImageCache<Song, PaletteBitmap>(size, IMPL_LRU) {
-
-        override suspend fun load(context: Context, key: Song): PaletteBitmap =
-            loadImage(context, key, 2000)
-
-        override fun id(key: Song): Long = key.id
-
-        private suspend fun loadImage(context: Context, song: Song, timeout: Long): PaletteBitmap =
-            try {
-                withTimeoutOrNot(timeout, Dispatchers.IO) {
-                    suspendCancellableCoroutine { continuation ->
-                        loadImage(context)
-                            .from(song)
-                            .withPalette()
-                            .into(
-                                PaletteColorTarget(
-                                    success = { drawable, color ->
-                                        if (drawable is BitmapDrawable) {
-                                            continuation.resume(PaletteBitmap(drawable.bitmap, color)) { tr, _, _ ->
-                                                cancel("", tr)
-                                            }
-                                        } else {
-                                            continuation.cancel()
-                                        }
-                                    },
-                                    defaultColor = themeFooterColor(context),
-                                )
-                            )
-                            .enqueue()
-                    }
-                }
-            } catch (e: TimeoutCancellationException) {
-                PaletteBitmap(
-                    AppCompatResources.getDrawable(context, R.drawable.default_album_art)!!.toBitmap(),
-                    themeFooterColor(context)
-                )
-            }
-
-    }
-
-    suspend fun fetchBitmap(context: Context, song: Song): Bitmap =
-        preloadImageCache.fetch(context, song).bitmap
     //endregion
 
 }
