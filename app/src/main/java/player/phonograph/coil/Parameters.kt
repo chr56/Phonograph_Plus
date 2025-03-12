123408879f55b8b170ef57ee5f9cc71ad6bbfdfa
chr56 <chr0056@gmail.com>
2025-03-12 21:24:15 +0800

    [coil] RetrieverDispatchingInterceptor: initial implementation

--- app/src/main/java/player/phonograph/coil/Parameters.kt
+++ app/src/main/java/player/phonograph/coil/Parameters.kt
@@ -10,6 +10,8 @@ const val PARAMETERS_KEY_PALETTE = "palette"
 const val PARAMETERS_KEY_RAW = "raw"
 const val PARAMETERS_KEY_QUICK_CACHE = "quick_cache"
 
+const val PARAMETERS_KEY_IMAGE_SOURCE_CONFIG = "image_source"
+
 
 fun Parameters.raw(default: Boolean): Boolean = value(PARAMETERS_KEY_RAW) ?: default
 fun Parameters.palette(default: Boolean): Boolean = value(PARAMETERS_KEY_PALETTE) ?: default
--- app/src/main/java/player/phonograph/coil/retriever/Config.kt
+++ app/src/main/java/player/phonograph/coil/retriever/Config.kt
@@ -6,6 +6,15 @@ package player.phonograph.coil.retriever
 
 import player.phonograph.mechanism.setting.CoilImageConfig
 import player.phonograph.model.coil.ImageSource
+import player.phonograph.model.coil.ImageSourceConfig
+import android.content.Context
+import android.util.Log
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 
 internal val retrieverFromConfig: List<ImageRetriever>
     get() {
@@ -19,4 +28,19 @@ internal fun ImageSource.retriever(): ImageRetriever = when (key) {
     ImageSource.IMAGE_SOURCE_J_AUDIO_TAGGER           -> JAudioTaggerRetriever()
     ImageSource.IMAGE_SOURCE_EXTERNAL_FILE            -> ExternalFileRetriever()
     else                                  -> throw IllegalArgumentException("Unknown ImageSource: $key")
+}
+
+private var sourceConfig: ImageSourceConfig? = null
+private var sourceConfigJob: Job? = null
+
+suspend fun collectSourceConfig(context: Context): ImageSourceConfig = sourceConfig ?: run {
+    val imageSourceConfigFlow = CoilImageConfig.currentImageSourceConfig(context)
+    if (sourceConfigJob == null) {
+        sourceConfigJob = CoroutineScope(Dispatchers.IO).launch {
+            imageSourceConfigFlow.collect { sourceConfig = it }
+        }
+    } else {
+        Log.d("ImageSourceConfig", "`collectSourceConfig` is already called once!")
+    }
+    return withContext(Dispatchers.IO) { imageSourceConfigFlow.first() }
 }
