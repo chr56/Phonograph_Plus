3d11e4cafb66eac1e12e6d5a400f2375682c12dd
chr56 <chr0056@gmail.com>
2025-04-16 16:13:54 +0800

    [settings-ui] NowPlayingScreenStylePreferenceDialog: initial implementation

--- app/src/main/java/player/phonograph/ui/modules/setting/PreferenceScreen.kt
+++ app/src/main/java/player/phonograph/ui/modules/setting/PreferenceScreen.kt
@@ -36,6 +36,7 @@ import player.phonograph.ui.modules.setting.dialog.MaterialColorPickerDialog
 import player.phonograph.ui.modules.setting.dialog.MonetColorPickerDialog
 import player.phonograph.ui.modules.setting.dialog.NotificationActionsConfigDialog
 import player.phonograph.ui.modules.setting.dialog.NowPlayingScreenPreferenceDialog
+import player.phonograph.ui.modules.setting.dialog.NowPlayingScreenStylePreferenceDialog
 import player.phonograph.ui.modules.setting.dialog.PathFilterPreferenceDialog
 import player.phonograph.util.NavigationUtil
 import player.phonograph.util.reportError
@@ -119,6 +120,12 @@ fun PhonographPreferenceScreen() {
                         val screen = Setting(context).Composites[Keys.nowPlayingScreen].flowData()
                         context.getString(screen.titleRes)
                     }
+                ))
+            DialogPref(
+                model = DialogPreferenceModel(
+                    dialog = NowPlayingScreenStylePreferenceDialog::class.java,
+                    titleRes = R.string.pref_title_player_style,
+                    summaryRes = R.string.pref_title_now_playing_screen_style
                 ))
             LibraryCategoriesSetting()
             BooleanPref(
