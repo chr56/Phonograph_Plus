30ed7a2b3a83d12886bba9c7c1f5de6cc4a16fc6
chr56 <chr0056@gmail.com>
2025-04-17 18:33:06 +0800

    [utils] extend `getTintedDrawable`

--- app/src/main/java/player/phonograph/util/theme/DrawableTint.kt
+++ app/src/main/java/player/phonograph/util/theme/DrawableTint.kt
@@ -9,40 +9,43 @@ import androidx.annotation.DrawableRes
 import androidx.core.content.res.ResourcesCompat
 import androidx.core.graphics.BlendModeColorFilterCompat
 import androidx.core.graphics.BlendModeCompat
-import androidx.core.graphics.drawable.DrawableCompat
+import androidx.fragment.app.Fragment
 import android.content.Context
+import android.content.res.Resources
 import android.graphics.drawable.Drawable
+import android.view.View
 
+
+fun Drawable.withColorFilter(
+    @ColorInt color: Int,
+    mode: BlendModeCompat = BlendModeCompat.SRC_IN,
+): Drawable = apply { colorFilter = blendColorFilter(color, mode) }
+
+fun Resources.getTintedDrawable(
+    @DrawableRes id: Int,
+    @ColorInt color: Int,
+    theme: Resources.Theme? = null,
+    mode: BlendModeCompat = BlendModeCompat.SRC_IN,
+): Drawable? = ResourcesCompat.getDrawable(this, id, theme)?.withColorFilter(color, mode)
 
 fun Context.getTintedDrawable(
     @DrawableRes id: Int,
     @ColorInt color: Int,
     mode: BlendModeCompat = BlendModeCompat.SRC_IN,
-): Drawable? {
-    return ResourcesCompat.getDrawable(this.resources, id, theme)?.also {
-        it.colorFilter = drawableColorFilter(color, mode)
-    }
-}
+): Drawable? = resources.getTintedDrawable(id, color, theme, mode)
 
-
-fun Drawable.makeTintedDrawable(
-    @ColorInt color: Int,
-    mode: BlendModeCompat = BlendModeCompat.SRC_IN,
-): Drawable {
-    return DrawableCompat.wrap(this).also { newDrawable ->
-        newDrawable.colorFilter = drawableColorFilter(color, mode)
-    }
-}
-
-fun Context.createTintedDrawable(
+fun Fragment.getTintedDrawable(
     @DrawableRes id: Int,
     @ColorInt color: Int,
     mode: BlendModeCompat = BlendModeCompat.SRC_IN,
-): Drawable? {
-    val drawable = ResourcesCompat.getDrawable(resources, id, theme) ?: return null
-    return drawable.makeTintedDrawable(color, mode)
-}
+): Drawable? = resources.getTintedDrawable(id, color, null, mode)
+
+fun View.getTintedDrawable(
+    @DrawableRes id: Int,
+    @ColorInt color: Int,
+    mode: BlendModeCompat = BlendModeCompat.SRC_IN,
+): Drawable? = resources.getTintedDrawable(id, color, context.theme, mode)
 
 
-fun drawableColorFilter(color: Int, mode: BlendModeCompat) =
+fun blendColorFilter(color: Int, mode: BlendModeCompat) =
     BlendModeColorFilterCompat.createBlendModeColorFilterCompat(color, mode)
