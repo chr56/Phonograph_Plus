98ce3d289147c5ec119cced6e73bcf82c3d7c3b2
chr56 <chr0056@gmail.com>
2025-04-26 12:17:56 +0800

    [mechanism] cleanup EventHub

--- app/src/main/java/player/phonograph/mechanism/event/EventHub.kt
+++ app/src/main/java/player/phonograph/mechanism/event/EventHub.kt
@@ -4,6 +4,8 @@
 
 package player.phonograph.mechanism.event
 
+import legacy.phonograph.MediaStoreCompat.Audio.Playlists
+import player.phonograph.App
 import player.phonograph.PACKAGE_NAME
 import androidx.lifecycle.DefaultLifecycleObserver
 import androidx.lifecycle.Lifecycle
@@ -13,6 +15,12 @@ import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
 import android.content.IntentFilter
+import android.database.ContentObserver
+import android.os.Handler
+import android.provider.MediaStore.Audio.Albums
+import android.provider.MediaStore.Audio.Artists
+import android.provider.MediaStore.Audio.Genres
+import android.provider.MediaStore.Audio.Media
 
 object EventHub {
 
@@ -84,6 +92,54 @@ object EventHub {
         object : LifeCycleEventReceiver(context, event) {
             override fun onEventReceived(context: Context, intent: Intent) = onEvent(context, intent)
         }
+    //endregion
+
+
+    //region Bridge
+    private lateinit var mediaStoreObserver: MediaStoreObserver
+
+    private class MediaStoreObserver(
+        private val playerHandler: Handler,
+    ) : ContentObserver(playerHandler), Runnable {
+
+        override fun onChange(selfChange: Boolean) {
+            // if a change is detected, remove any scheduled callback
+            // then post a new one. This is intended to prevent closely
+            // spaced events from generating multiple refresh calls
+            playerHandler.removeCallbacks(this)
+            playerHandler.postDelayed(this, REFRESH_DELAY)
+        }
+
+        override fun run() {
+            sendEvent(App.instance, EVENT_MEDIASTORE_CHANGED)
+        }
+
+        companion object {
+            // milliseconds to delay before calling refresh to aggregate events
+            private const val REFRESH_DELAY: Long = 500
+        }
+    }
+
+    fun setUpMediaStoreObserver(context: Context, playerHandler: Handler) {
+        mediaStoreObserver = MediaStoreObserver(playerHandler)
+        with(context.contentResolver) {
+            registerContentObserver(Media.EXTERNAL_CONTENT_URI, true, mediaStoreObserver)
+            registerContentObserver(Albums.EXTERNAL_CONTENT_URI, true, mediaStoreObserver)
+            registerContentObserver(Artists.EXTERNAL_CONTENT_URI, true, mediaStoreObserver)
+            registerContentObserver(Genres.EXTERNAL_CONTENT_URI, true, mediaStoreObserver)
+            registerContentObserver(Playlists.EXTERNAL_CONTENT_URI, true, mediaStoreObserver)
+
+            registerContentObserver(Media.INTERNAL_CONTENT_URI, true, mediaStoreObserver)
+            registerContentObserver(Albums.INTERNAL_CONTENT_URI, true, mediaStoreObserver)
+            registerContentObserver(Artists.INTERNAL_CONTENT_URI, true, mediaStoreObserver)
+            registerContentObserver(Genres.INTERNAL_CONTENT_URI, true, mediaStoreObserver)
+            registerContentObserver(Playlists.INTERNAL_CONTENT_URI, true, mediaStoreObserver)
+        }
+    }
+
+    fun unregisterMediaStoreObserver(context: Context) {
+        context.contentResolver.unregisterContentObserver(mediaStoreObserver)
+    }
     //endregion
 
 }
