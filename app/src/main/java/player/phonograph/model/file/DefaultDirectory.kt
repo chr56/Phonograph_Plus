6e20e5ddabb89f48a098b3b0c267a6a122bf654f
chr56 <chr0056@gmail.com>
2025-07-07 20:11:55 +0800

    [settings] add new exclude/include paths of settings

--- app/src/main/java/player/phonograph/model/file/DefaultDirectory.kt
+++ app/src/main/java/player/phonograph/model/file/DefaultDirectory.kt
@@ -6,7 +6,10 @@ package player.phonograph.model.file
 
 import android.os.Build.VERSION.SDK_INT
 import android.os.Build.VERSION_CODES
+import android.os.Environment.DIRECTORY_ALARMS
 import android.os.Environment.DIRECTORY_MUSIC
+import android.os.Environment.DIRECTORY_NOTIFICATIONS
+import android.os.Environment.DIRECTORY_RINGTONES
 import android.os.Environment.getDataDirectory
 import android.os.Environment.getExternalStorageDirectory
 import android.os.Environment.getExternalStoragePublicDirectory
@@ -17,7 +20,21 @@ import java.io.File
 private val DirectoryExternalStorageRoot: File get() = getExternalStorageDirectory()
 
 private val DirectoryMusic: File get() = getExternalStoragePublicDirectory(DIRECTORY_MUSIC)
+private val DirectoryAlarms: File get() = getExternalStoragePublicDirectory(DIRECTORY_ALARMS)
+private val DirectoryNotifications: File get() = getExternalStoragePublicDirectory(DIRECTORY_NOTIFICATIONS)
+private val DirectoryRingtones: File get() = getExternalStoragePublicDirectory(DIRECTORY_RINGTONES)
 
+val DefaultIncludedPaths: Set<String>
+    get() = setOf(
+        DirectoryMusic.path
+    )
+
+val DefaultExcludedPaths: Set<String>
+    get() = setOf(
+        DirectoryAlarms.path,
+        DirectoryNotifications.path,
+        DirectoryRingtones.path,
+    )
 
 val DefaultStartDirectory: String
     get() = lookupDirectory(DirectoryMusic)
