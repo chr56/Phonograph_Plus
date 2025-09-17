136d95dc7a9514a3dbbdcda216850c37a9f1426e
chr56 <chr0056@gmail.com>
2025-09-17 20:11:23 +0800

    [room] enable new sync executor

--- app/src/main/java/player/phonograph/repo/room/DatabaseActions.kt
+++ app/src/main/java/player/phonograph/repo/room/DatabaseActions.kt
@@ -7,8 +7,15 @@ package player.phonograph.repo.room
 import player.phonograph.R
 import player.phonograph.foundation.notification.ProgressNotificationConnection
 import player.phonograph.mechanism.event.EventHub
+import player.phonograph.model.repo.PROVIDER_MEDIASTORE_MIRROR
+import player.phonograph.model.repo.PROVIDER_MEDIASTORE_PARSED
+import player.phonograph.model.repo.sync.SyncExecutor
+import player.phonograph.model.repo.sync.ProgressConnection
 import player.phonograph.model.repo.sync.SyncResult
 import player.phonograph.repo.room.domain.BasicSyncExecutor
+import player.phonograph.repo.room.domain.RelationshipSyncExecutor
+import player.phonograph.settings.Keys
+import player.phonograph.settings.Setting
 import android.content.Context
 
 object DatabaseActions {
@@ -19,7 +26,11 @@ object DatabaseActions {
      */
     suspend fun sync(context: Context, musicDatabase: MusicDatabase, force: Boolean = false): SyncResult? {
 
-        val syncExecutor = BasicSyncExecutor(musicDatabase)
+        val mode = Setting(context)[Keys.musicLibraryBackend].read()
+        val syncExecutor = when (mode) {
+            PROVIDER_MEDIASTORE_PARSED -> RelationshipSyncExecutor(musicDatabase)
+            else                       -> BasicSyncExecutor(musicDatabase)
+        }
 
         return if (force || syncExecutor.check(context)) {
             val connection = ProgressNotificationConnection(context, R.string.action_refresh_database)
