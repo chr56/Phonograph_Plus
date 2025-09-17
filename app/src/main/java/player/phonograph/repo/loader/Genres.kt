18b3ba7ada1f9fac1bb48e034389ce9a9d31533d
chr56 <chr0056@gmail.com>
2025-09-17 17:05:32 +0800

    [loader] delegate support for
    Songs, Artists, Albums, Genres

--- app/src/main/java/player/phonograph/repo/loader/Genres.kt
+++ app/src/main/java/player/phonograph/repo/loader/Genres.kt
@@ -4,10 +4,34 @@
 
 package player.phonograph.repo.loader
 
+import player.phonograph.model.Genre
+import player.phonograph.model.Song
+import player.phonograph.model.repo.loader.Delegated
 import player.phonograph.model.repo.loader.IGenres
 import player.phonograph.repo.mediastore.MediaStoreGenres
+import player.phonograph.settings.Keys
+import player.phonograph.settings.Setting
+import android.content.Context
 
 /**
  * Endpoint for accessing genres
  */
-object Genres : IGenres by MediaStoreGenres
+object Genres : IGenres, Delegated<IGenres>() {
+    override fun onCreateDelegate(context: Context): IGenres {
+        val preference = Setting(context)[Keys.musicLibraryBackend]
+        val impl: IGenres = when (preference.data) {
+            else                       -> MediaStoreGenres
+        }
+        return impl
+    }
+
+    override suspend fun all(context: Context): List<Genre> =
+        delegate(context).all(context)
+
+    override suspend fun id(context: Context, id: Long): Genre? =
+        delegate(context).id(context, id)
+
+    override suspend fun songs(context: Context, genreId: Long): List<Song> =
+        MediaStoreGenres.songs(context, genreId) // todo
+
+}
--- app/src/main/java/player/phonograph/repo/loader/Songs.kt
+++ app/src/main/java/player/phonograph/repo/loader/Songs.kt
@@ -4,10 +4,49 @@
 
 package player.phonograph.repo.loader
 
+import player.phonograph.model.Song
+import player.phonograph.model.repo.loader.Delegated
 import player.phonograph.model.repo.loader.ISongs
 import player.phonograph.repo.mediastore.MediaStoreSongs
+import player.phonograph.settings.Keys
+import player.phonograph.settings.Setting
+import android.content.Context
 
 /**
  * Endpoint for accessing songs
  */
-object Songs : ISongs by MediaStoreSongs
+object Songs : ISongs, Delegated<ISongs>() {
+
+    override fun onCreateDelegate(context: Context): ISongs {
+        val preference = Setting(context)[Keys.musicLibraryBackend]
+        val impl: ISongs = when (preference.data) {
+            else                       -> MediaStoreSongs
+        }
+        return impl
+    }
+
+    override suspend fun all(context: Context): List<Song> =
+        delegate(context).all(context)
+
+    override suspend fun id(context: Context, id: Long): Song? =
+        delegate(context).id(context, id)
+
+    override suspend fun path(context: Context, path: String): Song? =
+        delegate(context).path(context, path)
+
+    override suspend fun artist(context: Context, artistId: Long): List<Song> =
+        delegate(context).artist(context, artistId)
+
+    override suspend fun album(context: Context, albumId: Long): List<Song> =
+        delegate(context).album(context, albumId)
+
+    override suspend fun searchByPath(context: Context, path: String, withoutPathFilter: Boolean): List<Song> =
+        delegate(context).searchByPath(context, path, withoutPathFilter)
+
+    override suspend fun searchByTitle(context: Context, title: String): List<Song> =
+        delegate(context).searchByTitle(context, title)
+
+    override suspend fun since(context: Context, timestamp: Long, useModifiedDate: Boolean): List<Song> =
+        delegate(context).since(context, timestamp, useModifiedDate)
+
+}
