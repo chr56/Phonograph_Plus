ca046f59c9ce9f46260a7767c8f2010aaae41a87
chr56 <chr0056@gmail.com>
2025-11-26 17:50:02 +0800

    [Release] release v1.11.2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -41,8 +41,8 @@ android {
         targetSdk = 35
 
         applicationId = "player.phonograph.plus"
-        versionCode = 1111
-        versionName = "1.11.2-dev2"
+        versionCode = 1112
+        versionName = "1.11.2"
 
 
         proguardFiles(File("proguard-rules-base.pro"), File("proguard-rules-app.pro"))
