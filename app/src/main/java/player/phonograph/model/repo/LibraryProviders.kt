3ae61a6b162cb5e1f04dcdc55d0a5f68d37de970
chr56 <chr0056@gmail.com>
2026-01-28 16:35:18 +0800

    [settings] disable `PROVIDER_MEDIASTORE_MIRROR` mode temporally

--- app/src/main/java/player/phonograph/model/repo/LibraryProviders.kt
+++ app/src/main/java/player/phonograph/model/repo/LibraryProviders.kt
@@ -11,7 +11,7 @@ const val PROVIDER_MEDIASTORE_PARSED = "mediastore_parsed"
 val LIBRARY_PROVIDERS: List<String>
     get() = listOf(
         PROVIDER_MEDIASTORE_DIRECT,
-        PROVIDER_MEDIASTORE_MIRROR,
+        // PROVIDER_MEDIASTORE_MIRROR,
         PROVIDER_MEDIASTORE_PARSED
     )
 
