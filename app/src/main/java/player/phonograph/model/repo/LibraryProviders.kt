30e5608ca53bbe9ed94420018f47538cc9c09beb
chr56 <chr0056@gmail.com>
2026-03-07 12:31:32 +0800

    [settings] update `music_library_backend` mode

--- app/src/main/java/player/phonograph/model/repo/LibraryProviders.kt
+++ app/src/main/java/player/phonograph/model/repo/LibraryProviders.kt
@@ -4,16 +4,8 @@
 
 package player.phonograph.model.repo
 
-const val PROVIDER_MEDIASTORE_DIRECT = "mediastore_direct"
-const val PROVIDER_MEDIASTORE_MIRROR = "mediastore_mirror"
+const val PROVIDER_MEDIASTORE_DIRECT = "mediastore"
 const val PROVIDER_MEDIASTORE_PARSED = "mediastore_parsed"
-
-val LIBRARY_PROVIDERS: List<String>
-    get() = listOf(
-        PROVIDER_MEDIASTORE_DIRECT,
-        PROVIDER_MEDIASTORE_MIRROR,
-        PROVIDER_MEDIASTORE_PARSED
-    )
 
 val DEFAULT_TAG_ABBR_FEATURES_ARTISTS: Set<String>
     get() = setOf(
