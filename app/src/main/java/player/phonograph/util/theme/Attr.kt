ae013663bc566956f83e665d1a83aee97a72b9a3
chr56 <chr0056@gmail.com>
2026-04-10 21:07:08 +0800

    [theme] more effectly attribute resolving

--- app/src/main/java/player/phonograph/util/theme/Attr.kt
+++ app/src/main/java/player/phonograph/util/theme/Attr.kt
@@ -1,5 +1,5 @@
 /*
- *  Copyright (c) 2022~2024 chr_56
+ *  Copyright (c) 2022~2026 chr_56
  */
 
 package player.phonograph.util.theme
@@ -8,7 +8,10 @@ import player.phonograph.R
 import androidx.annotation.AttrRes
 import androidx.annotation.CheckResult
 import androidx.annotation.ColorInt
+import androidx.annotation.ColorRes
+import androidx.annotation.StyleableRes
 import android.content.Context
+import android.util.TypedValue
 
 
 @CheckResult
@@ -52,12 +55,68 @@ fun themeFloatingBackgroundColor(context: Context) =
         context.getColor(R.color.background_medium_lightblack)
     )
 
+
+@ColorInt
+fun Context.resolveThemeColor(
+    @AttrRes attr: Int,
+    @ColorRes fallbackColorRes: Int,
+    fallbackColor: (() -> Int)? = null,
+): Int {
+    val value = resolve(attr, true)
+    return if (value != null) {
+        if (value.resourceId != 0) {
+            getColor(value.resourceId)
+        } else {
+            value.data
+        }
+    } else {
+        fallbackColor?.invoke() ?: getColor(fallbackColorRes)
+    }
+}
+
+fun Context.resolveThemeColors(
+    @StyleableRes attrs: IntArray,
+    @ColorRes fallbackColorRes: Int,
+    fallbackColor: (() -> Int)? = null,
+): IntArray {
+    val values = theme.obtainStyledAttributes(attrs)
+    val default: Int by lazy { (fallbackColor?.invoke() ?: getColor(fallbackColorRes)) }
+    return try {
+        IntArray(values.length()) { i ->
+            values.getColor(i, default)
+        }
+    } finally {
+        values.recycle()
+    }
+}
+
 @ColorInt
 fun Context.resolveColor(@AttrRes attr: Int, @ColorInt fallbackColor: Int): Int {
-    val a = theme.obtainStyledAttributes(intArrayOf(attr))
-    return try {
-        a.getColor(0, fallbackColor)
-    } finally {
-        a.recycle()
+    val value = resolve(attr, true)
+    return if (value != null) {
+        if (value.resourceId != 0) {
+            getColor(value.resourceId)
+        } else {
+            value.data
+        }
+    } else {
+        fallbackColor
     }
+}
+
+fun Context.resolveColors(@StyleableRes attrs: IntArray, @ColorInt fallbackColor: Int): IntArray {
+    val values = theme.obtainStyledAttributes(attrs)
+    return try {
+        IntArray(values.length()) { i ->
+            values.getColor(i, fallbackColor)
+        }
+    } finally {
+        values.recycle()
+    }
+}
+
+
+fun Context.resolve(attr: Int, deep: Boolean): TypedValue? {
+    val typedValue = TypedValue()
+    return if (theme.resolveAttribute(attr, typedValue, deep)) typedValue else null
 }
