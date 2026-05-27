ed4c8c45ed058c3ea51e37df50db47084853911c
chr56 <chr0056@gmail.com>
2026-05-27 11:36:09 +0800

    [ui] add grant all button in App Intro

--- app/src/main/java/player/phonograph/ui/modules/auxiliary/PhonographIntroActivity.kt
+++ app/src/main/java/player/phonograph/ui/modules/auxiliary/PhonographIntroActivity.kt
@@ -9,7 +9,9 @@ import com.github.appintro.AppIntroFragment
 import com.github.appintro.SlideBackgroundColorHolder
 import com.github.appintro.SlidePolicy
 import com.google.android.material.snackbar.Snackbar
+import lib.activityresultcontract.IRequestMultiplePermission
 import lib.activityresultcontract.IRequestPermission
+import lib.activityresultcontract.RequestMultiplePermissionsDelegate
 import lib.activityresultcontract.RequestPermissionDelegate
 import lib.activityresultcontract.registerActivityResultLauncherDelegate
 import lib.storage.launcher.IOpenFileStorageAccessible
@@ -33,20 +35,23 @@ import player.phonograph.util.permissions.permissionDescription
 import player.phonograph.util.permissions.permissionName
 import util.theme.materials.MaterialColor
 import androidx.annotation.StringRes
+import androidx.appcompat.widget.AppCompatButton
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import androidx.core.view.setMargins
 import androidx.core.view.updateLayoutParams
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentActivity
 import androidx.lifecycle.lifecycleScope
+import android.content.Context
 import android.content.pm.PackageManager
 import android.os.Build.VERSION.SDK_INT
 import android.os.Build.VERSION_CODES.VANILLA_ICE_CREAM
 import android.os.Bundle
+import android.view.Gravity
 import android.view.LayoutInflater
 import android.view.View
 import android.view.View.GONE
-import android.view.View.OnClickListener
 import android.view.ViewGroup
 import android.view.ViewGroup.LayoutParams.MATCH_PARENT
 import android.view.ViewGroup.LayoutParams.WRAP_CONTENT
@@ -58,7 +63,10 @@ import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 import java.io.FileInputStream
 
-class PhonographIntroActivity : AppIntro(), IOpenFileStorageAccessible, IRequestPermission {
+class PhonographIntroActivity : AppIntro(),
+                                IOpenFileStorageAccessible,
+                                IRequestPermission,
+                                IRequestMultiplePermission {
 
     private fun config() {
         isWizardMode = true
@@ -82,13 +90,15 @@ class PhonographIntroActivity : AppIntro(), IOpenFileStorageAccessible, IRequest
 
     override val openFileStorageAccessDelegate: OpenFileStorageAccessDelegate = OpenFileStorageAccessDelegate()
     override val requestPermissionDelegate: RequestPermissionDelegate = RequestPermissionDelegate()
-
+    override val requestMultiplePermissionsDelegate: RequestMultiplePermissionsDelegate = RequestMultiplePermissionsDelegate()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         config()
-        registerActivityResultLauncherDelegate(openFileStorageAccessDelegate, requestPermissionDelegate)
+        registerActivityResultLauncherDelegate(
+            openFileStorageAccessDelegate, requestPermissionDelegate, requestMultiplePermissionsDelegate
+        )
 
         addSlide(
             AppIntroFragment.createInstance(
@@ -140,12 +150,37 @@ class PhonographIntroActivity : AppIntro(), IOpenFileStorageAccessible, IRequest
         private val items get() = _items!!
 
         override fun setUpView(container: ViewGroup) {
-            _items = necessaryPermissions.map { detail ->
-                createViewItem(detail) { view ->
-                    val context = container.context
-                    val permissionsTool = (context as? IRequestPermission)?.requestPermissionDelegate
-                    if (permissionsTool != null) {
-                        permissionsTool.launch(detail.permission) {
+            _items = necessaryPermissions.map { detail -> createPermissionViewBinding(detail) }
+            val params = LinearLayout.LayoutParams(MATCH_PARENT, WRAP_CONTENT).apply { setMargins(32) }
+            for (itemBinding in items) {
+                container.addView(itemBinding.root, params)
+            }
+            container.addView(
+                createGrantAllView(container.context),
+                LinearLayout.LayoutParams(WRAP_CONTENT, WRAP_CONTENT).apply {
+                    setMargins(32)
+                    gravity = Gravity.START
+                }
+            )
+        }
+
+        override fun onDestroyView() {
+            super.onDestroyView()
+            _items = null
+        }
+
+        private fun createPermissionViewBinding(detail: PermissionDetail): ItemSimpleBinding =
+            ItemSimpleBinding.inflate(layoutInflater).also { itemBinding ->
+                val context = itemBinding.root.context
+                itemBinding.title.text = permissionName(context, detail.permission)
+                itemBinding.text.text = permissionDescription(context, detail.permission)
+                itemBinding.menu.visibility = GONE
+                updateItemBackgroundColor(itemBinding.root, detail.permission)
+
+                itemBinding.root.setOnClickListener { view ->
+                    val delegate = (view.context as? IRequestPermission)?.requestPermissionDelegate
+                    if (delegate != null) {
+                        delegate.launch(detail.permission) {
                             updateItemBackgroundColor(view, detail.permission)
                         }
                     } else {
@@ -157,32 +192,32 @@ class PhonographIntroActivity : AppIntro(), IOpenFileStorageAccessible, IRequest
                     }
                 }
             }
-            val params = LinearLayout.LayoutParams(MATCH_PARENT, WRAP_CONTENT).apply {
-                setMargins(32)
-            }
-            items.forEachIndexed { index, itemBinding ->
-                container.addView(itemBinding.root, index, params)
-            }
-        }
 
-        override fun onDestroyView() {
-            super.onDestroyView()
-            _items = null
-        }
-
-        private fun createViewItem(
-            detail: PermissionDetail,
-            onClick: OnClickListener,
-        ): ItemSimpleBinding {
-            return ItemSimpleBinding.inflate(layoutInflater).also { itemBinding ->
-                val context = itemBinding.root.context
-                itemBinding.title.text = permissionName(context, detail.permission)
-                itemBinding.text.text = permissionDescription(context, detail.permission)
-                itemBinding.root.setOnClickListener(onClick)
-                itemBinding.menu.visibility = GONE
-                updateItemBackgroundColor(itemBinding.root, detail.permission)
+        private fun createGrantAllView(context: Context): View =
+            AppCompatButton(context).apply {
+                text = context.getString(R.string.action_grant_all)
+                setOnClickListener { view ->
+                    val delegate = (view.context as? IRequestMultiplePermission)?.requestMultiplePermissionsDelegate
+                    if (delegate != null) {
+                        val permissions = necessaryPermissions.map { it.permission }.toTypedArray()
+                        delegate.launch(permissions) {
+                            try {
+                                for ((index, binding) in items.withIndex()) {
+                                    updateItemBackgroundColor(binding.root, permissions[index])
+                                }
+                            } catch (e: Exception) {
+                                e.printStackTrace()
+                            }
+                        }
+                    } else {
+                        Toast.makeText(
+                            requireContext(),
+                            "${context::class.java.simpleName} is unsupported for ask permissions",
+                            LENGTH_LONG
+                        ).show()
+                    }
+                }
             }
-        }
 
         private fun updateItemBackgroundColor(view: View, permission: String) {
             val granted = hasPermission(requireContext(), permission)
@@ -235,15 +270,14 @@ class PhonographIntroActivity : AppIntro(), IOpenFileStorageAccessible, IRequest
             contentBinding.checkUpgradeDesc.text = msg
             contentBinding.checkUpgradeChoose.setOnCheckedChangeListener { _, selected ->
                 when (selected) {
-                    R.id.enable  -> Settings(view.context)[Keys.checkUpgradeAtStartup].data = true
+                    R.id.enable -> Settings(view.context)[Keys.checkUpgradeAtStartup].data = true
                     R.id.disable -> Settings(view.context)[Keys.checkUpgradeAtStartup].data = false
                 }
             }
             contentBinding.backup.text = getString(R.string.action_import, getString(R.string.label_backup))
-            contentBinding.backup.setOnClickListener {
-                val activity = activity
-                require(activity is IOpenFileStorageAccessible)
-                activity.openFileStorageAccessDelegate.launch(
+            contentBinding.backup.setOnClickListener { view ->
+                val activity = view.context as FragmentActivity
+                (activity as IOpenFileStorageAccessible).openFileStorageAccessDelegate.launch(
                     OpenDocumentContract.Config(arrayOf("*/*"))
                 ) { uri ->
                     uri ?: return@launch
