57691704aaaa342fb465bd724c35464fe7690cff
chr56 <chr0056@gmail.com>
2026-06-09 15:46:44 +0800

    {test} test release

--- tools/changelog-generator/src/main/kotlin/util/phonograph/model/constants/Descriptions.kt
+++ tools/changelog-generator/src/main/kotlin/util/phonograph/model/constants/Descriptions.kt
@@ -6,8 +6,8 @@ package util.phonograph.model.constants
 
 const val OVERFLOWED_MESSAGE = "...(Visit project homepage to see full changelogs)"
 
-const val SIGNING_KEY_WARNING_EN = "We have changed the signing key since this version. You have to uninstall first before upgrading. \nHowever, if your device runs Android 9 and higher, you can install `IntermediaRelease` variants to transit smoothly without uninstalling first."
-const val SIGNING_KEY_WARNING_ZH = "自该版本起，我们使用了新的签名密钥；更新前需要先卸载再安装；\n但是，若设备运行 Android 9 及其以上，可安装带有 `IntermediaRelease` 的安装包，则可无需卸载即可迁到新签名密钥！"
+const val SIGNING_KEY_WARNING_EN = "THIS IS A TECHNICAL RELEASE; PLEASE IGNORE!"
+const val SIGNING_KEY_WARNING_ZH = "技术性版本请忽略！"
 
 const val PREVIEW_WARNING_EN =
     "This is a _Preview Channel_ Release (identified by `preview` suffix in the package name), stability and quality are not guaranteed."
