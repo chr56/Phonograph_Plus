423cd62577167beeb9d8af14d23e792402de56ad
chr56 <chr0056@gmail.com>
2026-06-23 23:00:06 +0800

    [changelog-generator] update descriptions

--- tools/changelog-generator/src/main/kotlin/util/phonograph/model/constants/Descriptions.kt
+++ tools/changelog-generator/src/main/kotlin/util/phonograph/model/constants/Descriptions.kt
@@ -6,7 +6,7 @@ package util.phonograph.model.constants
 
 const val OVERFLOWED_MESSAGE = "...(Visit project homepage to see full changelogs)"
 
-const val SIGNING_KEY_WARNING_EN = "We have changed the signing key since this version. You have to uninstall first before upgrading. \nHowever, if your device runs Android 9 and higher, you can install `IntermediaRelease` variants to transit smoothly without uninstalling first."
+const val SIGNING_KEY_WARNING_EN = "We have changed the signing key since this version. You have to _uninstall first_ before upgrading. \nHowever, if your device runs Android 9 and higher, you can install `IntermediaRelease` variants to transit smoothly without uninstalling first."
 const val SIGNING_KEY_WARNING_ZH = "自该版本起，我们使用了新的签名密钥；更新前需要先卸载再安装；\n但是，若设备运行 Android 9 及其以上，可安装带有 `IntermediaRelease` 的安装包，则可无需卸载即可迁到新签名密钥！"
 
 const val PREVIEW_WARNING_EN =
@@ -23,8 +23,8 @@ const val VARIANTS_DESCRIPTION_TITLE = "Version Variants Description / 版本说
 const val VARIANTS_DESCRIPTION_BODY = """
 -> [Version Guide](docs/Version_Guide.md) / [版本指南](docs/Version_Guide_ZH.md)
 
-**TL;DR**: If you are a user of Android 10, 7 and 7.1, please consider to use `Legacy`; If not, use `Modern`. The `Fdroid` is identical to the version on F‑droid; and the signature is same with others. Recently, you may need Intermediate variant to transit to new signing key.
-**太长不看**: 若为 Android 10 或 7 及 7.1 用户，请考虑使用 `Legacy` 版本；否则，请使用 `Modern` 版本。另外带 `Fdroid` 为 F-droid 上完全相同的版本，签名与其他版本相同。近期可能需要 Intermediate 版本过渡至新签名。
+**TL;DR**: If you are a user of Android 10, 7 and 7.1, please consider to use `Legacy`; If not, use `Modern`. The `Fdroid` is identical to the version on F‑droid; and the signature is same with others. Recently, you may need Intermediate variant to transit for upgrading.
+**太长不看**: 若为 Android 10 或 7 及 7.1 用户，请考虑使用 `Legacy` 版本；否则，请使用 `Modern` 版本。另外带 `Fdroid` 为 F-droid 上完全相同的版本，签名与其他版本相同。近期升级可能需要 Intermediate 版本过渡。
 """
 
 
@@ -33,5 +33,5 @@ __Download Links__: [Modern Variant](%s) | [Legacy Variant](%s) | [Intermediate 
 """
 
 
-const val SIGNING_KEY_NOTICE_EN = "Since version 2.0, signing key of Phonograph Plus has been replaced and rotated!\nYou need uninstall first before upgrading. However, if your device runs Android 9 and higher, you can install `IntermediaRelease` variants to transit smoothly without uninstalling first (then you can install any other variants). See also [here](docs/Version_Guide.md#signing-key-rotation-and-replacement)."
+const val SIGNING_KEY_NOTICE_EN = "Since version 2.0, signing key of Phonograph Plus has been replaced and rotated!\nYou have to uninstall first before upgrading. However, if your device runs Android 9 and higher, you can install `IntermediaRelease` variants to transit smoothly without uninstalling first (then you can install any other variants). See also [this guide](docs/Version_Guide.md#signing-key-rotation-and-replacement)."
 const val SIGNING_KEY_NOTICE_ZH = "自 v2.0 起，签名密钥已被更替或轮转！\n更新前需要先卸载再安装；但是，若设备 Android 版本大于9，可安装带有 `IntermediaRelease` 的安装包，则可无需卸载即可迁到新签名密钥（随后可安装其他版本）！(另见[此处](docs/Version_Guide_ZH.md#签名密钥轮转与更换))"
